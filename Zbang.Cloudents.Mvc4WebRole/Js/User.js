8e0304667e05bb0b77fb6be765192220989c2f0f
GuyGolanBB <pitbullxp@gmail.com>
2014-02-05 16:13:06 +0200

    userpage fixes

--- Zbang.Cloudents.Mvc4WebRole/Js/User.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/User.js
@@ -152,7 +152,7 @@
                 followingBoxesLength = self.followingBoxes().length,
                 maxFollowingBoxes = self.maxFollowingBoxes();
 
-            return (commonBoxesLength > consts.MINCOMMONBOXESVISIBLE || followingBoxesLength > consts.MINFOLLOWBOXESVISIBLE);
+            return (commonBoxesLength > consts.MINCOMMONBOXESVISIBLE || followingBoxesLength > consts.MINFOLLOWBOXESVISIBLE) && !self.viewSelf();
 
         });
         self.CoursesSectionVisible = ko.computed(function () {
@@ -323,17 +323,27 @@
             $('.upTabContent').hide();
             $(consts.UPTABS).removeClass(consts.CUPTAB + '2 ' + consts.CUPTAB + '3').addClass(consts.CUPTAB + '1')
             $('#filesSection').show();
-            eById('coursesShow').checked = false;
-            eById('friendsShow').checked = false;
-            eById('invitesShow').checked = false;
+            var cB = eById('coursesShow');
+            if (cB){
+                cB.checked = false;
+            }
+            cB = eById('friendsShow');
+
+            if (cB){
+                cB.checked = false;
+            }
+            cB = eById('invitesShow');
+            if (cB){
+                cB.checked = false;
+            }          
             var f = eById('filesCount');
             f.textContent = '' + f.getAttribute('data-label');
 
-            var a = eById('answersCount');
-            a.textContent = '' + a.getAttribute('data-label');
+            f = eById('answersCount');
+            f.textContent = '' + f.getAttribute('data-label');
             
-            var q = eById('questionsCount');
-            q.textContent = '' + q.getAttribute('data-label');
+            f = eById('questionsCount');
+            f.textContent = '' + f.getAttribute('data-label');
         }
 
         function getInitData() {
