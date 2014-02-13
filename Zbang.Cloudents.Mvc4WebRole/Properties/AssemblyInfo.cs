478fb0ae6a146caf4b98fb1199a51afb18b923df
Ram Yaari <Ram.y@outlook.com>
2014-02-13 11:30:30 +0200

    added cdn web config

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.3.5.*")]
-[assembly: AssemblyFileVersion("13.3.5.0")]
+[assembly: AssemblyVersion("14.0.0.*")]
+[assembly: AssemblyFileVersion("14.0.0.0")]
