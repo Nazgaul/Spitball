e41d77564ff8d9e7b7636cfe74765384a21a0c5b
Ram Yaari <Ram.y@outlook.com>
2014-02-23 16:42:04 +0200

    version 14.2.3

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("14.2.1.*")]
-[assembly: AssemblyFileVersion("14.2.1.0")]
+[assembly: AssemblyVersion("14.2.3.*")]
+[assembly: AssemblyFileVersion("14.2.3.0")]
