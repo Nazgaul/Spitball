4f9ce2d11f5074c44d4391ed1a62d941dfe7ae64
Ram Yaari <Ram.y@outlook.com>
2014-03-11 11:15:46 +0200

    Get updates and delete update server side.

---
+++ Zbang.Zbox.Domain.Commands/DeleteUpdatesCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zbang.Zbox.Infrastructure.Commands;
+
+namespace Zbang.Zbox.Domain.Commands
+{
+    public class DeleteUpdatesCommand :ICommand
+    {
+        public DeleteUpdatesCommand(long userId, long boxId)
+        {
+            UserId = userId;
+            BoxId = boxId;
+        }
+        public long UserId { get; private set; }
+        public long BoxId { get;private set; }
+    }
+}
