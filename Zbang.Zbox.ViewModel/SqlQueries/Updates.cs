e6d57f1c2a0fb7242984641da51a1d9a9a724eaa
Ram Yaari <Ram.y@outlook.com>
2014-03-11 10:45:07 +0200

    read serive to get updates of the users

---
+++ Zbang.Zbox.ViewModel/SqlQueries/Updates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbang.Zbox.ViewModel.SqlQueries
+{
+    public static class Updates
+    {
+        public const string GetUserUpdates = @"select BoxId,QuestionId,AnswerId,ItemId from zbox.zbox.NewUpadtes
+where UserId = @userid";
+    }
+}
