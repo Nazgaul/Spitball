5c30c3ba34f5ecf4df8bd8f590d094530d05b6ac
Ram Yaari <Ram.y@outlook.com>
2014-03-19 16:07:49 +0200

    fixes

--- Zbang.Zbox.ViewModel/DTOs/Emails/BoxDigestDto.cs
+++ Zbang.Zbox.ViewModel/DTOs/Emails/BoxDigestDto.cs
@@ -7,9 +7,25 @@ namespace Zbang.Zbox.ViewModel.DTOs.Emails
 {
     public class BoxDigestDto
     {
+        private string m_Picture;
         public long BoxId { get; set; }
         public string BoxName { get; set; }
-        public string BoxPicture { get; set; }
+        public string BoxPicture
+        {
+            get { return m_Picture; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    m_Picture = "https://www.cloudents.com/images/emptyState/my_default3.png";
+                }
+                else
+                {
+                    m_Picture = Zbang.Zbox.Infrastructure.Storage.BlobProvider.GetThumbnailUrl(value);
+                }
+            }
+
+        }
         public string UniversityName { get; set; }
 
         public string Url { get; set; }
