7323ef09e03afb9a676b8404f4cb85f06b1a7c1b
Ram Yaari <Ram.y@outlook.com>
2014-03-26 14:37:25 +0200

    bug fixes

--- Zbang.Cloudents.Mvc4WebRole/Models/ValidatePasswordLengthAttribute.cs
+++ Zbang.Cloudents.Mvc4WebRole/Models/ValidatePasswordLengthAttribute.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using System.Web.Security;
 using Zbang.Cloudents.Mvc4WebRole.Models.Resources;
 
 namespace Zbang.Cloudents.Mvc4WebRole.Models
 {
-    public sealed class ValidatePasswordLengthAttribute : ValidationAttribute
+    public sealed class ValidatePasswordLengthAttribute : ValidationAttribute, IClientValidatable
     {
         //private static string _defaultErrorMessage = ValidatePasswordResources.MustBeAtLeast;
         private readonly int _minCharacters = Membership.Provider.MinRequiredPasswordLength;
@@ -31,5 +32,13 @@ namespace Zbang.Cloudents.Mvc4WebRole.Models
         }
 
 
+
+        public System.Collections.Generic.IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+             return new[]{      
+                 new ModelClientValidationStringLengthRule(FormatErrorMessage(metadata.GetDisplayName())
+                     , _minCharacters, int.MaxValue) 
+             };// - See more at: http://timjames.me/mvc-3-password-length-dataannotation#sthash.Aebh0qbg.dpuf
+        }
     }
 }
