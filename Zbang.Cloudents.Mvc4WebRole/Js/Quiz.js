4e8b39d1baa1321abcf7e80df5d8ee80c4ebc575
GuyGolanBB <pitbullxp@gmail.com>
2014-03-30 09:27:52 +0200

    started quiz

---
+++ Zbang.Cloudents.Mvc4WebRole/Js/Quiz.js
@@ -0,0 +1,10 @@
+﻿(function ($, dataContext, ko, cd, ZboxResources, analytics, Modernizr) {
+    "use strict";
+
+    if (window.scriptLoaded.isLoaded('quiz')) {
+        return;
+    }
+
+
+
+})(jQuery, window.cd.data, window.ko, window.cd, window.ZboxResources, window.cd.analytics, Modernizr);
--- Zbang.Cloudents.Mvc4WebRole/Scripts/_references.js
+++ Zbang.Cloudents.Mvc4WebRole/Scripts/_references.js
@@ -80,3 +80,4 @@
 /// <reference path="../js/notificationsviewmodel.js" />
 /// <reference path="jquery-ui-1.10.4.js" />
 /// <reference path="jquery.mcustomscrollbar.js" />
+/// <reference path="../js/quiz.js" />
