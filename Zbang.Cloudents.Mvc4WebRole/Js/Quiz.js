0280635ad3c69e55000956d4de19c08933dbc31a
GuyGolanBB <pitbullxp@gmail.com>
2014-03-31 10:07:12 +0200

    quiz

--- Zbang.Cloudents.Mvc4WebRole/Js/Quiz.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/Quiz.js
@@ -8,13 +8,8 @@
         quizSideBar = eById('quizSideBar'),
         quizQuestionList = eById('quizQuestionList'),
         quizAddQuestion = eById('quizAddQuestion'),
+        quizPreview = eById('quizPreview'),
         saveBtn = eById('saveQuiz');
-
-    var consts = {
-        emptyQuestion : 1000,
-        validQuestion: 1001,
-        errorQuestion : 1002,
-    }
 
     function Quiz(data) {
         var that = this;
@@ -24,11 +19,17 @@
 
     function Question(data) {
         var that = this;
-        that.text = data.questionText;
+        that.index = data.index;
+        that.text = data.text || '';
         that.answers = data.answers;
         that.correctAnswer = data.correctAnswer;
         that.element = data.element;
+    }
 
+    function Answer(data) {
+        var that = this;
+        that.index = data.index;
+        that.text = data.text;
     }
     pubsub.subscribe('initQuiz', function () {
 
@@ -39,31 +40,85 @@
 
     }
 
+    function validateQuiz(quiz) {
+        if (!quiz.name) {
+            return false;
+        }
+        if (quiz.questions.length < 2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    function validateQuestion(question) {
+        return (question.text.length > 0) + (question.answers.length > 1) + (question.correctAnswer > -1);
+    }
+    function parseQuiz() {
+        var questions = quizQuestionList.querySelectorAll('.questionHolder'),
+            questionsArr = [], quizName;
+
+        quizName = quizSideBar.querySelector('.quizName').value;
 
 
-    function registerEvents() {
-        $(quizQuestionList).on('keyup', '.questionHolder', function (e) {
-            var question = this;
-
-            if (e.target.classList.contains('questionAnswer')) {
-                enableNextOption(e.target);
-                return;
+        for (var i = 0, l = questions.length; i < l; i++) {
+            var question = parseQuestion(questions[i]);
+            if (question) {
+                questionsArr.push(question);
             }
+        }
+        return new Quiz({ name: quizName, questions: questionsArr });
 
+        function parseQuestion(question) {
+            var answersArr = [],
+                correctAnswer = -1,
+                answers = question.querySelectorAll('.quizAnswer'),
+                questionText;
+            //check question text 
+            questionText = question.querySelector('.questionText').value;
 
-                
-            function enableNextOption(answer) {
-                var nextAnswer = answer.parentElement.nextElementSibling;
-
-                if (nextAnswer && answer.value.length) {
-                    nextAnswer.firstElementChild.disabled = nextAnswer.lastElementChild.disabled = false;
+            //get question answers
+            var aIndex = 1;
+            for (var i = 0, l = answers.length; i < l; i++) {
+                var answer = answers[i].querySelector('.questionAnswer');
+                if (answer.value.length > 0) {
+                    answersArr.push(new Answer({ index: aIndex, text: answer.value }));
+                    aIndex++;
+                    if (answer.nextElementSibling.checked) {
+                        correctAnswer = i;
+                    }
                 }
             }
 
+            var question = new Question({ text: questionText, answers: answersArr, correctAnswer: correctAnswer, element: question }),
+                validCount = validateQuestion(question);
+            if (validCount === 3) {
+                return question;
+            }
+        }
+    }
 
+    function registerEvents() {
+        $(quizQuestionList).on('keyup', '.questionAnswer', function (e) {
+            var nextAnswer = this.parentElement.nextElementSibling,
+                checkbox = this.nextElementSibling;
+
+            if (nextAnswer && this.value.length) {
+                nextAnswer.firstElementChild.disabled = false;
+            }
+            
+            //maybe check index greater than 2
+            checkbox.disabled = this.value.length === 0;
+
+        }); 
+        $(quizQuestionList).on('change', '.questionHolder', function (e) {
+            if (e.target.type !== 'checkbox') {
+                return;
+            }
+            var state = e.target.checked;
+            $(this).find('input').prop('checked', false);
+            e.target.checked = state;
         });
-
-
 
         $(quizQuestionList).on('click', '.quizRemoveQuestion', function () {
             $(this).parent().remove();
@@ -75,81 +130,27 @@
 
         $(saveQuiz).click(function () {
 
+            var quiz = parseQuiz();
+
+            if (validateQuiz(quiz)) {
+                console.log('saved');
+            } else {
+                console.log('not saved');
+            }
+
+            console.log(quiz);
+
+
+        });
+
+        $(quizPreview).click(function () {
             var quiz = parseQuiz(),
-                quizQuestions = quiz.questions,
-                errorElements = [];
+                previewObj = {};
 
-            if (!quiz.name) {
-                errorElements.push(quizSideBar.querySelector('.quizName'));
-            }
+            //previewObj.
+            //for (var i = 0; i < length; i++) {
 
-            for (var i = 0, l = quizQuestions; i < l; i++) {
-                var validCount = quizQuestions[i] + (answersArr.length > 1) + answerSelected;
-
-            }
-
-            function parseQuiz() {
-                var questions = quizQuestionList.querySelectorAll('.questionHolder'),
-                    questionsArr = [], quizName;
-
-                quizName = quizSideBar.querySelector('.quizName').value;
-            
-                
-                for (var i = 0, l = questions.length; i < l; i++) {
-
-                    questionsArr.push(parseQuestion(questions[i]));
-
-                    //switch (checkValidQuestion(questions[i])){
-                    //    //case consts.validQuestion:
-                    //    //    questions[i].style.border = '3px solid green';
-                    //    //    break;
-                    //    case consts.errorQuestion:
-                    //        errorElements.push(questions[i]);
-                    //        break;
-                    //    //case consts.emptyQuestion:
-                    //    //    questions[i].style.border = '3px solid blue';
-                    //    //    break;
-                    //    default:
-                    //        console.log('hacking not allowed');
-                    //}                        
-                    //valid
-                    //var validCount = isQuestionText + (answersArr.length > 1) + answerSelected;
-                    //switch (validCount) {
-                    //    case 0:
-                    //        return consts.emptyQuestion;
-                    //    case 3:
-                    //        return consts.validQuestion;
-                    //        break;
-                    //    default:
-                    //        return consts.errorQuestion;
-                    //}
-
-                }
-                return new Quiz({name : quizName, questions: questionsArr});                
-                
-                function parseQuestion(question) {
-                    var answersArr = [],
-                        correctAnswer,
-                        answers = question.querySelectorAll('.quizAnswer'),
-                        questionText;
-                    //check question text 
-                    questionText = question.querySelector('.questionText').value;
-
-                    //get question answers
-                    for (var i = 0, l = answers.length; i < l; i++) {
-                        var answer = answers[i].querySelector('.questionAnswer');
-                        if (answer.value.length > 0) {
-                            answersArr.push(answer.value);
-                            if (answer.nextElementSibling.checked) {
-                                correctAnswer = i;
-                            }                                
-                        }
-                    }
-
-                    return new Question({ text: questionText, answers: answersArr,correctAnswer : correctAnswer,element: question });
-                }
-            }
-
+            //}
         });
     }
     registerEvents();
