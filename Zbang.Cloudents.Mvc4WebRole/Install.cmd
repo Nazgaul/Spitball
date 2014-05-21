c9deaeea1e0fd46ba7b35175e34c8376add94d47
Ram Yaari <Ram.y@outlook.com>
2014-05-21 10:35:03 +0300

    Remove unused stuff

--- Zbang.Cloudents.Mvc4WebRole/Install.cmd
+++
@@ -1,3 +0,0 @@
-md "%~dp0appdata"
-reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%~dp0appdata" /f
-powershell -ExecutionPolicy Unrestricted .\Install.ps1 %ACTIVATIONKEY% %ENVIRONMENT%
