ce67d0edecf238b5c657d2a41fc4d4008ebf4bed
nazgaul <ram.y@outlook.com>
2014-06-23 14:45:49 +0300

    send grid subscribe user with forgot password

--- Zbang.Zbox.WorkerRole/Mail/ForgotPassword.cs
+++ Zbang.Zbox.WorkerRole/Mail/ForgotPassword.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Zbang.Zbox.Infrastructure.Consts;
-using Zbang.Zbox.Infrastructure.Exceptions;
 using Zbang.Zbox.Infrastructure.Mail;
 using Zbang.Zbox.Infrastructure.Transport;
 
@@ -18,30 +17,16 @@ namespace Zbang.Zbox.WorkerRole.Mail
         {
             m_MailComponent = mailComponent;
         }
-        public bool Excecute(Infrastructure.Transport.BaseMailData data)
+        public bool Excecute(BaseMailData data)
         {
-            //var parameters = data as ForgotPasswordData;
-
-            //if (parameters != null)
-            //{
-            //    m_MailComponent.GenerateAndSendEmail(parameters.EmailAddress,
-            //        new ForgotPasswordMailParams(parameters.NewPassword,
-            //       new CultureInfo(parameters.Culture)));
-            //    return true;
-            //}
-
-            //Throw.OnNull(parameters, "ForgotPasswordData");
             var parameters2 = data as ForgotPasswordData2;
-            if (parameters2 != null)
-            {
-                m_MailComponent.GenerateAndSendEmail(parameters2.EmailAddress,
-                    new ForgotPasswordMailParams2(parameters2.Code, string.Format(UrlConsts.PasswordUpdate, parameters2.Link),parameters2.Name,
-                   new CultureInfo(parameters2.Culture)));
-                return true;
-            }
-            Throw.OnNull(parameters2, "ForgotPasswordData");
-
-            return false;
+            if (parameters2 == null) return false;
+            var t = m_MailComponent.DeleteUnsubscribe(parameters2.EmailAddress);
+            t.Wait();
+            m_MailComponent.GenerateAndSendEmail(parameters2.EmailAddress,
+                new ForgotPasswordMailParams2(parameters2.Code, string.Format(UrlConsts.PasswordUpdate, parameters2.Link), parameters2.Name,
+                    new CultureInfo(parameters2.Culture)));
+            return true;
         }
     }
 }
--- Zbang.Zbox.WorkerRole/WorkerRole.cs
+++ Zbang.Zbox.WorkerRole/WorkerRole.cs
@@ -44,14 +44,14 @@ namespace Zbang.Zbox.WorkerRole
             {
                 return new[]
                        {
-                           m_Unity.Unity.Resolve<IJob>(UnityFactory.DigestEmail2,new IocParameterOverride("hourForEmailDigest",NotificationSettings.OnceADay))
+                           //m_Unity.Unity.Resolve<IJob>(UnityFactory.DigestEmail2,new IocParameterOverride("hourForEmailDigest",NotificationSettings.OnceADay))
                            //m_Unity.Unity.Resolve<IJob>(UnityFactory.DigestEmail2,new IocParameterOverride("hourForEmailDigest",NotificationSettings.OnEveryChange)),
                            //m_Unity.Unity.Resolve<IJob>(UnityFactory.DigestEmail2,new IocParameterOverride("hourForEmailDigest",NotificationSettings.OnceAWeek)),
                          // m_Unity.Resolve<IJob>(UnityFactory.GenerateDocumentCache)
                            //m_Unity.Resolve<IJob>(UnityFactory.AddFiles),
                            //m_Unity.Resolve<IJob>(UnityFactory.Transaction)
                            //m_Unity.Resolve<IJob>(UnityFactory.Dbi)
-                           //m_Unity.Unity.Resolve<IJob>(UnityFactory.MailProcess2),
+                           m_Unity.Unity.Resolve<IJob>(UnityFactory.MailProcess2)
                            //m_Unity.Unity.Resolve<IJob>(UnityFactory.EmailPartners)
                        };
             }
