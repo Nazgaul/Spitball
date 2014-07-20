c98b8a9406a1df8819da0b81bd74fe7760d53e8f
nazgaul <ram.y@outlook.com>
2014-07-20 16:56:39 +0300

    Change to Store
    add json result

---
+++ Zbang.Cloudents.Mvc4WebRole/Controllers/StoreController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Zbang.Cloudents.Mvc4WebRole.Extensions;
+using Zbang.Cloudents.Mvc4WebRole.Filters;
+using Zbang.Cloudents.Mvc4WebRole.Helpers;
+
+namespace Zbang.Cloudents.Mvc4WebRole.Controllers
+{
+    [AllowAnonymous]
+    public class StoreController : BaseController
+    {
+        // GET: Shopping
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, Ajax]
+        public async Task<ActionResult> Products(int? category)
+        {
+            var products = await ZboxReadService.GetProducts();
+            return this.CdJson(new JsonResponse(true, products));
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Sales()
+        {
+            return View();
+        }
+    }
+}
