945de00ca10fd12e302421c0b9daae145d26b445
nazgaul <ram.y@outlook.com>
2014-07-23 09:21:52 +0300

    version 18.1.6 worker role

--- Zbang.Zbox.WorkerRole/WorkerRole.cs
+++ Zbang.Zbox.WorkerRole/WorkerRole.cs
@@ -69,7 +69,8 @@ namespace Zbang.Zbox.WorkerRole
                 m_Unity.Resolve<IJob>(UnityFactory.Transaction),
                 m_Unity.Resolve<IJob>(UnityFactory.Dbi),
                 m_Unity.Unity.Resolve<IJob>(UnityFactory.MailProcess2), 
-                m_Unity.Unity.Resolve<IJob>(UnityFactory.EmailPartners)
+                m_Unity.Unity.Resolve<IJob>(UnityFactory.EmailPartners),
+                m_Unity.Resolve<IJob>(UnityFactory.Product)
             };
         }
 
