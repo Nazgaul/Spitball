2687339c34f23d8bed91d5628f5ffaff3356a44b
GuyGolanBB <pitbullxp@gmail.com>
2014-07-29 14:50:38 +0200

    FIXES

--- Zbang.Cloudents.Mvc4WebRole/Scripts/angular-mcustomscrollbar.js
+++ Zbang.Cloudents.Mvc4WebRole/Scripts/angular-mcustomscrollbar.js
@@ -23,12 +23,12 @@
             function setScroll() {
                 calcHeight();
               
-                $elem.mCustomScrollbar({ setHeight: height, theme: "dark", advanded: { updateOnContentResize: false } });
+                $elem.mCustomScrollbar({ setHeight: height, theme: "dark", /*advanded: { updateOnContentResize: false }*/ });
                 
             }
 
             function calcHeight() {
-                top = $attr.top ? parseInt($attr.top) : cd.getElementPosition($elem[0]).top;
+                top = $attr.top ? parseInt($attr.top) : $elem[0].getBoundingClientRect().top;
                 bottom = $attr.bottom ? parseInt($attr.bottom) : 0;
                 height = $win.height() - (top + bottom);
             }
