30c7d77da52a3b91626e25d573881df41a7d85b6
GuyGolanBB <pitbullxp@gmail.com>
2014-07-30 14:28:25 +0200

    fix searcxh

--- Zbang.Cloudents.Mvc4WebRole/Scripts/_references.js
+++ Zbang.Cloudents.Mvc4WebRole/Scripts/_references.js
@@ -163,6 +163,7 @@
 /// <reference path="../js/controllers/store/הןק'ctrl.js" />
 /// <reference path="../js/controllers/store/viewctrl.js" />
 /// <reference path="../js/controllers/store/checkoutctrl.js" />
+/// <reference path="../js/modules/carousel.js" />
 <<<<<<< HEAD
 /// <reference path="ng-scrollbar.js" />
 /// <reference path="stopwatch.js" />
