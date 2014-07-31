1f3732a3b0f8180e448fdb6324bc0e5c4ac624a7
danield770 <dddavid2@gmail.com>
2014-07-31 14:33:14 +0300

    Some store fixes

--- Zbang.Cloudents.Mvc4WebRole/Views/Store/Resources/StoreResources.Designer.cs
+++ Zbang.Cloudents.Mvc4WebRole/Views/Store/Resources/StoreResources.Designer.cs
@@ -676,7 +676,8 @@ namespace Zbang.Cloudents.Mvc4WebRole.Views.Store.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to לקבלת קוד קופון עליך להירשם לאתר או פנה לאגודת הסטודנטים בה אתה חבר.
+        ///   Looks up a localized string similar to לקבלת קוד קופון עליך להירשם לאתר
+        ///או פנה לאגודת הסטודנטים בה אתה חבר.
         /// </summary>
         public static string ToGetACoupon {
             get {
