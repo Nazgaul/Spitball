ce46436dbd90a6484e2616d5372ebc2347630d45
nazgaul <ram.y@outlook.com>
2014-08-10 15:09:45 +0300

    Fixes

--- Zbang.Zbox.Infrastructure.Transport/StoreContactData.cs
+++ Zbang.Zbox.Infrastructure.Transport/StoreContactData.cs
@@ -18,11 +18,15 @@ namespace Zbang.Zbox.Infrastructure.Transport
             Email = email;
             Text = text;
         }
-
+        [ProtoMember(1)]
         public string Name { get; private set; }
+        [ProtoMember(2)]
         public string Phone { get; set; }
+        [ProtoMember(3)]
         public string University { get; set; }
+        [ProtoMember(4)]
         public string Email { get; set; }
+        [ProtoMember(5)]
         public string Text { get; set; }
 
     }
