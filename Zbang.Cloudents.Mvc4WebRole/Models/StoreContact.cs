ee0d6d8f5117e2241ca7211699ef9a361ad07a90
danield770 <dddavid2@gmail.com>
2014-08-10 11:01:50 +0300

    contact localiztion in model

--- Zbang.Cloudents.Mvc4WebRole/Models/StoreContact.cs
+++ Zbang.Cloudents.Mvc4WebRole/Models/StoreContact.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-
+using Zbang.Cloudents.Mvc4WebRole.Views.Shared.Resources;
+using Zbang.Cloudents.Mvc4WebRole.Views.Store.Resources;
 namespace Zbang.Cloudents.Mvc4WebRole.Models
 {
     public class StoreContact
     {
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(DialogResources), ErrorMessageResourceName = "FieldRequired")]
+        [Display(ResourceType = typeof(SharedResources), Name = "Name")]
         public string Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessageResourceType = typeof(DialogResources), ErrorMessageResourceName = "FieldRequired")]
+        [Display(ResourceType = typeof(StoreResources), Name = "Telephone")]
         public string Phone { get; set; }
+
+        [Display(ResourceType = typeof(SharedResources), Name = "School")]
         public string University { get; set; }
-        [Required]
+
+        [Required(ErrorMessageResourceType = typeof(DialogResources), ErrorMessageResourceName = "FieldRequired")]
+        [Display(ResourceType = typeof(SharedResources), Name = "Email")]
         public String Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(DialogResources), ErrorMessageResourceName = "FieldRequired")]
+        [Display(ResourceType = typeof(SharedResources), Name = "Message")]
         public string Text { get; set; }
     }
 }
