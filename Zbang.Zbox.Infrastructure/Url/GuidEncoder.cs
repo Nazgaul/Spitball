3ca422c2b223643ce52cf9889c6afaf985082c68
nazgaul <ram.y@outlook.com>
2014-08-21 13:39:58 +0300

    remove unused

--- Zbang.Zbox.Infrastructure/Url/GuidEncoder.cs
+++ Zbang.Zbox.Infrastructure/Url/GuidEncoder.cs
@@ -12,7 +12,12 @@ namespace Zbang.Zbox.Infrastructure.Url
 
         public static string Encode(Guid guid)
         {
+            //var x = new Base62(500);
+            //var v = x.ToString();
+            //var z = new Base62(v);
             var enc = Convert.ToBase64String(guid.ToByteArray());
+
+            
             enc = enc.Replace("/", "_");
             enc = enc.Replace("+", "-");
             return enc.Substring(0, 22);
