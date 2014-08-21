38cc91a172708defaab939246bd8cdf25da12d0f
danield770 <dddavid2@gmail.com>
2014-08-21 12:04:13 +0300

    added partials for choose and create department with models

---
+++ Zbang.Cloudents.Mvc4WebRole/Models/ChooseDepartment.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Zbang.Cloudents.Mvc4WebRole.Models.Resources;
+
+namespace Zbang.Cloudents.Mvc4WebRole.Models
+{
+    public class ChooseDepartment
+    {
+        [Required(ErrorMessageResourceType = typeof(CreateUniversityResources), ErrorMessageResourceName = "FieldRequired")]
+        [Display(ResourceType = typeof(CreateUniversityResources), Name = "ChooseDept")]
+        public string Name { get; set; }
+    }
+}
