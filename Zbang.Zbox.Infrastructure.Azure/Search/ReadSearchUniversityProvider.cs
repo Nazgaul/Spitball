d4f0b52f3884038cfac126fa228fc7cdd874424e
nazgaul <ram.y@outlook.com>
2014-09-10 14:28:07 +0300

    bug 2357

--- Zbang.Zbox.Infrastructure.Azure/Search/ReadSearchUniversityProvider.cs
+++ Zbang.Zbox.Infrastructure.Azure/Search/ReadSearchUniversityProvider.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
-using Zbang.Zbox.Infrastructure.Security;
 using Zbang.Zbox.ViewModel.Dto.Library;
 
 namespace Zbang.Zbox.Infrastructure.Azure.Search
