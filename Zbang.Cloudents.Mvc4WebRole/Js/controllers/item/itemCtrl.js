f341a7cf1d1b36a5ddf7d78f251bce78f869930f
nazgaul <ram.y@outlook.com>
2014-09-16 17:12:46 +0300

    Starting to work on item view

--- Zbang.Cloudents.Mvc4WebRole/Js/controllers/item/itemCtrl.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/controllers/item/itemCtrl.js
@@ -1,9 +1,21 @@
 ﻿var mItem = angular.module('mItem', []);
 mItem.controller('ItemCtrl',
-        ['$scope','$routeParams',
-        function ($scope,$routeParams) {
-            //cd.pubsub.publish('initItem');
-            cd.pubsub.publish('item',$routeParams.itemId); //statistics
+        ['$scope', '$routeParams', 'sItem',
+        function ($scope, $routeParams, sItem) {
+            cd.pubsub.publish('initItem');
+
+            $scope.navigation = {};
+            sItem.nav({ id: $routeParams.itemId, boxId: $routeParams.boxId }).then(
+                function(response) {
+                    var data = response.success ? response.payload : [];
+                    $scope.navigation.previous = data.prev;
+                    $scope.navigation.next = data.next;
+                }
+            );
+
+          
+
+            cd.pubsub.publish('item', $routeParams.itemId); //statistics
             //todo proper return;
         }
         ]);
--- Zbang.Cloudents.Mvc4WebRole/Js/services/item.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/services/item.js
@@ -6,7 +6,6 @@
         var Item = '/Item/';
 
         return {
-
             'delete': function (data) {
                 var dfd = $q.defer();
                 $http.post(Item + 'Delete/', data).success(function (response) {
@@ -15,7 +14,16 @@
                     dfd.reject(response);
                 });
                 return dfd.promise;
-            }
+            },
+            nav : function (data) {
+            var dfd = $q.defer();
+                $http.get(Item + 'Navigation/', { params: data }).success(function (response) {
+                dfd.resolve(response);
+            }).error(function (response) {
+                dfd.reject(response);
+            });
+            return dfd.promise;
+        }
         };
     }
     ]);
