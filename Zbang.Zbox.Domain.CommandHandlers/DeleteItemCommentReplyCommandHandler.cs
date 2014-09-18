29bbe447c822ca99762cddedf0537e0c1a21920a
nazgaul <ram.y@outlook.com>
2014-09-18 09:40:15 +0300

    Fixes

--- Zbang.Zbox.Domain.CommandHandlers/DeleteItemCommentReplyCommandHandler.cs
+++ Zbang.Zbox.Domain.CommandHandlers/DeleteItemCommentReplyCommandHandler.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zbang.Zbox.Domain.Commands;
+using Zbang.Zbox.Domain.DataAccess;
 using Zbang.Zbox.Infrastructure.CommandHandlers;
+using Zbang.Zbox.Infrastructure.Repositories;
 
 namespace Zbang.Zbox.Domain.CommandHandlers
 {
@@ -25,17 +28,17 @@ namespace Zbang.Zbox.Domain.CommandHandlers
         {
             if (message == null) throw new ArgumentNullException("message");
 
-            var user = m_UserRepository.Load(message.UserId);
+            //var user = m_UserRepository.Load(message.UserId);
 
-            var itemComment = m_ItemCommentRepository.Load(message.AnnotationId);
-            if (!Equals(itemComment.Author, user))
-            {
-                throw new UnauthorizedAccessException("User is unauthorized to delete annotation");
-            }
-            itemComment.Item.DecreaseNumberOfComments();
-            m_ItemRepository.Save(itemComment.Item);
+            //var itemComment = m_ItemCommentRepository.Load(message.AnnotationId);
+            //if (!Equals(itemComment.Author, user))
+            //{
+            //    throw new UnauthorizedAccessException("User is unauthorized to delete annotation");
+            //}
+            //itemComment.Item.DecreaseNumberOfComments();
+            //m_ItemRepository.Save(itemComment.Item);
 
-            m_ItemCommentRepository.Delete(itemComment);
+            //m_ItemCommentRepository.Delete(itemComment);
         }
     }
 }
--- Zbang.Zbox.Domain.Commands/DeleteItemCommentReplyCommand.cs
+++ Zbang.Zbox.Domain.Commands/DeleteItemCommentReplyCommand.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zbang.Zbox.Infrastructure.Commands;
 
 namespace Zbang.Zbox.Domain.Commands
 {
-    class DeleteItemCommentReplyCommand
+  public  class DeleteItemCommentReplyCommand : ICommand
     {
     }
 }
