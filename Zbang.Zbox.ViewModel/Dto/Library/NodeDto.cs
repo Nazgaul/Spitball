adbeaddfbb3bb63a7f97797bd0ace71c8f097f41
nazgaul <ram.y@outlook.com>
2014-09-21 17:03:12 +0300

    bug 1004

--- Zbang.Zbox.ViewModel/Dto/Library/NodeDto.cs
+++ Zbang.Zbox.ViewModel/Dto/Library/NodeDto.cs
@@ -8,6 +8,6 @@ namespace Zbang.Zbox.ViewModel.Dto.Library
         public string Name { get; set; }
 
         public string Url { get; set; }
-      //  public string Color { get; set; }
+        public int NoBoxes { get; set; }
     }
 }
