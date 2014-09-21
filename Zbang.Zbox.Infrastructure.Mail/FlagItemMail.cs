a2ec977957b6c0a13372733a8d08fb4ca83b2ac2
nazgaul <ram.y@outlook.com>
2014-09-21 16:45:03 +0300

    bug 2474

--- Zbang.Zbox.Infrastructure.Mail/FlagItemMail.cs
+++ Zbang.Zbox.Infrastructure.Mail/FlagItemMail.cs
@@ -23,11 +23,18 @@ namespace Zbang.Zbox.Infrastructure.Mail
             message.Html = LoadMailTempate.LoadMailFromContent(parameters.UserCulture, "Zbang.Zbox.Infrastructure.Mail.MailTemplate.FlaggedItem");
             //message.Text = textBody;
             message.Subject = Subject;
-            message.AddSubstitution("{ItemUrl}", new List<string> { flagItemsParams.Url });
-            message.AddSubstitution("{ITEM NAME}", new List<string> { flagItemsParams.ItemName });
-            message.AddSubstitution("{REASON}", new List<string> { flagItemsParams.Reason });
-            message.AddSubstitution("{FLAGGER-USERNAME}", new List<string> { flagItemsParams.UserName });
-            message.AddSubstitution("{FLAGGER-EMAIL}", new List<string> { flagItemsParams.Email });
+
+            message.Html = message.Html.Replace("{ItemUrl}", flagItemsParams.Url);
+            message.Html = message.Html.Replace("{ITEM NAME}", flagItemsParams.ItemName);
+            message.Html = message.Html.Replace("{REASON}", flagItemsParams.Reason);
+            message.Html = message.Html.Replace("{FLAGGER-USERNAME}", flagItemsParams.UserName);
+            message.Html = message.Html.Replace("{FLAGGER-EMAIL}", flagItemsParams.Email);
+
+            //message.AddSubstitution("{ItemUrl}", new List<string> { flagItemsParams.Url });
+            //message.AddSubstitution("{ITEM NAME}", new List<string> { flagItemsParams.ItemName });
+            //message.AddSubstitution("{REASON}", new List<string> { flagItemsParams.Reason });
+            //message.AddSubstitution("{FLAGGER-USERNAME}", new List<string> { flagItemsParams.UserName });
+            //message.AddSubstitution("{FLAGGER-EMAIL}", new List<string> { flagItemsParams.Email });
             
         }
     }
