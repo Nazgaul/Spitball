c535d7d13d43b2d7d8b66d9ab327ab3c377d5dfc
nazgaul <ram.y@outlook.com>
2014-10-01 11:12:38 +0300

    bug 2573

--- Zbang.Cloudents.Mvc4WebRole/Js/controllers/item/itemFullScreenCtrl.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/controllers/item/itemFullScreenCtrl.js
@@ -1,11 +1,21 @@
 ﻿mItem.controller('itemFullScreenCtrl',
 [
-    '$scope', '$modalInstance', 
-    function ($scope, $modalInstance) {
+    '$scope', '$modalInstance','$sce',
+    function ($scope, $modalInstance, $sce) {
+        $scope.preview2 = '';
         $scope.$parent.fullScreen = true;
         $scope.closeFullScreen = function() {
             $modalInstance.close();
             $scope.$parent.fullScreen = false;
         };
+
+        $scope.$on('update', function () {
+            $scope.$apply(function () { //regular binding doesnt work
+                $scope.preview2 += $scope.$parent.preview;    
+
+            });
+        });
+       
+       
     }
 ]);
