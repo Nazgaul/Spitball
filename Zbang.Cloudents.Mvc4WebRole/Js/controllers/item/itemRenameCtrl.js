62211febc030f471848e7b7d5f0ef6dbf5bcbefe
nazgaul <ram.y@outlook.com>
2014-10-02 09:17:00 +0300

    xxxx

---
+++ Zbang.Cloudents.Mvc4WebRole/Js/controllers/item/itemRenameCtrl.js
@@ -0,0 +1,30 @@
+﻿mItem.controller('itemRenameCtrl',
+[
+    '$scope', '$modalInstance', 'data', 'sItem',
+function ($scope, $modalInstance, data, sItem) {
+    $scope.formdata = {}
+    $scope.formdata.newName = data.name;
+    $scope.formdata.id = data.id;
+
+    $scope.disable = false;
+
+    $scope.renameItem = function () {
+        $scope.disable = true;
+
+        sItem.rename($scope.formdata).then(function (response) {
+            $scope.disable = false;
+            if (!response.success) {
+                alert(response.payload);
+                return;
+            }
+            $modalInstance.close(response.payload);
+        });
+
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss();
+    };
+}
+
+]);
