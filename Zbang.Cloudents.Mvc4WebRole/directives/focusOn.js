9aee90683cd0004fb722f82e2f050f0bea118543
nazgaul <ram.y@outlook.com>
2014-10-02 09:14:56 +0300

    xxx

---
+++ Zbang.Cloudents.Mvc4WebRole/directives/focusOn.js
@@ -0,0 +1,22 @@
+﻿app.directive('focusOn',
+    [
+
+    function () {
+        return {
+            restrict: "A",
+            link: function (scope, elem, attrs) {
+                scope.$on('focusOn', function (e, name) {
+                    if (name === attrs.focusOn) {
+                        elem[0].focus();
+                    }
+                });
+            }
+        };
+    }
+    ]);
+
+
+
+
+
+
