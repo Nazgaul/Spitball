344b8c0bb9b3428484fca8063aab970714be412f
nazgaul <ram.y@outlook.com>
2014-10-14 12:02:13 +0300

    xxx

--- Zbang.Cloudents.Mvc4WebRole/Js/DataContext.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/DataContext.js
@@ -14,7 +14,7 @@
 
         }
         var dashboard = "/Dashboard/", library = "/Library/", cBOX = "/Box/", share = "/Share/", cAccount = "/Account/", search = '/Search/',
-             cITEM = "/Item/", get = 'get',
+            comment = '/comment/', cITEM = "/Item/", get = 'get',
         definition = {};
         //#region definition
 
