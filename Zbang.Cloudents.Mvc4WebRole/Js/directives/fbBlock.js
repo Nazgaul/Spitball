f78654106a66c491643a5b03b0bf205d74d45e74
GuyGolanBB <pitbullxp@gmail.com>
2014-11-04 16:04:12 +0200

    fix

--- Zbang.Cloudents.Mvc4WebRole/Js/directives/fbBlock.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/directives/fbBlock.js
@@ -7,44 +7,48 @@ app.directive('fbBlock',
                 restrict: "A",
                 scope: false,
                 link: function (scope, elem, attrs) {
-                    
-                    var fbDialog,element,fInterval, interval;
 
-                    if (!fbDialog) {
-                        fInterval = setInterval(function () {
-                            fbDialog = angular.element('.fb_dialog iframe').closest('.fb_dialog');
+                    var fbDialog, element, fInterval, interval;
 
-                            if (fbDialog.length) {
-                                clearInterval(fInterval);
-                                return;
+                    fInterval = setInterval(function () {
+                        fbDialog = angular.element('.fb_dialog iframe').closest('.fb_dialog');
+
+                        if (!fbDialog.length) {
+                            return;
+                        }
+                        clearInterval(fInterval);
+                        interval = setInterval(function () {
+                            if (fbDialog.position().top > 0) {
+                                clearInterval(interval);
+                                element = angular.element('<div></div>');
+                                element.css({
+                                    width: fbDialog.outerWidth(),
+                                    height: 90,
+                                    position: 'absolute',
+                                    zIndex: 999999,
+                                    opacity: 1,
+                                    top: fbDialog.position().top,
+                                    left: fbDialog.position().left
+                                });
                             }
 
                             angular.element(document.body).append(element);
-                        }, 50);
-                    }
-                    interval = setInterval(function () {
-                        if (fbDialog.position().top > 0) {
-                            clearInterval(interval);
-                            element = angular.element('<div></div>');
-                            element.css({
-                                width: fbDialog.outerWidth(),
-                                height: 90,
-                                position: 'absolute',
-                                zIndex: 999999,
-                                opacity: 1,
-                                top: fbDialog.position().top,
-                                left: fbDialog.position().left
-                            });
-                        }
+                        }, 20);
 
-                        angular.element(document.body).append(element);
-                    }, 50);
+                        return;
+                    }, 10);
+
+
 
 
                     scope.$on('$destroy', function () {
-                        element.remove();
+                        if (element) {
+                            element.remove();
+                            fbDialog = null;
+                        }
+       
                     });
-                    
+
                 }
             };
         }
