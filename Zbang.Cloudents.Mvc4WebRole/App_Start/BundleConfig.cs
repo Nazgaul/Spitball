50ef5365826ceae5a08fa7f1a5bfa6401adaf630
nazgaul <ram.y@outlook.com>
2014-11-12 08:09:02 +0200

    Some fixes

--- Zbang.Cloudents.Mvc4WebRole/App_Start/BundleConfig.cs
+++ Zbang.Cloudents.Mvc4WebRole/App_Start/BundleConfig.cs
@@ -498,7 +498,6 @@ namespace Zbang.Cloudents.Mvc4WebRole
         private static string RegisterJs(IEnumerable<JsFileWithCdn> jsFiles, JavaScriptBundle javaScriptBundleImp)
         {
             var jsBundle = javaScriptBundleImp;
-            jsBundle.ForceDebug();
             jsBundle.WithReleaseFileRenderer(new SquishItRenderer());
             foreach (var jsFile in jsFiles)
             {
