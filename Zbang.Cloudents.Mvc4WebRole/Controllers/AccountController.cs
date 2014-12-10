e9d4d1e18f9c6d634d5b8998e5cfd5866635690b
GuyGolanBB <pitbullxp@gmail.com>
2014-12-10 17:45:20 +0200

    as

--- Zbang.Cloudents.Mvc4WebRole/Controllers/AccountController.cs
+++ Zbang.Cloudents.Mvc4WebRole/Controllers/AccountController.cs
@@ -252,7 +252,7 @@ namespace Zbang.Cloudents.Mvc4WebRole.Controllers
         [HttpPost]
    //     [ValidateAntiForgeryToken]
         public JsonResult Register([ModelBinder(typeof(TrimModelBinder))] Register model)
-        {
+        { 
             if (!ModelState.IsValid)
             {
                 return JsonError(GetModelStateErrors());
