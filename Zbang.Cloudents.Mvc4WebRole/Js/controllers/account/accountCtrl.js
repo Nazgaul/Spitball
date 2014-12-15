545ec3930bb715a7da6a38047fcfcacca2abc2cf
GuyGolanBB <pitbullxp@gmail.com>
2014-12-15 09:44:25 +0200

    fix cdn fallback

--- Zbang.Cloudents.Mvc4WebRole/Js/controllers/account/accountCtrl.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/controllers/account/accountCtrl.js
@@ -1,11 +1,29 @@
 ﻿mAccount.controller('AccountCtrl',
-['$scope','$timeout',
-function ($scope,$timeout) {
+['$scope','$timeout','sAccount',
+function ($scope,$timeout,sAccount) {
     "use strict";
 
     $timeout(function () {
         $scope.$emit('viewContentLoaded');
     });
+
+    $scope.changeLocale = function(lang){
+         $angularCacheFactory.get('htmlCache').removeAll();
+
+                $analytics.eventTrack('Language Change', {
+                    category: 'Register Popup',
+                    label: 'User changed language to ' +lang
+                });
+
+                sAccount.changeLocale({ language: lang}).then(function () {
+         
+                 
+                    $window.location.reload();
+                });
+    };
+        
+
+
 
 }
 ]);
