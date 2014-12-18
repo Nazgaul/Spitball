f82927a883b54df93290d62faf92f4779d3f1629
nazgaul <ram.y@outlook.com>
2014-12-18 11:39:47 +0200

    version 24.1.5

---
+++ Zbang.Cloudents.Mvc4WebRole/Models/SettingsUnauth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zbang.Cloudents.Mvc4WebRole.Models
+{
+    public class SettingsUnauth
+    {
+        public bool ShowLanguage { get; set; }
+        public bool ShowLogIn { get; set; }
+    }
+}
