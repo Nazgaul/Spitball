1434ee92de1088745e614b9f9ccd2d94c28c1b57
GuyGolanBB <pitbullxp@gmail.com>
2014-12-23 10:30:00 +0200

    revert

--- Zbang.Cloudents.Mvc4WebRole/Js/controllers/account/notificationsCtrl.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/controllers/account/notificationsCtrl.js
@@ -7,19 +7,14 @@
             //    notificationsListLength: 7,
             //    notificationsListPage: 7,
             //};            
-            $scope.params = {};
 
             getDetails();
 
-            $scope.toggleNotifications = function () {
-
-                $scope.$broadcast('update-scroll');
-
-                $scope.params.isOpen = !$scope.params.isOpen;
+            $scope.openNotifications = function () {
 
                 $analytics.eventTrack('Notifications', {
                     category: 'Notifications',
-                    label: 'User ' + ($scope.params.isOpen ? 'opened' : 'closed') + ' notifications'
+                    label: 'User ' + ($scope.params.wasOpened ? 'closed' : 'opened') + ' notifications'
                 });
 
             };
@@ -63,19 +58,25 @@
             //$scope.addNotifications = function () {
             //    $scope.params.notificationsListLength += $scope.params.notificationsListPage;
             //};
-            
 
-            $scope.delete = function (event, notification) {
-                event.preventDefault();
+            $scope.updateState = function (isOpen) {
+                if (!isOpen) {
+                    return;
+                }
+
+                $scope.$broadcast('update-scroll');
+            }
+
+            $scope.delete = function (notification) {
                 sNotification.remove(notification.msgId);
                 getDetails();
             };
 
             function getDetails() {
                 $scope.notifications = sNotification.getAll();
-                $scope.params.newNotifications = sNotification.getUnreadLength();              
+                $scope.params.newNotifications = sNotification.getUnreadLength();
             }
-            
+
 
 
         }]);
