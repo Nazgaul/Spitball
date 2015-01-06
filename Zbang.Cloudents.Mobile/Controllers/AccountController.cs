7f8f881df46c8f4222110372d2c6152941a24994
nazgaul <ram.y@outlook.com>
2015-01-06 16:03:53 +0200

    Added points to user

--- Zbang.Cloudents.Mobile/Controllers/AccountController.cs
+++ Zbang.Cloudents.Mobile/Controllers/AccountController.cs
@@ -714,7 +714,14 @@ namespace Zbang.Cloudents.Mobile.Controllers
                 return JsonOk();
             }
             var retVal = ZboxReadService.GetUserData(new GetUserDetailsQuery(User.GetUserId()));
-            return JsonOk(new { retVal.Id, retVal.UniversityId, retVal.Name, retVal.Image, retVal.IsAdmin, retVal.FirstTimeDashboard });
+            return JsonOk(new { retVal.Id,
+                retVal.UniversityId,
+                retVal.Name, 
+                retVal.Image,
+                retVal.IsAdmin, 
+                retVal.FirstTimeDashboard,
+                retVal.Score
+            });
 
         }
 
