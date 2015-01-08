6a3d7b19b869d75f87ac78f68fe949808c4f5cd8
nazgaul <ram.y@outlook.com>
2015-01-08 15:30:45 +0200

    xxx

--- Zbang.Cloudents.Mobile/Js/shared/events/events.js
+++ Zbang.Cloudents.Mobile/Js/shared/events/events.js
@@ -12,86 +12,127 @@
                     element.off('focus');
                 });
             }
+        }).
+    directive('dragView', function () {
+        return {
+            restrict: "A",
+            link: function (scope, element, attr) {
+                var posX;
+
+                element.on('touchstart', function (e) {
+                    posX = e.touches[0].clientX;
+                });
+                element.on('touchmove', function (e) {
+
+                    var curPosX = e.touches[0].clientX;
+
+                    if (curPosX > posX) {
+                        return;
+                    }
+
+                    if (Math.abs(curPosX - posX) >= 135) {
+                        return;
+                    }
+
+                    element.css({ marginLeft: curPosX - posX + 'px' });
+
+                });
+                element.on('touchend', function (e) {
+                    element.css({ marginLeft: 0 });
+                });
+
+                scope.$on('$destroy', function () {
+                    element.off('touchstart');
+                    element.off('tocuhmove');
+                    element.off('touchend');
+                });
+
+            }
         }
+    }).
+    directive('focusInvalid', function () {
+        return {
+            restrict: "A",
+            link: function (scope, elem) {
+
+                // set up event handler on the form element
+                elem.on('submit', function () {
+
+                    // find the first invalid element
+                    var firstInvalid = elem[0].querySelector('.ng-invalid');
 
 
-    ).directive('dragView',
-   function () {
+                    // if we find one, set focus
+                    if (firstInvalid) {
+                        firstInvalid.focus();
+                    }
+                });
 
-       return {
-           restrict: "A",
-           link: function (scope, element, attr) {
-               var posX;
+                scope.$on('$destroy', function () {
+                    elem.off('submit');
+                });
 
-               element.on('touchstart', function (e) {                   
-                   posX = e.touches[0].clientX;
-               });
-               element.on('touchmove', function (e) {
-                   
-                   var curPosX = e.touches[0].clientX;
+            }
+        };
+    }).directive('closeKeyboard', function () {
+        return {
+            restrict: "A",
+            link: function (scope, elem) {
 
-                   if (curPosX > posX) {
-                       return;
-                   }
+                // set up event handler on the form element
+                elem.on('submit', function () {
 
-                   if (Math.abs(curPosX - posX) >= 135) {
-                       return;
-                   }
+                    elem[0].querySelector('input[type="search"]').blur(); //hack to close keyboard
 
-                   element.css({ marginLeft: curPosX - posX + 'px' });
+                });
 
-               });
-               element.on('touchend', function (e) {                   
-                   element.css({ marginLeft: 0 });
-               });
+                scope.$on('$destroy', function () {
+                    elem.off('submit');
+                })
+            }
+        };
+    }).
+    directive('stickyForm', ['$window', function ($window) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var $win = angular.element($window);
 
-               scope.$on('$destroy', function () {
-                   element.off('touchstart');
-                   element.off('tocuhmove');
-                   element.off('touchend');
-               });
+                $win.on('scroll', function () {
+                    var rectObj = element[0].getBoundingClientRect();
+                    if (rectObj.top <= 0) {
+                        element.addClass('sticky')
+                        return;
+                    }
 
-           }
-       }
-   }).directive('focusInvalid', function () {
-       return {
-           restrict: "A",
-           link: function (scope, elem) {
+                    element.removeClass('sticky')
 
-               // set up event handler on the form element
-               elem.on('submit', function () {
-
-                   // find the first invalid element
-                   var firstInvalid = elem[0].querySelector('.ng-invalid');
+                });
 
 
-                   // if we find one, set focus
-                   if (firstInvalid) {
-                       firstInvalid.focus();
-                   }
-               });
+                scope.$on('destroy', function () {
+                    $win.off('scroll');
+                });
+            }
+        };
+    }]).
+    directive('focusComment', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.on('click',function(){                    
+                    var parentElement = element[0].parentElement;
+                    while (parentElement.nodeName !== 'LI') {
+                        parentElement = parentElement.parentElement;
+                    }
 
-               scope.$on('$destroy', function () {
-                   elem.off('submit');
-               });
+                    var textarea = parentElement.querySelector('textarea');
+                    textarea.focus();
+                });
 
-           }
-       };
-   }).directive('closeKeyboard', function () {
-       return {
-           restrict: "A",
-           link: function (scope, elem) {
-
-               // set up event handler on the form element
-               elem.on('submit', function () {
-
-                   elem[0].querySelector('input[type="search"]').blur(); //hack to close keyboard
-
-               });
-
-               scope.$on('$destroy', function () {
-                   elem.off('submit');
-               })
-           }
-       };
-   });
+                scope.$on('destroy', function () {
+                    element.off('click');
+                });
+            }
+        };
+    });
