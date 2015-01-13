00f145fa81cf72832982b952089216853846eb47
danield770 <dddavid2@gmail.com>
2015-01-13 12:19:27 +0200

    letters in search list as well now

--- Zbang.Cloudents.Mobile/Js/shared/boxLetter/boxLetter.js
+++ Zbang.Cloudents.Mobile/Js/shared/boxLetter/boxLetter.js
@@ -1,16 +1,11 @@
-﻿angular.module('dashboard').directive('boxLetter',
+﻿angular.module('app').directive('boxLetter',
    function () {
        return {
            restrict: 'A',
            link: function (scope, element, attrs) {
                var random = Math.floor(Math.random() * 7) + 1;
 
-               var char;
-               if (scope.box) {
-                   char = scope.box.name[0].toUpperCase();
-               } else if (scope.recommendedBox) {
-                   char = scope.recommendedBox.name[0].toUpperCase();
-               }
+               var char = attrs.letter.toUpperCase();                                                
                code = char.charCodeAt(0).toString(),
                sum = 0;
                for (var i = 0; i < code.length; i++) {
