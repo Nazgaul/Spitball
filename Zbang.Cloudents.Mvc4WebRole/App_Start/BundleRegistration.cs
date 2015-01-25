f245a6d2691b00467282a57759daa275f0070aee
GuyGolanBB <pitbullxp@gmail.com>
2015-01-25 13:29:28 +0200

    remove double code

--- Zbang.Cloudents.Mvc4WebRole/App_Start/BundleRegistration.cs
+++ Zbang.Cloudents.Mvc4WebRole/App_Start/BundleRegistration.cs
@@ -105,10 +105,6 @@ namespace Zbang.Cloudents.Mvc4WebRole
                         //    ),
                         new JsFileWithCdn("~/scripts/angular-animate.js",
                             "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.4/angular-animate.min.js"),
-
-                            new JsFileWithCdn("~/scripts/angular-animate.js",
-                            "https://ajax.googleapis.com/ajax/libs/angularjs/1.3.4/angular-animate.min.js"),
-
                             new JsFileWithCdn("~/scripts/textAngular/textAngular-rangy.min.js"),
                             new JsFileWithCdn("~/scripts/textAngular/textAngular-sanitize.js"),
                             new JsFileWithCdn("~/scripts/textAngular/textAngular.js"),
