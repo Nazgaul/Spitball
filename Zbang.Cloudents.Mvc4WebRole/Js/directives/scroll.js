1782b4384d8dade836d8f354081188470cc4bd3b
GuyGolanBB <pitbullxp@gmail.com>
2015-02-11 14:11:55 +0200

    search

---
+++ Zbang.Cloudents.Mvc4WebRole/Js/directives/scroll.js
@@ -0,0 +1,29 @@
+﻿angular.module('scroll',[]).directive('scrollPaging',
+   ['$window', function ($window) {
+
+       return {
+           restrict: "A",
+           scope: {
+               onScroll: '&'
+           },
+           link: function (scope, element, attr) {
+               var $win = angular.element($window);
+
+               $win.on('scroll', isTriggerFunc);
+
+               scope.$on('$destroy', function () {
+                   $win.off('scroll', isTriggerFunc);
+               });
+
+               function isTriggerFunc() {
+                   var scrollTop = window.pageYOffset,
+                       scrollHeight = document.body.scrollHeight,
+                       windowHeight = window.innerHeight;
+
+                   if (scrollTop + windowHeight >= scrollHeight * 0.8) {
+                       scope.$apply(scope.onScroll);
+                   }
+               }
+           }
+       }
+   }]);
