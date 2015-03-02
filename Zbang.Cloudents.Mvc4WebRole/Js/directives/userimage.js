b3c9db30b100e815de991b78ddb14e5d1ced57fc
GuyGolanBB <pitbullxp@gmail.com>
2015-03-02 09:56:07 +0200

    fix user image empty state quiz

--- Zbang.Cloudents.Mvc4WebRole/Js/directives/userimage.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/directives/userimage.js
@@ -3,7 +3,8 @@
     function ($compile,sUserDetails) {
         "use strict";
         var imageTemplate = '<img class="userImg" alt=""  />',
-            letterTemplate = '<span class="userLetter"></span>';
+            letterTemplate = '<span class="userLetter"></span>',
+            emptyTemplate = '<img src="/images/user.svg" />';
         return {
             restrict: "E",
             scope: {
@@ -26,7 +27,7 @@
                     
                     innerEl[0].src = scope.image;
 
-                } else {
+                } else if (scope.name) {
                     innerEl = angular.element(letterTemplate);
                     className = elem[0].getAttribute('class');
                     if (className) {
@@ -34,7 +35,17 @@
                     }
 
                     innerEl[0].setAttribute('data-letter', scope.name[0]);
-                }               
+                } else {
+
+                    innerEl = angular.element(emptyTemplate);
+                    className = elem[0].getAttribute('class');
+                    if (className) {
+                        innerEl.addClass(className);
+                    }
+
+                    innerEl[0].height = elem[0].getAttribute('height');
+                    innerEl[0].width = elem[0].getAttribute('width');
+                }
 
                 if (scope.id && sUserDetails.getDetails().id != scope.id) {
                     innerEl.attr('user-tooltip-popup', scope.id);
