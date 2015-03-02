37d79cfaeedad0d8be694a6d4b0797d5966aaba9
nazgaul <ram.y@outlook.com>
2015-03-02 09:49:13 +0200

    bug 3492

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("27.0.8.*")]
-[assembly: AssemblyFileVersion("27.0.8.0")]
+[assembly: AssemblyVersion("27.1.0.*")]
+[assembly: AssemblyFileVersion("27.1.0.0")]
