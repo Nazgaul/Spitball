87eceb3c76d950b315ed8bbf2cdced6b8690c9d7
nazgaul <ram.y@outlook.com>
2015-03-03 15:43:09 +0200

    Some fixes

--- Zbang.Cloudents.Mobile/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mobile/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Zbang.Cloudents.Mvc4WebRole")]
+[assembly: AssemblyTitle("Zbang.Cloudents.Mobile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Zbang.Cloudents.Mvc4WebRole")]
+[assembly: AssemblyProduct("Zbang.Cloudents.Mobile")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
