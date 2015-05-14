fcc63ca76fb2d3b40ad812a76fda8ed7743bc9e3
nazgaul <ram.y@outlook.com>
2015-05-14 10:00:25 +0300

    version 2.3.7

--- Zbang.Cloudents.Mobile/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mobile/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("27.3.4.*")]
-[assembly: AssemblyFileVersion("27.3.4.0")]
+[assembly: AssemblyVersion("27.3.7.*")]
+[assembly: AssemblyFileVersion("27.3.7.0")]
