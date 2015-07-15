3fd81243089c49dd93eee4c7c4f6c78b7f37aec9
nazgaul <ram.y@outlook.com>
2015-07-15 15:15:04 +0300

    Some fixes in spitball

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("28.0.12.*")]
-[assembly: AssemblyFileVersion("28.0.12.0")]
+[assembly: AssemblyVersion("28.0.15.*")]
+[assembly: AssemblyFileVersion("28.0.15.0")]
