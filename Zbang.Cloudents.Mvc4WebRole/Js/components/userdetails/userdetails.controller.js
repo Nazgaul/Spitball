a3b5c3ad28726573e4b0b1178b1363fcda94fa32
nazgaul <ram.y@outlook.com>
2015-09-01 13:21:41 +0300

    unregister user.

--- Zbang.Cloudents.Mvc4WebRole/Js/components/userdetails/userdetails.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/userdetails/userdetails.controller.js
@@ -1,16 +1,41 @@
 ﻿/// <reference path="~/Scripts/angular.js" />
 (function () {
-    angular.module('app.userdetails').controller('UserDetails',userDetails);
+    angular.module('app.userdetails').controller('UserDetails', userDetails);
+    userDetails.$inject = ['userDetailsService'];
+
+    function userDetails(userDetailsService) {
+        var ud = this;
+        ud.isLoggedIn = false;
+
+        userDetailsService.getDetails().then(function (response) {
+            if (response.id) {
+                ud.isLoggedIn = true;
+            }
+            ud.id = response.id;
+            ud.name = response.name;
+            ud.image = response.image;
+            ud.score = response.score;
+        });
+
+        //ud.name = 'Guy Golan';
+        //ud.points = 500;
+        //ud.image = 'http://lorempixel.com/400/200/';
+    }
+})();
+
+(function () {
+    angular.module('app.userdetails').service('userDetailsService', userDetails);
     userDetails.$inject = ['ajaxService'];
 
     function userDetails(ajaxservice) {
-
-        ajaxservice.get('/account/details', null).then(function(response) {
-            console.log(response)
-        });
         var ud = this;
-        ud.name = 'Guy Golan';
-        ud.points = 500;
-        ud.image = 'http://lorempixel.com/400/200/';
+        ud.isLoggedIn = false;
+        ud.getDetails = function () {
+            return ajaxservice.get('/account/details', null, 1800000);
+        }
+
+        //ud.name = 'Guy Golan';
+        //ud.points = 500;
+        //ud.image = 'http://lorempixel.com/400/200/';
     }
 })();
