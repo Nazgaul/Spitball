8be8a80b5c70ceae81710d56991efe2be70650e2
nazgaul <ram.y@outlook.com>
2015-09-07 17:10:28 +0300

    Continue with box items

--- Zbang.Cloudents.Mvc4WebRole/Js/components/box/item.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/box/item.controller.js
@@ -7,7 +7,7 @@
         boxService.getItems($stateParams.boxId).then(function (response) {
             i.data = response;
             for (var j = 0; j < i.data.length; j++) {
-                i.data[j].thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(i.data[j].source) + '.jpg?width=240&height=340&mode=crop'; // item.source
+                i.data[j].thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(i.data[j].source) + '.jpg?width=368&height=520&mode=crop'; // item.source
             }
             $scope.$broadcast("boxItemsLoaded");
         });
--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("29.3.13.*")]
-[assembly: AssemblyFileVersion("29.3.13.0")]
+[assembly: AssemblyVersion("29.3.15.*")]
+[assembly: AssemblyFileVersion("29.3.15.0")]
