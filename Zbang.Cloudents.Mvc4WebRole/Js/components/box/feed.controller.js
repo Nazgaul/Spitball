9b7a7eede7ac3451f4f87e5d3640754cabec2609
nazgaul <ram.y@outlook.com>
2015-09-20 13:56:51 +0300

    Some fixes in feed.

--- Zbang.Cloudents.Mvc4WebRole/Js/components/box/feed.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/box/feed.controller.js
@@ -6,6 +6,17 @@
         var f = this;
         boxService.getFeed($stateParams.boxId).then(function (response) {
             f.data = response;
+
+            for (var i = 0; i < f.data.length; i++) {
+                for (var j = 0; j < f.data[i].files.length; j++) {
+                    f.data[i].files[j].thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(f.data[i].files[j].source) + '.jpg?width=100&height=125&mode=crop';
+                }
+                for (var k = 0; k < f.data[i].answers.length; k++) {
+                    for (var l = 0; l < f.data[i].answers[k].files; l++) {
+                        f.data[i].answers[k].files[l].thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(f.data[i].answers[k].files[l].source) + '.jpg?width=100&height=125&mode=crop';
+                    }
+                }
+            }
         });
     }
 })();
