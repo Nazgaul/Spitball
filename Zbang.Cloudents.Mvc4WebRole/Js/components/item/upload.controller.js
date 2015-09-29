c33e3ab2636482f2cda538bb72c2d7bd4b41e9a4
nazgaul <ram.y@outlook.com>
2015-09-29 15:52:48 +0300

    Upload

--- Zbang.Cloudents.Mvc4WebRole/Js/components/item/upload.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/item/upload.controller.js
@@ -1,8 +1,8 @@
 ﻿(function () {
     angular.module('app.upload').controller('Upload', upload);
-    upload.$inject = ['$scope', 'itemService', 'dropboxService', '$q', 'googleService'];
+    upload.$inject = ['$scope', 'itemService', 'dropboxService', '$q', 'googleService', '$timeout'];
 
-    function upload($scope, itemService, dropboxService, $q, googleService) {
+    function upload($scope, itemService, dropboxService, $q, googleService, $timeout) {
         var u = this;
         var cc = $scope.$parent.cc;
 
@@ -21,7 +21,7 @@
             $q.all([googleService.initDrive(), googleService.initGApi()]).then(function () {
                 u.googleDriveLoaded = true;
             });
-          
+
         });
         u.dropBoxLoaded = false;
         u.googleDriveLoaded = false;
@@ -63,7 +63,7 @@
                 return;
             }
             cc.submitFormProcess = true;
-            itemService.addLink(u.link, cc.box.id).then(function (response) {
+            itemService.addLink(u.link, cc.box.id).then(function () {
                 u.uploadStep = uploadChoose.none;
             }, function (response) {
                 cc.alert = response;
@@ -71,6 +71,59 @@
                 cc.submitFormProcess = false;
             });
 
+        }
+
+        u.fileUpload = {
+            url: '/upload/file/',
+            options: {
+                chunk_size: '3mb',
+                drop_element: 'dropElement'
+            },
+            callbacks: {
+                filesAdded: function (uploader, files) {
+                    $timeout(function () {
+                        uploader.start();
+                    }, 1);
+                },
+
+                beforeUpload: function (up, file) {
+                    up.settings.multipart_params = {
+                        fileName: file.name,
+                        fileSize: file.size,
+                        boxId: cc.box.id,
+                        question: false,
+                        isComment: false
+
+                    };
+
+
+                    //var data = {};
+                    //if (file.newQuestion) {
+                    //    data.newQuestion = true;
+                    //}
+                    //if (file.questionId) {
+                    //    data.questionId = file.questionId;
+                    //}
+
+                    //$rootScope.$broadcast('BeforeUpload', data);
+
+                },
+                //uploadProgress: function (uploader, file) {
+                //    $scope.loading = file.percent / 100.0;
+                //},
+                fileUploaded: function (uploader, file, response) {
+                    // $scope.loading = false;
+                    var obj = JSON.parse(response.response);
+                    if (obj.success) {
+                        //self.data.image = obj.payload;
+                        //userDetailsService.changeImage(obj.payload);
+                    }
+                },
+                error: function (uploader, error) {
+                    //$scope.loading = false;
+                    alert(error.message);
+                }
+            }
         }
     }
 })();
