11f2415b2050133d741d9cd9f6e035abb9e88757
nazgaul <ram.y@outlook.com>
2015-10-11 09:42:36 +0300

    Working with animation. fix some issue.

--- Zbang.Cloudents.Mvc4WebRole/Js/components/box/item.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/box/item.controller.js
@@ -4,26 +4,44 @@
 
     function items(boxService, $stateParams, $scope, $q) {
         var i = this;
+       
+
         $q.all([
             boxService.getItems($stateParams.boxId),
             boxService.getTabs($stateParams.boxId)
         ]).then(function (data) {
-            i.data = data[0];
-            for (var j = 0; j < i.data.length; j++) {
-                i.data[j].thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(i.data[j].source) + '.jpg?width=368&height=520&mode=crop'; // item.source
+            i.items = data[0];
+            for (var j = 0; j < i.items.length; j++) {
+                i.items[j].thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(i.items[j].source) + '.jpg?width=368&height=520&mode=crop'; // item.source
             }
             i.tabs = data[1];
-            $scope.$broadcast("mixItUp");
         });
 
-        $scope.$on('item_upload', function (event,response) {
+       
+
+
+        i.changeTab = function (tabid) {
+            i.tabSelectedId = tabid;
+        }
+
+        i.filterItems = function(item) {
+            if (!i.tabSelectedId) {
+                return true;
+            }
+            if (i.tabSelectedId === item.tabId) {
+                return true;
+            }
+            return false;
+        }
+
+        //upload
+        $scope.$on('item_upload', function (event, response) {
             if (response.boxId != $stateParams.boxId) { // string an int comarison
                 return;
             }
             var item = response.fileDto;
             item.thumbnail = 'https://az779114.vo.msecnd.net/preview/' + encodeURIComponent(item.source) + '.jpg?width=368&height=520&mode=crop'; // item.source
             i.data.unshift(item);
-            $scope.$broadcast("mixItUp");
         });
     }
 })();
