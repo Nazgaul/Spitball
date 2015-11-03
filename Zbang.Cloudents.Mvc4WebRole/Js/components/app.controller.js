6f70cca40bcf6c9d4705e665acf77664bc72f044
nazgaul <ram.y@outlook.com>
2015-11-03 15:29:17 +0200

    Some copy from v29
    implement new input stuff

--- Zbang.Cloudents.Mvc4WebRole/Js/components/app.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/app.controller.js
@@ -1,12 +1,14 @@
 ﻿(function () {
     angular.module('app').controller('AppController', appController);
-    appController.$inject = ['$scope'];
+    appController.$inject = ['$rootScope', '$window', '$location'];
 
-    function appController($scope) {
-        //$scope.$on('$viewContentLoaded', function () {
-        //    //TODO: maybe this is no good.
-        //    Metronic.init(); // init core components
-            
-        //});
+    function appController($rootScope, $window, $location) {
+        $rootScope.$on('$viewContentLoaded', function () {
+            var path = $location.path(),
+                absUrl = $location.absUrl(),
+                virtualUrl = absUrl.substring(absUrl.indexOf(path));
+            $window.dataLayer.push({ event: 'virtualPageView', virtualUrl: virtualUrl });
+        });
+
     }
 })();
--- Zbang.Cloudents.Mvc4WebRole/Js/homePage/homeScreen.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/homePage/homeScreen.js
@@ -31,10 +31,6 @@ window.addEventListener("load", function load(event) {
         localStorage.setItem(removeCloudentsBanner, '1');
     });
 
-    $('.mobileLink').on('click', function (e) {
-        ga('send', 'event', 'Mobile Link', e.target.id);
-    });
-
     var willShow = localStorage.getItem(removeCloudentsBanner);
     if (willShow != null) {
         $('.alert').alert('close');
@@ -52,8 +48,73 @@ window.addEventListener("load", function load(event) {
 
     ga('create', 'UA-9850006-3', 'auto');
     ga('send', 'pageview');
+
+
+    $('.mobileLink').click(function (e) {
+        ga('send', 'event', 'Mobile Link', e.target.id);
+    });
+
 }, false);
 
 
+//jQuery is required to run this code
+//
+
+//Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+//    get: function () {
+//        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+//    }
+//})
+
+(function () {
+    var min_w = 300; // minimum video width allowed
+    var vid_w_orig; // original video dimensions
+    var vid_h_orig, containerHeight, videoElement;
 
 
+    jQuery(function () { // runs after DOM has loaded
+        videoElement = jQuery('video');
+        vid_w_orig = parseInt(videoElement.attr('width'));
+        vid_h_orig = parseInt(videoElement.attr('height'));
+        containerHeight = $('#spitballFeatures').height();
+        jQuery(window).resize(function () { resizeToCover(); });
+        jQuery(window).trigger('resize');
+
+        //videoElement.bind('play', function() {
+        //    console.log('here');
+        //});
+    });
+
+    function resizeToCover() {
+
+        // set the video viewport to the window size
+        //jQuery('#video-viewport').width(jQuery(window).width());
+        //jQuery('#video-viewport').height(jQuery(window).height());
+
+        // use largest scale factor of horizontal/vertical
+        var scale_h = jQuery(window).width() / vid_w_orig;
+        var scale_v = jQuery(window).height() / vid_h_orig;
+        var scale = scale_h > scale_v ? scale_h : scale_v;
+
+        // don't allow scaled width < minimum video width
+        if (scale * vid_w_orig < min_w) {
+            scale = min_w / vid_w_orig;
+        };
+
+        // now scale the video
+        videoElement.width(scale * vid_w_orig);
+        videoElement.height(scale * vid_h_orig);
+
+        //console.log(videoElement[0].canPlayType());
+        // and center it by scrolling the video viewport
+        jQuery('#video-viewport').scrollLeft((videoElement.width() - jQuery(window).width()) / 2);
+        jQuery('#video-viewport').scrollTop((videoElement.height() - jQuery(window).height()) / 2);
+
+        var extra = 0;
+        if (parseInt($('#content').css('marginTop').replace(/[^-\d\.]/g, ''), 10) === 0) {
+            extra = 100;
+        }
+        $('#spitballFeatures').height(Math.max(containerHeight, jQuery(window).height() - extra));
+        //  jQuery('#video-viewport').after('.container').height(jQuery('#video-viewport').height());
+    };
+})();
