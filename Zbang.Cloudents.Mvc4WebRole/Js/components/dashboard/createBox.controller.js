8e276bb98750e7ec47ad9e4a6e9cecf052e54b9b
nazgaul <ram.y@outlook.com>
2015-11-17 15:09:43 +0200

    Start to work on create class.

--- Zbang.Cloudents.Mvc4WebRole/Js/components/dashboard/createBox.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/dashboard/createBox.controller.js
@@ -1,21 +1,16 @@
 ﻿(function () {
     angular.module('app.dashboard').controller('CreateBoxController', controller);
 
-    controller.$inject = ['dashboardService', '$location'];
-    function controller(dashboardService, $location) {
+    controller.$inject = ['dashboardService', '$location', '$scope'];
+    function controller(dashboardService, $location, $scope) {
         var self = this;
+        self.create = create;
 
 
-        self.createOn = false;
-
-        self.createBoxClick = function () {
-            self.createOn = true;
-        };
-
-
-        self.create = function () {
+        function create() {
+           
             dashboardService.createPrivateBox(self.boxName).then(function (response) {
-                self.createOn = false;
+                $scope.$parent.d.createBoxOn = false;
                 self.boxName = '';
                 $location.url(response.url);
             }, function (response) {
