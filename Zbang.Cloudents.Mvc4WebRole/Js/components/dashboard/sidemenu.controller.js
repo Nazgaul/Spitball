4167c7e22646e69555be2357a52e21a13613a275
nazgaul <ram.y@outlook.com>
2015-12-01 15:02:18 +0200

    box updates done functionallity
    form comment is now by default open

--- Zbang.Cloudents.Mvc4WebRole/Js/components/dashboard/sidemenu.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/dashboard/sidemenu.controller.js
@@ -1,4 +1,4 @@
-﻿(function() {
+﻿(function () {
     angular.module('app.dashboard').controller('SideMenu', dashboard);
     dashboard.$inject = ['dashboardService', 'userDetailsFactory', '$rootScope'];
 
@@ -11,7 +11,7 @@
         $rootScope.$on('universityChange', function () {
             getBoxes();
         });
-        userDetails.init().then(function() {
+        userDetails.init().then(function () {
             d.userUrl = userDetails.get().url;
 
             if (userDetails.get().university.id) {
@@ -21,13 +21,38 @@
 
         function getBoxes() {
             dashboardService.getBoxes(0).then(function (response2) {
-                d.courses = $.grep(response2, function (b) {
-                    return b.boxType === 'academic';
-                });
-                d.privateBoxes = $.grep(response2, function (b) {
-                    return b.boxType === 'box';
-                });
+                for (var i = 0; i < response2.length; i++) {
+                    var b = response2[i];
+                    if (b.boxType === 'academic') {
+                        d.courses.push(b);
+                    } else {
+                        d.privateBoxes.push(b);
+                    }
+                }
+                //d.courses = $.grep(response2, function (b) {
+                //    return b.boxType === 'academic';
+                //});
+                //d.privateBoxes = $.grep(response2, function (b) {
+                //    return b.boxType === 'box';
+                //});
             });
         }
+
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            if (fromState.parent === 'box') {
+                if (fromParams.boxtype === 'box') {
+                    var box = d.privateBoxes.filter(function(i) {
+                        return i.id == fromParams.boxId;
+                    }) || {};
+                    box[0].updates = 0;
+                } else {
+                    var abox = d.courses.filter(function (i) {
+                        return i.id == fromParams.boxId;
+                    }) || {};
+                    abox[0].updates = 0;
+                }
+
+            }
+        });
     }
 })();
