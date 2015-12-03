c5f976a30a874411c3012ea10a9ca0f591bab676
nazgaul <ram.y@outlook.com>
2015-12-03 15:05:24 +0200

    Remove notification

---
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/user/userNotification.controller.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    angular.module('app.user.details').controller('UserNotificationController', notification);
+
+    notification.$inject = ['userDetailsFactory', 'userService'];
+
+    function notification(userDetailsFactory, userService) {
+        var un = this;
+
+        userDetailsFactory.init().then(function () {
+            if (!userDetailsFactory.isAuthenticated()) {
+                return;
+            }
+            userService.getNotification(function(response) {
+                console.log(response);
+            });
+        });
+    }
+})()
