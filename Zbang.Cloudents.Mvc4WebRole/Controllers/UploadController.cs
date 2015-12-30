4c4f5da1f2874ccbf503b64f020c2751b92b0f1f
nazgaul <ram.y@outlook.com>
2015-12-30 14:24:26 +0200

    bug 4904

--- Zbang.Cloudents.Mvc4WebRole/Controllers/UploadController.cs
+++ Zbang.Cloudents.Mvc4WebRole/Controllers/UploadController.cs
@@ -42,7 +42,6 @@ namespace Zbang.Cloudents.Mvc4WebRole.Controllers
         [RemoveBoxCookie]
         public async Task<ActionResult> File(UploadFile model)
         {
-            await Task.Delay(TimeSpan.FromSeconds(10));
             var userId = User.GetUserId();
             try
             {
