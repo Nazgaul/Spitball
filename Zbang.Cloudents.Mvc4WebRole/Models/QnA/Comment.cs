72377ba2c13d4fd09ed9adb6ba388e21582d3a9a
nazgaul <ram.y@outlook.com>
2016-02-08 15:44:58 +0200

    bug 5256
    remove allow html tag from comment/reply

--- Zbang.Cloudents.Mvc4WebRole/Models/QnA/Comment.cs
+++ Zbang.Cloudents.Mvc4WebRole/Models/QnA/Comment.cs
@@ -5,7 +5,6 @@ namespace Zbang.Cloudents.Mvc4WebRole.Models.QnA
 {
     public class Comment
     {
-        [AllowHtml]
         public string Content { get; set; }
         [Range(1,long.MaxValue)]
         public long BoxId { get; set; }
--- Zbang.Cloudents.Mvc4WebRole/Models/QnA/Reply.cs
+++ Zbang.Cloudents.Mvc4WebRole/Models/QnA/Reply.cs
@@ -6,7 +6,7 @@ namespace Zbang.Cloudents.Mvc4WebRole.Models.QnA
 {
     public class Reply
     {
-        [AllowHtml]
+        //[AllowHtml]
         public string Content { get; set; }
         [Range(1, long.MaxValue)]
         public long BoxId { get; set; }
