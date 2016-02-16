67f207cb9bb120ab84594b33eac4596fea52711b
nazgaul <ram.y@outlook.com>
2016-02-16 13:52:34 +0200

    bug 5359

--- Zbang.Zbox.ViewModel/SqlQueries/Sql.cs
+++ Zbang.Zbox.ViewModel/SqlQueries/Sql.cs
@@ -227,6 +227,7 @@ b.MembersCount+b.ItemCount+(DATEDIFF(MINUTE,'20120101 05:00:00:000', b.UpdateTim
 from zbox.Box b
 where b.isdeleted = 0
 and b.boxid not in (select boxid from zbox.UserBoxRel ub where userid = @UserId)
+and b.discriminator = 2
 and b.University = @UniversityId
 order by rank desc";
 
