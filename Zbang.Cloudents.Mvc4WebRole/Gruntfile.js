dbf8772a85cabb4b0926b4f097be6e9f28f1d616
nazgaul <ram.y@outlook.com>
2016-04-07 13:23:27 +0300

    Working with vs 2015 grunt and gulp

---
+++ Zbang.Cloudents.Mvc4WebRole/Gruntfile.js
@@ -0,0 +1,38 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        rtlcss: {
+            myTask: {
+                // task options
+                options: {
+                    // generate source maps
+                    map: { inline: false },
+                    // rtlcss options
+                    opts: {
+                        clean: false
+                    },
+                    // rtlcss plugins
+                    plugins: [],
+                    // save unmodified files
+                    saveUnmodified: true,
+                },
+                expand: true,
+                cwd: 'ltr/',
+                dest: 'rtl/',
+                src: ['**/*.css']
+            }
+        }
+	//	bower: {
+	//		install: {
+	//			options: {
+	//				targetDir: "wwwroot/lib",
+	//				layout: "byComponent",
+	//				cleanTargetDir: false
+	//			}
+	//		}
+	//	}
+	});
+    //grunt.registerTask("default", ["bower:install"]);
+    
+    grunt.loadNpmTasks('grunt-rtlcss');
+    grunt.registerTask('rtlcss', ['rtlcss']);
+};
