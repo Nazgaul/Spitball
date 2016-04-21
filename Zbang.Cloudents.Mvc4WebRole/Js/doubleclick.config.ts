af3ffb992a9d1873ccb3ea0be96c78954ac7d24a
irenad88 <irena@cloudents.com>
2016-04-21 16:20:55 +0300

    Small side ad fix in item page

--- Zbang.Cloudents.Mvc4WebRole/Js/doubleclick.config.ts
+++ Zbang.Cloudents.Mvc4WebRole/Js/doubleclick.config.ts
@@ -8,7 +8,7 @@
             boxMenu = 'div-gpt-ad-1461243129238-1',
             menu = 'div-gpt-ad-1461243129238-2',
             topBox = 'div-gpt-ad-1461243129238-3',
-            itemSide = 'div-gpt-ad-1461243129238-4',
+            itemSide = 'div-gpt-ad-1461244713254-0',
             itemBetween = 'div-gpt-ad-1459950737650-5',
             topItem = 'div-gpt-ad-1461243129238-5',
             searchTop = 'div-gpt-ad-1461243129238-6';
@@ -19,7 +19,7 @@
              .defineSlot('/107474526/Box_250x250_ATF', [[234, 60], [220, 200]], boxMenu)
              .defineSlot('/107474526/Box_728x90_ATF', [[300, 75], [964, 100], [468, 60]], topBox)
             .defineSlot('/107474526/Dash_250x250_ATF', [[234, 60], [220, 200]], menu)
-            .defineSlot('/107474526/Item_160x300_Side', [160, 300], itemSide)
+            .defineSlot('/107474526/Item_160x300_Side', [160, 600], itemSide)
             .defineSlot('/107474526/Item_300x250_UTF', [[728, 90], [234, 60], [300, 600]], itemBetween)
             .defineSlot('/107474526/Item_728x90_ATF', [[300, 75], [468, 60], [970, 300]], topItem)
              .defineSlot('/107474526/search_728x90_ATF', [[300, 75], [964, 100], [468, 60]], searchTop);
@@ -58,7 +58,7 @@
             .addSize([0, 0], [300, 75]);
 
         doubleClickProvider.defineSizeMapping(itemSide)
-            .addSize([0, 0], [160, 300]);
+            .addSize([0, 0], [160, 600]);
 
         doubleClickProvider.setRefreshInterval(60000);
     }
