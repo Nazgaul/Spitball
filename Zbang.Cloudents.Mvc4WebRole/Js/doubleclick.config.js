c370ff2a12556dc5a51e99c4cc4134d825b8fd23
irenad88 <irena@cloudents.com>
2016-04-21 16:00:38 +0300

    Changed ad sizes

--- Zbang.Cloudents.Mvc4WebRole/Js/doubleclick.config.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/doubleclick.config.js
@@ -2,44 +2,43 @@
     angular.module('app').config(config);
     config.$inject = ['DoubleClickProvider'];
     function config(doubleClickProvider) {
-        var topDashboard = 'div-gpt-ad-1459950737650-0', boxMenu = 'div-gpt-ad-1459950737650-1', menu = 'div-gpt-ad-1459950737650-3', topBox = 'div-gpt-ad-1459950737650-2', itemSide = 'div-gpt-ad-1459950737650-4', itemBetween = 'div-gpt-ad-1459950737650-5', topItem = 'div-gpt-ad-1460629845791-0', searchTop = 'div-gpt-ad-1459950737650-7';
-        doubleClickProvider.defineSlot('/107474526/Dash(N)_728x90_ATF', [[300, 75], [728, 90], [468, 60]], topDashboard)
-            .defineSlot('/107474526/Box_250x250_ATF', [[234, 60], [250, 250]], boxMenu)
-            .defineSlot('/107474526/Box_728x90_ATF', [[300, 75], [728, 90], [468, 60]], topBox)
-            .defineSlot('/107474526/Dash_250x250_ATF', [[234, 60], [250, 250]], menu)
-            .defineSlot('/107474526/Item_160x300_Side', [160, 600], itemSide)
+        var topDashboard = 'div-gpt-ad-1461243129238-0', boxMenu = 'div-gpt-ad-1461243129238-1', menu = 'div-gpt-ad-1461243129238-2', topBox = 'div-gpt-ad-1461243129238-3', itemSide = 'div-gpt-ad-1461243129238-4', itemBetween = 'div-gpt-ad-1459950737650-5', topItem = 'div-gpt-ad-1461243129238-5', searchTop = 'div-gpt-ad-1461243129238-6';
+        doubleClickProvider.defineSlot('/107474526/Dash(N)_728x90_ATF', [[300, 75], [964, 100], [468, 60]], topDashboard)
+            .defineSlot('/107474526/Box_250x250_ATF', [[234, 60], [220, 200]], boxMenu)
+            .defineSlot('/107474526/Box_728x90_ATF', [[300, 75], [964, 100], [468, 60]], topBox)
+            .defineSlot('/107474526/Dash_250x250_ATF', [[234, 60], [220, 200]], menu)
+            .defineSlot('/107474526/Item_160x300_Side', [160, 300], itemSide)
             .defineSlot('/107474526/Item_300x250_UTF', [[728, 90], [234, 60], [300, 600]], itemBetween)
             .defineSlot('/107474526/Item_728x90_ATF', [[300, 75], [468, 60], [970, 300]], topItem)
-            .defineSlot('/107474526/search_728x90_ATF', [[300, 75], [728, 90], [468, 60]], searchTop);
+            .defineSlot('/107474526/search_728x90_ATF', [[300, 75], [964, 100], [468, 60]], searchTop);
         doubleClickProvider.defineSizeMapping(topDashboard)
-            .addSize([1050, 768], [728, 90])
+            .addSize([1000, 768], [964, 100])
             .addSize([640, 480], [468, 60])
             .addSize([0, 0], [300, 75]);
         doubleClickProvider.defineSizeMapping(topBox)
-            .addSize([1050, 768], [728, 90])
+            .addSize([1000, 768], [964, 100])
             .addSize([640, 480], [468, 60])
             .addSize([0, 0], [300, 75]);
         doubleClickProvider.defineSizeMapping(menu)
-            .addSize([1050, 850], [250, 250])
+            .addSize([960, 650], [220, 200])
             .addSize([0, 0], [234, 60]);
         doubleClickProvider.defineSizeMapping(boxMenu)
-            .addSize([1050, 850], [250, 250])
+            .addSize([960, 650], [220, 200])
             .addSize([0, 0], [234, 60]);
         doubleClickProvider.defineSizeMapping(searchTop)
-            .addSize([1050, 768], [728, 90])
+            .addSize([1000, 768], [964, 100])
             .addSize([640, 480], [468, 60])
             .addSize([0, 0], [300, 75]);
         doubleClickProvider.defineSizeMapping(topItem)
-            .addSize([1920, 1200], [970, 300])
-            .addSize([1050, 768], [728, 90])
+            .addSize([1000, 768], [964, 100])
             .addSize([640, 480], [468, 60])
             .addSize([0, 0], [300, 75]);
         doubleClickProvider.defineSizeMapping(itemBetween)
-            .addSize([1050, 768], [728, 90])
+            .addSize([1000, 768], [964, 100])
             .addSize([640, 480], [468, 60])
             .addSize([0, 0], [300, 75]);
         doubleClickProvider.defineSizeMapping(itemSide)
-            .addSize([0, 0], [160, 600]);
+            .addSize([0, 0], [160, 300]);
         doubleClickProvider.setRefreshInterval(60000);
     }
 })();
