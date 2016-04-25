0a3a4245c9eb73edc5ee8b9cbd29b337f45fdd8f
irenad88 <irena@cloudents.com>
2016-04-25 13:38:45 +0300

    Fixed quiz corner and colors in box search

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("16.4.24.1")]
-[assembly: AssemblyFileVersion("16.4.24.1")]
+[assembly: AssemblyVersion("16.4.25.3")]
+[assembly: AssemblyFileVersion("16.4.25.3")]
  
