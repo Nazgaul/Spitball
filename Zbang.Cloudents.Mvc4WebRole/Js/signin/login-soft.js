0f0de5b8429805308855b908a2d9cc82c3de8e08
irenad88 <irena@cloudents.com>
2016-05-03 16:48:57 +0300

    few bug fixes

--- Zbang.Cloudents.Mvc4WebRole/Js/signin/login-soft.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/signin/login-soft.js
@@ -57,6 +57,34 @@ var Login = function () {
     var handleForgetPassword = function () {
         var serializedEmail;
         var $forgotPassword = $('#f-forgot-password');
+
+        $('.forgot-password-form').validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+
+            highlight: function (element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
+
+            success: function (label) {
+                label.closest('.form-group').removeClass('has-error');
+                label.remove();
+            },
+
+            errorPlacement: function (error, element) {
+                error.insertAfter(element);
+            }
+        });
+
+
         $forgotPassword.submit(function (ev) {
             ev.preventDefault();
             $forgotPassword.find(':submit').attr('disabled', 'disabled');
