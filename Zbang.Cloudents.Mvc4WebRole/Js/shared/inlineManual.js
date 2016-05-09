feb33ca5f80c168ca61d434df34648e8b1bfbe8c
nazgaul <ram.y@outlook.com>
2016-05-09 11:42:00 +0300

    xxx

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManual.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManual.js
@@ -7,38 +7,33 @@
     function inlineManual($rootScope, $timeout, $document, userDetailsFactory) {
         var registeredUser = false;
         userDetailsFactory.init().then(function (userData) {
-            //if (!userData.id) {
-            //    return;
-            //}
-            //registeredUser = true;
-            //inlineManualTracking = {
-            //    uid: userData.id,
-            //    email: userData.email,
-            //    username: userData.name,
-            //    created: Math.round(userData.createTime.getTime() / 1000)
-            //    //updated: 1433168312,
-            //    //roles: ["administrator", "editor"],
-            //    //group: "Doe & Partners",
-            //    //plan: "Standard"
-            //}
-            //$timeout(injectInlineManual, 0, false);
+            if (!userData.id) {
+                return;
+            }
+            registeredUser = true;
+            inlineManualTracking = {
+                uid: userData.id,
+                email: userData.email,
+                username: userData.name,
+                created: Math.round(userData.createTime.getTime() / 1000)
+            };
+            $timeout(injectInlineManual, 0, false);
         });
-        //$rootScope.$on("$stateChangeSuccess", () => {
-        //    // Inline manual fix for angular
-        //    var element = $document.find('[ui-view][animation-class]');
-        //    $timeout(() => {
-        //        if (registeredUser && angular.isDefined(inline_manual_player)) {
-        //            inline_manual_player.manualReinit(element[0]);
-        //            inline_manual_player.setMetadata(inlineManualTracking);
-        //        }
-        //    }, 1000);
-        //});
-        //function injectInlineManual() {
-        //    var e = document.createElement("script"),
-        //        t = document.getElementsByTagName("script")[0];
-        //    e.async = true, e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
-        //        e.charset = "UTF-8", t.parentNode.insertBefore(e, t);
-        //}
+        $rootScope.$on("$stateChangeSuccess", function () {
+            // Inline manual fix for angular
+            //var element = $document.find('[ui-view][animation-class]');
+            $timeout(function () {
+                if (registeredUser && angular.isDefined(inline_manual_player)) {
+                    inline_manual_player.manualReinit();
+                    inline_manual_player.setMetadata(inlineManualTracking);
+                }
+            }, 1000);
+        });
+        function injectInlineManual() {
+            var e = document.createElement("script"), t = document.getElementsByTagName("script")[0];
+            e.async = true, e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
+                e.charset = "UTF-8", t.parentNode.insertBefore(e, t);
+        }
     }
 })();
 //# sourceMappingURL=inlineManual.js.map
