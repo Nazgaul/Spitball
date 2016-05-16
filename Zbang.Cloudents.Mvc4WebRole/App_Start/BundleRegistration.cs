635e53ef7c0fef8256e00b88e7ec39187c9a699a
irenad88 <irena@cloudents.com>
2016-05-16 14:43:14 +0300

    tried to fix inline manual

--- Zbang.Cloudents.Mvc4WebRole/App_Start/BundleRegistration.cs
+++ Zbang.Cloudents.Mvc4WebRole/App_Start/BundleRegistration.cs
@@ -333,7 +333,7 @@ namespace Zbang.Cloudents.Mvc4WebRole
                         new JsFileWithCdn("~/js/shared/resourceManager.js"),
                         new JsFileWithCdn("~/js/shared/versionChecker.js"),
                         new JsFileWithCdn("~/js/shared/intercom.js"),
-                        //new JsFileWithCdn("~/js/shared/inlineManual.js"),
+                        new JsFileWithCdn("~/js/shared/inlineManual.js"),
                         new JsFileWithCdn("~/js/shared/scrollToTop.directive.js"),
 
 
