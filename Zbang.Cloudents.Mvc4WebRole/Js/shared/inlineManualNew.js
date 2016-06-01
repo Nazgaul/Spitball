2977ddcc4d5adcedf68220d19a7c9a1be682d2cb
irenad88 <irena@cloudents.com>
2016-06-01 08:45:13 +0300

    Few bug fixes

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManualNew.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManualNew.js
@@ -1,13 +1,29 @@
-﻿!function () {
-    var e = document.createElement("script"),
-        t = document.getElementsByTagName("script")[0];
-    e.async = 1,
-    e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
-    e.charset = "UTF-8", t.parentNode.insertBefore(e, t)
-}();
+﻿setTimeout(function () {
+    !function () {
+        var e = document.createElement("script"),
+            t = document.getElementsByTagName("script")[0];
+        e.async = 1,
+        e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
+        e.charset = "UTF-8", t.parentNode.insertBefore(e, t)
+    }();
 
-inlineManualTracking = {
-    uid: "949532",
-    email: "inlinemanual@inlinemanual.com",
-    username: "inlinemanual inlinemanual"
-}
+    inlineManualTracking = {
+        uid: window.id
+    }
+}, 1000);
+
+
+//'use strict';
+//(function () {
+//    angular.module('app').run(inlineManual);
+//    inlineManual.$inject = ['$rootScope', '$timeout'];
+//    function inlineManual($rootScope, $timeout) {
+//        inlineManualTracking = {
+//            username: "inlinemanual inlinemanual"
+//        }
+
+//        if (angular.isDefined(inline_manual_player)) {
+//            inline_manual_player.manualReinit();
+//        }
+//    }
+//})();
--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.5.31.2")]
-[assembly: AssemblyFileVersion("6.5.31.2")]
+[assembly: AssemblyVersion("16.6.1.0")]
+[assembly: AssemblyFileVersion("16.6.1.0")]
  
