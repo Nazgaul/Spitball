a0e0e72013375348431b2b92202c03972262f4a1
irenad88 <irena@cloudents.com>
2016-06-01 17:09:09 +0300

    Few bug fixes

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManualNew.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManualNew.js
@@ -1,29 +1,33 @@
-﻿setTimeout(function () {
-    !function () {
-        var e = document.createElement("script"),
-            t = document.getElementsByTagName("script")[0];
-        e.async = 1,
-        e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
-        e.charset = "UTF-8", t.parentNode.insertBefore(e, t)
-    }();
+﻿//setTimeout(function () {
+//    !function () {
+//        var e = document.createElement("script"),
+//            t = document.getElementsByTagName("script")[0];
+//        e.async = 1,
+//        e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
+//        e.charset = "UTF-8", t.parentNode.insertBefore(e, t)
+//    }();
 
-    inlineManualTracking = {
-        uid: window.id
-    }
-}, 1000);
+//    inlineManualTracking = {
+//        //uid: window.id,
+//        username: "inlinemanual inlinemanual"
+//    }
+//    console.log('here');
+
+//}, 1000);
 
 
 //'use strict';
 //(function () {
 //    angular.module('app').run(inlineManual);
-//    inlineManual.$inject = ['$rootScope', '$timeout'];
-//    function inlineManual($rootScope, $timeout) {
-//        inlineManualTracking = {
-//            username: "inlinemanual inlinemanual"
-//        }
-
-//        if (angular.isDefined(inline_manual_player)) {
-//            inline_manual_player.manualReinit();
-//        }
+//    inlineManual.$inject = ['$rootScope'];
+//    function inlineManual($rootScope) {
+//        $rootScope.$on("$stateChangeSuccess", function () {
+//            if (angular.isDefined(window.inline_manual_player)) {
+//                setTimeout(function () {
+//                    console.log('reiniting');
+//                    inline_manual_player.manualReinit();
+//                }, 1500);
+//            }
+//        });
 //    }
 //})();
