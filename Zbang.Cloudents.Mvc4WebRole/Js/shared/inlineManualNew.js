40a9c5c6879965151ebf25b3a1c10293b6d18b3c
irenad88 <irena@cloudents.com>
2016-06-05 16:05:10 +0300

    Fixed inlineManual issues - now works.

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManualNew.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/inlineManualNew.js
@@ -1,38 +1,38 @@
-﻿//setTimeout(function () {
-//    !function () {
-//        var e = document.createElement("script"),
-//            t = document.getElementsByTagName("script")[0];
-//        e.async = 1,
-//        e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.js",
-//        e.charset = "UTF-8", t.parentNode.insertBefore(e, t)
-//    }();
+﻿setTimeout(function () {
+    inlineManualTracking = {
+        uid: window.id,
+        username: "inlinemanual inlinemanual"
+    };
+    inlineManualOptions = {
+        language: $('html').attr('lang')
+    };
+    !function () {
+        var e = document.createElement("script"),
+            t = document.getElementsByTagName("script")[0];
+        e.async = 1,
+        e.src = "https://inlinemanual.com/embed/player.48877e35a515f4d5093914d5e9e51176.bare.js",
+        e.charset = "UTF-8", t.parentNode.insertBefore(e, t),
+        e.onload = function () {
+            createInlineManualPlayer(inlineManualPlayerData);
 
-//    inlineManualTracking = {
-//        uid: window.id,
-//        username: "inlinemanual inlinemanual"
-//    }
-//}, 1000);
+            inline_manual_player.setCallbacks({
+                onProfileLoad: function (topic_id, step_id) {
+                    inline_manual_player.setMetadata(inlineManualTracking);
+                }
+            });
+        }
+    }();
 
-//(function () {
-//    angular.module('app').run(inlineManual);
-//    inlineManual.$inject = ['$rootScope','$timeout'];
-//    function inlineManual($rootScope, $timeout) {
-//        $rootScope.$on("$stateChangeSuccess", function (event, toState, toParams, fromState, fromParams) {
-//            if (angular.isDefined(window.inline_manual_player)) {
-//                $timeout(function () {
-//                    console.log('reiniting');
-//                    inline_manual_player.getProfile();
-//                    inline_manual_player.setCallbacks({
-//                        onProfileLoad: function (player, topic_id, step_id) {
-//                            console.log('onProfileLoad');
-//                            inlineManualTracking = {
-//                                uid: window.id,
-//                                username: "inlinemanual inlinemanual"
-//                            }
-//                        }
-//                    });
-//                }, 1500);
-//            }
-//        });
-//    }
-//})();
+}, 1000);
+
+(function () {
+    angular.module('app').run(inlineManual);
+    inlineManual.$inject = ['$rootScope', '$timeout'];
+    function inlineManual($rootScope, $timeout) {
+        $rootScope.$on("$stateChangeSuccess", function (event, toState, toParams, fromState, fromParams) {
+            if (angular.isDefined(window.inline_manual_player)) {
+                inline_manual_player.getProfile();
+            }
+        });
+    }
+})();
