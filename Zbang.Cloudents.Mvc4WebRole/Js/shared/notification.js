9950c50b20e622fd1050b1bb989576f828a0b035
nazgaul <ram.y@outlook.com>
2016-06-08 11:48:50 +0300

    Added automatic reconnect.

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/notification.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/notification.js
@@ -5,16 +5,19 @@
     desktopNotification.$inject = ['$document', '$mdToast', 'resManager'];
 
     function desktopNotification($document, $mdToast, resManager) {
-        if (!('Notification' in window)) {
-            return;
-        }
-        Notification.requestPermission(function (permission) {
-            //var notification = new Notification("Title", { body: 'HTML5 Web Notification API', icon: 'http://i.stack.imgur.com/Jzjhz.png?s=48&g=1', dir: 'auto' });
-            //setTimeout(function () {
-            //    notification.close();
-            //}, 3000);
-        });
 
+        requestPermission();
+        function requestPermission() {
+            if (!('Notification' in window)) {
+                return;
+            }
+            Notification.requestPermission(function (permission) {
+                //var notification = new Notification("Title", { body: 'HTML5 Web Notification API', icon: 'http://i.stack.imgur.com/Jzjhz.png?s=48&g=1', dir: 'auto' });
+                //setTimeout(function () {
+                //    notification.close();
+                //}, 3000);
+            });
+        }
         this.send = function(title, message, image)
         {
             if (!('Notification' in window)) {
