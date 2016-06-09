2028059d83b21f9481727a7b4b59dfdb364ca44a
nazgaul <ram.y@outlook.com>
2016-06-09 13:43:06 +0300

    Some refactoring

--- Zbang.Zbox.Domain.Common/TextManipulation.cs
+++ Zbang.Zbox.Domain.Common/TextManipulation.cs
@@ -95,15 +95,7 @@ namespace Zbang.Zbox.Domain.Common
             return sb.ToString();
         }
 
-        public static string RemoveExtraSpaces(string str)
-        {
-            if (string.IsNullOrEmpty(str))
-            {
-                return str;
-            }
-            var spaceReg = new Regex(@"\s+", RegexOptions.Compiled);
-            return spaceReg.Replace(str, " ");
-        }
+        
     }
 
 }
