b0a9be2bdc81d68e9a7f3265835bde02e0f60cb9
irenad88 <irena@cloudents.com>
2016-06-09 16:00:43 +0300

    Fixed keyboard action directive

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/keyboardAction.directive.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/keyboardAction.directive.js
@@ -5,26 +5,14 @@
     angular.module('app').directive('keyboardAction', keyboardAction);
 
     function keyboardAction() {
-        document.onkeydown = function (e) {
-            if (e.keyCode == e.target.attributes.keyboardAction) {
-                element.click();
-                console.log(e.which);
-            }
-        };
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
-                //$(document).bind('onkeydown', function () {
-                //    console.log('xxx')
-                //})
-                
-                
-                //document.onkeydown = function (e) {
-                //    if (e.keyCode == attrs.keyboardAction) {
-                //        element.click();
-                //        console.log(e.which);
-                //    }
-                //};
+                document.addEventListener('keydown', function (e) {
+                    if (e.keyCode == attrs.keyboardAction) {
+                        element.click();
+                    }
+                });
             }
         };
     }
