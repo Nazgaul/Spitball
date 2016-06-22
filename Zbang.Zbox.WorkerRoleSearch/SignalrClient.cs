4bf875477d1c3996ab35b38029e5af6f53a915be
nazgaul <ram.y@outlook.com>
2016-06-22 15:46:40 +0300

    Update time to unsubscribe list
    more logs on signalr faliure

--- Zbang.Zbox.WorkerRoleSearch/SignalrClient.cs
+++ Zbang.Zbox.WorkerRoleSearch/SignalrClient.cs
@@ -8,20 +8,31 @@ namespace Zbang.Zbox.WorkerRoleSearch
     public static class SignalrClient
     {
         private static IHubProxy Proxy { get; set; }
+        private static HubConnection _hub;
+
         public static async Task<IHubProxy> GetProxyAsync()
         {
             if (Proxy == null)
             {
-                var hubConnection = new HubConnection(ConfigFetcher.Fetch("signalR") + "/s");
-                Proxy = hubConnection.CreateHubProxy("SpitballHub");
-                await hubConnection.Start();
-                hubConnection.Closed += () =>
-                {
-                    TraceLog.WriteInfo("need to reconnect");
-                    hubConnection.Start();
-                };
+                await InitConnectionAsync();
+            }
+            if (_hub.State == ConnectionState.Disconnected)
+            {
+                await InitConnectionAsync();
             }
             return Proxy;
+        }
+
+        private static async Task InitConnectionAsync()
+        {
+            _hub = new HubConnection(ConfigFetcher.Fetch("signalR") + "/s");
+            Proxy = _hub.CreateHubProxy("SpitballHub");
+            await _hub.Start();
+            _hub.Closed += () =>
+            {
+                TraceLog.WriteInfo("need to reconnect");
+                _hub.Start();
+            };
         }
     }
 }
