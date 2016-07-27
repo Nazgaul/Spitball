59a7e769c5ad5320d8545c85c9a54f834955c8f0
nazgaul <ram.y@outlook.com>
2016-07-27 16:10:44 +0300

    version 6.7.27.1

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.7.27.0")]
-[assembly: AssemblyFileVersion("6.7.27.0")]
+[assembly: AssemblyVersion("6.7.27.1")]
+[assembly: AssemblyFileVersion("6.7.27.1")]
 
  
