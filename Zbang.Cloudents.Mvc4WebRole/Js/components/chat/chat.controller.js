a631390cb6d092acd4861b1d77e3e61658d2a3e5
nazgaul <ram.y@outlook.com>
2016-09-14 11:37:15 +0300

    Fix in chat.

--- Zbang.Cloudents.Mvc4WebRole/Js/components/chat/chat.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/chat/chat.controller.js
@@ -247,6 +247,7 @@
                 }
 
                 scrollToBotton();
+                updateUnread();
                 updateScope();
                 return;
             }
--- Zbang.Cloudents.Mvc4WebRole/Js/components/chat/chat.factory.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/chat/chat.factory.js
@@ -3,7 +3,6 @@ var app;
     "use strict";
     var unreadCount = 0;
     var ChatBus = (function () {
-        //static $inject = ["ajaxService2"];
         function ChatBus(ajaxService, userDetailsFactory) {
             var _this = this;
             this.ajaxService = ajaxService;
@@ -53,4 +52,3 @@ var app;
         .module("app.chat")
         .factory("chatBus", ChatBus.factory());
 })(app || (app = {}));
-//# sourceMappingURL=chat.factory.js.map
