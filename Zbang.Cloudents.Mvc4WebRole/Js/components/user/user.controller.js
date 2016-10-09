694c0b7d733ba533d9ad716be7dc7203640f3646
irenad88 <irena@cloudents.com>
2016-10-09 14:43:16 +0300

    more gamification changes

--- Zbang.Cloudents.Mvc4WebRole/Js/components/user/user.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/user/user.controller.js
@@ -58,11 +58,11 @@
         self.changeBadgesTab = function (tab) {
             self.badgesTab = tab;
             switch (self.badgesTab) {
-                case self.badgesState.levels:                    
+                case self.badgesState.levels:
                     break;
-                case self.badgesState.badges:                    
+                case self.badgesState.badges:
                     break;
-                case self.badgesState.community:                  
+                case self.badgesState.community:
                     break
             }
         }
@@ -83,8 +83,122 @@
         self.changeTab(self.tab);
         self.deleteItem = deleteItem;
         self.sendMessage = sendMessage;
-
-
+        self.showBadge = showBadge;
+        self.badgeInfo = null;
+        self.badges = [
+            {
+                name: 'Spitballer',
+                image: '',
+                progress: 100,
+                condition: 'Register',
+                points: 500
+            },
+            {
+                name: 'Explorer',
+                image: '',
+                progress: 100,
+                condition: 'Follow 3 Classes',
+                points: 500
+            },
+            {
+                name: 'Harambe',
+                image: '',
+                progress: 80,
+                condition: 'The ways to get this badge are secret. Seek and you shall find',
+                points: 500
+            },
+            {
+                name: 'Influencer',
+                image: '',
+                progress: 80,
+                condition: 'Spread the word. Share a class or post on Facebook',
+                points: 500
+            },
+            {
+                name: 'Explorer',
+                image: '',
+                progress: 80,
+                condition: 'Create a new class or department',
+                points: 500
+            },
+            {
+                name: 'Quizzy Lizzy',
+                image: '',
+                progress: 80,
+                condition: 'Create a quiz',
+                points: 500
+            },
+            {
+                name: 'Helpful Harry',
+                image: '',
+                progress: 80,
+                condition: 'You have one job: upload 3 documents to your class',
+                points: 500
+            },
+            {
+                name: 'Kiss from Keanu',
+                image: '',
+                progress: 80,
+                condition: 'Like three posts or docs that you really think are of great value to unlock this badge',
+                points: 500
+            },
+            {
+                name: 'Narrator',
+                image: '',
+                progress: 80,
+                condition: 'Your voice echoes through! Comment 3 times to get this badge',
+                points: 500
+            },
+            {
+                name: 'Quite Likely',
+                image: '',
+                progress: 80,
+                condition: 'Like 10 items on our site to get this badge',
+                points: 500
+            },
+            {
+                name: 'Sharing is Caring',
+                image: '',
+                progress: 80,
+                condition: 'Share a post or item 5 times. Stellar!',
+                points: 500
+            },
+            {
+                name: 'Early Bird',
+                image: '',
+                progress: 80,
+                condition: 'Create a new university to get this badge',
+                points: 500
+            },
+            {
+                name: 'Quiz Master',
+                image: '',
+                progress: 80,
+                condition: 'Score a 100% on a quiz',
+                points: 500
+            },
+            {
+                name: 'Secret Sally',
+                image: '',
+                progress: 80,
+                condition: 'Create a private study group',
+                points: 500
+            },
+            {
+                name: 'Chatterbox',
+                image: '',
+                progress: 80,
+                condition: 'Use the chat feature to communicate with a classmate',
+                points: 500
+            },
+            {
+                name: 'Private I',
+                image: '',
+                progress: 80,
+                condition: 'Join or create a private department',
+                points: 500
+            }
+        ];
 
         function sendMessage() {
             if ($mdMedia('gt-xs')) {
@@ -240,5 +354,16 @@
         function returnEmptyPromise() {
             return $q.when();
         }
+
+        function showBadge(badge) {
+            var badgeIndex = self.badges.indexOf(badge);
+            self.badgeInfo = {
+                data: badge,
+                next: self.badges[badgeIndex + 1],
+                prev: self.badges[badgeIndex - 1]
+            };
+
+        }
+
     }
 })();
