87c48f780a28e1eef99769f0f32e71ae09fdf51a
nazgaul <ram.y@outlook.com>
2016-10-20 14:57:53 +0300

    Some changes.

--- Zbang.Cloudents.Mvc4WebRole/Js/components/library/classChoose.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/library/classChoose.controller.js
@@ -2,10 +2,25 @@ var app;
 (function (app) {
     "use strict";
     var ClassChoose = (function () {
-        function ClassChoose(searchService) {
+        function ClassChoose(searchService, $scope) {
             this.searchService = searchService;
+            this.$scope = $scope;
+            this.stepTitle = 1;
         }
-        ClassChoose.$inject = ["searchService"];
+        ClassChoose.prototype.classSearch = function () {
+            var formElement = this.$scope["classChooseFrom"];
+            if (formElement.$valid) {
+                console.log('ss');
+            }
+            console.log('her');
+            if (this.term) {
+                this.stepTitle = 2;
+            }
+            else {
+                this.stepTitle = 1;
+            }
+        };
+        ClassChoose.$inject = ["searchService", "$scope"];
         return ClassChoose;
     }());
     angular.module("app.library").controller("ClassChoose", ClassChoose);
--- Zbang.Cloudents.Mvc4WebRole/Js/components/library/classChoose.controller.ts
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/library/classChoose.controller.ts
@@ -2,10 +2,24 @@
     "use strict";
 
     class ClassChoose {
-        static $inject = ["searchService"];
+        static $inject = ["searchService", "$scope"];
+        stepTitle = 1;
+        term;
+        constructor(private searchService: ISearchService, private $scope: angular.IScope) {
 
-        constructor(private searchService: ISearchService) {
-            
+        }
+        classSearch() {
+            const formElement: angular.IFormController = this.$scope["classChooseFrom"];
+
+            if (formElement.$valid) {
+                console.log('ss');
+            }
+            console.log('her')
+            if (this.term) {
+                this.stepTitle = 2;
+            } else {
+                this.stepTitle = 1;
+            }
         }
     }
 
