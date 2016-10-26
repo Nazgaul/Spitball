5075fc3409425ebd8448015b9a71060de9679c97
irenad88 <irena@cloudents.com>
2016-10-26 11:31:32 +0300

    moved intercom to typescript + fixed ng-style that didn't work on uni box

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/intercom.ts
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/intercom.ts
@@ -1,5 +1,72 @@
 ﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="../shared/userDetails.ts" />
+
+
+declare var Intercom: any;
+module app {
+    "use strict";
+    var started: boolean = false;
+    export interface IIntercom {
+        start(data: IUserData): void;
+        stop(): void;
+    }
+
+    class IntercomComponent implements IIntercom {
+        static $inject = ['userDetailsFactory', '$rootScope', '$mdMenu'];
+
+        constructor(private userDetailsFactory: app.IUserDetailsFactory,
+            private $rootScope: ng.IRootScopeService,
+            private $mdMenu: any) {
+            userDetailsFactory.init().then((data: IUserData) => {
+                this.start(data);
+            });
+        }
+        start(data: IUserData) {
+            if (started) {
+                return;
+            }
+            started = true;
+            // var dateCreate = new Date(data.createTime);
+            if (data.id) {
+                Intercom('boot', {
+                    app_id: "njmpgayv",
+                    name: data.name,
+                    email: data.email,
+                    created_at: Math.round(data.createTime.getTime() / 1000),
+                    user_id: data.id,
+                    user_image: data.image,
+                    university_id: data.university.id,
+                    university_name: data.university.name,
+                    reputation: data.score,
+                    language: data.culture,
+                    university_country: data.university.country,
+                    widget: {
+                        activator: '#Intercom'
+                    }
+                });
+                Intercom('onActivatorClick', () => {
+                    this.$mdMenu.hide(); //closes menu
+                });
+            }
+        }
+        stop() {
+            started = false;
+            Intercom('shutdown');
+        }
+
+        static factory() {
+            const factory = (userDetailsFactory, $rootScope, $mdMenu) => {
+                return new IntercomComponent(userDetailsFactory, $rootScope, $mdMenu);
+            };
+
+            factory["$inject"] = ['userDetailsFactory', '$rootScope', '$mdMenu'];
+            return factory;
+        }
+
+    }
+    angular.module("app").run(IntercomComponent.factory());
+}
+/*
 declare var Intercom: any;
 
 (() => {
@@ -49,6 +116,6 @@ declare var Intercom: any;
 
     }
 })();
-
+*/
 
 
