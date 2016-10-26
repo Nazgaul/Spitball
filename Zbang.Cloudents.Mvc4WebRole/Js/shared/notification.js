9fe14bc37fd4d80d402997bf3d435fd934314b1f
irenad88 <irena@cloudents.com>
2016-10-26 12:23:20 +0300

    moved notification service to typescript

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/notification.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/notification.js
@@ -1,24 +1,23 @@
-﻿(function () {
-    'use strict';
-    angular.module('app').service('notificationService', desktopNotification);
-    desktopNotification.$inject = ['$document', '$mdToast', 'resManager'];
-
-    function desktopNotification($document, $mdToast, resManager) {
-        requestPermission();
-        function requestPermission() {
+var app;
+(function (app) {
+    "use strict";
+    var DesktopNotification = (function () {
+        function DesktopNotification($document, $mdToast, resManager) {
+            this.$document = $document;
+            this.$mdToast = $mdToast;
+            this.resManager = resManager;
+            this.requestPermission();
+        }
+        DesktopNotification.prototype.requestPermission = function () {
             if (!('Notification' in window)) {
                 return;
             }
             Notification.requestPermission(function () {
-                //var notification = new Notification("Title", { body: 'HTML5 Web Notification API', icon: 'http://i.stack.imgur.com/Jzjhz.png?s=48&g=1', dir: 'auto' });
-                //setTimeout(function () {
-                //    notification.close();
-                //}, 3000);
             });
-        }
-        this.send = function (title, message, image, callback) {
+        };
+        DesktopNotification.prototype.send = function (title, message, image, callback) {
             if (!('Notification' in window)) {
-                showToaster();
+                this.showToaster();
                 return;
             }
             image = image || '/Images/3rdParty/logo_120_4_google.png';
@@ -33,22 +32,23 @@
                     }
                     notification.close();
                 };
-            } else {
-                showToaster();
             }
-        }
-
-        function showToaster() {
-            var element = $document.find('header')[0];
-
-            $mdToast.show(
-                 $mdToast.simple()
-                 .textContent(resManager.get('toasterChatMessage'))
-                 .capsule(true)
-                 .position('top right')
-                 .parent(element)
-                 .hideDelay(2000));
-        }
-    }
-
-})()
+            else {
+                this.showToaster();
+            }
+        };
+        DesktopNotification.prototype.showToaster = function () {
+            var element = this.$document.find('header')[0];
+            console.log('showing toaster');
+            this.$mdToast.show(this.$mdToast.simple()
+                .textContent(this.resManager.get('toasterChatMessage'))
+                .capsule(true)
+                .position('top right')
+                .parent(element)
+                .hideDelay(2000));
+        };
+        DesktopNotification.$inject = ['$document', '$mdToast', 'resManager'];
+        return DesktopNotification;
+    }());
+    angular.module("app").service("notificationService", DesktopNotification);
+})(app || (app = {}));
