7b9bea6c5c92564ce55747757b0457730de33210
nazgaul <ram.y@outlook.com>
2016-11-03 11:26:16 +0200

    Better performance add class.

--- Zbang.Cloudents.Mvc4WebRole/Js/shared/colorOnLength.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/shared/colorOnLength.js
@@ -5,7 +5,6 @@
             return {
                 restrict: 'A',
                 link: function (scope, element, attrs) {
-                    
                     var x = parseInt(attrs.dColor, 10);
                     if (!isNaN(x)){
                         element.addClass('color' + x % 10);
@@ -28,58 +27,15 @@
                             num += temp;
                         }
                     }
-                    element.addClass('color' + num % 10);
+                    element[0].className += (' color' + num % 10);
+                    //element.addClass('color' + num % 10);
                     if (attrs.colorOn) {
                         element.find(attrs.colorOn).addClass('color' + num % 10);
                     }
-                    //var x = parseInt(attrs.dColor, 10);
-                    //if (!isNaN(x)) {
-                    //    element.addClass('color' + x % 10);
-                    //} else {
-                    //    element.addClass('color' + attrs.dColor.length % 10);
-                    //}
                 }
             };
         }
     );
-
-    //angular.module('app').directive('colorScope',
-    //    function () {
-    //        return {
-    //            scope: {
-    //                colorScope: '='
-    //            },
-    //            restrict: 'A',
-    //            link: function (scope, element) {
-    //                scope.$watch("colorScope",
-    //                    function (newVal, oldVal) {
-    //                        if (newVal === oldVal) {
-    //                            return;
-    //                        }
-    //                        if (oldVal) {
-    //                            assignColor(newVal, oldVal);
-    //                        }
-    //                    });
-    //                assignColor(scope.dColorScope);
-    //                function assignColor(num, oldVal) {
-    //                    element.removeClass("color" + calculatenum(oldVal) % 10).addClass('color' + calculatenum(num) % 10);
-    //                }
-    //                function calculatenum(guid) {
-    //                    var num = 0;
-    //                    for (var i = 0; i < guid.length; i++) {
-    //                        var temp = parseInt(guid[i], 16);
-    //                        if (!isNaN(temp)) {
-    //                            num += temp;
-    //                        }
-    //                    }
-    //                    return num;
-
-    //                }
-    //            }
-                
-    //        };
-    //    }
-    //);
 })();
 
 (function() {
--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.11.2.*")]
-[assembly: AssemblyFileVersion("6.11.2.4")]
+[assembly: AssemblyVersion("6.11.3.*")]
+[assembly: AssemblyFileVersion("6.11.3.0")]
 
