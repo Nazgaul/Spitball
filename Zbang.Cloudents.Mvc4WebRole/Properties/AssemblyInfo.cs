d34395e9cb53e351e3d3922af5df4f5e0a4dec86
irenad88 <irena@cloudents.com>
2016-11-20 15:55:54 +0200

    fixed buttons display  some design fixes

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("6.11.20.*")]
-[assembly: AssemblyFileVersion("6.11.20.2")]
+[assembly: AssemblyFileVersion("6.11.20.3")]
 
