c975899456f8a23368f3314e96b4127a84900ca7
irenad88 <irena@cloudents.com>
2016-11-22 11:50:56 +0200

    version + small resource fix

--- Zbang.Cloudents.Mvc4WebRole/Views/Flashcard/Resources/FlashcardResources.Designer.cs
+++ Zbang.Cloudents.Mvc4WebRole/Views/Flashcard/Resources/FlashcardResources.Designer.cs
@@ -205,7 +205,7 @@ namespace Zbang.Cloudents.Mvc4WebRole.Views.Flashcard.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enjoy this set? give it a.
+        ///   Looks up a localized string similar to Enjoyed this set? give it a.
         /// </summary>
         public static string LikeLabel {
             get {
