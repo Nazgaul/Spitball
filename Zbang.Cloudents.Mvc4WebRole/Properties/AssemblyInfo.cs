98280b0960fe27707ddeb3b0f8070f889b074d46
irenad88 <irena@cloudents.com>
2016-11-23 12:09:50 +0200

    Removed like from last screen when its user's flashcard

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("6.11.22.*")]
-[assembly: AssemblyFileVersion("6.11.22.10")]
+[assembly: AssemblyFileVersion("6.11.23.0")]
