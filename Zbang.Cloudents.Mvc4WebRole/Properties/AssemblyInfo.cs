6d064f96ee60d5eeb064fa2c21b767c4c9ce32ba
irenad88 <irena@cloudents.com>
2016-11-28 15:36:44 +0200

    version

--- Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
+++ Zbang.Cloudents.Mvc4WebRole/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("6.11.28.*")]
-[assembly: AssemblyFileVersion("6.11.28.3")] 
+[assembly: AssemblyFileVersion("6.11.28.4")] 
