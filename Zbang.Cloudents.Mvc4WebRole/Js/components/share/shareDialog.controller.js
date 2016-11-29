1a7c1dd2a33c989ea4320ada787351a5d17f31e4
nazgaul <ram.y@outlook.com>
2016-11-29 13:40:23 +0200

    xxx

--- Zbang.Cloudents.Mvc4WebRole/Js/components/share/shareDialog.controller.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/share/shareDialog.controller.js
@@ -2,11 +2,13 @@ var app;
 (function (app) {
     "use strict";
     var ShareDialog = (function () {
-        function ShareDialog($mdDialog, data, $window) {
+        function ShareDialog($mdDialog, data, $window, analytics) {
             this.$mdDialog = $mdDialog;
             this.data = data;
             this.$window = $window;
+            this.analytics = analytics;
             this.url = location.origin + "/" + data.what + "/" + encodeBase64(data.id);
+            console.log(analytics);
             function encodeBase64(integer) {
                 var characterSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
                 if (integer === 0) {
@@ -25,17 +27,20 @@ var app;
             this.$mdDialog.cancel();
         };
         ShareDialog.prototype.facebook = function () {
+            this.analytics["send"]('social', "facebook", "share", this.url);
             var shareFb = 'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(this.url);
             this.$window.open(shareFb, "pop", "width=600, height=400, scrollbars=no");
         };
         ShareDialog.prototype.whatsapp = function () {
+            this.analytics["send"]('social', "whatsApp", "share", this.url);
             this.$window.open("whatsapp://send?text=" + this.url, "pop", "width=600, height=400, scrollbars=no");
         };
         ShareDialog.prototype.twitter = function () {
+            this.analytics["send"]('social', "twitter", "share", this.url);
             var shareTwiiter = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(this.url);
             this.$window.open(shareTwiiter, "pop", "width=600, height=400, scrollbars=no");
         };
-        ShareDialog.$inject = ["$mdDialog", "data", "$window"];
+        ShareDialog.$inject = ["$mdDialog", "data", "$window", "Analytics"];
         return ShareDialog;
     }());
     angular.module("app").controller("ShareDialog", ShareDialog);
