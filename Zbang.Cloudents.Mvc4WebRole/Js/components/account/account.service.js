0d0a39f070977b2ca34d6709d0fb121ecd264b55
irenad88 <irena@cloudents.com>
2016-12-13 12:39:36 +0200

    Moved account.service.js to typescript

--- Zbang.Cloudents.Mvc4WebRole/Js/components/account/account.service.js
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/account/account.service.js
@@ -1,97 +1,85 @@
-﻿(function () {
-    'use strict';
-    angular.module('app.account').service('accountService', accountService);
-    accountService.$inject = ['ajaxService2'];
-
-    function accountService(ajaxservice) {
-        var self = this;
-
-        self.changeEmail = function(email) {
-            return ajaxservice.post('/account/changeemail/', {
+var app;
+(function (app) {
+    "use strict";
+    var AaccountService = (function () {
+        function AaccountService(ajaxService) {
+            this.ajaxService = ajaxService;
+        }
+        AaccountService.prototype.changeEmail = function (email) {
+            return this.ajaxService.post('/account/changeemail/', {
                 email: email
             }, 'accountDetail');
-        }
-        self.submitCode = function(code) {
-            return ajaxservice.post('/account/entercode/' , {
+        };
+        AaccountService.prototype.submitCode = function (code) {
+            return this.ajaxService.post('/account/entercode/', {
                 code: code
             });
-        }
-        self.getAccountDetails = function () {
-            return ajaxservice.get('/account/settingsdata/');
-        }
-
-        
-
-        self.setAccountDetails = function (firstName, lastName) {
-            return ajaxservice.post('/account/changeprofile/', { firstName: firstName, lastName: lastName }, 'accountDetail');
-
-        }
-
-        self.searchUniversity = function (term) {
-            return ajaxservice.get('/university/searchuniversity/', {
+        };
+        AaccountService.prototype.getAccountDetails = function () {
+            return this.ajaxService.get('/account/settingsdata/');
+        };
+        AaccountService.prototype.setAccountDetails = function (firstName, lastName) {
+            return this.ajaxService.post('/account/changeprofile/', { firstName: firstName, lastName: lastName }, 'accountDetail');
+        };
+        AaccountService.prototype.searchUniversity = function (term) {
+            return this.ajaxService.get('/university/searchuniversity/', {
                 term: term
             });
-        }
-
-        self.updatePassword = function (oldPassword, newPassword) {
-            return ajaxservice.post('/account/changepassword/', {
+        };
+        AaccountService.prototype.updatePassword = function (oldPassword, newPassword) {
+            return this.ajaxService.post('/account/changepassword/', {
                 CurrentPassword: oldPassword,
                 NewPassword: newPassword
             });
-        }
-
-        self.getNotification = function() {
-            return ajaxservice.get('/user/notification/');
-        }
-
-
-        self.setNotification = function(boxId, notification) {
-            return ajaxservice.post('/box/changenotification/', {
+        };
+        AaccountService.prototype.getNotification = function () {
+            return this.ajaxService.get('/user/notification/');
+        };
+        AaccountService.prototype.setNotification = function (boxId, notification) {
+            return this.ajaxService.post('/box/changenotification/', {
                 boxId: boxId,
                 notification: notification
             });
-        }
-        self.setPersonalNotification = function(value) {
-            return ajaxservice.post('/account/changenotification/', {
+        };
+        AaccountService.prototype.setPersonalNotification = function (value) {
+            return this.ajaxService.post('/account/changenotification/', {
                 subscribe: value
             });
-        }
-
-        self.changeLocale = function(lang) {
-            return ajaxservice.post('/account/changelocale/', {
+        };
+        AaccountService.prototype.changeLocale = function (lang) {
+            return this.ajaxService.post('/account/changelocale/', {
                 language: lang
             }, 'accountDetail');
-        }
-
-       
-
-        self.facebookLogIn = function (token, boxId) {
-            return ajaxservice.post('/account/facebooklogin/', {
+        };
+        AaccountService.prototype.facebookLogIn = function (token, boxId) {
+            return this.ajaxService.post('/account/facebooklogin/', {
                 token: token,
                 boxId: boxId
             });
-        }
-        self.googleLogIn = function (token, boxId) {
-            return ajaxservice.post('/account/googlelogin/', {
+        };
+        AaccountService.prototype.googleLogIn = function (token, boxId) {
+            return this.ajaxService.post('/account/googlelogin/', {
                 token: token,
                 boxId: boxId
             });
-        }
-
-        self.closeddepartment = function() {
-            return ajaxservice.get('/university/closeddepartment/');
-        }
-        self.closedMembers = function(id) {
-            return ajaxservice.get('/university/closeddepartmentmembers/',
-            {
+        };
+        AaccountService.prototype.closeddepartment = function () {
+            return this.ajaxService.get('/university/closeddepartment/');
+        };
+        AaccountService.prototype.closedMembers = function (id) {
+            return this.ajaxService.get('/university/closeddepartmentmembers/', {
                 id: id
             });
-        }
-        self.approveRequest = function (id, userId) {
-            return ajaxservice.post('/university/approverequest/', {
+        };
+        AaccountService.prototype.approveRequest = function (id, userId) {
+            return this.ajaxService.post('/university/approverequest/', {
                 id: id,
                 userId: userId
             });
-        }
-    }
-})();
+        };
+        AaccountService.$inject = ["ajaxService2"];
+        return AaccountService;
+    }());
+    angular.module("app.account").service("accountService", AaccountService);
+})(app || (app = {}));
+//# sourceMappingURL=account.service.js.map
---
+++ Zbang.Cloudents.Mvc4WebRole/Js/components/account/account.service.ts
@@ -0,0 +1,219 @@
+﻿
+module app {
+    "use strict";
+
+    export interface IAaccountService {
+        changeEmail(email: string): angular.IPromise<any>;
+        submitCode(code: number): angular.IPromise<any>;
+        getAccountDetails(): angular.IPromise<any>;
+        setAccountDetails(firstName: string, lastName: string): angular.IPromise<any>;
+        searchUniversity(term: string): angular.IPromise<any>;
+        updatePassword(oldPassword: string, newPassword: string): angular.IPromise<any>;
+        getNotification(): angular.IPromise<any>;
+        setNotification(boxId: number, notification: string): angular.IPromise<any>;
+        setPersonalNotification(value: string): angular.IPromise<any>;
+        changeLocale(lang: string): angular.IPromise<any>;
+        facebookLogIn(token: string, boxId: number): angular.IPromise<any>;
+        googleLogIn(token: string, boxId: number): angular.IPromise<any>;
+        closeddepartment(): angular.IPromise<any>;
+        closedMembers(id: Guid): angular.IPromise<any>;
+        approveRequest(id: Guid, userId: number): angular.IPromise<any>;
+    }
+
+    class AaccountService implements IAaccountService {
+        static $inject = ["ajaxService2"];
+        constructor(private ajaxService: IAjaxService2) {
+
+        }
+        changeEmail(email: string) {
+            return this.ajaxService.post('/account/changeemail/', {
+                email: email
+            }, 'accountDetail');
+        }
+        submitCode(code: number) {
+            return this.ajaxService.post('/account/entercode/', {
+                code: code
+            });
+        }
+        getAccountDetails() {
+            return this.ajaxService.get('/account/settingsdata/');
+        }
+
+
+
+        setAccountDetails(firstName: string, lastName: string) {
+            return this.ajaxService.post('/account/changeprofile/', { firstName: firstName, lastName: lastName }, 'accountDetail');
+
+        }
+
+        searchUniversity(term: string) {
+            return this.ajaxService.get('/university/searchuniversity/', {
+                term: term
+            });
+        }
+
+        updatePassword(oldPassword: string, newPassword: string) {
+            return this.ajaxService.post('/account/changepassword/', {
+                CurrentPassword: oldPassword,
+                NewPassword: newPassword
+            });
+        }
+
+        getNotification() {
+            return this.ajaxService.get('/user/notification/');
+        }
+
+
+        setNotification(boxId: number, notification: string) {
+            return this.ajaxService.post('/box/changenotification/', {
+                boxId: boxId,
+                notification: notification
+            });
+        }
+        setPersonalNotification(value: string) {
+            return this.ajaxService.post('/account/changenotification/', {
+                subscribe: value
+            });
+        }
+
+        changeLocale(lang: string) {
+            return this.ajaxService.post('/account/changelocale/', {
+                language: lang
+            }, 'accountDetail');
+        }
+
+
+
+        facebookLogIn(token: string, boxId: number) {
+            return this.ajaxService.post('/account/facebooklogin/', {
+                token: token,
+                boxId: boxId
+            });
+        }
+        googleLogIn(token: string, boxId: number) {
+            return this.ajaxService.post('/account/googlelogin/', {
+                token: token,
+                boxId: boxId
+            });
+        }
+
+        closeddepartment() {
+            return this.ajaxService.get('/university/closeddepartment/');
+        }
+        closedMembers(id: Guid) {
+            return this.ajaxService.get('/university/closeddepartmentmembers/',
+                {
+                    id: id
+                });
+        }
+        approveRequest(id: Guid, userId: number) {
+            return this.ajaxService.post('/university/approverequest/', {
+                id: id,
+                userId: userId
+            });
+        }
+    }
+    angular.module("app.account").service("accountService", AaccountService);
+}
+
+
+
+//  # OLD account.service.js
+
+//(function () {
+//    'use strict';
+//    angular.module('app.account').service('accountService', accountService);
+//    accountService.$inject = ['ajaxService2'];
+
+//    function accountService(ajaxservice) {
+//        var self = this;
+
+//        self.changeEmail = function (email) {
+//            return ajaxservice.post('/account/changeemail/', {
+//                email: email
+//            }, 'accountDetail');
+//        }
+//        self.submitCode = function (code) {
+//            return ajaxservice.post('/account/entercode/', {
+//                code: code
+//            });
+//        }
+//        self.getAccountDetails = function () {
+//            return ajaxservice.get('/account/settingsdata/');
+//        }
+
+
+
+//        self.setAccountDetails = function (firstName, lastName) {
+//            return ajaxservice.post('/account/changeprofile/', { firstName: firstName, lastName: lastName }, 'accountDetail');
+
+//        }
+
+//        self.searchUniversity = function (term) {
+//            return ajaxservice.get('/university/searchuniversity/', {
+//                term: term
+//            });
+//        }
+
+//        self.updatePassword = function (oldPassword, newPassword) {
+//            return ajaxservice.post('/account/changepassword/', {
+//                CurrentPassword: oldPassword,
+//                NewPassword: newPassword
+//            });
+//        }
+
+//        self.getNotification = function () {
+//            return ajaxservice.get('/user/notification/');
+//        }
+
+
+//        self.setNotification = function (boxId, notification) {
+//            return ajaxservice.post('/box/changenotification/', {
+//                boxId: boxId,
+//                notification: notification
+//            });
+//        }
+//        self.setPersonalNotification = function (value) {
+//            return ajaxservice.post('/account/changenotification/', {
+//                subscribe: value
+//            });
+//        }
+
+//        self.changeLocale = function (lang) {
+//            return ajaxservice.post('/account/changelocale/', {
+//                language: lang
+//            }, 'accountDetail');
+//        }
+
+
+
+//        self.facebookLogIn = function (token, boxId) {
+//            return ajaxservice.post('/account/facebooklogin/', {
+//                token: token,
+//                boxId: boxId
+//            });
+//        }
+//        self.googleLogIn = function (token, boxId) {
+//            return ajaxservice.post('/account/googlelogin/', {
+//                token: token,
+//                boxId: boxId
+//            });
+//        }
+
+//        self.closeddepartment = function () {
+//            return ajaxservice.get('/university/closeddepartment/');
+//        }
+//        self.closedMembers = function (id) {
+//            return ajaxservice.get('/university/closeddepartmentmembers/',
+//                {
+//                    id: id
+//                });
+//        }
+//        self.approveRequest = function (id, userId) {
+//            return ajaxservice.post('/university/approverequest/', {
+//                id: id,
+//                userId: userId
+//            });
+//        }
+//    }
+//})();
