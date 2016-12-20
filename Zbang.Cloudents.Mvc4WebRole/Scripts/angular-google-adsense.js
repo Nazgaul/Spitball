7d79f6aee90b914faf553b11acf9912f5b3e8d22
nazgaul <ram.y@outlook.com>
2016-12-20 10:30:34 +0200

    xxx

--- Zbang.Cloudents.Mvc4WebRole/Scripts/angular-google-adsense.js
+++ Zbang.Cloudents.Mvc4WebRole/Scripts/angular-google-adsense.js
@@ -13,9 +13,10 @@
                 adClient: '@',
                 adSlot: '@',
                 inlineStyle: '@',
-                adFormat: '@'
+                adFormat: '@',
+                region: '@'
             },
-            template: '<div class="ads"><ins class="adsbygoogle" data-ad-client="{{adClient}}" data-ad-slot="{{adSlot}}" style="{{inlineStyle}}" data-ad-format="{{adFormat}}"></ins></div>',
+            template: '<div class="ads"><ins class="adsbygoogle" data-ad-client="{{adClient}}" data-ad-slot="{{adSlot}}" style="{{inlineStyle}}" data-ad-format="{{adFormat}}"  data-ad-region="{{region}}"></ins></div>',
             controller: ['Adsense', '$timeout', '$scope', function (Adsense, $timeout, $scope) {
                 if (!Adsense.isAlreadyLoaded) {
                     var s = document.createElement('script');
@@ -26,13 +27,7 @@
 
                     Adsense.isAlreadyLoaded = true;
                 }
-                $scope.$on('$stateChangeStart', function () {
-                    Object.keys($window).filter(function (k) { return k.indexOf('google') >= 0 }).forEach(
-                      function (key) {
-                          delete ($window[key]);
-                      }
-                    );
-                });
+                $scope.region = "region" + Math.random();
                
                 /**
                  * We need to wrap the call the AdSense in a $apply to update the bindings.
