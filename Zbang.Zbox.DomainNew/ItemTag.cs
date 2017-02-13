7ec59b1403b17a802b59cae5ba7ebe84b7d1e1f3
nazgaul <ram.y@outlook.com>
2017-02-13 13:32:06 +0200

    Some refactoring

---
+++ Zbang.Zbox.DomainNew/ItemTag.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Zbang.Zbox.Infrastructure.Enums;
+
+namespace Zbang.Zbox.Domain
+{
+    public class ItemTag
+    {
+        protected ItemTag()
+        {
+
+        }
+
+        protected ItemTag(TagType type) : this()
+        {
+            Type = type;
+        }
+        public ItemTag(Tag tag, Item item, TagType type) : this(type)
+        {
+
+            Tag = tag;
+            Item = item;
+        }
+        public ItemTag(Tag tag, FlashcardMeta item, TagType type) : this(type)
+        {
+
+            Tag = tag;
+            Flashcard = item;
+        }
+        public ItemTag(Tag tag, Quiz item, TagType type) : this(type)
+        {
+
+            Tag = tag;
+            Quiz = item;
+        }
+        public virtual Guid Id { get; set; }
+
+        public virtual TagType Type { get; set; }
+
+        public virtual Tag Tag { get; set; }
+        public virtual Item Item { get; set; }
+
+        public virtual Quiz Quiz { get; set; }
+
+        public virtual FlashcardMeta Flashcard { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as ItemTag;
+            if (item == null)
+            {
+                return false;
+            }
+            //bool equals = false;
+            return CheckEqualsWithNull(Tag, item.Tag)
+                   && CheckEqualsWithNull(Item, item.Item)
+                   && CheckEqualsWithNull(Item, item.Item)
+                   && CheckEqualsWithNull(Flashcard, item.Flashcard)
+                   && CheckEqualsWithNull(Quiz, item.Quiz);
+            
+        }
+
+        private static bool CheckEqualsWithNull(object a, object b)
+        {
+            if (a == null && b == null)
+            {
+                return true;
+            }
+            if (a != null && b == null)
+            {
+                return false;
+            }
+            if (a == null)
+            {
+                return false;
+            }
+            return a.Equals(b);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return
+                11 * Tag.GetHashCode()
+                + 13 * Item?.GetHashCode() ?? 0
+                + 17 * Flashcard?.GetHashCode() ?? 0
+                + 19 * Quiz?.GetHashCode() ?? 0;
+        }
+    }
+}
--- Zbang.Zbox.DomainNew/Tag.cs
+++ Zbang.Zbox.DomainNew/Tag.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Zbang.Zbox.Infrastructure.Enums;
 
 namespace Zbang.Zbox.Domain
 {
@@ -25,90 +21,5 @@ namespace Zbang.Zbox.Domain
         public virtual Guid Id { get; set; }
         public virtual string Name { get; set; }
         public virtual ISet<ItemTag> ItemTags { get; set; }
-    }
-
-    public class ItemTag
-    {
-        protected ItemTag()
-        {
-
-        }
-
-        protected ItemTag(TagType type) : this()
-        {
-            Type = type;
-        }
-        public ItemTag(Tag tag, Item item, TagType type) : this(type)
-        {
-
-            Tag = tag;
-            Item = item;
-        }
-        public ItemTag(Tag tag, FlashcardMeta item, TagType type) : this(type)
-        {
-
-            Tag = tag;
-            Flashcard = item;
-        }
-        public ItemTag(Tag tag, Quiz item, TagType type) : this(type)
-        {
-
-            Tag = tag;
-            Quiz = item;
-        }
-        public virtual Guid Id { get; set; }
-
-        public virtual TagType Type { get; set; }
-
-        public virtual Tag Tag { get; set; }
-        public virtual Item Item { get; set; }
-
-        public virtual Quiz Quiz { get; set; }
-
-        public virtual FlashcardMeta Flashcard { get; set; }
-
-
-        public override bool Equals(object obj)
-        {
-            var item = obj as ItemTag;
-            if (item == null)
-            {
-                return false;
-            }
-            //bool equals = false;
-            return CheckEqualsWithNull(Tag, item.Tag)
-                   && CheckEqualsWithNull(Item, item.Item)
-                   && CheckEqualsWithNull(Item, item.Item)
-            && CheckEqualsWithNull(Flashcard, item.Flashcard)
-            && CheckEqualsWithNull(Quiz, item.Quiz);
-            
-        }
-
-        private bool CheckEqualsWithNull(object a, object b)
-        {
-            if (a == null && b == null)
-            {
-                return true;
-            }
-            if (a != null && b == null)
-            {
-                return false;
-            }
-            if (a == null)
-            {
-                return false;
-            }
-            return a.Equals(b);
-
-        }
-
-        public override int GetHashCode()
-        {
-            return
-                11 * Tag.GetHashCode()
-                + 13 * Item?.GetHashCode() ?? 0
-            + 17 * Flashcard?.GetHashCode() ?? 0
-            + 19 * Quiz?.GetHashCode() ?? 0;
-        }
     }
 }
