59c6232f4077617580c385da4ddfb32fd402e05c
yifat <yifatbij@gmail.com>
2017-03-20 11:01:54 +0200

    more informative return json blob

--- Zbang.Zbox.ViewModel/SqlQueries/Jared.cs
+++ Zbang.Zbox.ViewModel/SqlQueries/Jared.cs
@@ -16,7 +16,19 @@ from "+ItemWhere+";";
 left join zbox.itemtaB bt ON i.itemtabid = bt.itemtabid
 join zbox.box b ON i.boxid=b.boxid 
 join zbox.library d ON b.libraryid=d.libraryId 
-where b.discriminator = 2 and i.isdeleted=0
+where b.discriminator = 2 and i.isdeleted=0 and b.university=173408 
+and (@name is null or (i.name like +'%' + @name + '%'))
+and (@university is null or (b.university in (select u.id from zbox.university u where u.universityname like +'%' + @university + '%')))
+and (@IsSearchType=0 or (i.itemtabid is not null)) 
+and (@IsReviewed=0 or (i.isreviewed=1)) 
+and (@box is null or (b.boxname like +'%' + @box + '%')) 
+and (@department is null or (d.name like +'%' + @department + '%')) 
+and (@boxid is null or (b.boxid=@boxid))";
+        private const string ItemWhereTabs = @"zbox.item i 
+join zbox.itemtaB bt ON i.itemtabid = bt.itemtabid
+join zbox.box b ON i.boxid=b.boxid 
+join zbox.library d ON b.libraryid=d.libraryId 
+where b.discriminator = 2 and i.isdeleted=0 and b.university=173408 
 and (@name is null or (i.name like +'%' + @name + '%'))
 and (@university is null or (b.university in (select u.id from zbox.university u where u.universityname like +'%' + @university + '%')))
 and (@IsSearchType=0 or (i.itemtabid is not null)) 
@@ -31,11 +43,11 @@ item.itemid as itemId,
 itemtabname as name 
 from zbox.ItemTab tab 
 join zbox.item item ON tab.boxid=item.boxid 
-where item.itemid in (select top 10 i.itemid from " + ItemWhere + ");";
+where tab.boxid in (select top 10 b.boxid from " + ItemWhereTabs + ");";
 
         public const string ItemTags = @"select item.itemid itemId,
                                         t.name tag
 from zbox.item item join zbox.itemtag it on item.itemid=it.itemid join zbox.tag t on it.tagid=t.id
-where it.itemid in(select top 10 i.itemid from " + ItemWhere + ");";
+where it.itemid in (select top 10 i.itemid from " + ItemWhere + ");";
     }
 }
