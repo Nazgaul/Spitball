e0daa25a9618a73263d7c04e41986e4012dbd3e9
nazgaul <ram.y@outlook.com>
2017-04-18 13:56:43 +0300

    Some fixes in Tutor

--- Zbang.Cloudents.Jared/Controllers/TutorController.cs
+++ Zbang.Cloudents.Jared/Controllers/TutorController.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Web.Http;
 using Microsoft.Azure.Mobile.Server.Config;
+using Zbang.Cloudents.Jared.Models;
 
 namespace Zbang.Cloudents.Jared.Controllers
 {
@@ -12,55 +13,42 @@ namespace Zbang.Cloudents.Jared.Controllers
     public class TutorController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<Tutor> Get()
+        public IEnumerable<Tutor> Get(string q)
         {
-            string json;
-            List<Tutor> tutors;
+            //string json;
+            //List<Tutor> tutors;
             using (StreamReader r = new StreamReader(System.AppContext.BaseDirectory + @"/DataObjects/tutor.json"))
             {
-                json = r.ReadToEnd();
-                tutors = JsonConvert.DeserializeObject<List<Tutor>>(json);
-            }
-
-            return tutors;
-        }
-        [Route("api/tutor/tutorbykeyword"), HttpGet]
-        public IEnumerable<Tutor> GetTutorByKeyWords(string keywords)
-        {
-            var list = Get().OrderBy(t => -(t.KeyWords.IndexOf(keywords)+t.Subject.IndexOf(keywords)));
-            return list;
-        }
-
-
-        public class Tutor
-        {
-            //public IEnumerable<string> KeyWords;
-            public string Subject;
-            private IEnumerable<string> keyList;
-            public string KeyWords;
-            public string Education;
-            public string Description;
-            public string Gender;
-            public string Name;
-            public string Image;
-            public string Location;
-            public int? Views;
-            public int? Likes;
-            public IEnumerable<string> getKeyList()
-            {
-                if (keyList == null || !keyList.Any())
+                var json = r.ReadToEnd();
+                var tutors = JsonConvert.DeserializeObject<List<Tutor>>(json);
+                if (string.IsNullOrEmpty(q))
                 {
-                    keyList = KeyWords.Split(',');
+                    return tutors;
                 }
-                return keyList;
+                else
+                {
+                    return tutors.Where(w => w.Name.Contains(q));
+                }
             }
+
+            //return tutors;
         }
+        //[Route("api/tutor/tutorbykeyword"), HttpGet]
+        //public IEnumerable<Tutor> GetTutorByKeyWords(string keywords)
+        //{
+        //    var list = Get().OrderBy(t => -(t.KeyWords.IndexOf(keywords)+t.Subject.IndexOf(keywords)));
+        //    return list;
+        //}
+
+
+        
+      
 
 
         // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
     }
 }
