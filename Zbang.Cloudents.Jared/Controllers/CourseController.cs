aed7db8e0c5e2a99dcc9d5eb35a68820fb9f0083
yifat <yifatbij@gmail.com>
2017-04-20 17:50:30 +0300

    Fix issue in Wiki project implement follow course in Jared

---
+++ Zbang.Cloudents.Jared/Controllers/CourseController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Http;
+using Microsoft.Azure.Mobile.Server.Config;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Zbang.Cloudents.Jared.DataObjects;
+using Zbang.Zbox.Domain.Commands;
+using Zbang.Zbox.Domain.Common;
+using Zbang.Zbox.Infrastructure.Extensions;
+
+namespace Zbang.Cloudents.Jared.Controllers
+{
+    [MobileAppController]
+    public class CourseController : ApiController
+    {
+        private readonly IZboxWriteService m_ZboxWriteService;
+        public CourseController(IZboxWriteService zboxWriteService)
+        {
+            m_ZboxWriteService = zboxWriteService;
+        }
+        
+        // GET api/Course
+        public string Get()
+        {
+            return "Hello from custom controller!";
+        }
+
+        [Route("api/course/follow")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> FollowAsync(FollowRequest model)
+        {
+            if (model == null)
+            {
+                return Request.CreateBadRequestResponse();
+            }
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateBadRequestResponse();
+            }
+            var command = new SubscribeToSharedBoxCommand(User.GetUserId(), model.BoxId);
+            await m_ZboxWriteService.SubscribeToSharedBoxAsync(command);
+            return Request.CreateResponse();
+        }
+    }
+}
---
+++ Zbang.Cloudents.JaredTests3/Controllers/CourseControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zbang.Cloudents.Jared.Controllers;
+using Rhino.Mocks;
+using Zbang.Zbox.Infrastructure.Repositories;
+using Zbang.Zbox.Infrastructure.Storage;
+using Zbang.Zbox.ReadServices;
+using System.Threading.Tasks;
+using Zbang.Zbox.Infrastructure.Ioc;
+using System.Threading;
+using System.Net.Http;
+using System.Web.Http;
+using Zbang.Zbox.Domain.Common;
+using Zbang.Zbox.Domain.Services;
+using Zbang.Zbox.Infrastructure.CommandHandlers;
+using Zbang.Zbox.Infrastructure.Cache;
+using System.Security.Principal;
+using System.Web;
+using System.Security.Claims;
+using System;
+using Zbang.Zbox.Domain.DataAccess;
+using Zbang.Zbox.Domain;
+
+namespace Zbang.Cloudents.Jared.Controllers.Tests
+{
+    [TestClass()]
+    public class CourseControllerTests
+    {
+        private IZboxWriteService m_ZboxWriteService;
+        private readonly ICommandBus m_CommandBus;
+        private readonly IWithCache m_Cache;
+        private CourseController controller;
+        [TestInitialize]
+        public void Setup()
+        {
+            var localStorageProvider = MockRepository.GenerateStub<ILocalStorageProvider>();
+            var m_CommandBus = MockRepository.GenerateMock<ICommandBus>();
+            var m_Cache = MockRepository.GenerateMock<IWithCache>();
+            IocFactory.IocWrapper.RegisterInstance(localStorageProvider);
+
+            m_ZboxWriteService = new ZboxWriteService(m_CommandBus, m_Cache);
+            controller = new CourseController(m_ZboxWriteService);
+            controller.Request = new HttpRequestMessage();
+            controller.Request.SetConfiguration(new HttpConfiguration());
+        }
+
+        [TestMethod()]
+        public async Task FollowAsyncTest()
+        {
+            var x = await controller.FollowAsync(null);
+        }
+    }
+}
