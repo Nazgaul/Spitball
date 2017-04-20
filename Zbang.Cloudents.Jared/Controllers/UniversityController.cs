e787907b1ae701878f1e3fc823a302b2a67055bf
nazgaul <ram.y@outlook.com>
2017-04-20 14:08:48 +0300

    Create UniversityController api

--- Zbang.Cloudents.Jared/Controllers/UniversityController.cs
+++ Zbang.Cloudents.Jared/Controllers/UniversityController.cs
@@ -1,15 +1,44 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
 using Microsoft.Azure.Mobile.Server.Config;
+using Zbang.Cloudents.Jared.Models;
+using Zbang.Zbox.Domain.Commands;
+using Zbang.Zbox.Domain.Common;
+using Zbang.Zbox.Infrastructure.Extensions;
 
 namespace Zbang.Cloudents.Jared.Controllers
 {
+
     [MobileAppController]
     public class UniversityController : ApiController
     {
-        // GET api/University
-        public string Get()
+        private readonly IZboxWriteService m_ZboxWriteService;
+
+        public UniversityController(IZboxWriteService zboxWriteService)
         {
-            return "Hello from custom controller!";
+            m_ZboxWriteService = zboxWriteService;
+        }
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Post(CreateUniversityRequest model)
+        {
+            if (model == null)
+            {
+                return Request.CreateBadRequestResponse();
+            }
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateBadRequestResponse();
+            }
+            var command = new CreateUniversityCommand(model.Name, model.Country, User.GetUserId());
+            await m_ZboxWriteService.CreateUniversityAsync(command).ConfigureAwait(false);
+
+
+            return Request.CreateResponse(command.Id);
+
+
+
         }
     }
 }
---
+++ Zbang.Cloudents.Jared/Models/CreateUniversityRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Zbang.Cloudents.Jared.Models
+{
+    public class CreateUniversityRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        public string Country { get; set; }
+    }
+}
