a4d60efd7888cf2d0516d47b59ed26485f8334af
nazgaul <ram.y@outlook.com>
2017-06-28 11:51:05 +0300

    Remove log

--- Zbang.Zbox.WorkerRoleSearch/SignalrClient.cs
+++ Zbang.Zbox.WorkerRoleSearch/SignalrClient.cs
@@ -1,13 +1,12 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client;
 using Zbang.Zbox.Infrastructure.Extensions;
-using Zbang.Zbox.Infrastructure.Trace;
 
 namespace Zbang.Zbox.WorkerRoleSearch
 {
     public static class SignalrClient
     {
-        private static IHubProxy Proxy { get; set; }
+        static IHubProxy Proxy { get; set; }
         private static HubConnection _hub;
 
         public static async Task<IHubProxy> GetProxyAsync()
