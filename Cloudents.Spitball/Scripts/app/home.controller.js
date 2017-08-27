3c7faae75ec55acc12c3ae4c1dedf1acd174c72c
yifatBi <yifatbij@gmail.com>
2017-08-27 17:44:27 +0300

    more UI functionality

--- Cloudents.Spitball/Scripts/app/home.controller.js
+++ Cloudents.Spitball/Scripts/app/home.controller.js
@@ -1,24 +1,98 @@
-var appf = angular.module('myApp', []);
-var myApp;
-(function (myApp) {
+var appf = angular.module("myApp", []);
+var MyApp;
+(function (MyApp) {
     "use strict";
     var HomeController = (function () {
-        function HomeController($scope, $location) {
+        function HomeController($scope, $http) {
             this.$scope = $scope;
+            this.$http = $http;
+            this.jobTypes = [{ code: "full", name: "full-time" }, { code: "part", name: "part-time" }, { code: "con", name: "contractor" }];
+            this.sourceList = [
+                { code: "spitball.com", name: "spitball.com" }, { code: "Quizlet.com", name: "Quizlet.com" }
+            ];
+            this.relDatsSort = [{ code: "relevance", name: "relevance" }, { code: "date", name: "date" }];
+            this.tutorSort = [{ code: "relevance", name: "relevance" }, { code: "price", name: "price" }, { code: "distance", name: "distance" }, { code: "rating", name: "rating" }];
+            this.jobSort = [{ code: "distance", name: "distance" }].concat(this.relDatsSort);
+            this.myCourses = [{ code: "1234", name: "Phsico" }, { code: "425", name: "calcus" }];
+            this.jobOptions = [
+                { name: "all", code: "", data: [] }, { name: "job type", code: "job_type", data: this.jobTypes },
+                { name: "paid", code: "paid", data: [] }
+            ];
+            this.tutorOption = [
+                {
+                    name: "all",
+                    code: "all",
+                    data: []
+                }, {
+                    name: "Online",
+                    code: "online",
+                    data: []
+                }, {
+                    name: "in person",
+                    code: "person",
+                    data: []
+                }
+            ];
+            this.flashcardOptions = [
+                {
+                    name: "all",
+                    code: "all",
+                    data: []
+                },
+                {
+                    name: "Sources",
+                    code: "source",
+                    data: this.sourceList
+                }
+            ];
+            this.purchaseOptions = [
+                {
+                    name: "all",
+                    code: "all",
+                    data: []
+                },
+                {
+                    name: "open now",
+                    code: "open",
+                    data: this.sourceList
+                }
+            ];
+            this.docOption = [
+                { name: "all", code: "", data: [] }, { name: "my courses", code: "my_course", data: this.myCourses }
+            ];
+            this.optionDic = {
+                'note': { filter: this.docOption, sort: this.relDatsSort }, 'flashcard': {
+                    filter: this.flashcardOptions,
+                    sort: this.relDatsSort
+                }, 'tutor-copy': {
+                    filter: this.tutorOption,
+                    sort: this.tutorSort
+                },
+                'jobs': { filter: this.jobOptions, sort: this.jobSort }
+            };
             this.isOpen = false;
-            this.sec = 'ask-copy';
+            this.sec = "ask-copy";
         }
         HomeController.prototype.changeSection = function (item) {
             this.isOpen = false;
             this.sec = item;
+            this.options = [];
             console.log("change");
+            this.sublist = [];
+            this.options = this.optionDic[item];
+            document.getElementById('qFilter').focus();
         };
-        HomeController.$inject = ["$scope", "$location"];
+        HomeController.prototype.search = function () {
+            //this.$http.get("/search", {'term':this.term}).then(function(response) {
+            // console.log(response);
+            //})
+        };
         return HomeController;
     }());
-    myApp.HomeController = HomeController;
-    appf.controller('HomeController', HomeController);
-})(myApp || (myApp = {}));
+    HomeController.$inject = ["$scope", "$http"];
+    MyApp.HomeController = HomeController;
+    appf.controller("HomeController", HomeController);
+})(MyApp || (MyApp = {}));
 //}
 //    homeController.$inject = ['$location'];
 //    function homeController($location) {
--- Cloudents.Spitball/Scripts/app/home.controller.ts
+++ Cloudents.Spitball/Scripts/app/home.controller.ts
@@ -1,27 +1,120 @@
-﻿var appf = angular.module('myApp', []);
-module myApp {
-    "use strict";
-    export interface IStoreListController {
-        isOpen: boolean;
-        sec: string;
-    }
+﻿var appf = angular.module("myApp", []);
 
-export class HomeController {
-    static $inject = ["$scope", "$location"];
-    isOpen:false;
-    sec:'ask-copy';
-    constructor(private $scope: angular.IScope, $location: angular.ILocationService) {
-        this.isOpen= false;
-        this.sec= 'ask-copy';
-    }
-   
-    changeSection(item) {
-        this.isOpen = false;
-        this.sec=item;
-        console.log("change");
-    }
-    }
-appf.controller('HomeController', HomeController as any);
+module MyApp {
+	"use strict";
+
+	export interface IStoreListController {
+		isOpen: boolean;
+		sec: string;
+	}
+
+	interface IOptionItemVal {
+		code: string;
+		name: string;
+	}
+
+	interface IOptionItem {
+		code: string;
+		name: string;
+        data: IOptionItemVal[];
+	}
+
+	export class HomeController {
+		static $inject = ["$scope", "$http"];
+		isOpen: boolean;
+        sec: string;
+        term: string;
+		sublist: IOptionItemVal[];
+        private jobTypes: IOptionItemVal[] = [{ code: "full", name: "full-time" }, { code: "part", name: "part-time" }, { code: "con", name: "contractor" }];
+		private sourceList: IOptionItemVal[] = [
+            { code: "spitball.com", name: "spitball.com" }, { code: "Quizlet.com", name: "Quizlet.com" }
+        ];
+        private relDatsSort: IOptionItemVal[]=[{code:"relevance",name:"relevance"}, {code:"date",name:"date"}];
+        private tutorSort: IOptionItemVal[] = [{ code: "relevance", name: "relevance" }, { code: "price", name: "price" }, { code: "distance", name: "distance" }, { code: "rating", name: "rating" }];
+        private jobSort: IOptionItemVal[] = [{ code: "distance", name: "distance" }].concat(this.relDatsSort);
+        private myCourses: IOptionItemVal[] = [{ code: "1234", name: "Phsico" }, { code: "425", name: "calcus" }];
+		private jobOptions: IOptionItem[] = [
+			{ name: "all", code: "", data: [] }, { name: "job type", code: "job_type", data: this.jobTypes },
+			{ name: "paid", code: "paid", data: [] }
+		];
+		private tutorOption: IOptionItem[] = [
+			{
+				name: "all",
+				code: "all",
+				data: []
+			}, {
+				name: "Online",
+				code: "online",
+				data: []
+			}, {
+				name: "in person",
+				code: "person",
+				data: []
+            }
+		];
+		private flashcardOptions: IOptionItem[] = [
+			{
+				name: "all",
+				code: "all",
+				data: []
+			},
+			{
+				name: "Sources",
+				code: "source",
+				data: this.sourceList
+			}
+        ];
+        private purchaseOptions: IOptionItem[] = [
+	        {
+		        name: "all",
+		        code: "all",
+		        data: []
+	        },
+	        {
+		        name: "open now",
+		        code: "open",
+		        data: this.sourceList
+	        }];
+		private docOption: IOptionItem[] = [
+			{ name: "all", code: "", data: [] }, { name: "my courses", code: "my_course", data: this.myCourses }
+		];
+
+
+        private optionDic: any = {
+            'note': { filter: this.docOption, sort: this.relDatsSort}, 'flashcard': {
+                filter: this.flashcardOptions,
+                sort: this.relDatsSort
+            }, 'tutor-copy': {
+	            filter: this.tutorOption,
+	            sort: this.tutorSort
+            },
+            'jobs': { filter: this.jobOptions, sort: this.jobSort}
+        };
+
+		options: IOptionItem[];
+
+        constructor(private $scope: angular.IScope, private $http: angular.IHttpService) {
+			this.isOpen = false;
+            this.sec = "ask-copy";
+		}
+
+		changeSection(item) {
+			this.isOpen = false;
+			this.sec = item;
+			this.options= [];
+			console.log("change");
+			this.sublist = [];
+			this.options = this.optionDic[item];
+			document.getElementById('qFilter').focus();
+        }
+		search() {
+            //this.$http.get("/search", {'term':this.term}).then(function(response) {
+	           // console.log(response);
+            //})
+		}
+	}
+
+	appf.controller("HomeController", HomeController as any);
 }
 
 //}
@@ -33,7 +126,7 @@ appf.controller('HomeController', HomeController as any);
 //        var vm = this;
 //        vm.title = 'homeController';
 //        vm.sec = 'ask-copy';
-        
+
 //        function changeSection(item) {
 //            vm.isOpen = false;
 //            vm.sec = item;
