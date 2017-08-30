01eee0ebbcc4c90a042bcead3302f0e0646b1335
nazgaul <ram.y@outlook.com>
2017-08-30 15:11:46 +0300

    fixed untracked files

---
+++ Cloudents.Core.Spitball/wwwroot/scripts/app.js
@@ -0,0 +1,15 @@
+"use strict";
+alert("hi");
+alert("hello");
+console.log("this is a test");
+function hello() {
+    alert("hi");
+    alert("hi");
+    alert("hi");
+}
+var Test = (function () {
+    function Test() {
+    }
+    return Test;
+}());
+//# sourceMappingURL=app.js.map
