d657b310f81ce26ec94e908de8a8a72a0f77ecfd
nazgaul <ram.y@outlook.com>
2017-08-30 15:51:39 +0300

    Some fixes.

--- Cloudents.Core.Spitball/Program.cs
+++ Cloudents.Core.Spitball/Program.cs
@@ -20,6 +20,7 @@ namespace Cloudents.Core.Spitball
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
