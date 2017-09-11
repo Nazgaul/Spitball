fc93bc75fc49383eb69324b193b6a71a606d938f
Yifat Biezuner <yifat@cloudents.com>
2017-09-11 17:33:54 +0300

    remove type script and vendor

--- Cloudents.Core.Spitball/ClientApp/boot.js
+++ Cloudents.Core.Spitball/ClientApp/boot.js
@@ -13,38 +13,25 @@ Vue.use(SvgSprite, {
 });
 
 
-const routes = [
-    { path: '/', component: require('./components/sections/sections.vue.html') },
-    //{ path: '/', component: require('./components/home/home.vue.html') },
-    { path: '/counter', component: require('./components/counter/counter.vue.html') },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
-];
+//const routes = [
+//    { path: '/', component: require('./components/sections/sections.vue.html') },
+//    //{ path: '/', component: require('./components/home/home.vue.html') },
+//    { path: '/counter', component: require('./components/counter/counter.vue.html') },
+//    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+//];
 
 //new Vue({
 //    el: '#app-root',
 //    router: new VueRouter({ mode: 'history', routes: routes }),
 //    render: h => h(require('./components/app/app.vue.html'))
 //});
+Vue.component('app-aside', {
+
+   render: h => h(require('./components/sections/aside/navbar.vue.html')),
+
+    props: ['isOpen']
+});
 var aa=new Vue({
     el: '#sections',
-    created: function () {
-        this.fetchProducts();
-    },
-    methods: {
-        fetchProducts() {
-            {
-                this.$http.get('/search').then((response) => {
-                    this.products = response.body;
-                    console.log("11");
-                });
-            }
-        }
-    },
-    data() {
-        return {
-            sectionOpen: false,
-            isOpen: false
-        }
-    },
     render: h => h(require('./components/sections/sections.vue.html'))
 });
--- Cloudents.Core.Spitball/webpack.config.js
+++ Cloudents.Core.Spitball/webpack.config.js
@@ -1,7 +1,7 @@
 ﻿const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 const bundleOutputDir = './wwwroot/dist';
 
 module.exports = (env) => {
@@ -14,10 +14,10 @@ module.exports = (env) => {
         entry: { 'main': './ClientApp/boot.js' },
         module: {
             rules: [
-                { test: /\.vue\.html$/, include: /ClientApp/, loader: 'vue-loader', options: { loaders: { js: 'awesome-typescript-loader?silent=true' } } },
-                { test: /\.ts$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=true' },
+                { test: /\.vue\.html$/, include: /ClientApp/, loader: 'vue-loader' },
                 { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
+                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' }
             ]
         },
         output: {
@@ -26,16 +26,12 @@ module.exports = (env) => {
             publicPath: 'dist/'
         },
         plugins: [
-            new CheckerPlugin(),
             new webpack.DefinePlugin({
                 'process.env': {
                     NODE_ENV: JSON.stringify(isDevBuild ? 'development' : 'production')
                 }
-            }),
-            new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./wwwroot/dist/vendor-manifest.json')
             })
+         
         ].concat(isDevBuild ? [
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
