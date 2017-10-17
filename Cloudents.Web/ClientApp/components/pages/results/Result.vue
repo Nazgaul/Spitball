088bf5a23b3e88d6e60eee613055fbfdf9673201
nazgaul <ram.y@outlook.com>
2017-10-17 16:07:23 +0300

    xxx

--- Cloudents.Web/ClientApp/components/pages/results/Result.vue
+++ Cloudents.Web/ClientApp/components/pages/results/Result.vue
@@ -13,22 +13,6 @@
                 </v-layout>
                 <radio-list :values="subFilters" @click="$_changeSubFilter" class="sub-search" model="subFilter" :value="subFilter"></radio-list>
             </v-container>
-            
-            
-            <!--<v-container grid-list-md>
-                <v-layout row wrap>
-                    <v-flex>
-                        <radio-list class="search" :values="page.filter" @click="$_changeFilter" model="filter" :value="filterOptions"></radio-list>
-                    </v-flex>
-                    <v-flex>
-                        <radio-list v-if="page.sort" :values="page.sort" @click="$_updateSort" model="sort" class="search sort" :value="$_defaultSort(page.sort[0].id)"></radio-list>
-                    </v-flex>
-
-                </v-layout>
-                <v-flex xs12>
-                    <radio-list :values="subFilters" @click="$_changeSubFilter" class="sub-search" model="subFilter" :value="subFilter"></radio-list>
-                </v-flex>
-            </v-container>-->
         </slot>
         <slot name="data">
             <scroll-list :loadMore="!isEmpty" v-if="pageData.data" @scroll="value => {pageData.data=pageData.data.concat(value) }">
