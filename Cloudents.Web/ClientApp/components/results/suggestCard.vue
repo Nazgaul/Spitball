b7cdc2bfc1a88e4cf3f1827dfff96f719235ee01
Yifat Biezuner <yifat@cloudents.com>
2017-11-14 16:19:06 +0200

    case #8658 settings page basic functionality

---
+++ Cloudents.Web/ClientApp/components/results/suggestCard.vue
@@ -0,0 +1,15 @@
+<template functional>
+    <div class="mt-2 mb-2 pa-2 suggest">I found some sweet {{props.name}} for you. You can thank me later:)
+    <v-btn>Show me</v-btn>
+    </div>
+</template>
+<style scoped>
+    .suggest{
+        background: #3293f6;
+        width: 504px;
+        cursor: pointer;
+        border-radius: 4px;
+        color: white;
+    }
+</style>
+
