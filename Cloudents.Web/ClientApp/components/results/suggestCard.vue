003555e5fadcd899dcee6827bbaab922b234fcb7
Ram Yaari <ram.y@outlook.com>
2017-11-16 11:17:48 +0200

    Fixes of suggestCard

--- Cloudents.Web/ClientApp/components/results/suggestCard.vue
+++ Cloudents.Web/ClientApp/components/results/suggestCard.vue
@@ -1,15 +1,57 @@
 <template functional>
-    <div class="mt-2 mb-2 pa-2 suggest">I found some sweet {{props.name}} for you. You can thank me later:)
-    <v-btn>Show me</v-btn>
-    </div>
+    <button type="button" class="suggest elevation-1">
+        <v-container >
+            <v-layout row>
+                <v-flex>
+                    <div class="logo-wrap">
+                        <logo></logo>
+                    </div>
+                </v-flex>
+                <v-flex class="text">
+                    I found some sweet {{props.name}} for you.  You can thank me later:)
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </button>
+    <!--<button type="button" class="mt-2 mb-2 pa-2 suggest">
+
+        I found some sweet  for you. You can thank me later:)
+
+        asda
+    </button>-->
+    <!--{{props.name}}-->
 </template>
-<style scoped>
-    .suggest{
-        background: #3293f6;
-        width: 504px;
-        cursor: pointer;
+<script>
+    import logo from "./svg/sb-logo-copy.svg";
+    export default {
+        components: { logo }
+    }
+</script>
+<style scoped lang="less">
+    .logo-wrap {
+        width: 64px;
+        height: 64px;
+        background: #000;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    svg {
+      //  width: 49px;
+      //  height: 28px;
+    }
+    .text {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    } 
+    .suggest {
+        background: #FFF;
+        width: 100%;
+        //cursor: pointer;
         border-radius: 4px;
-        color: white;
+        color: #000;
     }
 </style>
-
