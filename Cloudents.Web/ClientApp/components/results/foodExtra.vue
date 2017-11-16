db6f197ead2c3e062a4fc4e184f509d612ac630f
Ram Yaari <ram.y@outlook.com>
2017-11-16 17:31:08 +0200

    Some fixes.

--- Cloudents.Web/ClientApp/components/results/foodExtra.vue
+++ Cloudents.Web/ClientApp/components/results/foodExtra.vue
@@ -1,6 +1,6 @@
 ﻿<template>
     <iframe 
-            frameborder="0" class="pa-2 pl-5 flex"
+            frameborder="0" class="ml-2 flex"
             :src="url" allowfullscreen></iframe>
 
 </template>
@@ -13,9 +13,13 @@
                 return "//www.google.com/maps/embed/v1/place?key=AIzaSyCV88wIotTJsgAffA3h5x74WzD_np_WqMU&q=place_id:" + this.place}
         }
     }
-
-   
 </script>
+<style scoped>
+    iframe {
+        width:100%;
+        height:50vh; /*TODO: this is a temp solution*/
+    }
+</style>
 
 
 
