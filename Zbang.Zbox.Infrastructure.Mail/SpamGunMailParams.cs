f60bf1b6afd5fafc5fb6e7bda80fb646ec92e702
Ram Yaari <ram.y@outlook.com>
2017-11-23 12:08:18 +0200

    case 9002
    remove greek partner

--- Zbang.Zbox.Infrastructure.Mail/SpamGunMailParams.cs
+++ Zbang.Zbox.Infrastructure.Mail/SpamGunMailParams.cs
@@ -4,11 +4,10 @@
     {
         public SpamGunMailParams(
              string body,
-            string universityUrl, string name, string subject, string category, string htmlBody)
+             string name, string subject, string category, string htmlBody)
             :base(new System.Globalization.CultureInfo("en-US"),senderName: "Olivia Williams <olivia@spitball.co>")
         {
             Body = body;
-            UniversityUrl = universityUrl;
             Name = name;
             Subject = subject;
             Category = category;
@@ -19,7 +18,6 @@
 
         public string HtmlBody { get; }
         public string Name { get; }
-        public string UniversityUrl { get; }
 
         public string Subject { get; }
 
@@ -27,32 +25,32 @@
         public override string MailResolver => nameof(SpamGunMailParams);
     }
 
-    public class GreekPartnerMailParams : MailParameters
-    {
-        public GreekPartnerMailParams(
-             string body,
-            string universityUrl, string name, string subject, string category, string school, string chapter)
-            : base(new System.Globalization.CultureInfo("en-US"), senderName: "Justin Liao <justin@spitball.co>")
-        {
-            Body = body;
-            UniversityUrl = universityUrl;
-            Name = name;
-            Subject = subject;
-            Category = category;
-            School = school;
-            Chapter = chapter;
-        }
+    //public class GreekPartnerMailParams : MailParameters
+    //{
+    //    public GreekPartnerMailParams(
+    //         string body,
+    //        string universityUrl, string name, string subject, string category, string school, string chapter)
+    //        : base(new System.Globalization.CultureInfo("en-US"), senderName: "Justin Liao <justin@spitball.co>")
+    //    {
+    //        Body = body;
+    //        UniversityUrl = universityUrl;
+    //        Name = name;
+    //        Subject = subject;
+    //        Category = category;
+    //        School = school;
+    //        Chapter = chapter;
+    //    }
 
-        public string Body { get; }
-        public string Name { get; }
-        public string UniversityUrl { get; }
+    //    public string Body { get; }
+    //    public string Name { get; }
+    //    public string UniversityUrl { get; }
 
-        public string Subject { get; }
+    //    public string Subject { get; }
 
-        public string School { get; }
-        public string Chapter { get; }
+    //    public string School { get; }
+    //    public string Chapter { get; }
 
-        public string Category { get; }
-        public override string MailResolver => nameof(GreekPartnerMailParams);
-    }
+    //    public string Category { get; }
+    //    public override string MailResolver => nameof(GreekPartnerMailParams);
+    //}
 }
