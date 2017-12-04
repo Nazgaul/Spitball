055139842a13f09ee77b0b8a0f6704a3326cebbb
irenad88 <irenad88@gmail.com>
2017-12-04 09:37:26 +0200

    final touches in filter section

--- Cloudents.Web/ClientApp/components/helpers/radioList.vue
+++ Cloudents.Web/ClientApp/components/helpers/radioList.vue
@@ -1,13 +1,13 @@
 ﻿<template functional>
     <v-expansion-panel expand>
-        <v-expansion-panel-content v-for="k in props.values" :key="k.modelId" hide-actions>
+        <v-expansion-panel-content v-for="(k,index) in props.values" :key="k.modelId" hide-actions v-bind:value=true>
             <template slot="header">
                 <div>{{k.title}}</div>
                 <div class="header__icon">
                     <v-icon>sbf-arrow-button</v-icon>
                 </div>
             </template>
-            <div class="sort-filter">
+            <div class="sort-filter pa-2">
                 <div v-if="k.data" v-for="s in k.data" :key="(s.id?s.id:s)" class="filter">
                     <input type="checkbox" :id="s" :checked="props.checkesVals.includes(s.id?s.id.toString():s.toString())" @change="props.callback({id:k.modelId,val:(s.id?s.id:s),type:$event})"/>
                     <label :for="s">{{s.name?s.name:s}}</label>
