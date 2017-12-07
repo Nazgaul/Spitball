827e708162e5e392365db1c2155737e8f8b0c018
Yifat Biezuner <yifat@cloudents.com>
2017-12-07 15:15:35 +0200

    fix issue with remove flow

--- Cloudents.Web/ClientApp/routes.js
+++ Cloudents.Web/ClientApp/routes.js
@@ -77,7 +77,7 @@ export const routes = [
             '/' + RouteTypes.jobRoute,
             '/' + RouteTypes.foodRoute
         ], components: resultPage, props: resultProps, meta: {
-            showHeader: true,isStatic:true
+            showHeader: true
             
         }
     },
