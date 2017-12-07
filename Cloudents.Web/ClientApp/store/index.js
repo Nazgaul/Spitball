5bb349e1e8e919746c20d33e6ce996490aceeb87
Yifat Biezuner <yifat@cloudents.com>
2017-12-07 14:56:42 +0200

    case 9089

--- Cloudents.Web/ClientApp/store/index.js
+++ Cloudents.Web/ClientApp/store/index.js
@@ -1,13 +1,12 @@
 ﻿import Vue from "vue";
 import Vuex from "vuex";
-import Flow from "./flow";
 import Search from "./search";
 import User from "./User";
 import createPersistedState from "vuex-persistedstate"
 
 Vue.use(Vuex);
 export default new Vuex.Store({
-    modules: { Flow, Search, User },
+    modules: { Search, User },
     plugins: [
         createPersistedState({ paths: ["User"] })
     ]
