3fcb93dc0f8cf7f5ac4f33aa35106bce2f1d601a
Yifat Biezuner <yifat@cloudents.com>
2017-12-14 16:55:51 +0200

    hot fix for filter added values

--- Cloudents.Infrastructure/Data/CourseIdToNameRepository.cs
+++ Cloudents.Infrastructure/Data/CourseIdToNameRepository.cs
@@ -20,8 +20,9 @@ namespace Cloudents.Infrastructure.Data
         {
             return _repository.WithConnectionAsync(c => c.QueryAsync<CourseNameDto>(
                 @"SELECT COALESCE(CourseCode,BoxName) as Name 
-FROM zbox.box WHERE IsDeleted = 0 and BoxId IN @BoxIds;",
+FROM zbox.box WHERE (IsDeleted IS NULL OR IsDeleted=0) AND BoxId IN @BoxIds;",
                 new { BoxIds = query }), token);
         }
     }
+    //TODO issue in added course isDeleted NULL
 }
