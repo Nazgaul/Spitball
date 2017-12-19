e24d953976067728cf836dd0ecf3f05a378dec06
Yifat Biezuner <yifat@cloudents.com>
2017-12-19 18:23:20 +0200

    remove filter button trigger in mobile when there is no filter option

--- Cloudents.Web/ClientApp/components/header/headerBookDetailsMobile.vue
+++ Cloudents.Web/ClientApp/components/header/headerBookDetailsMobile.vue
@@ -1,25 +1,16 @@
 <template>
     <div>
         <first-line :name="name"></first-line>
-        <v-tabs value="buy" grow>
-            <v-tabs-bar class="cyan" dark>
-                <v-tabs-item v-for="tab in sortOptions" :key="tab.id" :href="tab.id" :id="tab.id" @click="$_changeTab(tab.id)"
-                             class="mr-2 vertical">
-                    {{tab.name}}
-                </v-tabs-item>
-                <v-tabs-slider color="white"></v-tabs-slider>
-            </v-tabs-bar>
-        </v-tabs>
+       <type-filter-tab></type-filter-tab>
     </div>
 </template>
 
 <script>
-    import TabMixin from "../details/bookDetailsSort"
+    import TypeFilterTab from "../details/bookDetailsTabsMobile.vue"
     import FirstLine from './headerFirstLineMobile.vue'
     export default {
         name: "header-book-details-mobile",
-        mixins:[TabMixin],
-        components:{FirstLine},
+        components:{FirstLine,TypeFilterTab},
         props:{name:{type:String}}
     }
 </script>
