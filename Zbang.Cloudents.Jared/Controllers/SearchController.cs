fd291d6022e3a6be6c6e7f76877e947cc3879f15
Ram Yaari <ram.y@outlook.com>
2017-12-19 11:03:33 +0200

    Trying to start next link - unsuccessful

--- Zbang.Cloudents.Jared/Controllers/SearchController.cs
+++ Zbang.Cloudents.Jared/Controllers/SearchController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Cloudents.Core.DTOs;
 using Cloudents.Core.Interfaces;
 using Cloudents.Core.Request;
 using Microsoft.Azure.Mobile.Server.Config;
@@ -23,6 +20,12 @@ namespace Zbang.Cloudents.Jared.Controllers
         private readonly Lazy<IFlashcardSearch> _flashcardProvider;
         private readonly Lazy<IQuestionSearch> _questionProvider;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="searchProvider"></param>
+        /// <param name="flashcardProvider"></param>
+        /// <param name="questionProvider"></param>
         public SearchController(
             Lazy<IDocumentCseSearch> searchProvider, Lazy<IFlashcardSearch> flashcardProvider,
             Lazy<IQuestionSearch> questionProvider)
@@ -38,18 +41,22 @@ namespace Zbang.Cloudents.Jared.Controllers
         /// <param name="model"></param>
         /// <param name="token"></param>
         /// <returns></returns>
-        [Route("api/search/documents"), HttpGet]
+        [Route("api/search/documents",Name = "DocumentSearch"), HttpGet]
         public async Task<HttpResponseMessage> SearchDocumentAsync([FromUri] SearchRequest model,
             CancellationToken token)
         {
             var query = new SearchQuery(model.Query, model.University, model.Course, model.Source, model.Page.GetValueOrDefault(),
                 model.Sort.GetValueOrDefault());
-
             var result = await _searchProvider.Value.SearchAsync(query, token).ConfigureAwait(false);
+
+            //https://stackoverflow.com/questions/8391055/passing-an-array-to-routevalues-and-have-it-render-model-binder-friendly-url
+            //var nextPageLink = Url.Link("DocumentSearch",
+            //    model.GetNextPage());
             return Request.CreateResponse(new
             {
                 documents = result.Result,
-                result.Facet
+                result.Facet,
+                //nextPageLink
             });
         }
 
