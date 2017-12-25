de72ce7c7cc0fce1048a3ae84f195fd27d18dae6
Ram Yaari <ram.y@outlook.com>
2017-12-25 09:16:29 +0200

    Extra fixes.

--- Cloudents.Web/ClientApp/client.js
+++ Cloudents.Web/ClientApp/client.js
@@ -2,11 +2,11 @@
 
 router.push("/");
 router.onReady(() => {
-  const matchedComponents = router.getMatchedComponents()
-  console.log(matchedComponents);
-  router.options.routes.forEach(route => {
-    console.log(route);
-  });
+  //const matchedComponents = router.getMatchedComponents()
+  //console.log(matchedComponents);
+  //router.options.routes.forEach(route => {
+  //  console.log(route);
+  //});
   //router.beforeResolve((to, from, next) => {
   //  const matched = router.getMatchedComponents(to)
   //  console.log(matched);
