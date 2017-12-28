5a5c2bfef23c2079da1f64d2b5979f753724ab72
irenad88 <irenad88@gmail.com>
2017-12-28 11:44:35 +0200

    Personalize dialog design

--- Cloudents.Web/ClientApp/components/settings/ResultPersonalize.vue
+++ Cloudents.Web/ClientApp/components/settings/ResultPersonalize.vue
@@ -1,11 +1,11 @@
 ﻿<template>
-     <v-dialog ref="person" v-model="showDialog" max-width="518" :fullscreen="isSearch" :content-class="isSearch?'dialog-choose':''" :overlay="!isSearch">
-        <v-card v-show="!isSearch">
+     <v-dialog ref="person" v-model="showDialog" max-width="520" :fullscreen="isSearch" :content-class="isSearch?'dialog-choose':''" :overlay="!isSearch">
+        <v-card class="personalize-dialog" v-show="!isSearch">
             <v-card-title class="">Personalize Results</v-card-title>
-            <v-card-text>
-                <v-text-field type="search" auto-grow solo prepend-icon="sbf-search" placeholder="Where do you go to school?" @click="$_personalize"></v-text-field>
+            <v-card-text class="px-4 pb-2">
+                <v-text-field class="elevation-0" type="search" auto-grow solo prepend-icon="sbf-search" placeholder="Where do you go to school?" @click="$_personalize"></v-text-field>
             </v-card-text>
-            <v-card-actions>
+            <v-card-actions class="pb-1">
                 <v-spacer></v-spacer>
                 <v-btn flat="flat" @click.native="showDialog = false">Not now</v-btn>
                 <v-btn flat="flat" @click.native="$_personalize">Personalize</v-btn>
@@ -14,7 +14,7 @@
          <search-item v-model="showDialog" v-show="isSearch" :type="type" :keep="keep" :isFirst="isfirst"></search-item>
     </v-dialog>
 </template>
-<style lang="less" src="./ResultPersonalize.less" scoped>
+<style lang="less" src="./ResultPersonalize.less">
 </style>
 <script src="./ResultPersonalize.js">  
 </script>
