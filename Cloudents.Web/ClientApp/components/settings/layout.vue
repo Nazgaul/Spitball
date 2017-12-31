0a4637730756c5647b2687e0df9ef115f0b7b528
irenad88 <irenad88@gmail.com>
2017-12-31 15:33:33 +0200

    case 9360

--- Cloudents.Web/ClientApp/components/settings/layout.vue
+++ Cloudents.Web/ClientApp/components/settings/layout.vue
@@ -6,7 +6,7 @@
             </button>
             <slot :name="`${props.type}ExtraClose`"></slot>
         </v-layout>
-        <div class="d-header pt-3">
+        <div class="d-header">
             <div>
                 <v-layout row align-center justify-center class="title-text mt-3">
                     <img class="mr-2 elevation-1 uni-icon" v-if="props.titleImage" :src="props.titleImage" />
@@ -18,7 +18,7 @@
                     <v-layout row justify-center>
                         <v-flex><slot name="inputField"></slot></v-flex>
                     </v-layout>
-                    <v-container fluid slot="selectedItems" class="selected-items px-2 pt-3 pb-0" v-if="props.selectedCourse">
+                    <v-container fluid slot="selectedItems" class="selected-items px-2 pt-3 pb-0" v-if="props.selectedCourse && props.selectedCourse.length">
                         <v-layout row wrap justify-start>
                             <template v-for="course in props.selectedCourse">
                                 <slot name="selectedItems" :course="course"></slot>
@@ -32,13 +32,13 @@
         <div class="loader" v-if="props.search&&props.isLoading">
             <v-progress-circular indeterminate v-bind:size="50"></v-progress-circular>
         </div>
-        <v-flex class="results-container px-2 mt-2" v-else-if="props.search&&!props.isLoading">
-            <v-layout fluid class="d-result" row justify-start wrap>
+        <v-flex class="results-container px-1 mt-2">
+            <v-layout v-if="props.search&&!props.isLoading" fluid class="d-result" row justify-start wrap>
                 <template v-for="item in props.items" v-if="props.items.length">
                     <slot name="results" :item="item"></slot>
                 </template>
-                <slot :name="`${props.type}ExtraItem`"></slot>
             </v-layout>
+            <slot :name="`${props.type}ExtraItem`"></slot>
             <slot name="actionContent">
             </slot>
         </v-flex>
