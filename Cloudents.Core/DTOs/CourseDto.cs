3da6fe150a95d289adf33d7b5e5aacb7de64735e
Ram Yaari <ram.y@outlook.com>
2018-01-03 17:34:47 +0200

    case 9412

--- Cloudents.Core/DTOs/CourseDto.cs
+++ Cloudents.Core/DTOs/CourseDto.cs
@@ -1,8 +1,23 @@
-﻿namespace Cloudents.Core.DTOs
+﻿using System.Collections.Generic;
+
+namespace Cloudents.Core.DTOs
 {
     public class CourseDto
     {
         public long Id { get; set; }
         public string Name { get; set; }
+    }
+
+    public class CourseDtoEquality : IEqualityComparer<CourseDto>
+    {
+        public bool Equals(CourseDto x, CourseDto y)
+        {
+            return x?.Id == y?.Id;
+        }
+
+        public int GetHashCode(CourseDto obj)
+        {
+            return obj.Id.GetHashCode();
+        }
     }
 }
