d0f82377acbd042d20ae45559bb9c1d98d8a7787
irenad88 <irenad88@gmail.com>
2018-01-04 16:14:15 +0200

    Safari fixes...

--- Cloudents.Web/ClientApp/components/home/features.vue
+++ Cloudents.Web/ClientApp/components/home/features.vue
@@ -3,11 +3,11 @@
         <v-divider class="hidden-xs-only"></v-divider>
         <v-layout row justify-space-around>
             <v-flex v-for="(feature, index) in props.features" :key="index" :class="`feature ${index}`" xs4 px-1 mb-3 text-xs-center>
-                <v-layout class="wrapper" column align-top justify-space-between d-flex wrap>
-                    <v-flex class="icon-wrapper" row d-flex align-center justify-center xs12>
+                <v-layout class="wrapper" column align-center justify-space-between>
+                    <v-flex class="icon-wrapper" row d-flex align-center justify-center>
                         <slot :type="feature.icon" name="item"></slot>
                     </v-flex>
-                    <v-flex xs12>
+                    <v-flex>
                         <div class="data text-xs-center">
                             <h3>{{feature.title}}</h3>
                             <p class="hidden-sm-and-down">{{feature.text}}</p>
