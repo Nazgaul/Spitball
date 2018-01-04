1a85e19830f42cdc44610bea9f82e71af753ea54
Ram Yaari <ram.y@outlook.com>
2018-01-04 11:05:56 +0200

    case 9314
    case 9276

--- Cloudents.Web/ClientApp/components/results/Result.vue
+++ Cloudents.Web/ClientApp/components/results/Result.vue
@@ -15,7 +15,7 @@
                     </v-chip>
                 </template>
             </div>
-            <scroll-list v-if="page&&items" @scroll="value => {items=items.concat(value) }" :token="pageData.token">
+            <scroll-list v-if="items.length" @scroll="value => {items=items.concat(value) }" :token="pageData.token">
                 <v-container class="pa-0">
                     <v-layout column>
                         <v-flex order-xs1 v-if="showPersonalizeField&&!university" class="personalize-wrapper pa-3 mb-2 elevation-1">
@@ -30,6 +30,25 @@
                     </v-layout>
                 </v-container>
             </scroll-list>
+            <div v-else>
+                <div class="result-cell elevation-1 mb-2 empty-state" xs-12>
+                    <v-layout row class="ma-3">
+                        <v-flex class="img-wrap mr-3">
+                                <empty-state></empty-state>
+                        </v-flex>
+                        <v-flex>
+                            <h6 class="mb-3">Your search - {{term}} - did not match any records.</h6>
+                            <div class="sug mb-2">Suggestions:</div>
+                            <ul>
+                                <li>Check your spelling.</li>
+                                <li>Try different keywords.</li>
+                                <li>Try more general keywords.</li>
+                                <li>Try fewer keywords.</li>
+                            </ul>
+                        </v-flex>
+                    </v-layout>
+                </div>
+            </div>
         </div>
         <template slot="sideBar" v-if="page">
             <component :is="($vuetify.breakpoint.xsOnly?'mobile-':'')+'sort-and-filter'"
@@ -41,13 +60,13 @@
                        :filterCallback="$_updateFilter"
                        :filterVal="filterSelection">
                 <img :src="universityImage" slot="courseTitlePrefix" width="24" height="24" v-if="universityImage" />
-              <template slot="courseEmptyState" v-if="!myCourses.length">
-                  <button v-if="$vuetify.breakpoint.xsOnly" class="edit-list hidden-sm-and-up" @click="$_openPersonalize()">Empty state mobile</button>
-                  <div class="course-empty-state" v-else>
-                    <div>Add your school and courses for better results</div>
-                    <v-btn @click="$_openPersonalize">Personalize</v-btn>
-                </div>
-              </template>
+                <template slot="courseEmptyState" v-if="!myCourses.length">
+                    <button v-if="$vuetify.breakpoint.xsOnly" class="edit-list hidden-sm-and-up" @click="$_openPersonalize()">Empty state mobile</button>
+                    <div class="course-empty-state" v-else>
+                        <div>Add your school and courses for better results</div>
+                        <v-btn @click="$_openPersonalize">Personalize</v-btn>
+                    </div>
+                </template>
                 <template slot="courseExtraState" v-else>
                     <button class="add-course" @click="$_openPersonalize" type="button">
                         <plus-btn></plus-btn><span>Add Course</span>
@@ -56,7 +75,7 @@
                 <button v-if="$vuetify.breakpoint.xsOnly" slot="mobileExtra" class="edit-list" @click.stop.prevent="$_openPersonalize()" type="button">Edit List</button>
             </component>
         </template>
-        
+
 
         <component slot="rightSide" v-if="hasExtra&&!isEmpty" :is="name+'-extra'" :place="selectedItem"></component>
         <router-link slot="suggestCell" v-if="!hasExtra" tag="v-flex" class="result-cell hidden-md-and-down elevation-1 mb-2 xs-12 order-xs3 " :to="{path:'/'+currentSuggest,query:{q:this.query.q}}">
