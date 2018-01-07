75f3cd002c71334a609829303ae2aa27805afa37
Yifat Biezuner <yifat@cloudents.com>
2018-01-07 11:10:21 +0200

    fix issue in settalize pages

--- Cloudents.Web/ClientApp/components/satellite/partner.vue
+++ Cloudents.Web/ClientApp/components/satellite/partner.vue
@@ -1,5 +1,5 @@
 <template>
-   <general-page header="./img/partners-header.png" icon="./img/partners-icon.png">
+   <general-page header="partners-header.png" icon="partners-icon.png">
             Does your site or app make students’ lives easier?
 If so, reach out to us and let’s chat - we’re always looking to add quality content to Spitball. Email us at 
 <a href="mailto:partner@spitball.co">partner@spitball.co</a>
