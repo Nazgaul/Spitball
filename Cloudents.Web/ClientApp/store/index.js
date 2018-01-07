078b7cc5f6fcba924694ed7142daa16f54046885
Ram Yaari <ram.y@outlook.com>
2018-01-07 16:51:21 +0200

    case 9430

--- Cloudents.Web/ClientApp/store/index.js
+++ Cloudents.Web/ClientApp/store/index.js
@@ -7,12 +7,27 @@ import User from "./User";
 //TODO: server side fix
 import createPersistedState from "vuex-persistedstate"
 
-const plugins = [];
+function canWriteStorage(storage) {
+    try {
+        storage.setItem('@@', 1);
+        storage.removeItem('@@');
+        return true;
+    } catch (e) { }
+
+    return false;
+}
+
+
+const plugins = [createPersistedState(
+    {
+        paths: ["User"],
+        storage: canWriteStorage(localStorage) ? localStorage : sessionStorage
+    }
+)];
+
 Vue.use(Vuex);
 const store = new Vuex.Store({
     modules: { Search, User },
-    plugins: [
-       createPersistedState({ paths: ["User"] })
-    ]
+    plugins: plugins
 });
 export default store;
