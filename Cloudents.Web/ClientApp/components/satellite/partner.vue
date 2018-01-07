03b697f1b44882fec026d60863eae4c514904d71
irenad88 <irenad88@gmail.com>
2018-01-07 13:43:53 +0200

    Design fixes at satellite pages

--- Cloudents.Web/ClientApp/components/satellite/partner.vue
+++ Cloudents.Web/ClientApp/components/satellite/partner.vue
@@ -1,8 +1,10 @@
 <template>
    <general-page header="./img/partners-header.png" icon="./img/partners-icon.png">
-            Does your site or app make students’ lives easier?
-If so, reach out to us and let’s chat - we’re always looking to add quality content to Spitball. Email us at 
-<a href="mailto:partner@spitball.co">partner@spitball.co</a>
+       <div class="partner">
+           Does your site or app make students’ lives easier?
+           If so, reach out to us and let’s chat - we’re always looking to add quality content to Spitball. Email us at
+           <a href="mailto:partner@spitball.co">partner@spitball.co</a>
+       </div>
   </general-page>
 </template>
 <script>
@@ -11,12 +13,4 @@ export default {
   components: {generalPage}
 }
 </script>
-<style lang="less" scoped>
-    .wrapper {
-        font-size: 18px;
-        line-height: 30px;
-        letter-spacing: -0.2px;
-        color:#302eb5;
-    }
-</style>
 
