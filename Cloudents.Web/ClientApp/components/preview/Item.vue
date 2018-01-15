57deb2c2395f8a90f97236d9f3e50b1970f7c178
irenad88 <irenad88@gmail.com>
2018-01-15 13:05:07 +0200

    changed the item header

--- Cloudents.Web/ClientApp/components/preview/Item.vue
+++ Cloudents.Web/ClientApp/components/preview/Item.vue
@@ -1,23 +1,5 @@
 ﻿<template>
     <div class="item">
-        <slot name="header">
-            <v-toolbar fixed class="item-toolbar elevation-0" height="80">
-                <v-layout class="toolbar-content" column>
-                    <v-flex>
-                        <v-layout row align-center justify-space-between>
-                            <div class="item-name">{{item.name}}</div>
-                            <item-actions></item-actions>
-                        </v-layout>
-                    </v-flex>
-                    <v-flex class="item-meta mt-2">
-                        <v-layout row align-center justify-space-between>
-                            <div class="author">{{item.author}} woop</div>
-                            <div class="date">{{item.date | mediumDate}}</div>
-                        </v-layout>
-                    </v-flex>
-                </v-layout>
-            </v-toolbar>
-        </slot>
         <div class="item-content">
             <div class="page text-xs-center" v-for="(page,index) in item.preview" :key="index">
                 <component class="page-content" :is="currentComponent" :src="page" :class="item.type+'-content'"></component>
