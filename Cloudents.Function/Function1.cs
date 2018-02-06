1cab1de311357325e047f2bdc7d4bd09f7e27564
Ram Yaari <ram.y@outlook.com>
2018-02-06 10:25:38 +0200

    Still ok

--- Cloudents.Function/Function1.cs
+++ Cloudents.Function/Function1.cs
@@ -1,4 +1,5 @@
 using System;
+using Cloudents.Core.Interfaces;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 
@@ -6,9 +7,11 @@ namespace Cloudents.Function
 {
     public static class Function1
     {
+       // private static ServiceLocator _locator = new ServiceLocator();
         [FunctionName("KeepAlive")]
         public static void KeepAlive([TimerTrigger("0 */4 * * * *", RunOnStartup = true)]TimerInfo myTimer, TraceWriter log)
         {
+            //var bus = _locator.Instance.GetInstance<ICommandBus>();
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
---
+++ Cloudents.Function/ServiceLocator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extras.CommonServiceLocator;
+using Cloudents.Infrastructure.Framework;
+using CommonServiceLocator;
+
+namespace Cloudents.Function
+{
+    public class ServiceLocator
+    {
+        public ServiceLocator()
+        {
+            this.BuildContainer();
+        }
+
+        public IServiceLocator Instance { get; private set; }
+
+        private void BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            var sql = GetEnvironmentVariable("sql");
+            //builder.RegisterModule(new ModuleDb(ConfigurationManager.ConnectionStrings["ZBox"].ConnectionString));
+
+            var container = builder.Build();
+
+            // Create service locator.
+           // var csl = new AutofacServiceLocator(container);
+
+
+            // Set the service locator created.
+
+           // CommonServiceLocator.ServiceLocator.SetLocatorProvider(() => csl);
+           // this.Instance = CommonServiceLocator.ServiceLocator.Current;
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return name + ": " +
+                   System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
