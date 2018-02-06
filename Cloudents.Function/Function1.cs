d1ca8d0217fb9832de927492ef947e8b957c58e5
Ram Yaari <ram.y@outlook.com>
2018-02-06 10:09:59 +0200

    Continue working

--- Cloudents.Function/Function1.cs
+++ Cloudents.Function/Function1.cs
@@ -6,8 +6,8 @@ namespace Cloudents.Function
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */4 * * * *")]TimerInfo myTimer, TraceWriter log)
+        [FunctionName("KeepAlive")]
+        public static void KeepAlive([TimerTrigger("0 */4 * * * *", RunOnStartup = true)]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
         }
