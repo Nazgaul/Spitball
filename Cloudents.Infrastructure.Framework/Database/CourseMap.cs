07ef150b9ea90a0973cebb699cda0b9248127a54
Ram Yaari <ram.y@outlook.com>
2018-02-07 14:25:25 +0200

    Web job to process data.
    Remove azure function.

--- Cloudents.Infrastructure.Framework/Database/CourseMap.cs
+++ Cloudents.Infrastructure.Framework/Database/CourseMap.cs
@@ -30,8 +30,8 @@ namespace Cloudents.Infrastructure.Framework.Database
             Id(x => x.Id).GeneratedBy.GuidComb();
             Map(e => e.Host);
             Map(e => e.DateTime);
-            Map(e => e.UrlSource);
-            Map(e => e.UrlTarget);
+            Map(e => e.UrlSource).Length(8000);
+            Map(e => e.UrlTarget).Length(8000);
             Map(e => e.SourceLocation);
             Map(e => e.AggregateCount);
         }
--- Cloudents.Web/Controllers/UrlController.cs
+++ Cloudents.Web/Controllers/UrlController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using Cloudents.Core.Command;
-using Cloudents.Core.Interfaces;
 using Cloudents.Core.Storage;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,33 +7,19 @@ namespace Cloudents.Web.Controllers
 {
     public class UrlController : Controller
     {
-        private readonly ICommandBus _bus;
+        private readonly IQueueProvider _queueProvider;
 
-        public UrlController(ICommandBus bus)
+        public UrlController(IQueueProvider queueProvider)
         {
-            _bus = bus;
+            _queueProvider = queueProvider;
         }
-        //private readonly IRestClient _client;
-        //private readonly IQueueProvider _queueProvider;
-
-        //public UrlController(IRestClient client, IQueueProvider queueProvider)
-        //{
-        //    _client = client;
-        //    _queueProvider = queueProvider;
-        //}
 
         public async Task<IActionResult> Index(string host, string url,int location, CancellationToken token)
         {
             var referer = Request.Headers["Referer"].ToString();
             var message = new UrlRedirectQueueMessage(host, url, referer, location);
 
-            var command = new CreateUrlStatsCommand(host, DateTime.UtcNow, url, referer,
-                location);
-
-            await _bus.DispatchAsync(command, token).ConfigureAwait(false);
-
-            //await _queueProvider.InsertMessageAsync(QueueName.UrlRedirect, message, token).ConfigureAwait(false);
-            //var p = await _client.UrlRedirectAsync(new Uri(url)).ConfigureAwait(false);
+            await _queueProvider.InsertMessageAsync(QueueName.UrlRedirect, message, token).ConfigureAwait(false);
             return Redirect(url);
         }
     }
