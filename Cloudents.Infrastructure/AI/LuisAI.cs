eaadc9f028dc950f47afc19bb4ad653c4caed212
Ram Yaari <ram.y@outlook.com>
2018-02-14 17:11:51 +0200

    Return cache

--- Cloudents.Infrastructure/AI/LuisAI.cs
+++ Cloudents.Infrastructure/AI/LuisAI.cs
@@ -25,7 +25,7 @@ namespace Cloudents.Infrastructure.AI
             _client = client;
         }
 
-        //[Cache(TimeConst.Day, "ai")]
+        [Cache(TimeConst.Day, "ai")]
         public async Task<AiDto> InterpretStringAsync(string sentence, CancellationToken token)
         {
             if (sentence == null) throw new ArgumentNullException(nameof(sentence));
