ea29cbafbcca35601fbb1a8477a514944e822a51
Ram Yaari <ram.y@outlook.com>
2018-02-14 17:21:35 +0200

    case 9880 Food vertical - results are replicated on paging

--- Cloudents.Web/ClientApp/components/helpers/infinateScroll.vue
+++ Cloudents.Web/ClientApp/components/helpers/infinateScroll.vue
@@ -1,61 +1,63 @@
 ﻿<template>
-        <div v-scroll="{callback:this.scrollList}"><slot></slot></div>
+    <div v-scroll="{callback:this.scrollList}"><slot></slot></div>
 </template>
 <script>
 
     export default {
         data() {
             return {
-                page: this.token||1,
-                currentToken:null,
-                isLoading:false,
-                isComplete:false
+                page: this.token || 1,
+                currentToken: null,
+                isLoading: false,
+                isComplete: false
             }
         },
 
-        props: { token: { type: [String, Number]}},
+        props: { token: { type: [String, Number] } },
 
-        created(){
-            this.currentToken=this.token;
+        created() {
+            this.currentToken = this.token;
         },
-        watch:{
-            '$route':'$_resetScrolling'
+        watch: {
+            '$route': '$_resetScrolling'
         },
 
         methods: {
-            keepLoad(){
-                let totalHeight=this.$el.offsetHeight;
-                let currentScroll=window.pageYOffset || document.documentElement.scrollTop;
-                let scrollOffset=(currentScroll>(0.75*totalHeight-document.documentElement.clientHeight));
-                let retVal=((window.pageYOffset>0 || document.documentElement.scrollTop>0)&&scrollOffset&&
-                    !this.isLoading&&!this.isComplete);
-                return retVal},
-            scrollList () {
+            keepLoad() {
+                let totalHeight = this.$el.offsetHeight;
+                let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+                let scrollOffset = (currentScroll > (0.75 * totalHeight - document.documentElement.clientHeight));
+                let retVal = ((window.pageYOffset > 0 || document.documentElement.scrollTop > 0) && scrollOffset &&
+                    !this.isLoading && !this.isComplete);
+                return retVal
+            },
+            scrollList() {
                 if (this.keepLoad()) {
-                    let page=this.token?this.currentToken:this.page;
-                    this.isLoading=true;
+                    debugger;
+                    let page = this.token ? (this.currentToken || this.token) : this.page;
+                    this.isLoading = true;
                     this.$store.dispatch('fetchingData', { name: this.$route.path.slice(1), params: this.$route.query, page })
-                        .then(({data:res}) => {
+                        .then(({ data: res }) => {
                             if (res.data && res.data.length && !res.hasOwnProperty('token') ||
                                 (res.hasOwnProperty('token') && res.token)) {
                                 this.$emit('scroll', res.data);
                                 if (res.hasOwnProperty('token')) {
                                     this.currentToken = res.token;
                                 } else { this.page++; }
-                                this.isLoading=false;
+                                this.isLoading = false;
                             } else {
                                 this.page = 1;
-                                this.isComplete=true;
+                                this.isComplete = true;
                             }
                         }).catch(reason => {
-                        this.isComplete=true;
-                    })
+                            this.isComplete = true;
+                        })
                 }
             },
-            $_resetScrolling(){
-                    this.page=this.token?this.token:1;
-                    this.isLoading=false;
-                    this.isComplete=false;
+            $_resetScrolling() {
+                this.page = this.token ? this.token : 1;
+                this.isLoading = false;
+                this.isComplete = false;
             }
 
         }
