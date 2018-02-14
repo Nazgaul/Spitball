cebd3539f660d050a03caa696ccb0ce005a48ea5
Ram Yaari <ram.y@outlook.com>
2018-02-14 09:09:20 +0200

    Update nuget packages.

--- Cloudents.Infrastructure.Test/CacheResultInterceptorTests.cs
+++ Cloudents.Infrastructure.Test/CacheResultInterceptorTests.cs
@@ -10,68 +10,68 @@ namespace Cloudents.Infrastructure.Test
     [TestClass]
     public class CacheResultInterceptorTests
     {
-        //[TestMethod]
-        //public void GetInvocationSignature_BookDetailPaging_Works()
-        //{
-        //    //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
-        //    var bookRequest1 = new object[] { "make%20war", 150, 0, CancellationToken.None };
-        //    var bookRequest2 = new object[] { "make%20war", 150, 1, CancellationToken.None };
-        //    var type = new PrivateType(typeof(CacheResultInterceptor));
-        //    var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
-        //    var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
+        [TestMethod]
+        public void GetInvocationSignature_BookDetailPaging_Works()
+        {
+            //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
+            var bookRequest1 = new object[] { "make%20war", 150, 0, CancellationToken.None };
+            var bookRequest2 = new object[] { "make%20war", 150, 1, CancellationToken.None };
+            var type = new PrivateType(typeof(CacheResultInterceptor));
+            var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
+            var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
 
-        //    Assert.AreNotEqual(result1, result2);
-        //}
+            Assert.AreNotEqual(result1, result2);
+        }
 
-        //[TestMethod]
-        //public void GetInvocationSignature_BingDifferentTerm_Works()
-        //{
-        //    var searchModel1 = new SearchModel(new[] { "biology" }, null, SearchRequestSort.None,
-        //        CustomApiKey.Documents, null, null, "biology", null);
-        //    var searchModel2 = new SearchModel(new[] { "chemistry" }, null, SearchRequestSort.None,
-        //        CustomApiKey.Documents, null, null, "biology", null);
-        //    //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
-        //    var bookRequest1 = new object[] { searchModel1, 0, CancellationToken.None };
-        //    var bookRequest2 = new object[] { searchModel2, 0, CancellationToken.None };
-        //    var type = new PrivateType(typeof(CacheResultInterceptor));
-        //    var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
-        //    var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
+        [TestMethod]
+        public void GetInvocationSignature_BingDifferentTerm_Works()
+        {
+            var searchModel1 = new SearchModel(new[] { "biology" }, null, SearchRequestSort.None,
+                CustomApiKey.Documents, null, null, "biology", null);
+            var searchModel2 = new SearchModel(new[] { "chemistry" }, null, SearchRequestSort.None,
+                CustomApiKey.Documents, null, null, "biology", null);
+            //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
+            var bookRequest1 = new object[] { searchModel1, 0, CancellationToken.None };
+            var bookRequest2 = new object[] { searchModel2, 0, CancellationToken.None };
+            var type = new PrivateType(typeof(CacheResultInterceptor));
+            var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
+            var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
 
-        //    Assert.AreNotEqual(result1, result2);
-        //}
+            Assert.AreNotEqual(result1, result2);
+        }
 
-        //[TestMethod]
-        //public void GetInvocationSignature_DifferentArrayOrder_SameResultWorks()
-        //{
-        //    var searchModel1 = new SearchModel(new[] { "Linear Algebra" }, new[] { "spitball", "koofers" }, SearchRequestSort.None,
-        //        CustomApiKey.Documents, null, null, "biology", null);
-        //    var searchModel2 = new SearchModel(new[] { "Linear Algebra" }, new[] { "koofers", "spitball", }, SearchRequestSort.None,
-        //        CustomApiKey.Documents, null, null, "biology", null);
-        //    //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
-        //    var bookRequest1 = new object[] { searchModel1, 0, CancellationToken.None };
-        //    var bookRequest2 = new object[] { searchModel2, 0, CancellationToken.None };
-        //    var type = new PrivateType(typeof(CacheResultInterceptor));
-        //    var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
-        //    var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
+        [TestMethod]
+        public void GetInvocationSignature_DifferentArrayOrder_SameResultWorks()
+        {
+            var searchModel1 = new SearchModel(new[] { "Linear Algebra" }, new[] { "spitball", "koofers" }, SearchRequestSort.None,
+                CustomApiKey.Documents, null, null, "biology", null);
+            var searchModel2 = new SearchModel(new[] { "Linear Algebra" }, new[] { "koofers", "spitball", }, SearchRequestSort.None,
+                CustomApiKey.Documents, null, null, "biology", null);
+            //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
+            var bookRequest1 = new object[] { searchModel1, 0, CancellationToken.None };
+            var bookRequest2 = new object[] { searchModel2, 0, CancellationToken.None };
+            var type = new PrivateType(typeof(CacheResultInterceptor));
+            var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
+            var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
 
-        //    Assert.AreEqual(result1, result2);
-        //}
+            Assert.AreEqual(result1, result2);
+        }
 
-        //[TestMethod]
-        //public void GetInvocationSignature_BingDifferentKey_Works()
-        //{
-        //    var searchModel1 = new SearchModel(new[] { "biology" }, null, SearchRequestSort.None,
-        //        CustomApiKey.Documents, null, null, "biology", null);
-        //    var searchModel2 = new SearchModel(new[] { "biology" }, null, SearchRequestSort.None,
-        //        CustomApiKey.Flashcard, null, null, "biology", null);
-        //    //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
-        //    var bookRequest1 = new object[] { searchModel1, 0, CancellationToken.None };
-        //    var bookRequest2 = new object[] { searchModel2, 0, CancellationToken.None };
+        [TestMethod]
+        public void GetInvocationSignature_BingDifferentKey_Works()
+        {
+            var searchModel1 = new SearchModel(new[] { "biology" }, null, SearchRequestSort.None,
+                CustomApiKey.Documents, null, null, "biology", null);
+            var searchModel2 = new SearchModel(new[] { "biology" }, null, SearchRequestSort.None,
+                CustomApiKey.Flashcard, null, null, "biology", null);
+            //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
+            var bookRequest1 = new object[] { searchModel1, 0, CancellationToken.None };
+            var bookRequest2 = new object[] { searchModel2, 0, CancellationToken.None };
 
-        //    var type = new PrivateType(typeof(CacheResultInterceptor));
-        //    var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
-        //    var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
-        //    Assert.AreNotEqual(result1, result2);
-        //}
+            var type = new PrivateType(typeof(CacheResultInterceptor));
+            var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest1);
+            var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, bookRequest2);
+            Assert.AreNotEqual(result1, result2);
+        }
     }
 }
