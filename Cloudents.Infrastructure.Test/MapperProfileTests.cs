9d93ebe08d587333160f480f99500ed6ee2e41ac
Ram Yaari <ram.y@outlook.com>
2018-02-18 16:38:05 +0200

    update nuget packages.

--- Cloudents.Infrastructure.Test/MapperProfileTests.cs
+++ Cloudents.Infrastructure.Test/MapperProfileTests.cs
@@ -15,18 +15,5 @@ namespace Cloudents.Infrastructure.Test
 
             Assert.IsTrue(true);
         }
-
-
-
-        [TestMethod]
-        public void IpConverterConvert_JsonWithGeoPoint_PointCreate()
-        {
-            Mapper.Initialize(cfg => cfg.AddProfile(new MapperProfile()));
-            var json =
-                "{\"ip\":\"86.143.189.86\",\"country_code\":\"GB\",\"country_name\":\"United Kingdom\",\"region_code\":\"ENG\",\"region_name\":\"England\",\"city\":\"Rayleigh\",\"zip_code\":\"SS6\",\"time_zone\":\"Europe/London\",\"latitude\":51.5833,\"longitude\":0.6167,\"metro_code\":0}";
-            var data = Mapper.Map<Location>(json);
-
-            Assert.AreEqual(data.Point.Latitude, 51.5833);
-        }
     }
 }
--- Cloudents.WebJob/Functions.cs
+++ Cloudents.WebJob/Functions.cs
@@ -1,10 +1,14 @@
 ﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Cloudents.Core.Command;
 using Cloudents.Core.Interfaces;
 using Cloudents.Core.Storage;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 
 namespace Cloudents.WebJob
 {
@@ -31,6 +35,28 @@ namespace Cloudents.WebJob
 
             await _commandBus.DispatchAsync(command, token).ConfigureAwait(false);
             await log.WriteLineAsync("Finish Process").ConfigureAwait(false);
+        }
+
+        [FunctionName("Redirect")]
+        public async Task<HttpResponseMessage> ProcessRedirectAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get",Route="redirect")]
+            HttpRequestMessage req, TextWriter log)
+        {
+            await log.WriteLineAsync("C# HTTP trigger function processed a request.");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
     }
 }
