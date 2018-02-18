967d6628be11e10066db146eb58b6b9b95f3b064
irenad88 <irenad88@gmail.com>
2018-02-18 14:46:40 +0200

    case 9886

--- Cloudents.Web/ClientApp/components/results/Result.vue
+++ Cloudents.Web/ClientApp/components/results/Result.vue
@@ -29,7 +29,7 @@
                                 </div>
                             </v-flex>
                             <slot name="resultData" :items="items">
-                                <v-flex order-xs1 v-if="isAcademic&&showPersonalizeField&&!university" class="personalize-wrapper pa-3 mb-2 elevation-1">
+                                <v-flex order-xs1 v-if="isAcademic&&showPersonalizeField&&!university && !loading" class="personalize-wrapper pa-3 mb-2 elevation-1">
                                     <v-text-field class="elevation-0" type="search" solo prepend-icon="sbf-search" placeholder="Where do you go to school?" @click="$_openPersonalize"></v-text-field>
                                 </v-flex>
                                 <v-flex class="result-cell elevation-1 mb-2" xs-12 v-for="(item,index) in items" :key="index" :class="(index>6?'order-xs6': index>2 ? 'order-xs3' : 'order-xs2')">
