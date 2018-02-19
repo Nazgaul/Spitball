af1c722ab0949fe405f940d30700c297ebc08af5
Ram Yaari <ram.y@outlook.com>
2018-02-19 09:04:51 +0200

    Fix issue 404 on old site.

---
+++ Cloudents.Web.Test/IntegrationTests/DocumentControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cloudents.Web.Test.IntegrationTests
+{
+    [TestClass]
+    public class DocumentControllerTests : ServerInit
+    {
+        [TestMethod]
+        public async Task LinkToHeb_RedirectResult()
+        {
+            var response = await _client.GetAsync("/item/המרכז-האקדמי-לב-מכון-טל/25405/סימולציה/401065/consoleapplication1.xml").ConfigureAwait(false);
+            var p = response.Headers.Location;
+            Assert.IsTrue(p.AbsolutePath.EndsWith("/"));
+        }
+    }
+}
