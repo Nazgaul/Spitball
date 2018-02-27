f5b49819fbbd435d96852a7ae940a99a8bc3b572
Yifat Biezuner <yifatbij@gmail.com>
2018-02-27 15:41:21 +0200

    case 9820

--- Cloudents.Web/ClientApp/components/home/horizontalStrip.vue
+++ Cloudents.Web/ClientApp/components/home/horizontalStrip.vue
@@ -1,7 +1,8 @@
 ﻿﻿
 <template functional>
     <section class="strips-section">
-        <router-link  :to="strip.link" class="strip d-block" v-for="(strip,index) in props.strips" :class="strip.class" :key="index" >
+     <template v-for="(strip,index) in props.strips">
+         <div @click="props.analytics(`#${index}_${strip.title}`)"><router-link  :to="strip.link" class="strip d-block" :class="strip.class" :key="index">
             <v-layout row justify-center align-center wrap class="strip-content px-2">
                 <v-flex class="image-wrapper mb-3" sm12 md6>
                     <div class="images">
@@ -23,7 +24,7 @@
                     <p class="strip-text mt-2 mb-0 text-xs-center text-sm-left">{{strip.text}}</p>
                 </v-flex>
             </v-layout>
-        </router-link>
+        </router-link></div></template>
     </section>
 </template>
 
