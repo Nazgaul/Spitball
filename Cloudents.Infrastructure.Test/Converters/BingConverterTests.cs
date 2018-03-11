45df66f57556242ac77c77bebcca2115f331996d
Ram Yaari <ram.y@outlook.com>
2018-03-11 10:21:25 +0200

    Fix unit test.

--- Cloudents.Infrastructure.Test/Converters/BingConverterTests.cs
+++ Cloudents.Infrastructure.Test/Converters/BingConverterTests.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Autofac.Extras.Moq;
 using AutoMapper;
 using Cloudents.Core.Interfaces;
 using Cloudents.Core.Storage;
 using Cloudents.Infrastructure.Converters;
 using Cloudents.Infrastructure.Search;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -12,52 +14,69 @@ namespace Cloudents.Infrastructure.Test.Converters
     [TestClass]
     public class BingConverterTests
     {
-        private readonly Mock<IKeyGenerator> _keyGenerator = new Mock<IKeyGenerator>();
-        private readonly Mock<IMappingOperationOptions> _mockOptions = new Mock<IMappingOperationOptions>();
+        //private readonly Mock<IKeyGenerator> _keyGenerator = new Mock<IKeyGenerator>();
+        //private readonly Mock<IMappingOperationOptions> _mockOptions = new Mock<IMappingOperationOptions>();
         private readonly Mock<IRuntimeMapper> _mockMapping = new Mock<IRuntimeMapper>();
 
+        private BingConverter _bingConverter;
         private ResolutionContext _context;
+
         //private readonly Mock<ReplaceImageProvider> _replaceImageMock = new Mock<ReplaceImageProvider>(new Mock<IBlobProvider<SpitballContainer>>().Object);
-        
-        private readonly Mock<ReplaceImageProvider> _replaceImageMock = new Mock<ReplaceImageProvider>(new Mock<IBlobProvider<SpitballContainer>>().Object);
 
         [TestInitialize]
         public void Setup()
         {
-            _mockOptions.Setup(s => s.Items).Returns(new Dictionary<string, object>());
-            _context = new ResolutionContext(_mockOptions.Object, _mockMapping.Object);
+            using (var _autofackMock = AutoMock.GetLoose())
+            {
+                var mockOptions = _autofackMock.Mock<IMappingOperationOptions>();
+                mockOptions.Setup(s => s.Items).Returns(new Dictionary<string, object>());
+                _context = new ResolutionContext(mockOptions.Object, _mockMapping.Object);
+                _bingConverter = _autofackMock.Create<BingConverter>();
+            }
         }
+
+       
 
         [TestMethod]
         public void ConvertToResult_CourseHeroWebPage_ShareSaleUrl()
         {
             const string courseHeroLink =
                 "https://www.coursehero.com/file/11150425/Calculus-Basics-you-should-know-class-notes/";
-            var bingConverter = new BingConverter(_keyGenerator.Object, _replaceImageMock.Object);
+
+            // var bingConverter = mock.Create<BingConverter>();
+
+
+            //var bingConverter = new BingConverter(_keyGenerator.Object, _replaceImageMock.Object);
             var argument = new BingSearch.WebPage
             {
                 Url = courseHeroLink
 
             };
-            var result = bingConverter.Convert(argument, null, _context);
+            var result = _bingConverter.Convert(argument, null, _context);
             const string resultLink =
                 "http://shareasale.com/r.cfm?b=661825&u=1469379&m=55976&urllink=www.coursehero.com/file/11150425/Calculus-Basics-you-should-know-class-notes/&afftrack=";
             Assert.AreEqual(result.Url, resultLink);
-            Assert.AreEqual(result.Source, "www.coursehero.com");
+            result.Source.Should().BeEquivalentTo("coursehero");
+            //Assert.AreEqual(result.Source, "coursehero");
+
         }
 
         [TestMethod]
         public void ConvertToResult_RegularWebPage_RegularUrl()
         {
-            var bingConverter = new BingConverter(_keyGenerator.Object, _replaceImageMock.Object);
+            //using (var mock = AutoMock.GetLoose())
+            //{
+            // var bingConverter = mock.Create<BingConverter>();
+            //var bingConverter = new BingConverter(_keyGenerator.Object, _replaceImageMock.Object);
             var argument = new BingSearch.WebPage
             {
                 Url = "https://www.spitball.co"
 
             };
-            var result = bingConverter.Convert(argument, null, _context);
+            var result = _bingConverter.Convert(argument, null, _context);
             Assert.AreEqual(result.Url, "https://www.spitball.co");
-            Assert.AreEqual(result.Source, "www.spitball.co");
+            Assert.AreEqual(result.Source, "spitball");
+            //}
         }
     }
 }
