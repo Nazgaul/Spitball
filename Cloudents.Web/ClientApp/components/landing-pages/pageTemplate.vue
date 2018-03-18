f2fc0d30eafc945232e4793e169fa85108f5b357
irenad88 <irenad88@gmail.com>
2018-03-18 14:10:57 +0200

    started landing page input animations

--- Cloudents.Web/ClientApp/components/landing-pages/pageTemplate.vue
+++ Cloudents.Web/ClientApp/components/landing-pages/pageTemplate.vue
@@ -3,24 +3,21 @@
          :style="'background-image: url('+ require(`./img/${contentObj.background}`)+')'">
         <div class="landing-page">
 
-            <router-link class="logo-link" :to="{name:'home'}">
+            <router-link class="logo-link collapsible" :to="{name:'home'}">
                 <app-logo class="logo"></app-logo>
             </router-link>
             <div class="page-data">
-                <h1 v-html="contentObj.titleHtml"></h1>
+                <h1 v-html="contentObj.titleHtml" class="collapsible"></h1>
                 <div class="content-wrapper">
-                    <h2 v-html="contentObj.bodyHtml"></h2>
-                    <p class="hidden-xs-only">Fill out the field and start spitballing!</p>
+                    <h2 v-html="contentObj.bodyHtml" class="collapsible"></h2>
+                    <p class="hidden-xs-only collapsible">Fill out the field and start spitballing!</p>
                     <div class="search-wrapper">
                         <form action="." method="get" @submit.prevent="search">
                             <search-input class="term-field" :placeholder="contentObj.placeholders.term" v-model="msg" :search-on-selection="false"></search-input>
                             <search-input class="uni-field mb-0" :disabled="!msg.length" :placeholder="contentObj.placeholders.uni"
                                           v-if="contentObj.placeholders.uni" search-type="uni"
                                           v-model="uni" :search-on-selection="false"></search-input>
-                            <button type="submit">
-                                <v-icon class="hidden-md-and-up">sbf-search</v-icon>
-                                <span class="hidden-sm-and-down">Start Spitballing</span>
-                            </button>
+                            <button type="submit">Start Spitballing</button>
                         </form>
                     </div>
                 </div>
