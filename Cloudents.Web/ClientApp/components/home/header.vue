dbeb4176a06b60bf06f6583d53a6134551423203
irenad88 <irenad88@gmail.com>
2018-03-20 17:14:27 +0200

    Final fixes to smart app banner

--- Cloudents.Web/ClientApp/components/home/header.vue
+++ Cloudents.Web/ClientApp/components/home/header.vue
@@ -1,6 +1,6 @@
 ﻿﻿<template>
 <div>
-    <smart-app-banner v-if="showSmartAppBanner"></smart-app-banner>
+    <smart-app-banner v-if="showSmartAppBanner && $vuetify.breakpoint.xsOnly"></smart-app-banner>
     <v-toolbar :height="$vuetify.breakpoint.smAndDown? 64 : 110" flat class="h-p-header" :class="{scroll: scrollTop}" :extended="$vuetify.breakpoint.smAndDown && showText" app :fixed="$vuetify.breakpoint.mdAndUp" v-scroll="onScroll">
         <v-toolbar-title class="ml-2">
             <logo class="logo"></logo>
