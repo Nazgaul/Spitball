9e003ae33c0ebd67b11b30fbdee9346d2fcf72e7
Ram Yaari <ram.y@outlook.com>
2018-03-26 10:54:06 +0300

    case 10095 Item page returns error 500

--- Cloudents.Infrastructure.Storage/ModuleStorage.cs
+++ Cloudents.Infrastructure.Storage/ModuleStorage.cs
@@ -6,7 +6,7 @@ namespace Cloudents.Infrastructure.Storage
 {
     [ModuleRegistration(Core.Enum.System.Console)]
     [ModuleRegistration(Core.Enum.System.Api)]
-    //[ModuleRegistration(Core.Enum.System.Web)]
+    [ModuleRegistration(Core.Enum.System.Web)]
     public class ModuleStorage : Module
     {
         protected override void Load(ContainerBuilder builder)
@@ -21,12 +21,6 @@ namespace Cloudents.Infrastructure.Storage
             builder.RegisterType<QueueProvider>().AsImplementedInterfaces();
 
             builder.RegisterGeneric(typeof(BlobProvider<>)).AsImplementedInterfaces();
-
-            //builder.Register(c =>
-            //{
-            //    var key = c.Resolve<IConfigurationKeys>().LocalStorageData;
-            //    return new TempStorageProvider(c.Resolve<ILogger>(), key);
-            //}).As<ITempStorageProvider>();
         }
     }
 }
