faab61bb59078e75cc3276bd952c3da89cd8aae5
irenad88 <irenad88@gmail.com>
2018-04-10 11:17:01 +0300

    Added google pixel

--- Cloudents.Web/ClientApp/components/results/studyblueCard.vue
+++ Cloudents.Web/ClientApp/components/results/studyblueCard.vue
@@ -1,5 +1,5 @@
 <template functional>
-    <a class="studyblue d-block cell" target="_blank" :href="'https://www.studyblue.com/notes/answers/'+props.searchterm+'?utm_source=spitball&utm_medium=referral'">
+    <a class="studyblue d-block cell" target="_blank" :href="'https://www.studyblue.com/notes/answers/'+props.searchterm+'?utm_source=spitball&utm_medium=referral'" @click="goog_report_conversion('https://www.studyblue.com/notes/answers/'+props.searchterm+'?utm_source=spitball&utm_medium=referral')">
         <v-layout row align-center>
             <img src="./img/study-blue-logo.png" alt="">
             <div class="cell-text">Not finding what you're looking for? <br class="hidden-xs-only"/>Click here to ask StudyBlue's Homework Help</div>
