9d85e2fc55b336cea813745fe92fd93196e3d2e0
irenad88 <irenad88@gmail.com>
2018-05-15 13:22:33 +0300

    Created a question template for Artem

---
+++ Cloudents.Web/ClientApp/components/question/question.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        Your html goes here - make sure to keep only one element insid template tag - otherwise vue wont work...
+    </div>
+</template>
+
+<style src="./question.less" lang="less"></style>
+<script src="./question.js"></script>
