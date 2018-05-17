b9e7a96a66dc7a88a564cb5c7714619b11f5b45a
irenad88 <irenad88@gmail.com>
2018-05-17 17:36:20 +0300

    Started verticals redesign implementation

--- Cloudents.Web/ClientApp/routeTypes.js
+++ Cloudents.Web/ClientApp/routeTypes.js
@@ -1,4 +1,5 @@
 ﻿//note if you change the value in here you need to chanem in main.less
+export const marketRoute = "marketplace";
 export const questionRoute = "ask";
 export const flashcardRoute = "flashcard";
 export const notesRoute = "note";
