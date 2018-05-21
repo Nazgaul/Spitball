6460b788711596cb70a3ab9bc4317e8e54b2d583
mynko.a <mynko.a@dbbest.com>
2018-05-21 15:00:31 +0300

    Finish Profile page

--- Cloudents.Web/ClientApp/components/profile/helpers/market-item/market-item.vue
+++ Cloudents.Web/ClientApp/components/profile/helpers/market-item/market-item.vue
@@ -6,15 +6,16 @@
             :style="{background: `url(${item.image})`}"
             class="market-item" :class="classItem" >
         
-            
-            <div class="price">
-                <span>$39</span>
-            </div>   
+            <div class="header">
+                 <div class="price">
+                    <span>$39</span>
+                </div>   
 
-            <div class="votes">
-                <v-icon  class="upvotes">sbf-chevron-down</v-icon>
-                <span>3 Upvotes</span>
-            </div>   
+                <div class="votes">
+                    <v-icon  class="upvotes">sbf-chevron-down</v-icon>
+                    <span>3 Upvotes</span>
+                </div>   
+            </div>           
             
             <div class="item-info">
                 <span>Sony Xperia</span>
