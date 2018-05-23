5514f4a5672e540549be0a1935ac14e59be5cc87
irenad88 <irenad88@gmail.com>
2018-05-23 09:27:49 +0300

    started file upload

--- Cloudents.Web/ClientApp/components/question/helpers/question-text-area/questionTextArea.js
+++ Cloudents.Web/ClientApp/components/question/helpers/question-text-area/questionTextArea.js
@@ -1,3 +1,5 @@
+import questionService from "../../../../services/questionService";
+
 var Uploader = require('html5-uploader');
 
 export default {
@@ -5,6 +7,7 @@ export default {
     data() {
         return {
             files: [],
+            filesFromServer: [],
             previewList: []
         }
     },
@@ -22,13 +25,58 @@ export default {
         var $preview = this.$el.querySelector('#input-multiple-preview');
 
 
+
         multiple.on('files:added', function (uploadedFiles) {
             for (var file of uploadedFiles) {
+
                 // TODO: check for duplications fast-deep-equal
                 if (self.files.indexOf(file) === -1) {
                     self.files.push(file);
+                    var formData = new FormData();
+                    formData.append(file.name || 'filename', file);
+                    debugger;
+                    questionService.uploadFile(formData)
+                        .then(function (response) {
+                            debugger;
+                            self.filesFromServer.push(response.fileName);
+                        });
                 }
             }
+
+
+
+
+
+            // function handleFileSelect(evt) {
+                var files = uploadedFiles; // FileList object
+
+                // Loop through the FileList and render image files as thumbnails.
+                for (var i = 0, f; f = files[i]; i++) {
+
+                    // Only process image files.
+                    if (!f.type.match('image.*')) {
+                        continue;
+                    }
+
+                    var reader = new FileReader();
+
+                    // Closure to capture the file information.
+                    reader.onload = (function(theFile) {
+                        return function(e) {
+                            // Render thumbnail.
+                            var span = document.createElement('span');
+                            span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                                '" title="', escape(theFile.name), '"/>'].join('');
+                            document.getElementById('list').insertBefore(span, null);
+                        };
+                    })(f);
+
+                    // Read in the image file as a data URL.
+                    reader.readAsDataURL(f);
+                }
+            // }
+
+
         });
         multiple.on('file:preview', function (file, $img) {
             if ($img) {
