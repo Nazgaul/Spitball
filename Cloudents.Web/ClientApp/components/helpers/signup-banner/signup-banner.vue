0ca882b735d61880fc2c9051508e9bc72033cd09
mynko.a <mynko.a@dbbest.com>
2018-05-30 18:16:49 +0300

    Added sign up popup (banner)

---
+++ Cloudents.Web/ClientApp/components/helpers/signup-banner/signup-banner.vue
@@ -0,0 +1,28 @@
+<template>
+   <v-flex xs12 class="signup-banner">
+        <v-flex xs12>
+            <button class="back-button">
+                <v-icon right>sbf-close</v-icon>
+            </button>
+        </v-flex>
+        <v-flex xs12 class="text-block">
+            <p class="medium">Don’t miss the</p>
+            <p class="large">Revolution</p>
+            <p class="medium text-right">of the student economy</p>
+            <p class="small">Join a community where you pay less and earn more for everything school-related</p>
+
+            <div class="buttons">
+                <button class="sign-up">Sign Up</button>
+                <button class="learn-more">Learn More</button>
+            </div>
+            
+        </v-flex>
+    </v-flex>
+</template>
+
+
+<script>
+    export default { }
+</script>
+
+<style src="./signup-banner.less" lang="less"></style>
