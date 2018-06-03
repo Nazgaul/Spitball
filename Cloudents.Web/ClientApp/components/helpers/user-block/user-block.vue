4c2b52c1e65d3a421c29019499e3637ee8793454
irenad88 <irenad88@gmail.com>
2018-06-03 10:43:36 +0300

    #155

--- Cloudents.Web/ClientApp/components/helpers/user-block/user-block.vue
+++ Cloudents.Web/ClientApp/components/helpers/user-block/user-block.vue
@@ -2,7 +2,7 @@
          <div class="user-block" :class="classType">
             <img  class="avatar" :src="user.img" />
             <div class="text">
-                <span class="user-date"><a href="/user">{{user.name}}</a> · 1 hour ago </span>
+                <span class="user-date"><router-link :to="{path:'/profile/'+user.id}">{{user.name}}</router-link> · 1 hour ago </span>
                 <span class="name">{{user.name}}</span>
                 <p class="last-msg" v-if="text">{{text}}</p>
                 <p class="university" v-if="user.universityName">{{user.universityName}}</p>
