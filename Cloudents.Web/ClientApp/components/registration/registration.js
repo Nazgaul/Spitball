4455519a03c5c91daea46c7ce2b006b3abfe1f32
irenad88 <irenad88@gmail.com>
2018-06-06 10:24:29 +0300

    Changes to allow Artem to work easily on reg design - to be reverted once he's done.

--- Cloudents.Web/ClientApp/components/registration/registration.js
+++ Cloudents.Web/ClientApp/components/registration/registration.js
@@ -41,6 +41,6 @@ export default {
         }
     },
     created() {
-        this.step = this.$route.meta.step || this.getRegistrationStep;
+        this.step = 0;//this.$route.meta.step || this.getRegistrationStep; TODO: revert after design changes are implemented
     }
 }
