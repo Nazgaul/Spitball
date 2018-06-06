025c8c1541c12567b6c829148effd49492cd7ad2
Ram <ram.y@outlook.com>
2018-06-06 11:19:07 +0300

    compile fix.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -75,7 +75,6 @@ namespace ConsoleApp
            
             //var c = a.TransferMoneyAsync("10f158cd550649e9f99e48a9c7e2547b65f101a2f928c3e0172e425067e51bb4", "0xAcfB119204a93BbDa781C972D27AeAB8671c63f4", 10, default);
             //var b = a.GetBalanceAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", default);
-            }, default);
 
             //var c = a.TransferMoneyAsync("10f158cd550649e9f99e48a9c7e2547b65f101a2f928c3e0172e425067e51bb4", "0xAcfB119204a93BbDa781C972D27AeAB8671c63f4", 10, default);
             //var b = a.GetBalanceAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", default);
