3f7d8b18f3f238c31aedf0b2d9dd7aa65191ee82
irenad88 <irenad88@gmail.com>
2018-06-07 14:32:57 +0300

    reverted verticals design to old design

--- Cloudents.Web/ClientApp/components/header/verticalsTabs.vue
+++ Cloudents.Web/ClientApp/components/header/verticalsTabs.vue
@@ -1,17 +1,14 @@
 <template>
     <v-flex class="line verticals">
         <v-layout row>
-            <!--<div class="gap ma-0" v-if="$vuetify.breakpoint.mdAndUp"></div>-->
-            <v-tabs grow class="verticals-bar" :value="currentSelection" :scrollable="false">
+            <div class="gap ma-0" v-if="$vuetify.breakpoint.mdAndUp"></div>
+            <v-tabs class="verticals-bar" :value="currentSelection" :scrollable="false">
                 <v-tabs-bar>
                     <v-tabs-item v-for="tab in verticals" :key="tab.id" :href="tab.id" :id="tab.id"
                                  @click="$_updateType(tab.id)"
-                                 :class="[tab.id===currentSelection?'tabs__item--active':'']"
+                                 :class="['spitball-text-'+tab.id,tab.id===currentSelection?'tabs__item--active':'']"
                                  class="mr-4 vertical">
-                        <span class="vertical-title" :class="'spitball-bg-'+tab.id">
-                            <v-icon>{{tab.icon}}</v-icon>
-                            <span>{{tab.name}}</span>
-                        </span>
+                        {{tab.name}}
                     </v-tabs-item>
                     <v-tabs-slider :color="`color-${currentVertical}`"></v-tabs-slider>
                 </v-tabs-bar>
