8ec581ab6a908e8972895840e3ac265947baf7d4
mynko.a <mynko.a@dbbest.com>
2018-06-11 18:45:56 +0300

    Redesign preview block of attached files

--- Cloudents.Web/ClientApp/components/question/helpers/extended-text-area/extendedTextArea.vue
+++ Cloudents.Web/ClientApp/components/question/helpers/extended-text-area/extendedTextArea.vue
@@ -1,7 +1,8 @@
 <template>
         <v-flex xs12 class="question_area">
             <div class="textarea">
-                <div class="text-block" :class="{'with-preview':previewList.length}" v-if="!fullPreview">
+                <!-- :class="{'with-preview':previewList.length}" v-if="!fullPreview" -->
+                <div class="text-block" >
                     <textarea rows="9" @input="updateValue($event.target.value)" :value="value" autofocus="isFocused"
                         placeholder="Type in your question (Ex: Find the derivative of y = (x + 2)2 - 5x3)"></textarea>
                     <ul class="actions_text">
@@ -34,20 +35,25 @@
                         </li>
                     </ul>
                 </div>            
-                <ul class="preview-list" :class="{'full-preview':fullPreview}" v-if="previewList.length">
-                    <div class="show-more" v-if="previewList.length>1 && !fullPreview" @click="togglePreview">+ {{previewList.length-1}} more</div> 
-                    <!-- v-html="image"  -->
-                    <li v-if="previewList.length" v-for="(image,index) in previewList" :class="{'use-hover':previewList.length==1 || fullPreview}">
+                
+                <!-- <span class="close-btn" v-if="fullPreview" @click="togglePreview">
+                    <v-icon right>sbf-close</v-icon>
+                </span> -->
+            </div>
+            <div class="files">
+                <ul class="preview-list" v-if="previewList.length">
+                    <li v-if="previewList.length" v-for="(image,index) in previewList">
                         <button class="hover-trash" @click="deletePreview(index)">
-                            <v-icon>sbf-trash</v-icon>
+                            <v-icon>sbf-close</v-icon>
                         </button>
-                        <div class="background"></div>
                         <img :src="image" />
                     </li>
+                    <li class="add-file">
+                        <label for="file-input">
+                                <v-icon>sbf-close</v-icon>
+                        </label>
+                    </li>
                 </ul>
-                <span class="close-btn" v-if="fullPreview" @click="togglePreview">
-                    <v-icon right>sbf-close</v-icon>
-                </span>
             </div>
         </v-flex>
 </template>
