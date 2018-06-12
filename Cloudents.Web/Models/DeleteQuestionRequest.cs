2ad07304d7c040baec3b302fcb75eab5e74dde9a
Ram <ram.y@outlook.com>
2018-06-12 11:16:18 +0300

    #177 delete question implementation

---
+++ Cloudents.Web/Models/DeleteQuestionRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cloudents.Web.Models
+{
+    public class DeleteQuestionRequest
+    {
+        [FromRoute]
+        [Range(1, long.MaxValue)]
+        public long Id { get; set; }
+    }
+}
