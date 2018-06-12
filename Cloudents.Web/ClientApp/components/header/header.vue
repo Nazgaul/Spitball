eddc1902b1aaa56e1d1c900e4b2d9a01b32e70cd
irenad88 <irenad88@gmail.com>
2018-06-12 13:05:35 +0300

    small fix in header size

--- Cloudents.Web/ClientApp/components/header/header.vue
+++ Cloudents.Web/ClientApp/components/header/header.vue
@@ -1,7 +1,7 @@
 ﻿<template>
         <main-header ref="mainHeader" :userText="userText" 
                      :submitRoute="submitRoute" :currentSelection="currentSelection"
-                     :toolbarHeight="$vuetify.breakpoint.mdAndUp ? 104 : 152">
+                     :toolbarHeight="$vuetify.breakpoint.mdAndUp ? 104 : 144">
             <verticals-tabs :currentSelection="currentSelection" slot="extraHeader"></verticals-tabs>
         </main-header>
 </template>
