f3538bc7efd30451b1e1979b557520a8f22e6981
yifatBi <yifatbij@gmail.com>
2018-06-12 13:14:27 +0300

    implement gallery in question

--- Cloudents.Web/ClientApp/components/question/helpers/question-card/question-card.vue
+++ Cloudents.Web/ClientApp/components/question/helpers/question-card/question-card.vue
@@ -29,7 +29,7 @@
         <p class="q-text" :class="{'answer': typeAnswer}">{{cardData.text}}</p>
 
         <!-- v-if="cardData.files.length" -->
-        <div class="gallery" >
+        <div class="gallery" v-if="gallery&&gallery.length">
             <v-carousel left-control-icon="sbf-arrow-right left" right-control-icon="sbf-arrow-right" interval="600000" hide-delimiters>
                 <v-carousel-item v-for="(item,i) in gallery" v-bind:src="item" :key="i"></v-carousel-item>
             </v-carousel>
