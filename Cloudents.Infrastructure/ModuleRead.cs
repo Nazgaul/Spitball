8577e6a1725c813168a531d508f6dc69c104d80d
Ram <ram.y@outlook.com>
2018-06-14 12:16:11 +0300

    Remove of luis

--- Cloudents.Infrastructure/ModuleRead.cs
+++ Cloudents.Infrastructure/ModuleRead.cs
@@ -6,7 +6,6 @@ using AutoMapper;
 using Cloudents.Core.Attributes;
 using Cloudents.Core.Interfaces;
 using Cloudents.Core.Query;
-using Cloudents.Infrastructure.AI;
 using Cloudents.Infrastructure.Auth;
 using Cloudents.Infrastructure.Data;
 using Cloudents.Infrastructure.Domain;
@@ -17,7 +16,6 @@ using Cloudents.Infrastructure.Search.Job;
 using Cloudents.Infrastructure.Search.Tutor;
 using JetBrains.Annotations;
 using Microsoft.Azure.Search;
-using Microsoft.Cognitive.LUIS;
 using BingSearch = Cloudents.Infrastructure.Search.BingSearch;
 using ICacheProvider = Nager.PublicSuffix.ICacheProvider;
 using Module = Autofac.Module;
@@ -33,16 +31,6 @@ namespace Cloudents.Infrastructure
         protected override void Load(ContainerBuilder builder)
         {
             var currentAssembly = Assembly.GetExecutingAssembly();
-            //builder.RegisterModule<ModuleInfrastructureBase>();
-            //builder.RegisterModule(new ModuleInfrastructureBase(_searchServiceCredentials, _redisConnectionString));
-
-            builder.RegisterType<LuisAI>().As<IAi>()
-                .EnableInterfaceInterceptors()
-                .InterceptedBy(typeof(CacheResultInterceptor));
-            builder.RegisterType<AiDecision>().As<IDecision>();
-            builder.RegisterType<EngineProcess>().As<IEngineProcess>();
-
-            builder.Register(_ => new LuisClient("a1a0245f-4cb3-42d6-8bb2-62b6cfe7d5a3", "6effb3962e284a9ba73dfb57fa1cfe40")).AsImplementedInterfaces();
 
             builder.RegisterType<DocumentDbRepositoryUnitOfWork>().AsSelf().As<IStartable>().SingleInstance().AutoActivate();
             builder.RegisterGeneric(typeof(DocumentDbRepository<>)).AsImplementedInterfaces();
