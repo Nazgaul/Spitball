1b3b82435d63bb0990f59bd38f3ad4453abab497
Ram <ram.y@outlook.com>
2018-06-17 13:05:18 +0300

    Answer my profile page.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -38,12 +38,13 @@ namespace ConsoleApp
 {
     static class Program
     {
+        private static IContainer container;
         static async Task Main()
         {
             var builder = new ContainerBuilder();
             var keys = new ConfigurationKeys
             {
-                Db = ConfigurationManager.ConnectionStrings["ZBoxProd"].ConnectionString,
+                Db = ConfigurationManager.ConnectionStrings["ZBox"].ConnectionString,
                 MailGunDb = ConfigurationManager.ConnectionStrings["MailGun"].ConnectionString,
                 Search = new SearchServiceCredentials(
 
@@ -53,7 +54,7 @@ namespace ConsoleApp
                 Storage = ConfigurationManager.AppSettings["StorageConnectionString"],
                 LocalStorageData = new LocalStorageData(AppDomain.CurrentDomain.BaseDirectory, 200),
                 BlockChainNetwork = "http://spito5-dns-reg1.northeurope.cloudapp.azure.com:8545",
-                ServiceBus = ConfigurationManager.AppSettings["ServiceBusProd"]
+                ServiceBus = ConfigurationManager.AppSettings["ServiceBus"]
             };
 
             builder.Register(_ => keys).As<IConfigurationKeys>();
@@ -65,84 +66,30 @@ namespace ConsoleApp
                 Assembly.Load("Cloudents.Infrastructure.Data"),
                 Assembly.Load("Cloudents.Core"));
             //builder.RegisterType<TutorMeSearch>().AsSelf();
-            var container = builder.Build();
+            container = builder.Build();
 
-           
+            //await SendMoneyAsync();
 
-            //topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+            var t = container.Resolve<IUserRepository>();
+            var z = await t.GetUserProfileAsync(551, default);
 
-            //var x = new ServiceClientCredentials()
-            //var t = new ServiceBusManagementClient()
-
-            
-            // await SendMessagesAsync(10);
-
-            
-           // var z = ty.Split(new[] {'.', '@'}, StringSplitOptions.RemoveEmptyEntries)[0];
-            var c = container.Resolve<IServiceBusProvider>();
-            var a = container.Resolve<IUserRepository>();
-
-            foreach (var user in await a.GetQueryable().ToListAsync())
-            {
-                //if (user.EmailConfirmed)
-                //{
-                    var x = new TalkJsUser(user.Id, user.Name)
-                    {
-                        Email = user.Email
-                    };
-                    await c.InsertMessageAsync(x, default);
-                //}
-
-            }
-            //var t = await a.GetQuestionsAsync(new QuestionsQuery()
+            //var t = container.Resolve<IQuestionRepository>();
+            //var z = await t.GetQuestionsAsync(new QuestionsQuery()
             //{
-            //    Term = "files"
+            //    Term = "Irena's question"
             //}, default);
-            
-
-            //var t = await a.GetQuestionDtoAsync(704, default);
-            
-            //var registerEmail = new RegistrationEmail("ram@cloudents.com","https://dev.spitball.co");
-            //await a.InsertMessageAsync(registerEmail, default);
-
-           // var z = a.GetAddress("2ee004d7bdc744205d8a3d31a1b2cd4816b8f860448427b570290c9a1cb571d6");
-            //await a.SetInitialBalanceAsync(z, default).ConfigureAwait(false);
-            //var c = a.TransferMoneyAsync("10f158cd550649e9f99e48a9c7e2547b65f101a2f928c3e0172e425067e51bb4", "0xAcfB119204a93BbDa781C972D27AeAB8671c63f4", 10, default);
-            //var b = a.GetBalanceAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", default);
-
-            //var c = a.TransferMoneyAsync("10f158cd550649e9f99e48a9c7e2547b65f101a2f928c3e0172e425067e51bb4", "0xAcfB119204a93BbDa781C972D27AeAB8671c63f4", 10, default);
-            //var b = a.GetBalanceAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", default);
-            //var b = await a.CreateNewTokens("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", 10, default);
-            ////var (privateKey, publicAddress) = a.CreateAccount();
-            //var b = a.GetBalanceAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", default);
-
-            //var a = container.Resolve<IBlockChainCrowdSaleService>();
-            //var b = await a.BuyTokensAsync("10f158cd550649e9f99e48a9c7e2547b65f101a2f928c3e0172e425067e51bb4", 10, default);
-            //await a.Withdrawal(default);
-
-            //Guid answer = Guid.NewGuid();
-            // var c = container.Resolve<IBlockChainQAndAContract>();
-            ////var command = new CreateQuestionCommand()
-            ////{
-            ////    Price = 0.5m,
-            ////    SubjectId = 1,
-            ////    Text = "123",
-            ////    UserId = 11
-            ////};
-            ////await c.DispatchAsync(command, default);
-
-            //await c.SubmitQuestionAsync(3, 1, "0x116CC5B77f994A4D375791A99DF12f19921138ea", default);
-            //await c.SubmitAnswerAsync(3, answer, default);
-
-            //await c.UpVoteAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", 3, answer, default);
-            //await c.MarkAsCorrectAsync("0x27e739f9dF8135fD1946b0b5584BcE49E22000af", "0x27e739f9dF8135fD1946b0b5584BcE49E22000af", 3, answer, default);
-            // var h = await c.UpVoteListAsync(3, answer, default);
-            // Console.WriteLine(h[0]);
-            //Guid.Parse("078d1202-834a-4634-9aec-1bdf1127368c")
 
             Console.WriteLine("Finish");
             Console.ReadLine();
             //await topicClient.CloseAsync();
+        }
+
+        public static Task SendMoneyAsync()
+        {
+            var t = container.Resolve<IBlockChainErc20Service>();
+            var pb = t.GetAddress("38d68c294410244dcd009346c756436a64530d7ddb0611e62fa79f9f721cebb0");
+            return t.SetInitialBalanceAsync(pb, default);
+            //
         }
 
     }
