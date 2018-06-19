fab9326cf967ee66681b6093898994c6f4c4e769
yifatBi <yifatbij@gmail.com>
2018-06-19 13:24:31 +0300

    case #10304

--- Cloudents.Web/ClientApp/components/satellite/header.vue
+++ Cloudents.Web/ClientApp/components/satellite/header.vue
@@ -1,15 +1,12 @@
 ﻿<template>
     <main-header ref="mainHeader"
                  :toolbarHeight="$vuetify.breakpoint.smAndUp ? 108 : 104">
-        <!--<v-flex slot="extraHeader" fill-height align-end d-flex class="hidden-sm-and-down">-->
-            <!--<div  class="gap"></div>-->
-        <v-flex class="line verticals" slot="extraHeader" v-if="$vuetify.breakpoint.mdAndUp">
+        <v-flex class="line" slot="extraHeader" v-if="$vuetify.breakpoint.mdAndUp">
             <v-layout row>
-                <!--<div class="gap ma-0" v-if="$vuetify.breakpoint.mdAndUp"></div>-->
-            <v-tabs class="verticals-bar" :scrollable="false" centered>
+            <v-tabs  :scrollable="false" centered>
                 <v-tabs-bar>
                     <v-tabs-item router v-for="tab in verticals" :key="tab.name" :to="tab.name"
-                                 class="mr-1 vertical">
+                                 class="vertical">
                         {{tab.display}}
                     </v-tabs-item>
                     <v-tabs-slider color="color-dark-blue"></v-tabs-slider>
