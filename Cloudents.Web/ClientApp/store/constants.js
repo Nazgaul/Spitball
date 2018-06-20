3fcf0673ad3440a865af498817f6fe23b0b167fd
yifatBi <yifatbij@gmail.com>
2018-06-20 12:55:52 +0300

    case #10303

--- Cloudents.Web/ClientApp/store/constants.js
+++ Cloudents.Web/ClientApp/store/constants.js
@@ -1,3 +1,4 @@
 export const MAX_HISTORY_LENGTH = 8;
 export const MAX_VERTICAL_HISTORY_LENGTH = 3;
 export const REGISTRATION_STEPS = ['email', 'phone', 'account'];
+export const dollarCalculate=(balance)=>balance/40;
