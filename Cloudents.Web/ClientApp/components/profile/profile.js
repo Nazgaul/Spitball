5c17cd0375427937dd39b6481c4b9ac3bef54ad1
irenad88 <irenad88@gmail.com>
2018-06-21 15:01:44 +0300

    Added profile empty states

--- Cloudents.Web/ClientApp/components/profile/profile.js
+++ Cloudents.Web/ClientApp/components/profile/profile.js
@@ -6,13 +6,23 @@ import {mapGetters} from 'vuex'
 
 export default {
     components: {questionCard, userBlock},
-    props:{
-        id:{Number}
+    props: {
+        id: {Number}
     },
     data() {
         return {
-            activeTab: 1,            
-            profileData: null
+            activeTab: 1,
+            profileData: null,
+            emptyState: {
+                questions: {
+                    text: 'You have a homework problem?',
+                    btnText: 'Put it on sale…'
+                },
+                answers: {
+                    text: 'You have a homework problem?',
+                    btnText: 'Put it on sale…'
+                }
+            }
         }
     },
     methods: {
@@ -25,14 +35,46 @@ export default {
         isMobile() {
             return this.$vuetify.breakpoint.xsOnly;
         },
-        myAnswers(){
-            return this.profileData.answer?this.profileData.answer.map(i=> {return {...i, user:this.profileData.user,answersNum:i.answers,filesNum:i.files,price:accountService.calculateDollar(i.price)}}):[]
+        myAnswers() {
+            return this.profileData.answer ? this.profileData.answer.map(i => {
+                return {
+                    ...i,
+                    user: this.profileData.user,
+                    answersNum: i.answers,
+                    filesNum: i.files,
+                    price: accountService.calculateDollar(i.price)
+                }
+            }) : []
         },
-        questions(){
-            return this.profileData.ask?this.profileData.ask.map(i=> {return {...i, user:this.profileData.user,answersNum:i.answers,filesNum:i.files,price:accountService.calculateDollar(i.price)}}):[]
+        questions() {
+            return this.profileData.ask ? this.profileData.ask.map(i => {
+                return {
+                    ...i,
+                    user: this.profileData.user,
+                    answersNum: i.answers,
+                    filesNum: i.files,
+                    price: accountService.calculateDollar(i.price)
+                }
+            }) : []
         },
         isMyProfile() {
             return this.accountUser && this.accountUser.id && this.profileData ? this.profileData.user.id == this.accountUser.id : false;
+        },
+        emptyStateData() {
+            var questions = {
+                text: 'You have<br/>a homework problem?',
+                boldText: 'Put it on sale…',
+                btnText: 'Ask Your Question',
+                btnUrl: 'newQuestion'
+            };
+            var answers = {
+                text: 'Help your friends,<br/>answer on their questions',
+                boldText: 'and make some money',
+                btnText: 'Answer',
+                btnUrl: 'home'
+            };
+            return this.activeTab === 1 ? questions : answers;
+
         }
     },
     created() {
