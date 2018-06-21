5c50c3a90e535593f43d0ad4871fdcb2230a2de7
yifatBi <yifatbij@gmail.com>
2018-06-21 09:41:22 +0300

    case #10323

--- Cloudents.Web/ClientApp/services/accountService.js
+++ Cloudents.Web/ClientApp/services/accountService.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import {dollarCalculate} from "../store/constants";
 
 export default {
     getAccount:() => axios.get("/Account"),
@@ -8,5 +7,4 @@ export default {
     setUniversity: (universityId) => axios.post("/Account/university", {universityId}),
     getProfile:(id) => axios.get("/Profile/" + id),
     logout: () => axios.post("/Account/logout"),
-    calculateDollar:(balance)=>dollarCalculate(balance).toFixed(2)
 }
