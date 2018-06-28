9998b4bcc7585b7bf665ac41eff8fec88e3e7208
yifatBi <yifatbij@gmail.com>
2018-06-28 16:23:07 +0300

    suggestQuestion

--- Cloudents.Web/ClientApp/components/questionsSuggestPopUp/questionSuggestPopUp.vue
+++ Cloudents.Web/ClientApp/components/questionsSuggestPopUp/questionSuggestPopUp.vue
@@ -2,14 +2,19 @@
     <div>
         <h2>Great Job!</h2>
         <p>
-            Let’s wait for eidan2134 to see if accepts your answer.
-            In the meantime, below is another question that you should be able to with.
+            Let’s wait for eidan2134 to see if accepts your answer. In the meantime, below is another question that you should be able
+            to with.
         </p>
-        <question-card :cardData="cardList[0]" class="user-question mb-3" detailed-view></question-card>
+        <v-carousel>
+            <v-carousel-item v-for="(item,i) in cardList" :src="item.subject" :key="i">
+                <question-card :cardData="cardList[0]" class="user-question mb-3" detailed-view></question-card>
+            </v-carousel-item>
+        </v-carousel>
+        <!-- <question-card :cardData="cardList[0]" class="user-question mb-3" detailed-view></question-card> -->
 
     </div>
 </template>
 
-<script src="questionSuggestPopUp.js"></script>
+<script src="./questionSuggestPopUp.js"></script>
 
 <style src="./questionSuggestPopUp.less" lang="less"></style>
