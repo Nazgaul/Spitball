f5e67882451b75eb815ca8b1bd03a7da757146bf
irenad88 <irenad88@gmail.com>
2018-06-28 14:53:16 +0300

    Question suggest popup (-Beny)

---
+++ Cloudents.Web/ClientApp/components/questionsSuggestPopUp/questionSuggestPopUp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Great Job!</h2>
+        <p>
+            Let’s wait for eidan2134 to see if accepts your answer.
+            In the meantime, below is another question that you should be able to with.
+        </p>
+        <question-card :cardData="cardList[0]" class="user-question mb-3" detailed-view></question-card>
+
+    </div>
+</template>
+
+<script src="questionSuggestPopUp.js"></script>
+
+<style src="./questionSuggestPopUp.less" lang="less"></style>
