f3f314405c3a36ba60dc17769028f0d0e4f74940
Ram <ram.y@outlook.com>
2018-07-08 12:14:47 +0300

    case 10476 - starting to bring back the create course.

---
+++ Cloudents.Core/Command/CreateCourseCommand.cs
@@ -0,0 +1,21 @@
+﻿
+using Cloudents.Core.Interfaces;
+
+namespace Cloudents.Core.Command
+{
+    public class CreateCourseCommand : ICommand
+    {
+        public CreateCourseCommand(string name, long universityId)
+        {
+            Name = name;
+            UniversityId = universityId;
+        }
+
+
+        public string Name { get; }
+
+        public long UniversityId { get; }
+
+        public long Id { get; set; }
+    }
+}
