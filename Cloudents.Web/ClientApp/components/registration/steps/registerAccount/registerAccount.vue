35a90c7e88ca04a0671a3a2ae13538bbc3fd2bad
crow7m <crow7m@gmail.com>
2018-07-12 18:06:48 +0300

    congrats

--- Cloudents.Web/ClientApp/components/registration/steps/registerAccount/registerAccount.vue
+++ Cloudents.Web/ClientApp/components/registration/steps/registerAccount/registerAccount.vue
@@ -3,8 +3,8 @@
         <step-template>
             <div slot="step-data" class="limited-width done">
                 <h1>CONGRATS!</h1>
-                <p>You were just rewarded your first {{initialPointsNum}} SBL.</p>
-                <p>You can spend them to get help with your <br/> Homework questions.</p>
+                <p class="congrats">You were just rewarded your first <br/><span class="blue-points">{{initialPointsNum}} SBL.</span></p>
+                <p class="congrats">You can spend them to get help with your <br/> Homework questions.</p>
                 <button class="continue-btn" @click="finishRegistration">Let's Start</button>
             </div>
             <img slot="step-image" :src="require(`../../img/done.png`)"/>
