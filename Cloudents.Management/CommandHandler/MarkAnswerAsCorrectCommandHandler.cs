ef7fd4829f7a163de01ca0dca42436431d056ac2
Ram <ram.y@outlook.com>
2018-07-22 11:25:26 +0300

    Some fixes.

--- Cloudents.Management/CommandHandler/MarkAnswerAsCorrectCommandHandler.cs
+++ Cloudents.Management/CommandHandler/MarkAnswerAsCorrectCommandHandler.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using Cloudents.Core;
 using Cloudents.Core.Entities.Db;
 using Cloudents.Core.Interfaces;
-using Cloudents.Core.Message;
-using Cloudents.Core.Storage;
 using Cloudents.Management.Command;
 using JetBrains.Annotations;
 
@@ -15,17 +11,13 @@ namespace Cloudents.Management.CommandHandler
     public class MarkAnswerAsCorrectCommandHandler : ICommandHandler<MarkAnswerAsCorrectCommand>
     {
         private readonly IRepository<Question> _questionRepository;
-        private readonly IRepository<Answer> _answerRepository;
-        private readonly IServiceBusProvider _serviceBusProvider;
 
-        private readonly ICommandHandler<Cloudents.Core.Command.MarkAnswerAsCorrectCommand> _commandHandler;
+        private readonly ICommandHandler<Core.Command.MarkAnswerAsCorrectCommand> _commandHandler;
 
         public MarkAnswerAsCorrectCommandHandler(IRepository<Question> questionRepository,
-            IRepository<Answer> answerRepository, IServiceBusProvider serviceBusProvider, ICommandHandler<Core.Command.MarkAnswerAsCorrectCommand> commandHandler)
+            ICommandHandler<Core.Command.MarkAnswerAsCorrectCommand> commandHandler)
         {
             _questionRepository = questionRepository;
-            _answerRepository = answerRepository;
-            _serviceBusProvider = serviceBusProvider;
             _commandHandler = commandHandler;
         }
 
