f5f2c32064496ef866e79fc1df1fa52bb856dcf3
Ram <ram.y@outlook.com>
2018-07-30 14:03:01 +0300

    pop of answer always have questions.

--- Cloudents.Infrastructure.Data/Query/NextQuestionsQueryHandler.cs
+++ Cloudents.Infrastructure.Data/Query/NextQuestionsQueryHandler.cs
@@ -28,6 +28,13 @@ namespace Cloudents.Infrastructure.Data.Query
             Question questionAlias = null;
             User userAlias = null;
 
+            var detachSubQuery = DetachedCriteria.For<Question>();
+
+            var detachedQuery = QueryOver.Of<Question>()
+                .Select(s => s.Subject.Id)
+                .Where(w => w.Id == query.QuestionId)
+                .Take(1);
+
             return await _session.QueryOver(() => questionAlias)
                 .JoinAlias(x => x.Subject, () => commentAlias)
                 .JoinAlias(x => x.User, () => userAlias)
@@ -48,13 +55,40 @@ namespace Cloudents.Infrastructure.Data.Query
                 .Where(w => w.CorrectAnswer == null)
                 .Where(w => w.User.Id != query.UserId)
                 .Where(w => w.Id != query.QuestionId)
-                .WithSubquery.Where(p => p.Subject.Id == QueryOver.Of<Question>()
-                                             .Select(s => s.Subject.Id)
-                                             .Where(w => w.Id == query.QuestionId)
-                                             .Take(1).As<int>())
+                //.WithSubquery.Where(p => p.Subject.Id == QueryOver.Of<Question>()
+                //                             .Select(s => s.Subject.Id)
 
+                //                             .Where(w => w.Id == query.QuestionId)
+                //                             .Take(1).As<int>())
                 .TransformUsing(new DeepTransformer<QuestionDto>())
-                .OrderByRandom()
+                .OrderBy(Projections.Conditional(
+                    
+                    Subqueries.PropertyEq(nameof(Question.Subject), detachedQuery.DetachedCriteria)
+                    
+
+                    //Restrictions.on
+                    //Subqueries.PropertyEq("", detachSubQuery)
+                    //Restrictions.Where(()=> commentAlias.Id == detachedQuery.As<int>())
+                    //Restrictions.EqProperty(() => questionAlias.Subject.Id, QueryOver.Of<Question>()
+                    //                                 .Select(s => s.Subject.Id)
+
+                    //                                 .Where(w => w.Id == query.QuestionId)
+                    //                                 .Take(1).DetachedCriteria())
+                    // Restrictions.Where(Subqueries.PropertyEq("xxx", detachSubQuery)))
+
+                    //Restrictions.Where(() => questionAlias.Subject.Id == 
+                    //                         QueryOver.Of<Question>()
+                    //                             .Select(s => s.Subject.Id)
+
+                    //                             .Where(w => w.Id == query.QuestionId)
+                    //                             .Take(1).As<int>()
+                    //                         )
+                    , Projections.Constant(0), Projections.Constant(1)
+                    )).Asc
+                
+                .ThenBy(Projections.SqlFunction(SbDialect.RandomOrder, NHibernateUtil.Guid)).Asc
+               
+                //.OrderByRandom()
                 .Take(3).ListAsync<QuestionDto>(token).ConfigureAwait(false);
         }
     }
--- Cloudents.Infrastructure.Data/SbDialect.cs
+++ Cloudents.Infrastructure.Data/SbDialect.cs
@@ -1,16 +1,30 @@
-﻿using NHibernate.Dialect;
+﻿using NHibernate;
+using NHibernate.Dialect;
 using NHibernate.Dialect.Function;
 
 namespace Cloudents.Infrastructure.Data
 {
     public class SbDialect : MsSql2012Dialect
     {
+        internal const string RandomOrder = "random_Order";
+
         protected override void RegisterFunctions()
         {
             base.RegisterFunctions();
             RegisterFunction("FullTextContains", new StandardSQLFunction("contains", null));
+
+            RegisterFunction(RandomOrder, new StandardSQLFunction("NEWID", NHibernateUtil.Guid));
             //RegisterFunction("NEWID()", new StandardSQLFunction("NEWID()", NHibernateUtil.Guid));
 
+        }
+    }
+
+    public class MySqliteDialect : SQLiteDialect
+    {
+        protected override void RegisterFunctions()
+        {
+            base.RegisterFunctions();
+            RegisterFunction(SbDialect.RandomOrder, new StandardSQLFunction("random", NHibernateUtil.Guid));
         }
     }
 
