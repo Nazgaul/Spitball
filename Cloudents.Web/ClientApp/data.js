f7909cc067e9c54d8efbeafb8aa1095e961dcc95
crow7m <crow7m@gmail.com>
2018-08-01 15:40:48 +0300

    verticals moved to nav.js from data

--- Cloudents.Web/ClientApp/data.js
+++ Cloudents.Web/ClientApp/data.js
@@ -1,97 +1,25 @@
-﻿import * as routes from "./routeTypes";
-
-export let verticals =
-    {
-
-        // marketplace: {
-        //     id: routes.marketRoute,
-        //     name: "Marketplace",
-        //     icon: "sbf-marketplace"
-        //
-        // },
-        ask: {
-            filter:[{ id: "source", name: "subject" }],
-            id: routes.questionRoute,
-            name: "Homework Help",
-            icon: "sbf-ask-q" //TODO do we need this.....
-        },
-        note: {
-            id: routes.notesRoute,
-            name: "Study Documents",
-            needLocation:false,
-            filter: [{ id: "course", name: "My Courses" }, { id: "source", name: "sources" }],
-            sort: [
-                { id: "relevance", name: "relevance" },
-                { id: "date", name: "date" }
-            ],
-            icon: "sbf-note"
-        },
-        flashcard: {
-            id: routes.flashcardRoute,
-            name: "Flashcards",
-            needLocation: false,
-            filter: [
-                { id: "course", name: "My Courses" },
-                { id: "source", name: "sources" }
-            ],
-            sort: [
-                { id: "relevance", name: "relevance" },
-                { id: "date", name: "date" }
-            ],
-            icon: "sbf-flashcards"
-        },
-        tutor: {
-            id: routes.tutorRoute,
-            name: "Tutors",
-            needLocation: true,
-            filter: [
-                { id: "online", name: "Online Lessons" },
-                { id: "inPerson", name: "In Person" }
-            ],
-            sort: [
-                { id: "relevance", name: "relevance" },
-                { id: "price", name: "price" }
-                // { id: "distance", name: "distance" }
-            ],
-            icon: "sbf-tutor"
-        },
-        book: {
-            id: routes.bookRoute,
-            name: "Textbooks",
-            icon: "sbf-textbooks"
-        },
-        job: {
-            id: routes.jobRoute,
-            name: "Jobs",
-            needLocation: true,
-            filter: [{ id: "jobType", name: "job type" }],
-            sort: [
-                { id: "relevance", name: "relevance" },
-                // { id: "distance", name: "distance" },
-                { id: "date", name: "date" }
-            ],
-            icon: "sbf-job"
-        }
-    };
+﻿import verticals from './services/navigation/vertical-navigation/nav'
 export let details = {
     bookDetails: {
         filter: [ { id: "new", name: "new" }, { id: "rental", name: "rental" }, { id: "eBook", name: "eBook" },{ id: "used", name: "used" }],
         sort: [{ id: "price", name: "price" }]
     }
 };
-export let actionFunction = {
-    ...verticals,
-    ...details
-};
+// TODO remove in case evertyhing works
+// export let actionFunction = {
+//     ...verticals,
+//     ...details
+// };
 export let verticalsList = [];
 export let names = [];
 export let page = [];
 export let verticalsNavbar = [];
 export let verticalsName=[];
 
-for (let v in verticals) {
-    let item = verticals[v];
-    verticalsName.push(v);
+for (let i in verticals) {
+    console.log('works like a charm', i)
+    let item = verticals[i].data;
+    verticalsName.push(i);
     names.push({ 'id': item.id, 'name': item.name });
     verticalsNavbar.push(
         {
@@ -100,16 +28,16 @@ for (let v in verticals) {
             'icon': item.icon
             //image: item.image
         });
-    verticalsList.push(verticals[v]);
-    page[v] = {
+    verticalsList.push(verticals[i]);
+    page[i] = {
         // title: item.resultTitle,
         //emptyText: item.emptyState,
         filter: item.filter,
         sort: item.sort
     }
 }
-for (let v in details) {
-    let item = details[v];
-    page[v] = { filter: item.filter, sort: item.sort }
+for (let i in details) {
+    let item = details[i];
+    page[i] = { filter: item.filter, sort: item.sort }
 }
 
