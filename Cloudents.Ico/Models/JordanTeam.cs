e4ed659cdb926c8afd4ea9e973242bccbb7af23e
Hadar Keidar <Hadar@cloudents.com>
2018-08-01 14:01:58 +0300

    Hadar ICO Team commit

---
+++ Cloudents.Ico/Models/JordanTeam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cloudents.Ico.Models
+{
+    public class JordanTeam : Team
+    {
+        public JordanTeam() : base("Jordan")
+        {
+        }
+
+        public override string Image => "/images/muscari/Jordan.png";
+        public override string LinkdinLink => "https://www.linkedin.com/in/jordanweiss/";
+    }
+}
