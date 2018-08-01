b3134d2f992aa5f3bd619c434b0c9226b3ad61b8
Ram <ram.y@outlook.com>
2018-08-01 17:48:08 +0300

    case 10296

--- Cloudents.Web/Models/PhoneNumberRequest.cs
+++ Cloudents.Web/Models/PhoneNumberRequest.cs
@@ -5,18 +5,12 @@ namespace Cloudents.Web.Models
     public class PhoneNumberRequest
     {
         [Required]
-        [RegularExpression(@"^\+?[1-9]\d{1,14}$")]
+        [RegularExpression(@"^\+?[1-9]\d{1,14}$",ErrorMessage = "Phone number is invalid")]
         public string Number { get; set; }
 
         public override string ToString()
         {
             return $"{nameof(Number)}: {Number}";
         }
-    }
-
-    public class TokenRequest
-    {
-        [Required]
-        public string Token { get; set; }
     }
 }
