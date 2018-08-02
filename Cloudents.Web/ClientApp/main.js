407ea90e672c04cd634ef3e4628789415bc482dc
crow7m <crow7m@gmail.com>
2018-08-02 13:06:06 +0300

    semicolomns

--- Cloudents.Web/ClientApp/main.js
+++ Cloudents.Web/ClientApp/main.js
@@ -75,7 +75,7 @@ import {
     VPagination,
     VDataTable
 
-} from "vuetify"
+} from "vuetify";
 import * as route from "./routes";
 
 import { constants } from "./utilities/constants";
@@ -111,9 +111,9 @@ const router = new VueRouter({
     scrollBehavior(to, from, savedPosition) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-              resolve({ x: 0, y: 0 })
-            }, 500)
-          })
+              resolve({ x: 0, y: 0 });
+            }, 500);
+          });
           
         //gaby: deprecated not actually saving the last scroll position.
         // if (savedPosition) {
@@ -144,7 +144,7 @@ Vue.use(VueAnalytics, {
                 page: route.path,
                 title: route.name ? route.name.charAt(0).toUpperCase() + route.name.slice(1) : '',
                 location: window.location.href
-            }
+            };
         },
         exception: true
     }
@@ -159,7 +159,7 @@ Vue.filter('capitalize',
             var tempVal = values[v];
             values[v] = tempVal.charAt(0).toUpperCase() + tempVal.slice(1);
         }
-        return values.join(" ")
+        return values.join(" ");
         //return value.charAt(0).toUpperCase() + value.slice(1);
     });
 //#endregion
@@ -175,26 +175,26 @@ Vue.filter('fixedPoints', function (value) {
     if (!value) return 0;
     if (value.toString().indexOf('.') === -1) return value;
     // debugger
-    return parseFloat(value).toFixed(2)
+    return parseFloat(value).toFixed(2);
 });
 
 Vue.filter('dollarVal', function (value) {
     if (!value) return 0;
-    return parseFloat(value / 40).toFixed(2)
+    return parseFloat(value / 40).toFixed(2);
 });
 
 Vue.filter('dateFromISO', function (value) {
     let d = new Date(value);
     //return load if no data
     if (!value) {
-        return 'Loading..'
+        return 'Loading..';
     }
     return `${d.getUTCMonth() + 1}/${d.getUTCDate()}/${d.getUTCFullYear()}`;
 });
 // filter for numbers, format numbers to local formats. Read more: 'toLocaleString'
 Vue.filter('currencyLocalyFilter', function (value) {
-    let amount = Number(value)
-    return amount && amount.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) || '0'
+    let amount = Number(value);
+    return amount && amount.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) || '0';
 });
 
 Vue.filter('commasFilter', function (value) {
