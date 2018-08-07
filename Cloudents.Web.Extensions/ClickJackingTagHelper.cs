d3b2e8236a33153f5c5d5965c7e1ebd69f729afe
Ram <ram.y@outlook.com>
2018-08-07 09:41:49 +0300

    Remove unused code
    clickjacking fix

---
+++ Cloudents.Web.Extensions/ClickJackingTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cloudents.Web.Extensions
+{
+    public static class ClickJackingTagHelper
+    {
+        public const string FullScript = "<style id=\"acj\">body{display:none !important;}</style>" +
+                                         "<script type=\"text/javascript\">" +
+                                         "if(self===top){var acj = document.getElementById(\"acj\"); acj.parentNode.removeChild(acj)"+
+                                         "}else top.location=self.location;"+
+                                         "</script> ";
+
+    }
+}
