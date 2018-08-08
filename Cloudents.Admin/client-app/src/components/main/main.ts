eae947fa6edaacda957dd6b8d87dd8291199c10a
Gabriel <gaby.str@gmail.com>
2018-08-08 13:14:54 +0300

    main page created

---
+++ Cloudents.Admin/client-app/src/components/main/main.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+
+@Component({})
+export default class Main extends Vue{
+    msg: string = "Hello From Main"
+
+}
---
+++ Cloudents.Admin/client-app/src/components/main/main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        {{msg}}     
+    </div>
+</template>
+
+<script lang="ts" src="./main.ts"></script>
+
+
+
+<style scoped>
+
+</style>
