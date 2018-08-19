06d9f0bcd52ebda2c9718b502a9a5bb69a9bf0df
Hadar Keidar <Hadar@cloudents.com>
2018-08-19 14:56:45 +0300

    Hadar cash-out feature added

---
+++ Cloudents.Core/Query/Admin/CashOutQuery.cs
@@ -0,0 +1,13 @@
+﻿using Cloudents.Core.DTOs.Admin;
+using Cloudents.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudents.Core.Query.Admin
+{
+    public class CashOutQuery : IQuery<IEnumerable<CashOutDto>>
+    {
+
+    }
+}
---
+++ Cloudents.Management/Pages/CashOut.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Cloudents.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Cloudents.Core.DTOs.Admin;
+using Cloudents.Core.Query.Admin;
+using System.Threading;
+
+namespace Cloudents.Management.Pages
+{
+    public class CashOutModel : PageModel
+    {
+        private readonly Lazy<ICommandBus> _commandBus;
+        private readonly IQueryBus _queryBus;
+
+
+        public CashOutModel(Lazy<ICommandBus> commandBus, IQueryBus queryBus)
+        {
+            _commandBus = commandBus;
+            _queryBus = queryBus;
+        }
+
+
+        public async Task OnGet(CancellationToken token)
+        {
+            var query = new CashOutQuery();
+            CashOuts = await _queryBus.QueryAsync(query, token);
+        }
+
+        [ViewData]
+        public IEnumerable<CashOutDto> CashOuts { get; set; }
+        
+    }
+}
