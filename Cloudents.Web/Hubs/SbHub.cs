636f30add6cb87a760c0718907276b15f8db70a1
Ram <ram.y@outlook.com>
2018-08-22 09:07:50 +0300

    zzzz

--- Cloudents.Web/Hubs/SbHub.cs
+++ Cloudents.Web/Hubs/SbHub.cs
@@ -1,15 +1,29 @@
 ﻿using System.Threading.Tasks;
+using Cloudents.Core.DTOs;
+using Cloudents.Web.Models;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Cloudents.Web.Hubs
 {
     public class SbHub :Hub
     {
-
         public const string MethodName = "Message";
         public async Task SendMessage(string message)
         {
             await Clients.All.SendAsync(MethodName, message);
+        }
+
+        public async Task Message(object entity)
+        {
+
+
+
+            //var newObj = new
+            //{
+            //    type="entity",
+
+            //}
+            //await Clients.All.SendAsync(MethodName, new SignalRTransportType<object>("entity", SignalRAction.Add, dto))
         }
     }
 }
