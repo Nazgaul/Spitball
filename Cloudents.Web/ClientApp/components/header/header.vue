a326b61e110105a114f0e0e92849a3e3a14e199e
Gabriel <gaby.str@gmail.com>
2018-08-28 17:13:40 +0300

    hide header on question page in mobile case 10774

--- Cloudents.Web/ClientApp/components/header/header.vue
+++ Cloudents.Web/ClientApp/components/header/header.vue
@@ -1,5 +1,5 @@
 ﻿<template>
-        <main-header ref="mainHeader" :userText="userText" 
+        <main-header v-if="!hideHeaderMobile" ref="mainHeader" :userText="userText" 
                      :submitRoute="submitRoute" :currentSelection="currentSelection"
                      :toolbarHeight="$vuetify.breakpoint.smAndUp ? 104 : 144">
             <verticals-tabs :currentSelection="currentSelection" slot="extraHeader"></verticals-tabs>
