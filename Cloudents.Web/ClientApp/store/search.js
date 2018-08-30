5e8e8535186ce345ac2a6cab3bd19df267b713e2
Gabriel <gaby.str@gmail.com>
2018-08-30 14:39:39 +0300

    change only answer number in question

--- Cloudents.Web/ClientApp/store/search.js
+++ Cloudents.Web/ClientApp/store/search.js
@@ -100,7 +100,6 @@ const mutations = {
                 if(currentQuestion.id === questionToUpdate.id){
                     //replace the question from the list
                     state.itemsPerVertical.ask.data[questionIndex].answers = questionToUpdate.answers;
-                    state.itemsPerVertical.ask.data[questionIndex].watchingNow = questionToUpdate.watchingNow;
                     return;
                 }
             }
