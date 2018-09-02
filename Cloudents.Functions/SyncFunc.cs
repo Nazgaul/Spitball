4e3e37b857b4f66bbf849cb454ef6fa9a25553ce
Ram <ram.y@outlook.com>
2018-09-02 19:04:06 +0300

    Build azure function sync

--- Cloudents.Functions/SyncFunc.cs
+++ Cloudents.Functions/SyncFunc.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Cloudents.Core.DTOs;
+﻿using Cloudents.Core.DTOs;
 using Cloudents.Core.Interfaces;
 using Cloudents.Core.Query;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Cloudents.Functions
 {
     public static class SyncFunc
     {
-        public static async Task SyncAsync<T,TU>(
+        public static async Task SyncAsync<T, TU>(
             CloudBlockBlob blob,
             IReadRepositoryAsync<(IEnumerable<T> update, IEnumerable<SearchWriteBaseDto> delete, long version),
                 SyncAzureQuery> repository,
             ISearchServiceWrite<TU> searchServiceWrite,
-            Func<T,TU> createObj,
+            Func<T, TU> createObj,
             TraceWriter log,
             CancellationToken token) where TU : class, ISearchObject, new()
         {
@@ -44,6 +44,60 @@ namespace Cloudents.Functions
 
                     var updateList = update.Select(createObj).ToList();
                     var deleteCourses = delete.Select(s => s.Id.ToString()).ToList();
+                    await searchServiceWrite.UpdateDataAsync(updateList, deleteCourses, token).ConfigureAwait(false);
+                    query.Page++;
+                    currentVersion = Math.Max(currentVersion, version);
+                    await blob.UploadTextAsync(query.ToString(), token).ConfigureAwait(false);
+                    if (updateList.Count == 0 && deleteCourses.Count == 0)
+                    {
+                        break;
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+            }
+
+            if (!token.IsCancellationRequested)
+            {
+                var newVersion = new SyncAzureQuery(currentVersion, 0);
+                await blob.UploadTextAsync(newVersion.ToString(), token).ConfigureAwait(false);
+            }
+        }
+
+
+        public static async Task SyncAsync2<T, TU>(
+            CloudBlockBlob blob,
+            //IQueryBus bus,
+            ISearchServiceWrite<TU> searchServiceWrite,
+            Func<T, TU> createObj,
+            Func<SyncAzureQuery,  Task<(IEnumerable<T> update, IEnumerable<long> delete, long version)>> getDataFunc,
+            TraceWriter log,
+            CancellationToken token) where TU : class, ISearchObject, new()
+        {
+            var query = SyncAzureQuery.Empty();
+            if (await blob.ExistsAsync(token).ConfigureAwait(false))
+            {
+                var text = await blob.DownloadTextAsync(token).ConfigureAwait(false);
+                query = SyncAzureQuery.ConvertFromString(text);
+            }
+            log.Info($"process {query}");
+
+            //if (query.Version == 0 && query.Page == 0)
+            //{
+            //    await searchServiceWrite.CreateOrUpdateAsync(token).ConfigureAwait(false);
+            //}
+
+            var currentVersion = query.Version;
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    //var (update, delete, version) = await bus.QueryAsync<(IEnumerable<T> update, IEnumerable<long> delete, long version)>(query, token);
+                    var (update, delete, version) = await getDataFunc(query);
+                    var updateList = update.Select(createObj).ToList();
+                    var deleteCourses = delete.Select(s => s.ToString()).ToList();
                     await searchServiceWrite.UpdateDataAsync(updateList, deleteCourses, token).ConfigureAwait(false);
                     query.Page++;
                     currentVersion = Math.Max(currentVersion, version);
