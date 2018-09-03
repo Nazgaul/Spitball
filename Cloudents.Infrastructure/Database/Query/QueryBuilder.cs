ec0aab21777afbcf35a5d220900ed68c8ff6d012
Ram <ram.y@outlook.com>
2018-09-03 13:10:09 +0300

    Continue azure search question.

--- Cloudents.Infrastructure/Database/Query/QueryBuilder.cs
+++ Cloudents.Infrastructure/Database/Query/QueryBuilder.cs
@@ -31,7 +31,18 @@ namespace Cloudents.Infrastructure.Database.Query
                 Debug.Assert(t != null, nameof(t) + " != null");
                 return t.TableName;
             });
-            
+        }
+
+        public string BuildTable<T>(string alias)
+        {
+            var retVal = BuildTable<T>();
+            return $"{retVal} As {alias}";
+            //var retVal =  _tableDictionary.GetOrAdd(typeof(T), (f) =>
+            //{
+            //    var t = _sessionFactoryImplementor.GetClassMetadata(f) as AbstractEntityPersister;
+            //    Debug.Assert(t != null, nameof(t) + " != null");
+            //    //return $"{t.TableName} As {alias}";
+            //});
         }
 
         public string BuildInitVersionTable<T>(string aliasTable, string crossTableAlias)
@@ -55,17 +66,44 @@ namespace Cloudents.Infrastructure.Database.Query
 
         public string BuildProperty<T>(Expression<Func<T, object>> expression)
         {
-            
             var memberName = expression.GetName();
-           
             var keyValue = new KeyValuePair<Type, string>(typeof(T), memberName);
-
             return _propertyDictionary.GetOrAdd(keyValue, f =>
             {
                 var t = _sessionFactoryImplementor.GetClassMetadata(f.Key) as AbstractEntityPersister;
                 Debug.Assert(t != null, nameof(t) + " != null");
                 return t.GetPropertyColumnNames(f.Value).First();
             });
+        }
+
+        public string BuildProperty<T>(Expression<Func<T, object>> expression,string alias)
+        {
+            var b = BuildProperty(expression);
+            return $"{b} as {alias}";
+        }
+
+        public string BuildProperty<T,TU>(Expression<Func<T, object>> expression, Expression<Func<TU, object>> alias)
+        {
+            var b = BuildProperty(expression);
+            return $"{b} as {alias.GetName()}";
+        }
+
+        public string BuildProperty<T>(string tableAlias, Expression<Func<T, object>> expression)
+        {
+            var b = BuildProperty(expression);
+            return $"{tableAlias}.{b}";
+        }
+
+        public string BuildProperty<T>(string tableAlias, Expression<Func<T, object>> expression, string alias)
+        {
+            var b = BuildProperty(expression,alias);
+            return $"{tableAlias}.{b} as {alias}";
+        }
+
+        public string BuildProperty<T,TU>(string tableAlias, Expression<Func<T, object>> expression, Expression<Func<TU, object>> alias)
+        {
+            var b = BuildProperty(expression, alias);
+            return $"{tableAlias}.{b}";
         }
     }
 }
--- Cloudents.Infrastructure/Database/Query/QuestionSyncAzureSearchQueryHandler.cs
+++ Cloudents.Infrastructure/Database/Query/QuestionSyncAzureSearchQueryHandler.cs
@@ -1,27 +1,51 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using Cloudents.Core.DTOs;
+﻿using Cloudents.Core.DTOs;
 using Cloudents.Core.Entities.Db;
 using Cloudents.Core.Interfaces;
 using Cloudents.Core.Query;
 using Cloudents.Infrastructure.Data;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using QuestionSearch = Cloudents.Core.Entities.Search.Question;
 
 namespace Cloudents.Infrastructure.Database.Query
 {
     [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "Ioc inject")]
-    public class QuestionSyncAzureSearchQueryHandler : SyncAzureSearchQueryHandler<Question>,
-        IQueryHandler<SyncAzureQuery, (IEnumerable<AzureSyncBaseDto<Question>> update, IEnumerable<long> delete, long version)>
+    public class QuestionSyncAzureSearchQueryHandler : SyncAzureSearchQueryHandler<QuestionSearch>,
+        IQueryHandler<SyncAzureQuery, (IEnumerable<AzureSyncBaseDto<QuestionSearch>> update, IEnumerable<long> delete, long version)>
     {
+
+        private const string QuestionAlias = "q";
+        private const string UserAlias = "u";
+        private const string QuestionSubjectAlias = "qs";
 
         public QuestionSyncAzureSearchQueryHandler(QueryBuilder queryBuilder, DapperRepository dapperRepository) :
             base(queryBuilder, dapperRepository)
         {
         }
 
-       
+
 
         protected override string VersionSql => $"select * {QueryBuilder.BuildDiffVersionTable<Question>("q", "c", 56123)}";
 
-        protected override string FirstQuery => $"select * {QueryBuilder.BuildInitVersionTable<Question>("q", "c")}";
+        protected override string FirstQuery => $@"
+select 
+{QueryBuilder.BuildProperty<User, QuestionSearch>(UserAlias, t => t.Id, x => x.UserId)},
+{QueryBuilder.BuildProperty<User, QuestionSearch>(UserAlias, t => t.Name, x => x.UserName)},
+{QueryBuilder.BuildProperty<User, QuestionSearch>(UserAlias, t => t.Image, x => x.UserImage)},
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Id, x => x.Id)},
+(select count(*) from sb.Answer where QuestionId = q.id) AnswerCount,
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Updated, x => x.DateTime)},
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Attachments, x => x.FilesCount)},
+CASE when q.CorrectAnswer_id IS null Then 0 else 1 
+      END HasCorrectAnswer,
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Price, x => x.Price)},
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Text, x => x.Text)},
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Color, x => x.Color)},
+{QueryBuilder.BuildProperty<QuestionSubject, QuestionSearch>(QuestionSubjectAlias, t => t.Text, x => x.SubjectText)},
+{QueryBuilder.BuildProperty<Question, QuestionSearch>(QuestionAlias, t => t.Subject, x => x.Subject)},
+c.Id as TTT, c.SYS_CHANGE_VERSION
+{QueryBuilder.BuildInitVersionTable<Question>(QuestionAlias, "c")} 
+join {QueryBuilder.BuildTable<User>(UserAlias)} on {QueryBuilder.BuildProperty<Question>(QuestionAlias, t => t.User)}={QueryBuilder.BuildProperty<User>(UserAlias, u => u.Id)}
+join {QueryBuilder.BuildTable<QuestionSubject>(QuestionSubjectAlias)} on {QueryBuilder.BuildProperty<Question>(QuestionAlias, t => t.Subject)}={QueryBuilder.BuildProperty<QuestionSubject>(QuestionSubjectAlias, u => u.Id)}";
     }
 }
