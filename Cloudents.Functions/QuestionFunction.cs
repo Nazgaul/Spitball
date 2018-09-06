5d317468f8a80b9350a1b4cb9e4450d876d461ba
Ram <ram.y@outlook.com>
2018-09-06 11:38:58 +0300

    Ico site. implment white paper.

--- Cloudents.Functions/QuestionFunction.cs
+++ Cloudents.Functions/QuestionFunction.cs
@@ -45,8 +45,8 @@ namespace Cloudents.Functions
             CancellationToken token)
         {
             //var (update, delete, version) = await bus.QueryAsync<(IEnumerable<QuestionAzureSyncDto> update, IEnumerable<long> delete, long version)>(query, token);
-            await SyncFunc.SyncAsync2(blob, searchServiceWrite, s => s.Data
-            , q => bus.QueryAsync<(IEnumerable<AzureSyncBaseDto<Question>> update, IEnumerable<long> delete, long version)>(q, token),
+            await SyncFunc.SyncAsync2(blob, searchServiceWrite, s => s
+            , q => bus.QueryAsync<(IEnumerable<Question> update, IEnumerable<long> delete, long version)>(q, token),
                 log, token);
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
         }
