6f51b4e2171c3d85f56e64b467f9b084c80ca640
Ram <ram.y@outlook.com>
2018-09-12 12:42:33 +0300

    Some new idea.

---
+++ Cloudents.Functions/Sync/ISync.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cloudents.Core.Entities.Search;
+using Cloudents.Core.Query;
+
+namespace Cloudents.Functions.Sync
+{
+    public interface IDbToSearchSync
+    {
+        SyncAzureQuery GetCurrentState();
+
+        Task CreateIndex();
+
+
+        Task DoSync();
+        //Task<(IEnumerable<T> update, IEnumerable<long> delete, long version)>GetData<T>();
+
+    }
+
+    //public class QuestionDbToSearchSync : IDbToSearchSync
+    //{
+
+
+        
+
+    //    //public SyncAzureQuery GetCurrentState()
+    //    //{
+    //    //    throw new System.NotImplementedException();
+    //    //}
+
+    //    //public Task CreateIndex()
+    //    //{
+    //    //    throw new System.NotImplementedException();
+    //    //}
+
+    //    //public Task<(IEnumerable<Question> update, IEnumerable<long> delete, long version)> GetData<Question>()
+    //    //{
+    //    //    throw new System.NotImplementedException();
+    //    //}
+
+    //    //public Task<(IEnumerable<Question> update, IEnumerable<long> delete, long version)> GetData()
+    //    //{
+    //    //    throw new System.NotImplementedException();
+    //    //}
+    //}
+}
