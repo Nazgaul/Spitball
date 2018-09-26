36cf9f55711a70f97eb184b28b41d7248624b49b
crow7m <crow7m@gmail.com>
2018-09-26 15:16:03 +0300

    redo result

--- Cloudents.Web/ClientApp/components/results/Result.vue
+++ Cloudents.Web/ClientApp/components/results/Result.vue
@@ -49,70 +49,7 @@
                                                   v-language:placeholder></v-text-field>
                                 </v-flex>
 
-                                <!--static cards AB test-->
-                                <v-flex class="result-cell mb-3 static-card-need-to-ask" xs-12>
-                                    <router-link :to="{path:'/question/'}" class="mb-5 need-to-ask-router-link">
-                                        <div class="need-to-ask">
-                                            <div class="top-block static-top">
-                                                <user-avatar slot="activator"
-                                                             :user-name="accountUser ? accountUser.name : 'John Doe123'"/>
-                                                <span class="static-ab-text">What do you need to ask?</span>
-                                            </div>
-                                            <div class="static-center">
-
-                                            </div>
-                                            <div class="static-bottom-block">
-                                                <button class="static-btn btn-subject">
-                                                    <v-icon>sbf-books</v-icon>
-                                                    Pick a subject
-                                                </button>
-                                                <button class="static-btn btn-price">
-                                                    <v-icon>sbf-hand-coin</v-icon>
-                                                    Place your price
-                                                </button>
-                                            </div>
-
-                                        </div>
-
-                                    </router-link>
-                                </v-flex>
-
-                                <v-flex class="result-cell mb-3 static-card-what-is-question" xs-12>
-                                    <router-link :to="{path:'/question/'}" class="mb-5 what-is-question-router-link">
-                                        <div class="what-is-question">
-                                            <div class="top-block static-top">
-                                                <p class="static-ab-text">
-                                                    What is<br/> your <b>homework<br/> question?</b>
-                                                </p>
-                                            </div>
-                                            <div class="static-bottom-block">
-                                                <askQuestionBtn class="static-ask-question"></askQuestionBtn>
-                                            </div>
-                                        </div>
-
-                                    </router-link>
-                                </v-flex>
-
-                                <v-flex class="result-cell mb-3 static-card-what-is-hw-question" xs-12>
-                                    <router-link :to="{path:'/question/'}" class="mb-5 what-is-hw-question-router-link">
-                                        <div class="what-is-hw-question">
-                                            <div class="top-block static-top">
-                                                <user-avatar slot="activator"
-                                                             :user-name="accountUser ? accountUser.name : 'John Doe123'"/>
-                                                <span class="static-user-name">{{accountUser ? accountUser.name: 'John Doe123'}}</span>
-                                            </div>
-                                            <div class="static-center">
-                                                <p class="static-ab-text">
-                                                    What is your HW question?
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </router-link>
-                                </v-flex>
-                                <!--end static cards AB test-->
-
-
-                                <v-flex class="result-cell mb-3" xs-12 v-for="(item,index) in items" :key="index"
+                               <v-flex class="result-cell mb-3" xs-12 v-for="(item,index) in items" :key="index"
                                         :class="(index>6?'order-xs6': index>2 ? 'order-xs3' : 'order-xs2')">
                                     <component v-if="item.template !== 'ask' " :is="'result-'+item.template"
                                                :item="item" :key="index" :index="index" class="cell"></component>
