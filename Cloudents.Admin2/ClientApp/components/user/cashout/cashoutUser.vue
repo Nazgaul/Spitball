e92788ffab7f7d9078c27f6cd2409cd434971471
Gabriel <gaby.str@gmail.com>
2018-09-27 10:22:35 +0300

    added user functions

--- Cloudents.Admin2/ClientApp/components/user/cashout/cashoutUser.vue
+++ Cloudents.Admin2/ClientApp/components/user/cashout/cashoutUser.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Cashout</h1>
+    <h1>Cashout List</h1>
 </template>
 
 <script>
--- Cloudents.Admin2/ClientApp/components/user/token/tokenUser.vue
+++ Cloudents.Admin2/ClientApp/components/user/token/tokenUser.vue
@@ -1,13 +1,32 @@
 <template>
-    <h1>Give Tokens To User</h1>
+    <div class="user-tokens-container">
+        <h1>Give Tokens To User</h1>
+        <div class="user-inputs-container">
+            <span>User Id: <input type="text" v-model="userId" placeholder="insert user id"/></span>
+            <span>Tokens: <input type="text" v-model.number="tokens" placeholder="set amount of tokens to apply"/></span>
+        </div>
+    </div>
+    
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            userId: null,
+            tokens: null
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.user-tokens-container{
+    .user-inputs-container{
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+}
 
 </style>
