7632f857000d72132e0951f4ec79096799cbae13
Ram <ram.y@outlook.com>
2018-10-11 10:57:57 +0300

    Getting country first from claim then from cookie

--- Cloudents.Web/Binders/CountryModelBinder.cs
+++ Cloudents.Web/Binders/CountryModelBinder.cs
@@ -4,7 +4,10 @@ using Cloudents.Web.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Cloudents.Web.Identity;
 
 namespace Cloudents.Web.Binders
 {
@@ -22,7 +25,14 @@ namespace Cloudents.Web.Binders
 
         public async Task BindModelAsync(ModelBindingContext bindingContext)
         {
-            var cookieValue = bindingContext.HttpContext.Request.Cookies["country"];
+            var cookieValue = bindingContext.HttpContext.User.Claims.FirstOrDefault(f =>
+                string.Equals(f.Type, AppClaimsPrincipalFactory.Country.ToString(),
+                    StringComparison.OrdinalIgnoreCase))?.Value;
+            if (cookieValue == null)
+            {
+                cookieValue = bindingContext.HttpContext.Request.Cookies["country"];
+            }
+
             if (cookieValue == null)
             {
                 var query = new CountryQuery(bindingContext.HttpContext.Connection.GetIpAddress());
@@ -39,12 +49,19 @@ namespace Cloudents.Web.Binders
                     bindingContext.Result = ModelBindingResult.Failed();
                     return;
                 }
-                bindingContext.HttpContext.Response.Cookies.Append("country", cookieValue, new CookieOptions()
-                {
-                    Expires = DateTimeOffset.UtcNow.AddYears(1),
-                    HttpOnly = true,
-                    Secure = true
-                });
+                var claimsIdentity = (ClaimsIdentity)bindingContext.HttpContext.User.Identity;
+                var identity = new ClaimsIdentity(claimsIdentity);
+
+               
+                //context.Authentication.AuthenticationResponseGrant = new AuthenticationResponseGrant
+                //    (new ClaimsPrincipal(identity), new AuthenticationProperties { IsPersistent = true });
+                //bindingContext.HttpContext.User.
+                //bindingContext.HttpContext.Response.Cookies.Append("country", cookieValue, new CookieOptions()
+                //{
+                //    Expires = DateTimeOffset.UtcNow.AddYears(1),
+                //    HttpOnly = true,
+                //    Secure = true
+                //});
 
             }
             bindingContext.Result = ModelBindingResult.Success(cookieValue);
