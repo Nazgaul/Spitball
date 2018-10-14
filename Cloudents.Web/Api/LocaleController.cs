e97d4afcd591309db083f57d7ae4972f0dec22ac
Ram <ram.y@outlook.com>
2018-10-14 13:15:01 +0300

    Added the ability get resource by path

--- Cloudents.Web/Api/LocaleController.cs
+++ Cloudents.Web/Api/LocaleController.cs
@@ -1,32 +1,43 @@
-﻿using System.Collections;
+﻿using Cloudents.Core;
+using Cloudents.Core.Extension;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Resources;
-using Cloudents.Core.Extension;
-using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace Cloudents.Web.Api
 {
     [Route("api/[controller]"), ApiController]
     public class LocaleController : ControllerBase
     {
-        private static readonly ConcurrentDictionary<CultureInfo, Dictionary<string, string>> CacheDictionary = new ConcurrentDictionary<CultureInfo, Dictionary<string, string>>();
+        private static readonly ConcurrentDictionary<(CultureInfo, string), Dictionary<string, string>> CacheDictionary = new ConcurrentDictionary<(CultureInfo, string), Dictionary<string, string>>();
 
+        private static readonly Regex DefaultRegex =
+            new Regex(@"Cloudents.Web.Resources.Js.([A-Za-z0-9\-]+).resources", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
         //public Task<IViewComponentResult> InvokeAsync()
         //{
         //    var jsonString = CacheDictionary.GetOrAdd(CultureInfo.CurrentUICulture, _ => ParseJsResource());
         //    return Task.FromResult<IViewComponentResult>(View("Default", jsonString));
         //}
 
-        private static Dictionary<string, string> ParseJsResource()
+        private static Dictionary<string, string> ParseJsResource(string location)
         {
             var assembly = Assembly.GetExecutingAssembly();
             var resources = assembly.GetManifestResourceNames();
             var dic = new Dictionary<string, string>();
-            foreach (var rawResourceLocation in resources.Where(w => w.Contains("Cloudents.Web.Resources.Js")))
+
+            var regex = DefaultRegex;
+            if (location != null)
+            {
+                regex = new Regex($@"Cloudents.Web.Resources.Js.{location}.([A-Za-z0-9\-]+).resources", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+            }
+            foreach (var rawResourceLocation in resources.Where(w => regex.Match(w).Success)) // w.Contains("Cloudents.Web.Resources.Js")))
             {
                 var resourceStr = rawResourceLocation.Substring(0, rawResourceLocation.LastIndexOf('.'));
                 var resource = new ResourceManager(resourceStr, assembly);
@@ -50,9 +61,10 @@ namespace Cloudents.Web.Api
 
         // GET
         [HttpGet]
-        public Dictionary<string, string> Get(string locale)
+        [ResponseCache(Duration = TimeConst.Week, VaryByQueryKeys = new[] { "*" })]
+        public Dictionary<string, string> Get(string locale, string resource)
         {
-            var dic = CacheDictionary.GetOrAdd(CultureInfo.CurrentUICulture, _ => ParseJsResource());
+            var dic = CacheDictionary.GetOrAdd((CultureInfo.CurrentUICulture, resource), _ => ParseJsResource(resource));
             return dic;
         }
     }
