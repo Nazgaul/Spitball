193de7c63ba8ed91de240ba36a3bb0ffba2f80c5
Ram <ram.y@outlook.com>
2018-10-21 14:17:01 +0300

    case 11170

--- Cloudents.Web/Controllers/HomeController.cs
+++ Cloudents.Web/Controllers/HomeController.cs
@@ -1,16 +1,11 @@
 ﻿using Cloudents.Core.Entities.Db;
 using Cloudents.Core.Extension;
 using JetBrains.Annotations;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Net;
 using System.Threading.Tasks;
 using Cloudents.Web.Binders;
-using Cloudents.Web.Extensions;
-using Microsoft.Extensions.Configuration;
 
 namespace Cloudents.Web.Controllers
 {
@@ -18,29 +13,16 @@ namespace Cloudents.Web.Controllers
     public class HomeController : Controller
     {
         internal const string Referral = "referral";
-        private readonly List<IPAddress> _officeIps = new List<IPAddress>();
 
 
-        public HomeController(IConfiguration configuration)
-        {
-            var ipsStr = configuration["Ips"];
-
-            if (ipsStr == null) return;
-            foreach (var ipStr in ipsStr.Split(','))
-            {
-                if (IPAddress.TryParse(ipStr, out var ip))
-                {
-                    _officeIps.Add(ip);
-                }
-            }
-        }
+      
 
         [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Index(
             [ModelBinder(typeof(CountryModelBinder))] string country,
             [FromHeader(Name = "User-Agent")] string userAgent,
-            [FromQuery] bool? isNew, [FromQuery, CanBeNull] string referral, 
-            [FromServices]IHostingEnvironment env)
+            [FromQuery, CanBeNull] string referral
+            )
         {
             if (!string.IsNullOrEmpty(referral))
             {
@@ -53,34 +35,7 @@ namespace Cloudents.Web.Controllers
             }
 
             ViewBag.country = country ?? "us";
-
-            if (env.IsDevelopment())
-            {
-                return View();
-            }
-
-            if (env.IsStaging())
-            {
-                return View();
-            }
-
-            if (isNew.GetValueOrDefault(false))
-            {
-                return View();
-            }
-
-            var requestIp = HttpContext.Connection.GetIpAddress();//.MapToIPv4().;
-            
-
-
-            if (_officeIps.Contains(requestIp))
-            {
-                return View();
-            }
-            if (string.Equals(country, "il", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return this.RedirectToOldSite();
-            }
+           
             return View();
         }
 
