c704a4b3e441efad72bf6dc3be2433f071255887
Gabriel <gaby.str@gmail.com>
2018-10-28 14:23:32 +0200

    get university list from server impl and fixed filter

--- Cloudents.Web/ClientApp/components/helpers/uni-select/steps/set_class.vue
+++ Cloudents.Web/ClientApp/components/helpers/uni-select/steps/set_class.vue
@@ -114,9 +114,16 @@ export default {
     },
     filters:{
         boldText(value, search){
-            //mark the text bold according to the search value
-            if (!value) return '';
-            return value.replace(search, '<b>' + search + '</b>')
+           if (!value) return '';
+            let match = value.toLowerCase().indexOf(search.toLowerCase()) > -1;
+            if(match){
+                let startIndex = value.toLowerCase().indexOf(search.toLowerCase())
+                let endIndex = search.length;
+                let word = value.substr(startIndex, endIndex)
+                return value.replace(word, '<b>' + word + '</b>')
+            }else{
+                return value;
+            }
         }
     },
     methods:{
