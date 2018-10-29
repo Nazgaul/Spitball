48709f9040b0ef7441c18b35267cd3548e23a710
Gabriel <gaby.str@gmail.com>
2018-10-29 14:22:07 +0200

    no worries div added

--- Cloudents.Web/ClientApp/components/helpers/uni-select/uniSelect.js
+++ Cloudents.Web/ClientApp/components/helpers/uni-select/uniSelect.js
@@ -1,13 +1,17 @@
 import SetSchoolLanding from './steps/set_school_landing.vue'
 import SetSchool from './steps/set_school.vue'
 import SetClass from './steps/set_class.vue'
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex'
+import noWorries from './popups/noWorries/noWorries.vue'
+import sbDialog from '../../wrappers/sb-dialog/sb-dialog.vue';
 
 export default {
     components:{
         SetSchoolLanding,
         SetSchool,
-        SetClass
+        SetClass,
+        noWorries,
+        sbDialog
     },
 
     data(){
@@ -17,7 +21,8 @@ export default {
             fnMethods: {
                 changeStep: this.changeStep,
                 changeSchoolName: this.changeSchoolName
-            }
+            },
+            beforeLeave: false,
         }
     },
 
@@ -41,6 +46,9 @@ export default {
         closeInterface(){
             this.setUniversityPopStorage_session();
             this.changeSelectUniState(false);
+        },
+        closeNoWorriesPopup(){
+            this.beforeLeave = false;
         }
     }
 }
