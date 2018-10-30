4968b28a982c347dd091466a9cca724a84d1f20b
Ram <ram.y@outlook.com>
2018-10-30 09:08:04 +0200

    xxx

--- Cloudents.Web/Api/UniversityController.cs
+++ Cloudents.Web/Api/UniversityController.cs
@@ -61,16 +61,16 @@ namespace Cloudents.Web.Api
             return result;
         }
 
-        [Route("Name")]
-        [HttpGet]
-        public async Task<IEnumerable<UniversityDto>> Get([FromServices] IQueryBus _queryBus, CancellationToken token)
-        {
-            var userId = _userManager.GetLongUserId(User);
-            var query = new UniversityQuery(userId);
-            var t = await _queryBus.QueryAsync<string>(query, token);
-            return t;
+        //[Route("Name")]
+        //[HttpGet]
+        //public async Task<IEnumerable<UniversityDto>> Get([FromServices] IQueryBus _queryBus, CancellationToken token)
+        //{
+        //    var userId = _userManager.GetLongUserId(User);
+        //    var query = new UniversityQuery(userId);
+        //    var t = await _queryBus.QueryAsync<string>(query, token);
+        //    return t;
 
-        }
+        //}
 
         [HttpPost("assign")]
         public async Task<IActionResult> AssignUniversityAsync([FromBody] AssignUniversityRequest model, CancellationToken token)
