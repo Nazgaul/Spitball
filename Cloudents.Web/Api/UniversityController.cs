53c87c4d32ff06a2e67abff9d08be4ea84ed5df8
Hadar Keidar <Hadar@cloudents.com>
2018-10-30 09:33:59 +0200

    fix

--- Cloudents.Web/Api/UniversityController.cs
+++ Cloudents.Web/Api/UniversityController.cs
@@ -61,15 +61,15 @@ namespace Cloudents.Web.Api
             return result;
         }
 
-        //[Route("Name")]
-        //[HttpGet]
-        //public async Task<IEnumerable<UniversityDto>> Get([FromServices] IQueryBus _queryBus, CancellationToken token)
-        //{
-        //    var userId = _userManager.GetLongUserId(User);
-        //    var query = new UniversityQuery(userId);
+        [Route("Name")]
+        [HttpGet]
+        public async Task<IEnumerable<UniversityDto>> Get([FromServices] IQueryBus _queryBus, CancellationToken token)
+        {
+            var userId = _userManager.GetLongUserId(User);
+            var query = new UniversityQuery(userId);
             var t = await _queryBus.QueryAsync<IEnumerable<UniversityDto>>(query, token);
-        //    return t;
-        //}
+            return t;
+        }
 
         [HttpPost("assign")]
         public async Task<IActionResult> AssignUniversityAsync([FromBody] AssignUniversityRequest model, CancellationToken token)
