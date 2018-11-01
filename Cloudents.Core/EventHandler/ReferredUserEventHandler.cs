3517f867bc95e3e5cce953078621a6e0371733f7
Hadar Keidar <Hadar@cloudents.com>
2018-11-01 10:13:50 +0200

    Referring User Email

---
+++ Cloudents.Core/EventHandler/ReferredUserEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using Cloudents.Core.Event;
+using Cloudents.Core.Interfaces;
+using Cloudents.Core.Message.Email;
+using Cloudents.Core.Storage;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cloudents.Core.EventHandler
+{
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global", Justification = "Ioc inject")]
+    class ReferredUserEventHandler : IEventHandler<ReferredUserEvent>
+    {
+       
+        private readonly IQueueProvider _serviceBusProvider;
+       
+
+        public ReferredUserEventHandler(IQueueProvider serviceBusProvider)
+        {
+            _serviceBusProvider = serviceBusProvider;
+        }
+
+        public async Task HandleAsync(ReferredUserEvent eventMessage, CancellationToken token)
+        {
+            await _serviceBusProvider.InsertMessageAsync(
+                  new ReferralBonusEmail(eventMessage.Transaction.User.Email, eventMessage.Transaction.User.Culture), token);
+        }
+    }
+}
---
+++ Cloudents.Core/Message/Email/ReferralBonusEmail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Text;
+
+namespace Cloudents.Core.Message.Email
+{
+    public class ReferralBonusEmail : BaseEmail
+    {
+        public ReferralBonusEmail(string to, CultureInfo info)
+            : base(to, "Test String", info)
+        {
+           
+        }
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global", Justification = "Using it with reflection")]
+        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global", Justification = "Using it with reflection")]
+    
+        public override string Campaign => "Referral User";
+
+        protected override IDictionary<CultureInfo, string> Templates => new Dictionary<CultureInfo, string>()
+        {
+            
+            {Language.English.Culture ,"5560b10d-bb82-43bf-85eb-1f2947bdb2d8" }
+        };
+    }
+} 
