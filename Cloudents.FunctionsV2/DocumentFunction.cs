0c069528af0bbcba8dc25e4771ce5f41e48cec68
Ram <ram.y@outlook.com>
2018-11-04 17:20:23 +0200

    Finish doc write from blob storage.

--- Cloudents.FunctionsV2/DocumentFunction.cs
+++ Cloudents.FunctionsV2/DocumentFunction.cs
@@ -1,21 +1,49 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+using Cloudents.Core.Command;
+using Cloudents.Core.Entities.Search;
+using Cloudents.Core.Extension;
+using Cloudents.Core.Interfaces;
 using Cloudents.FunctionsV2.Sync;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Cloudents.FunctionsV2
 {
     public static class DocumentFunction
     {
         [FunctionName("BlobFunction")]
-        public static void Run(
-            [BlobTrigger("spitball-files/files/{id}/text.txt")]string text, IDictionary<string, string> metadata,
-            string name, ILogger log)
+        public static async Task RunAsync(
+            [BlobTrigger("spitball-files/files/{id}/text.txt")]string text, long id, IDictionary<string, string> metadata,
+            [Inject] ISearchServiceWrite<Document> searchInstance,
+            [Inject] ICommandBus commandBus,
+            [Inject] ITextAnalysis textAnalysis,
+            ILogger log, CancellationToken token)
         {
+            var lang = await textAnalysis.DetectLanguageAsync(text.Truncate(5000), token);
+            int? pageCount = null;
+            if (metadata.TryGetValue("PageCount", out var pageCountStr) &&
+                int.TryParse(pageCountStr, out var pageCount2))
+            {
+                pageCount = pageCount2;
+            }
 
+            var command = new UpdateDocumentMetaCommand(id, lang, pageCount);
+            await commandBus.DispatchAsync(command, token);
+
+            await searchInstance.UpdateDataAsync(new[]
+                 {
+                    new Document()
+                    {
+                        Id = id.ToString(),
+                        Content = text,
+                        Language = lang.TwoLetterISOLanguageName,
+                        MetaContent = text.Truncate(200, true)
+                    }
+                }, token
+             );
             //log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
 
