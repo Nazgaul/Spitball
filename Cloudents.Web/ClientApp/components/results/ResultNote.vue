747871c8a56d9bf9382f36d7d9381d1d7b7343f7
crow7m <crow7m@gmail.com>
2018-11-07 11:00:35 +0200

    ellipsis directive added, doc card done

--- Cloudents.Web/ClientApp/components/results/ResultNote.vue
+++ Cloudents.Web/ClientApp/components/results/ResultNote.vue
@@ -29,7 +29,7 @@
                     <div class="content-wrap">
                         <div class="title-wrap">
                             <v-icon class="doc mr-2">sbf-document-note</v-icon>
-                            <span class="doc-title">{{item.title}}</span>
+                            <span class="doc-title" v-line-clamp="1">{{item.title}}</span>
                         </div>
                         <div class="content-text">
                             <span v-line-clamp="2">{{item.snippet}}</span>
@@ -48,7 +48,6 @@
                         <span class="sb-doc-info">{{docDownloads}}</span>
                     </div>
                 </v-flex>
-
             </v-flex>
         </v-container>
     </a>
