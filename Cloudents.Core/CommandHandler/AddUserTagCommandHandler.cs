aa1b2393d9f72037fcb08b1db83d209826a471be
Ram <ram.y@outlook.com>
2018-11-11 13:51:42 +0200

    Rename of file/class
    Starting to work on write of tags user

---
+++ Cloudents.Core/CommandHandler/AddUserTagCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using Cloudents.Core.Command;
+using Cloudents.Core.Entities.Db;
+using Cloudents.Core.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cloudents.Core.CommandHandler
+{
+    public class AddUserTagCommandHandler : ICommandHandler<AddUserTagCommand>
+    {
+        private readonly ITagRepository _tagRepository;
+        private readonly IRepository<User> _userRepository;
+
+        public AddUserTagCommandHandler(ITagRepository tagRepository, IRepository<User> userRepository)
+        {
+            _tagRepository = tagRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task ExecuteAsync(AddUserTagCommand message, CancellationToken token)
+        {
+            var user = await _userRepository.LoadAsync(message.UserId, token);
+            var tag = await _tagRepository.GetOrAddAsync(message.Tag, token);
+            if (user.Tags.Add(tag))
+            {
+                tag.Count++;
+                await _tagRepository.UpdateAsync(tag, token);
+            }
+            await _userRepository.UpdateAsync(user, token);
+        }
+    }
+}
