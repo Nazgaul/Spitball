7067165d178aa84613420fdb3bb724a324f206e0
crow7m <crow7m@gmail.com>
2018-11-15 10:07:28 +0200

    referral resourses for document share

--- Cloudents.Web/ClientApp/components/question/helpers/referralDialog/consts.js
+++ Cloudents.Web/ClientApp/components/question/helpers/referralDialog/consts.js
@@ -1,7 +1,5 @@
 import { LanguageService } from "../../../../services/language/languageService";
 
-
-
 function getReferallMessages(type, url) {
     let types = {
         menu: {
@@ -11,25 +9,18 @@ function getReferallMessages(type, url) {
                 text: LanguageService.getValueByKey("referralDialog_get_your_homework") + " " + encodeURIComponent(url),
                 twitterText: LanguageService.getValueByKey("referralDialog_join_me") + " " + LanguageService.getValueByKey("referralDialog_get_your_homework_twitter"),
                 whatsAppText: LanguageService.getValueByKey("referralDialog_join_me") + " " + LanguageService.getValueByKey("referralDialog_get_your_homework") + " " + encodeURIComponent(url),
-
-
         },
         uploadReffer: {
                 url: url,
                 encodedUrl: encodeURIComponent(url),
-                title: LanguageService.getValueByKey("referralDialog_join_me"),
-                text: LanguageService.getValueByKey("referralDialog_get_your_homework") + " " + encodeURIComponent(url),
-                twitterText: LanguageService.getValueByKey("referralDialog_join_me") + " " + LanguageService.getValueByKey("referralDialog_get_your_homework_twitter"),
-                whatsAppText: LanguageService.getValueByKey("referralDialog_join_me") + " " + LanguageService.getValueByKey("referralDialog_get_your_homework") + " " + encodeURIComponent(url),
+                title: LanguageService.getValueByKey("referralDialog_title_document"),
+                text: LanguageService.getValueByKey("referralDialog_get_document_text") + " " + encodeURIComponent(url),
+                twitterText: LanguageService.getValueByKey("referralDialog_title_document") + " " + LanguageService.getValueByKey("referralDialog_get_document_text"),
+                whatsAppText: LanguageService.getValueByKey("referralDialog_title_document") + " " + LanguageService.getValueByKey("referralDialog_get_document_text") + " " + encodeURIComponent(url),
             }
-
-
     };
     return types[type]
-
 }
-
-
 
 export  {
     getReferallMessages,
