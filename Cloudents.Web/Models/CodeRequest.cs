4e9e5b4d1cdabd14e480509dcf8b0747847a6a9e
Ram <ram.y@outlook.com>
2018-11-25 16:51:29 +0200

    Added length of sms code

--- Cloudents.Web/Models/CodeRequest.cs
+++ Cloudents.Web/Models/CodeRequest.cs
@@ -5,6 +5,7 @@ namespace Cloudents.Web.Models
     public class CodeRequest
     {
         [Required(ErrorMessage = "Required")]
+        [StringLength(6,MinimumLength = 6, ErrorMessage = "StringLength")]
         public string Number { get; set; }
 
         public override string ToString()
