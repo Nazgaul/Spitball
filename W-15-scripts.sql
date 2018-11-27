97054a76b307fd9859c05310c9344e01e6e27652
Hadar Keidar <Hadar@cloudents.com>
2018-11-27 15:13:04 +0200

    case 11578

--- W-15-scripts.sql
+++ W-15-scripts.sql
@@ -2,3 +2,10 @@
     ON sb.Document;  
 
 	--REMOVE ab testing
+
+
+--remove from sb.transaction the fat generated from migrate the old users
+begin tran
+delete from sb.[Transaction]
+where [User_id] in (select Id from sb.[User] where OldUser = 1 and PhoneNumberConfirmed = 0)
+commit
