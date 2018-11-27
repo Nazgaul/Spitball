99a6ad996604f6611d7079e00f7ae6f44c49e579
crow7m <crow7m@gmail.com>
2018-11-27 17:54:10 +0200

    admin

---
+++ Cloudents.Admin2/ClientApp/components/document/document.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <v-tabs centered  class="document-nav"  color="transparent">
+            <v-tabs-slider color="#3532d5"></v-tabs-slider>
+            <v-tab to="/document/approveDelete">Approve or Delete</v-tab>
+        </v-tabs>
+        <div class="document-content">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+
+
+<style lang="scss" scoped>
+    .document-nav{
+        display:flex;
+        justify-content: center;
+        .v-tabs__bar{
+            background-color: transparent;
+        }
+        a{
+            margin:2px;
+            text-decoration: none;
+            font-size: 15px;
+            border-radius: 25px;
+            padding: 5px;
+            font-weight: bold;
+        }
+        .router-link-active{
+            color: rgba(0,0,0,0.87);
+        }
+    }
+    .document-content{
+        padding: 5px;
+    }
+</style>
---
+++ Cloudents.Admin2/ClientApp/components/document/documentComponents/approveDelete/approveDeleteService.js
@@ -0,0 +1,50 @@
+import { connectivityModule } from '../../../../services/connectivity.module'
+
+let url = 'AdminDocument';
+function DocumentItem(ObjInit){
+    this.id = ObjInit.id || '';
+    this.preview = ObjInit.preview || '';
+};
+function createDocumentItem(ObjInit){
+    return new DocumentItem(ObjInit)
+}
+
+export default {
+
+    getDocument: (ids) => {
+        return connectivityModule.http.get(`${url}`)
+            .then((resp) => {
+                console.log(resp, 'success get 20 docs');
+                resp.forEach(function(doc) {
+                    createDocumentItem(doc)
+                });
+                return Promise.resolve(resp)
+            }, (error) => {
+                console.log(error, 'error get 20 docs');
+                return Promise.reject(error)
+            })
+    },
+    deleteDocument: (id) => {
+        return connectivityModule.http.delete(`${url}/${id}`)
+            .then((resp) => {
+                console.log(resp, 'delete success');
+                return Promise.resolve(resp)
+            }, (error) => {
+                console.log(error, 'error delete');
+                return Promise.reject(error)
+            })
+    },
+
+    approveDocument: (id) =>{
+        return connectivityModule.http.post(`${url}`, {id})
+            .then((resp) => {
+                console.log(resp, 'post doc success');
+                return Promise.resolve(resp)
+            }, (error) => {
+                console.log(error, 'error post doc');
+                return Promise.reject(error)
+            })
+    }
+
+
+}
