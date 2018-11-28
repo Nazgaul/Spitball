f89d4d2fdeca5d88470893ac67941d008473b363
Hadar Keidar <Hadar@cloudents.com>
2018-11-28 08:45:04 +0200

    Case 11578

--- W-15-scripts.sql
+++ W-15-scripts.sql
@@ -7,3 +7,8 @@ add state nvarchar(255) null
 	--REMOVE ab testing
 
 	--need to run FixFilesAsync in console app on production
+
+begin tran
+delete from sb.[Transaction]
+where [User_id] in (select Id from sb.[User] where EmailConfirmed = 0 and PhoneNumberConfirmed = 0)
+commit
