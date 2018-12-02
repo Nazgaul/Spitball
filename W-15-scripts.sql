8eeef9fe202a236614f78943313862ec49162479
Hadar Keidar <Hadar@cloudents.com>
2018-12-02 14:40:24 +0200

    Case 11711

--- W-15-scripts.sql
+++ W-15-scripts.sql
@@ -1,1 +1,17 @@
 ﻿
+--Update document university
+BEGIN tran
+update D
+set UniversityId = t.University
+from sb.Document D
+join (select U2.Id, d.Id
+		from sb.Document d 
+		join zbox.Item i 
+			on d.OldId = i.ItemId
+		join zbox.Box b 
+			on i.BoxId = b.BoxId
+		join Zbox.University u
+			on b.University = u.Id
+		join sb.University u2
+			on u.UniversityName = u2.Name) t (University, tId) on t.tId = D.Id
+commit
