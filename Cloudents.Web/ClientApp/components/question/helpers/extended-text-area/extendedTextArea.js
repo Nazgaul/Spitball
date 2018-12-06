cbc3155ce70503848bdbaa169c121318a9abc3c3
Gabriel <gaby.str@gmail.com>
2018-12-06 16:10:28 +0200

    case 11787 case 11782

--- Cloudents.Web/ClientApp/components/question/helpers/extended-text-area/extendedTextArea.js
+++ Cloudents.Web/ClientApp/components/question/helpers/extended-text-area/extendedTextArea.js
@@ -1,16 +1,18 @@
 import colorsSet from '../colorsSet';
-import Vue from 'vue';
+//import Vue from 'vue';
 import FileUpload from 'vue-upload-component/src';
 //docs here https://lian-yue.github.io/vue-upload-component   --vue-upload-component--
-Vue.component('file-upload', FileUpload);
+//Vue.component('file-upload', FileUpload);
 export default {
-
     props: {
         value: {type: String},
         error: {},
         actionType: {type: String, default: 'answer'},
         isFocused: false,
         uploadUrl: {type: String}
+    },
+    components:{
+        FileUpload
     },
     data() {
         return {
@@ -23,11 +25,11 @@ export default {
             },
             isFirefox: global.isFirefox,
             files: [],
-            extensions: ['jpeg', 'jpe', 'jpg', 'gif', 'png', 'webp', 'bmp']
+            extensions: ['jpeg', 'jpe', 'jpg', 'gif', 'png', 'webp', 'bmp'],
+            componentUniqueId: `instance-${this._uid}` 
         }
-
-
     },
+    
     methods: {
         updateValue: function (value) {
             this.$emit('input', value);
@@ -126,5 +128,7 @@ export default {
         });
 
     },
-
+created(){
+    console.log(`router path: ${this.$route.fullPath} component`, this)
+}
 }
