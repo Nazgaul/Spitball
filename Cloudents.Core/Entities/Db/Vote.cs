d144d1f859c33dce87b9f014e679ca2155ffffe4
Ram <ram.y@outlook.com>
2018-12-11 15:30:26 +0200

    Started to work on votes.

---
+++ Cloudents.Core/Entities/Db/Vote.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cloudents.Core.Entities.Db
+{
+    public class Vote
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public Document Document { get; set; }
+    }
+}
