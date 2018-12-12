e5325f50f8b32cf3bb8c2b3dbb47ce9604faa00a
Ram <ram.y@outlook.com>
2018-12-12 12:49:47 +0200

    Fixes of delete.

---
+++ Cloudents.Core/Entities/Db/ItemComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Cloudents.Core.Enum;
+
+namespace Cloudents.Core.Entities.Db
+{
+    [SuppressMessage("ReSharper", "VirtualMemberCallInConstructor", Justification = "nhibernate proxy")]
+
+    public class ItemComponent
+    {
+        public ItemComponent()
+        {
+            Votes = new List<Vote>();
+        }
+        public virtual ItemState State { get; set; }
+        public virtual DateTime? DeletedOn { get; set; }
+        public virtual string FlagReason { get; set; }
+
+        public virtual ICollection<Vote> Votes { get; protected set; }
+
+        public virtual int VoteCount { get; set; }
+    }
+}
--- Cloudents.Core/Entities/Db/RowDetail.cs
+++ Cloudents.Core/Entities/Db/RowDetail.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using Cloudents.Core.Enum;
 
 namespace Cloudents.Core.Entities.Db
 {
@@ -34,22 +31,5 @@ namespace Cloudents.Core.Entities.Db
 
         public virtual DateTime CreationTime { get; private set; }
         public virtual DateTime UpdateTime { get; private set; }
-    }
-
-    [SuppressMessage("ReSharper", "VirtualMemberCallInConstructor", Justification = "nhibernate proxy")]
-
-    public class ItemComponent
-    {
-        public ItemComponent()
-        {
-            Votes = new List<Vote>();
-        }
-        public virtual ItemState State { get; set; }
-        public virtual DateTime? DeletedOn { get; set; }
-        public virtual string FlagReason { get; set; }
-
-        public virtual ICollection<Vote> Votes { get; protected set; }
-
-        public virtual int VoteCount { get; set; }
     }
 }
