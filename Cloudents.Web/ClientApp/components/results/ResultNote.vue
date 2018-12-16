ca7ed5e69663ea18f0605f7c07daaf1f002ffe38
Gabriel <gaby.str@gmail.com>
2018-12-16 16:34:42 +0200

    case 11850 case 11856

--- Cloudents.Web/ClientApp/components/results/ResultNote.vue
+++ Cloudents.Web/ClientApp/components/results/ResultNote.vue
@@ -1,6 +1,6 @@
 <template>
     <a
-            :target="($vuetify.breakpoint.xsOnly)?'_self':'_blank'"
+            :target="($vuetify.breakpoint.xsOnly || isOurs)?'_self':'_blank'"
             @click.native="(isOurs ? $_spitball($event):'')"
             :href="url"
             :class="['d-block', 'note-block']">
