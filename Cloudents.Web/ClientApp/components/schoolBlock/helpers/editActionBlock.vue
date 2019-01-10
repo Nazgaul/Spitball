c9ff641813854fc95d82f3bf08b803d85ce17636
crow7m <crow7m@gmail.com>
2019-01-10 18:06:55 +0200

    almost there bug with id edit-block

---
+++ Cloudents.Web/ClientApp/components/schoolBlock/helpers/editActionBlock.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="edit-action-block" >
+        <div class="edit-action d-flex align-baseline">
+            <span  class="edit-class mr-2" v-language:inner>schoolBlock_edit_classes</span>
+            <v-icon class="edit-icon">sbf-edit-icon</v-icon>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "editActionBlock"
+    }
+</script>
+
+<style lang="less">
+    @import "../../../styles/mixin.less";
+    @backgroundGrey: #ececec;
+
+    .edit-action-block {
+        background-color: @backgroundGrey;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding: 8px;
+        @media(max-width: @screen-xs){
+            box-shadow: 0 0 11px 0 rgba(0, 0, 0, 0.43);
+            z-index: 9;
+        }
+        .edit-action {
+            cursor: pointer;
+            z-index: 1;
+        }
+        .edit-icon {
+            font-size: 16px;
+        }
+        .edit-class {
+            font-family: @fontFiraSans;
+            font-size: 13px;
+            letter-spacing: -0.3px;
+            color: @colorBlackNew;
+        }
+    }
+
+</style>
--- Cloudents.Web/ClientApp/components/schoolBlock/schoolBlock.vue
+++ Cloudents.Web/ClientApp/components/schoolBlock/schoolBlock.vue
@@ -1,78 +1,92 @@
 <template>
-    <div style="display: flex;" class="school-block">
-        <v-layout row>
-            <v-flex xs12>
-                <div class="content-wrap">
-                    <div class="university-holder d-flex" row>
-                        <div v-show="schoolName">
-                            <v-icon class="university-icon">sbf-university-columns</v-icon>
-                            <span class="university-name">{{schoolName}}</span>
+    <transition name="fade">
+        <div style="" class="school-block">
+            <v-layout row>
+                <v-flex xs12>
+                    <div class="content-wrap">
+                        <div class="university-holder d-flex" row>
+                            <div v-show="schoolName && !mobileFilterState">
+                                <v-icon class="university-icon">sbf-university-columns</v-icon>
+                                <span class="university-name">{{schoolName}}</span>
+                            </div>
+                            <div v-show="mobileFilterState">
+                                <span class="filter-text" v-language:inner>schoolBlock_filter_classes</span>
+                            </div>
+                            <div class="empty-school" v-show="!schoolName" @click="openPersonalizeUniversity()">
+                                <v-icon class="empty-university-icon">sbf-university-columns</v-icon>
+                                <span class="empty-university-name"
+                                      v-language:inner>schoolBlock_school_empty_text</span>
+                            </div>
                         </div>
-                        <div class="empty-school" v-show="!schoolName" @click="openPersonalizeUniversity()">
-                            <v-icon class="empty-university-icon">sbf-university-columns</v-icon>
-                            <span class="empty-university-name" v-language:inner>schoolBlock_school_empty_text</span>
-                        </div>
-                    </div>
-                    <div class="classes-holder">
-                        <div v-show="!showAllClassesBlock">
-                            <v-chip name="sbf-class-chip" v-if="isClassesSet" class="sbf-class-chip mb-2"
-                                    v-for="(singleClass, index) in classesList"
-                                    @click="updateClass(singleClass)"
-                                    :selected="singleClass.isSelected"
-                                    v-show="index < desClassesToShow"
-                                    :key="index">{{singleClass.text}}
-                            </v-chip>
-                            <v-chip name="sbf-class-chip" class="sbf-class-chip total border-none mb-2"
-                                    v-show="classesList.length > desClassesToShow"
-                                    @click="openAllClasses()">
-                            <span>
-                               {{classesPlus}}
-                            </span>
-                            </v-chip>
-                            <v-chip v-if="!isClassesSet" name="add class" class="sbf-class-chip empty-state-class mb-2"
-                                    outline
-                                    @click="openPersonalizeCourse()">
-                                <v-icon class="edit-icon">sbf-edit-icon</v-icon>
-                                <span v-language:inner>schoolBlock_add_class</span>
-                            </v-chip>
-                        </div>
+                        <div class="classes-holder" >
+                            <div v-show="!showAllClassesBlock"
+                                 :class="[$vuetify.breakpoint.xsOnly ? 'd-flex  wrap-it align-start' : '']">
+                                <transition-group name="list">
+                                    <v-chip name="sbf-class-chip list-item" key="chip_one"
+                                            v-for="(singleClass, index) in classesList"
+                                            v-if="isClassesSet"
+                                            class="sbf-class-chip"
+                                            :class="[$vuetify.breakpoint.xsOnly ? 'mb-2' : '',
+                                            mobileFilterState ?  'full-width-chip' : '']"
+                                            @click="updateClass(singleClass)"
+                                            :selected="singleClass.isSelected"
+                                            v-show="index < classesToShow"
+                                            :key="index">{{singleClass.text}}
+                                    </v-chip>
+                                </transition-group>
+                                <transition name="fade-total">
+                                    <v-chip name="sbf-class-chip" key="chip_two"
+                                            class="sbf-class-chip total"
+                                            :class="[$vuetify.breakpoint.smAndUp ? 'border-none' : '' ]"
+                                            v-show="classesList.length > classesToShow"
+                                            @click.prevent.stop="openAllClasses()">
+                                        <span>
+                                           {{classesPlus}}
+                                        </span>
+                                    </v-chip>
+                                </transition>
 
+                                <v-chip v-if="!isClassesSet" name="add class" class="sbf-class-chip empty-state-class"
 
-                        <div class="all-classes" v-if="showAllClassesBlock" transition="fade-transition">
-                            <div class="classes-list-wrap">
-                                <v-chip name="sbf-class-chip" class="class-chip-item mb-2"
-                                        v-for="(singleClass, index) in classesList"
-                                        @click="updateClass(singleClass)"
-                                        :selected="singleClass.isSelected"
-                                        :key="index">{{singleClass.text}}
+                                        @click="openPersonalizeCourse()">
+                                    <v-icon class="edit-icon">sbf-edit-icon</v-icon>
+                                    <span v-language:inner>schoolBlock_add_class</span>
                                 </v-chip>
                             </div>
-                            <div class="edit-action-block">
-                                <div>
-                                    <v-icon>sbf-edit-icon</v-icon>
-                                    <span v-language:inner>schoolBlock_edit_classes</span>
+                            <div class="all-classes" v-show="showAllClassesBlock" transition="fade-transition"
+                                 id="school_block_classesList">
+                                <div class="classes-list-wrap">
+                                    <v-chip class="class-chip-item"
+                                            v-for="(singleClass, index) in classesList"
+                                            @click="updateClass(singleClass)"
+                                            :selected="singleClass.isSelected"
+                                            :key="index">{{singleClass.text}}
+                                    </v-chip>
                                 </div>
+                                <edit-action-block  @click.prevent.stop="openPersonalizeCourse()"></edit-action-block>
                             </div>
                         </div>
                     </div>
-                </div>
-            </v-flex>
-        </v-layout>
-
-    </div>
+                </v-flex>
+            </v-layout>
+            <edit-action-block id="edit-mobile-target" v-show="mobileFilterState"  @click.prevent="openPersonalizeCourse()"></edit-action-block>
+        </div>
+    </transition>
 </template>
 
 <script>
     import { mapGetters, mapActions, mapMutations } from 'vuex';
     import schoolBlockService from '../../services/schoolBlockService'
-
+    import editActionBlock from './helpers/editActionBlock.vue'
     export default {
+        components:{editActionBlock},
         name: "schoolBlock",
         data() {
             return {
                 showAllClassesBlock: false,
                 selectedChips: {},
-                desClassesToShow: 5,
+                classesToShow: this.$vuetify.breakpoint.smAndUp ? 5 : 3,
+                mobileFilterState: false
             }
         },
         computed: {
@@ -84,7 +98,7 @@
             ]),
             classesPlus() {
                 if (!!this.classesList)
-                    return `+${this.classesList.length - this.desClassesToShow}`
+                    return `+${this.classesList.length - this.classesToShow}`
             },
             isLoggedIn() {
                 return !!this.accountUser
@@ -142,12 +156,21 @@
                 this.$router.push({query: newQueryObject});
             },
             openAllClasses() {
-                this.showAllClassesBlock = true;
+                if (this.$vuetify.breakpoint.smAndUp) {
+                    this.showAllClassesBlock = true
+                } else {
+                    this.classesToShow = this.classesList.length;
+                    this.mobileFilterState = true;
+                }
+            },
+            closeAllClasses() {
+                this.showAllClassesBlock = false;
             },
             openPersonalizeCourse() {
                 if (!this.isLoggedIn) {
                     this.updateLoginDialogState(true);
                 } else {
+                    this.closeAllClasses();
                     let steps = this.getAllSteps;
                     this.updateCurrentStep(steps.set_class);
                     this.changeSelectUniState(true);
@@ -162,17 +185,40 @@
                     this.changeSelectUniState(true);
                 }
             },
-
-
+            detectOutsideClick(event) {
+                let specifiedElement = document.getElementById('school_block_classesList');
+                let isClickInside = specifiedElement.contains(event.target);
+                if (this.$vuetify.breakpoint.smAndUp && this.showAllClassesBlock && !isClickInside) {
+                    this.closeAllClasses();
+                } else {
+                    if(event.target.id && event.target.id ==="edit-mobile-target"){
+                        this.openPersonalizeCourse()
+                    }else{
+                        this.classesToShow = 3;
+                        this.mobileFilterState = false;
+                    }
+                }
+            }
+        },
+        beforeMount: function () {
+            if (document) {
+                document.addEventListener('click', this.detectOutsideClick)
+            }
+        },
+        beforeDestroy: function () {
+            if (document) {
+                document.removeEventListener('click', this.detectOutsideClick)
+            }
         },
         created() {
             if (!!this.$route.query.Course) {
-                let courses = [].concat(this.$route.query.Course)
+                let courses = [].concat(this.$route.query.Course);
                 courses.forEach(courseName => {
                     this.selectedChips[courseName] = true;
                 })
             }
         }
+
     }
 
 </script>
