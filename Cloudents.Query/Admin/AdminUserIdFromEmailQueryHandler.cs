5c7820da770e48ee34b71793ce7aa9373f70553b
Hadar Keidar <Hadar@cloudents.com>
2019-01-20 16:20:24 +0200

    Added Email as input to UserInfo API

---
+++ Cloudents.Query/Admin/AdminUserIdFromEmailQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Cloudents.Infrastructure.Data;
+using Cloudents.Query.Query.Admin;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cloudents.Query.Admin
+{
+    public class AdminUserIdFromEmailQueryHandler : IQueryHandler<AdminUserIdFromEmailQuery, long>
+    {
+        private readonly DapperRepository _dapper;
+
+        public AdminUserIdFromEmailQueryHandler(DapperRepository dapper)
+        {
+            _dapper = dapper;
+        }
+
+        public async Task<long> GetAsync(AdminUserIdFromEmailQuery query, CancellationToken token)
+        {
+            var result = await _dapper.WithConnectionAsync(async connection =>
+            {
+                var grid = connection.QueryFirst<long>("select top 1 * from sb.[User] where email = @Email", new { query.Email });
+                return grid;
+            }, token);
+            return result;
+        }
+    }
+}
