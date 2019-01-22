88c1dc5b5157267c323fa6b4ee2313de3034490a
Ram <ram.y@outlook.com>
2019-01-22 13:07:54 +0200

    Remove test method

--- Cloudents.FunctionsV2/CommunicationFunction.cs
+++ Cloudents.FunctionsV2/CommunicationFunction.cs
@@ -50,29 +50,29 @@ namespace Cloudents.FunctionsV2
             log.LogInformation("finish sending email");
         }
 
-        [FunctionName("FunctionEmailTest")]
-        public static async Task EmailFunctionTimerAsync(
-            [TimerTrigger("0 */1 * * * *", RunOnStartup = true)]TimerInfo myTimer,
-            [SendGrid(ApiKey = "SendgridKey", From = "Spitball <no-reply @spitball.co>")]
-            IAsyncCollector<SendGridMessage> emailProvider,
+        //[FunctionName("FunctionEmailTest")]
+        //public static async Task EmailFunctionTimerAsync(
+        //    [TimerTrigger("0 */1 * * * *", RunOnStartup = true)]TimerInfo myTimer,
+        //    [SendGrid(ApiKey = "SendgridKey", From = "Spitball <no-reply @spitball.co>")]
+        //    IAsyncCollector<SendGridMessage> emailProvider,
 
-            [Inject] ILifetimeScope lifetimeScope,
-            IBinder binder,
-            ILogger log,
-            CancellationToken token)
-        {
+        //    [Inject] ILifetimeScope lifetimeScope,
+        //    IBinder binder,
+        //    ILogger log,
+        //    CancellationToken token)
+        //{
 
 
-            var message = new DocumentPurchasedMessage(Guid.Parse("03467747-BB06-438F-9F53-A9C600F2FABC"));
+        //    var message = new DocumentPurchasedMessage(Guid.Parse("03467747-BB06-438F-9F53-A9C600F2FABC"));
 
-            var handlerType =
-                typeof(ISystemOperation<>).MakeGenericType(message.GetType());
-            using (var child = lifetimeScope.BeginLifetimeScope())
-            {
-                dynamic operation = child.Resolve(handlerType);
-                await operation.DoOperationAsync((dynamic)message, binder, token);
-            }
-        }
+        //    var handlerType =
+        //        typeof(ISystemOperation<>).MakeGenericType(message.GetType());
+        //    using (var child = lifetimeScope.BeginLifetimeScope())
+        //    {
+        //        dynamic operation = child.Resolve(handlerType);
+        //        await operation.DoOperationAsync((dynamic)message, binder, token);
+        //    }
+        //}
 
         private static async Task ProcessEmail(IAsyncCollector<SendGridMessage> emailProvider, ILogger log,
             BaseEmail topicMessage, CancellationToken token)
