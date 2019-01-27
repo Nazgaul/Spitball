42a32b49fe1ddb949fae8acd600635c6f01f8e18
crow7m <crow7m@gmail.com>
2019-01-27 16:16:00 +0200

    onboard almost there

--- Cloudents.Web/ClientApp/components/helpers/onBoardGuide/onBoardSteps/welcomeStep.vue
+++ Cloudents.Web/ClientApp/components/helpers/onBoardGuide/onBoardSteps/welcomeStep.vue
@@ -1,13 +1,49 @@
 <template>
-    
+    <div class="onboard-step-wrap">
+        <div class="title-row"  v-show="$vuetify.breakpoint.smAndUp">
+          <span class="step-title"  v-language:inner>onboard_welcome_title</span>
+            <span class="step-subtitle"  v-language:inner>welcome_subtitle</span>
+        </div>
+        <div class="title-row" v-show="$vuetify.breakpoint.xsOnly">
+            <span class="step-title" v-language:inner>onboard_welcome_title</span>
+        </div>
+        <div class="images-row" >
+            <div class="left-block" v-if="!$vuetify.breakpoint.xsOnly">
+                <div class="img-wrap">
+                    <bell-image  class="block-image"></bell-image>
+                </div>
+                <div class="text-wrap">
+                    <span class="bottom-title">onboard_welcome_bottom_title_learn</span>
+                    <span  class="bottom-subtitle">onboard_welcome_bottom_subtitle_learn</span>
+                </div>
+            </div>
+            <div class="right-block">
+                <div class="img-wrap">
+                    <list-image  class="block-image"></list-image>
+                </div>
+                <div class="text-wrap" v-show="$vuetify.breakpoint.smAndUp">
+                    <span class="bottom-title" v-language:inner>onboard_welcome_bottom_title_earn</span>
+                    <span class="bottom-subtitle" v-language:inner>onboard_welcome_bottom_subtitle_earn</span>
+                </div>
+                <div class="text-wrap" v-show="$vuetify.breakpoint.xsOnly">
+                    <span class="bottom-subtitle" v-language:inner>onboard_welcome_bottom_subtitle_earn</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import listImage from '../images/list.svg';
+    import bellImage from '../images/earn_1st_step.svg'
     export default {
+        components: {listImage, bellImage},
         name: "welcomeStep"
     }
 </script>
 
-<style scoped>
+<style lang="less">
+@import "./onBoardSteps.less";
+
 
 </style>
