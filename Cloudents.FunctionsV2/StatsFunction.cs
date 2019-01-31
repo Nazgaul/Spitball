ea47c1323498bcbf27e659b7c8a38cfad86fafc6
Ram <ram.y@outlook.com>
2019-01-31 09:27:46 +0200

    Remove $ from system on server.

--- Cloudents.FunctionsV2/StatsFunction.cs
+++ Cloudents.FunctionsV2/StatsFunction.cs
@@ -3,8 +3,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Cloudents.Command;
-using Cloudents.Command.Command;
+using Dapper;
+using Cloudents.Infrastructure.Data;
 using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
@@ -13,14 +13,20 @@ namespace Cloudents.FunctionsV2
     public static class StatsFunction
     {
         [FunctionName("UpdateStatsFunction")]
-        public static async Task Run([TimerTrigger("0 0 0 * * *", RunOnStartup = true)]TimerInfo myTimer,
-            [Inject] ICommandBus commandBus,
+        public static async Task Run([TimerTrigger("0 0 0 * * *")]TimerInfo myTimer,
+            [Inject] DapperRepository repository,
             ILogger log,
             CancellationToken token)
         {
             log.LogInformation("UpdateStatsFunction invoke");
-            var command = new UpdateHomeStatsCommand();
-            await commandBus.DispatchAsync(command, token);
+            using (var conn = repository.OpenConnection())
+            {
+                await conn.ExecuteAsync(@"update sb.[HomeStats]
+                            set[users] = (select count(1) from sb.[User])
+	                        ,[answers] = (select count(1) from sb.Answer)
+	                        ,[SBLs] = (select sum(price) from sb.[Transaction] where[Type] = 'Earned')
+	                        )");
+            }
             log.LogInformation($"UpdateStatsFunction function executed at: {DateTime.Now}");
         }
     }
