eef71545c50cb106518419649f1c587576a4af46
Hadar Keidar <Hadar@cloudents.com>
2019-02-03 12:29:17 +0200

    small fix

--- Cloudents.Query/Admin/AdminUserDetailsQueryHandler.cs
+++ Cloudents.Query/Admin/AdminUserDetailsQueryHandler.cs
@@ -21,19 +21,19 @@ namespace Cloudents.Query.Admin
 
         public async Task<UserDetailsDto> GetAsync(AdminUserDetailsQuery query, CancellationToken token)
         {
-            var sql = @"select U.Id, U.Name, Email, PhoneNumberHash as PhoneNumber, Un.Name as University, U.Country, U.Score, U.FraudScore, isnull(count(distinct T.Id),0) as ReferredCount, U.Balance, 
-	            case when U.LockOutEnd is null or U.LockOutEnd < getutcdate() then 1
-	            else 0 end as IsActive,
-				isnull((select min(Created) from sb.[Transaction] where [Action] = 'SignUp' and User_id = U.Id),cast(0 as datetime)) as Joined
-                            from sb.[User] U
-                            join sb.University Un
-	                            on U.UniversityId2 = Un.Id
-                            left join sb.[Transaction] T
-	                            on U.Id = T.[User_id] and T.[Action] = 'ReferringUser'
-                            where U.Id = @Id
-                group by U.Id, U.Name, Email, PhoneNumberHash, Un.Name, U.Country, U.Score, U.FraudScore, U.Balance, 
-                case when U.LockOutEnd is null or U.LockOutEnd < getutcdate() then 1
-	                else 0 end";
+            var sql = @"select U.Id, U.Name, Email, PhoneNumberHash as PhoneNumber, Un.Name as University, U.Country, U.Score, U.FraudScore, 
+		(select count(1) from sb.[Transaction] T where  U.Id = T.[User_id] and T.[Action] = 'ReferringUser')  as ReferredCount,
+		U.Balance, 
+	        case when U.LockOutEnd is null or U.LockOutEnd < getutcdate() then 1
+	        else 0 end as IsActive,
+			isnull((select min(Created) from sb.[Transaction] where [Action] = 'SignUp' and User_id = U.Id),cast(0 as datetime)) as Joined
+                        from sb.[User] U
+                        left join sb.University Un
+	                        on U.UniversityId2 = Un.Id
+                        where U.Id = @Id
+            group by U.Id, U.Name, Email, PhoneNumberHash, Un.Name, U.Country, U.Score, U.FraudScore, U.Balance, 
+            case when U.LockOutEnd is null or U.LockOutEnd < getutcdate() then 1
+	            else 0 end";
             using (var connection = new SqlConnection(_provider.Db.Db))
             {
                 return await connection.QuerySingleOrDefaultAsync<UserDetailsDto>(sql,
