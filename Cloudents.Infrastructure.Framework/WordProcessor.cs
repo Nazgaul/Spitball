6cf8ec484bd3594de2dfa88fc9360b36e947f5b6
Ram <ram.y@outlook.com>
2019-02-03 17:38:56 +0200

    Added lazy loading in several processor.

--- Cloudents.Infrastructure.Framework/WordProcessor.cs
+++ Cloudents.Infrastructure.Framework/WordProcessor.cs
@@ -36,16 +36,17 @@ namespace Cloudents.Infrastructure.Framework
             }
         }
 
-        private Document _word;
+        private Lazy<Document> _word;
 
         public void Init(Stream stream)
         {
-            _word = new Document(stream);
+            _word = new Lazy<Document>(()=> new Document(stream));
         }
         public (string text, int pagesCount) ExtractMetaContent()
         {
-            var txt = ExtractDocumentText(_word);
-            return (txt, _word.PageCount);
+            var t = _word.Value;
+            var txt = ExtractDocumentText(t);
+            return (txt, t.PageCount);
         }
 
        
@@ -62,7 +63,7 @@ namespace Cloudents.Infrastructure.Framework
                 PageCount = 1
             };
 
-            var diff = Enumerable.Range(0, _word.PageCount);
+            var diff = Enumerable.Range(0, _word.Value.PageCount);
             diff = diff.Except(previewDelta);
             foreach (var item in diff)
             {
@@ -72,7 +73,7 @@ namespace Cloudents.Infrastructure.Framework
                 }
 
                 var ms = new MemoryStream();
-                _word.Save(ms, svgOptions);
+                _word.Value.Save(ms, svgOptions);
                 ms.Seek(0, SeekOrigin.Begin);
                 t.Add(pagePreviewCallback(ms, $"{item}.jpg").ContinueWith(_ => ms.Dispose(), token));
             }
