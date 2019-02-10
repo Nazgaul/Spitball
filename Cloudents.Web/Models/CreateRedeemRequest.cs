10d5f6bf325723a659ee17274ecc12d244432663
Ram <ram.y@outlook.com>
2019-02-10 17:45:04 +0200

    SPITBALL-475 Do not grant SBL to Tier 3 users after Registration
    Remove the Amount getting from client side on redeem. there is only 1000

--- Cloudents.Web/Models/CreateRedeemRequest.cs
+++ Cloudents.Web/Models/CreateRedeemRequest.cs
@@ -1,36 +1,35 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 
 namespace Cloudents.Web.Models
 {
     public class CreateRedeemRequest
     {
-        [RedeemValidator(ErrorMessage = "InvalidRedeemValue")]
-        public decimal Amount { get; set; }
+        //[RedeemValidator(ErrorMessage = "InvalidRedeemValue")]
+        //public decimal Amount { get; set; }
 
-        public override string ToString()
-        {
-            return $"{nameof(Amount)}: {Amount}";
-        }
+        //public override string ToString()
+        //{
+        //    return $"{nameof(Amount)}: {Amount}";
+        //}
     }
 
-    [System.AttributeUsage(System.AttributeTargets.All)]
-    public sealed class RedeemValidatorAttribute : ValidationAttribute
-    {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            var val = (decimal)value;
+    //[System.AttributeUsage(System.AttributeTargets.All)]
+    //public sealed class RedeemValidatorAttribute : ValidationAttribute
+    //{
+    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    //    {
+    //        var val = (decimal)value;
 
-            var validValues = new[] { 1000M };
+    //        var validValues = new[] { 1000M };
 
-            if (validValues.Contains(val))
-            {
-                return ValidationResult.Success;
-            }
-            return new ValidationResult(ErrorMessage);
-        }
-    }
+    //        if (validValues.Contains(val))
+    //        {
+    //            return ValidationResult.Success;
+    //        }
+    //        return new ValidationResult(ErrorMessage);
+    //    }
+    //}
 
     public sealed class PayPalTransactionRequest
     {
