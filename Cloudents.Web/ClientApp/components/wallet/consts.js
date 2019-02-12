9731424f053d313b6ed74de90c6b7b29ef0e493c
crow7m <crow7m@gmail.com>
2019-02-12 14:33:58 +0200

    wallet redeem clean up

--- Cloudents.Web/ClientApp/components/wallet/consts.js
+++ Cloudents.Web/ClientApp/components/wallet/consts.js
@@ -1,19 +1,4 @@
 export const cashOutCards =  [
-    //{
-    //    cost: 4000,
-    //    pointsForDollar: 40,
-    //    image: global.country.toLowerCase() === "il" ? "buyme4" :'100',
-    //},
-    //{
-    //    cost: 3000,
-    //    pointsForDollar: 60,
-    //    image: global.country.toLowerCase() === "il" ? "buyme3" : '50',
-    //},
-    //{
-    //    cost: 2000,
-    //    pointsForDollar: 80,
-    //    image: global.country.toLowerCase() === "il" ? "buyme2" : '25',
-    //},
     {
         cost: 1000,
         pointsForDollar: 100,
