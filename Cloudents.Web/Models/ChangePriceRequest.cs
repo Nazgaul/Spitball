482730b7ea3330c6374db7c439474f7d215a282c
Ram <ram.y@outlook.com>
2019-02-18 08:54:46 +0200

    SPITBALL-524 Upload Document - Server needs to check for price limit

--- Cloudents.Web/Models/ChangePriceRequest.cs
+++ Cloudents.Web/Models/ChangePriceRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace Cloudents.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Cloudents.Core.Entities;
+
+namespace Cloudents.Web.Models
 {
     public class ChangePriceRequest
     {
         public long Id { get; set; }
+
+        [Range(0, (int)Document.PriceLimit)]
         public decimal Price { get; set; }
     }
 }
