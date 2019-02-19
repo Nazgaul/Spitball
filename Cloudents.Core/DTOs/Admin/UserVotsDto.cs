486ebd41b019b86ae3d5e43265a182a5d827eb92
Hadar Keidar <Hadar@cloudents.com>
2019-02-19 11:52:11 +0200

    Case SPITBALL-539 Admin Site - Add users action made by them and by other on their content

---
+++ Cloudents.Core/DTOs/Admin/UserVotsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudents.Core.DTOs.Admin
+{
+    public class UserVotsDto
+    {
+        public DateTime Created { get; set; }
+        public string ItemText { get; set; }
+        public char ItemType { get; set; }
+    }
+}
--- Cloudents.Query/Admin/AdminUserVotsQueryHandler.cs
+++ Cloudents.Query/Admin/AdminUserVotsQueryHandler.cs
@@ -11,19 +11,18 @@ using System.Threading.Tasks;
 
 namespace Cloudents.Query.Admin
 {
-    public class AdminUserUpVotsQueryHandler : IQueryHandler<AdminUserUpVotsQuery, IEnumerable<UserUpVotsDto>>
+    public class AdminUserVotsQueryHandler : IQueryHandler<AdminUserVotsQuery, IEnumerable<UserVotsDto>>
     {
         private readonly IConfigurationKeys _provider;
-        public AdminUserUpVotsQueryHandler(IConfigurationKeys provider)
+        public AdminUserVotsQueryHandler(IConfigurationKeys provider)
         {
             _provider = provider;
         }
         private const int PageSize = 200;
 
-
-        public async Task<IEnumerable<UserUpVotsDto>> GetAsync(AdminUserUpVotsQuery query, CancellationToken token)
+        public async Task<IEnumerable<UserVotsDto>> GetAsync(AdminUserVotsQuery query, CancellationToken token)
         {
-            const string sql = @"select Id, CreationTime, 
+            const string sql = @"select CreationTime as Created, 
 	                                case when V.DocumentId is not null then (select Name from sb.Document where Id = V.DocumentId)
 	                                when V.QuestionId is not null then (select Text from sb.Question where Id = V.QuestionId)
 	                                when V.AnswerId is not null then (select Text from sb.Answer where Id = V.AnswerId) end as ItemText,
@@ -31,16 +30,17 @@ namespace Cloudents.Query.Admin
 	                                when V.QuestionId is not null then 'Q'
 	                                when V.AnswerId is not null then 'A'end as ItemType
                                 from sb.Vote V
-                                where VoteType = 1 and UserId = @Id 
-                                order by 1
+                                where VoteType = @Type and UserId = @Id 
+                                order by 1 desc
                                 OFFSET @pageSize * @PageNumber ROWS
                                 FETCH NEXT @pageSize ROWS ONLY;";
             using (var connection = new SqlConnection(_provider.Db.Db))
             {
-                return await connection.QueryAsync<UserUpVotsDto>(sql,
+                return await connection.QueryAsync<UserVotsDto>(sql,
                     new
                     {
                         id = query.Id,
+                        type = query.Type,
                         PageNumber = query.Page,
                         PageSize
                     });
