4d25ad8a11982358f0351fe4124561b35dd77ee5
crow7m <crow7m@gmail.com>
2019-03-04 16:02:09 +0200

    chat done navs started

--- Cloudents.Web/ClientApp/components/tutor/chat/helpers/message.vue
+++ Cloudents.Web/ClientApp/components/tutor/chat/helpers/message.vue
@@ -57,7 +57,7 @@
                 color: white;
                 background: linear-gradient(to bottom, #00D0EA 0%, #0085D1 100%);
                 background-attachment: fixed;
-                padding: 16px;
+                padding: 6px 12px;
                 border-radius: 16px;
                 .message-text{
                     text-align: left;
@@ -68,7 +68,7 @@
                 justify-items: flex-end;
                 background-color: #eee;
                 color: #000000;
-                padding: 16px;
+                padding: 6px 12px;
                 border-radius: 16px;
                 .message-text{
                     text-align: right;
