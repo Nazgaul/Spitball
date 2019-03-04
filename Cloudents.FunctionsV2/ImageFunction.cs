1b072ba338e7bf819a6f7624cba3ff62e9946682
Ram <ram.y@outlook.com>
2019-03-04 17:22:37 +0200

    Added hash to the mix.

--- Cloudents.FunctionsV2/ImageFunction.cs
+++ Cloudents.FunctionsV2/ImageFunction.cs
@@ -1,3 +1,4 @@
+using System;
 using Cloudents.Core;
 using Cloudents.FunctionsV2.Di;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +12,9 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using SixLabors.Primitives;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.DataProtection;
+using Newtonsoft.Json;
+using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
 
 namespace Cloudents.FunctionsV2
 {
@@ -18,20 +22,37 @@ namespace Cloudents.FunctionsV2
     {
         [FunctionName("ImageFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image/{id}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image/{id}/{hash}")]
+            HttpRequest req, string hash, long id,
             [Blob("spitball-files/files/{id}")] CloudBlobDirectory directory,
+            [Inject] IDataProtectionProvider dataProtectProvider,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var dataProtector = dataProtectProvider.CreateProtector("image");
+            if (string.IsNullOrEmpty(hash))
+            {
+                return new BadRequestResult();
+            }
+
+            var objectStr = dataProtector.Unprotect(hash);
+            var properties = JsonConvert.DeserializeObject<ImageProperties>(objectStr);
+
+
+            if (properties.Id != id)
+            {
+                return new BadRequestResult();
+            }
+
             int.TryParse(req.Query["width"], out var width);
             int.TryParse(req.Query["height"], out var height);
-            int.TryParse(req.Query["page"], out var page);
+            
             if (width == 0 || height == 0)
             {
                 return new BadRequestResult();
             }
 
-            var blob = directory.GetBlobReference($"preview-{page}.jpg");
+            var blob = directory.GetBlobReference($"preview-{properties.Page}.jpg");
             using (var sr = await blob.OpenReadAsync())
             {
                 var image = Image.Load<Rgba32>(sr);
@@ -40,6 +61,19 @@ namespace Cloudents.FunctionsV2
                     Mode = ResizeMode.Crop,
                     Size = new Size(width, height)
                 }));
+                if (properties.Blur)
+                {
+                    if (properties.Page == 0)
+                    {
+                        image.Mutate(x => x.BoxBlur(5, new Rectangle(0, height / 2, width, height / 2)));
+                    }
+                    else
+                    {
+                        image.Mutate(x => x.BoxBlur(5));
+
+                    }
+                }
+
                 return new FileCallbackResult("image/jpg", (stream, context) =>
                     {
                         context.HttpContext.Response.Headers.Add("Cache-Control", $"public, max-age={TimeConst.Year}");
@@ -48,9 +82,14 @@ namespace Cloudents.FunctionsV2
                         return Task.CompletedTask;
                     }
                     );
-
-
             }
+        }
+
+        public class ImageProperties
+        {
+            public long Id { get; set; }
+            public int Page { get; set; }
+            public bool Blur { get; set; }
 
         }
     }
