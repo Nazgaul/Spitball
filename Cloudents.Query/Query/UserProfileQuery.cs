6589bab20029a42d4aa3c0feb06d7e185aa18f15
Ram <ram.y@outlook.com>
2019-03-12 16:29:03 +0200

    Remove tags from system in the meantime
    Added constraint to save the user Profile is auth cookie.

--- Cloudents.Query/Query/UserProfileQuery.cs
+++ Cloudents.Query/Query/UserProfileQuery.cs
@@ -41,14 +41,14 @@ where UserId = :id");
                 var coursesFuture = sqlCourses.Future<string>();
 
 
-                var sqlTags = _session.CreateSQLQuery(@"Select  distinct TagId from sb.DocumentsTags where DocumentId in (
-Select id from sb.Document where CourseName in (
-Select CourseId from sb.UsersCourses where userid = :id
-)
-)");
-                sqlTags.SetInt64("id", query.Id);
+//                var sqlTags = _session.CreateSQLQuery(@"Select  distinct TagId from sb.DocumentsTags where DocumentId in (
+//Select id from sb.Document where CourseName in (
+//Select CourseId from sb.UsersCourses where userid = :id
+//)
+//)");
+//                sqlTags.SetInt64("id", query.Id);
 
-                var tagsFuture = sqlTags.Future<string>();
+                //var tagsFuture = sqlTags.Future<string>();
 
                 var universityFuture = _session.Query<RegularUser>()
                     .Fetch(f => f.University)
@@ -59,14 +59,14 @@ Select CourseId from sb.UsersCourses where userid = :id
 
 
                 var courses = await coursesFuture.GetEnumerableAsync(token);
-                var tags = await tagsFuture.GetEnumerableAsync(token);
+               // var tags = await tagsFuture.GetEnumerableAsync(token);
                 var university = universityFuture?.Value;
 
                 return new UserProfile
                 {
                     University = university,
                     Courses = courses?.ToList(),
-                    Tags = tags,
+                    Tags = null,
 
                 };
             }
