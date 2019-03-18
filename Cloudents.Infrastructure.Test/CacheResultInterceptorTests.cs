84b48c1a697ed71e68c3e88534a954f3ae6348c3
Ram <ram.y@outlook.com>
2019-03-18 14:23:32 +0200

    Remove unused code.

--- Cloudents.Infrastructure.Test/CacheResultInterceptorTests.cs
+++ Cloudents.Infrastructure.Test/CacheResultInterceptorTests.cs
@@ -11,18 +11,18 @@ namespace Cloudents.Infrastructure.Test
 {
     public class CacheResultInterceptorTests
     {
-        [Fact]
-        public void GetInvocationSignature_BookDetailPaging_Works()
-        {
-            //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
-            var bookRequest1 = new object[] { "make%20war", 150, 0, CancellationToken.None };
-            var bookRequest2 = new object[] { "make%20war", 150, 1, CancellationToken.None };
-            var type = new PrivateType(typeof(CacheResultInterceptor));
-            var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest1 });
-            var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest2 });
+        //[Fact]
+        //public void GetInvocationSignature_BookDetailPaging_Works()
+        //{
+        //    //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
+        //    var bookRequest1 = new object[] { "make%20war", 150, 0, CancellationToken.None };
+        //    var bookRequest2 = new object[] { "make%20war", 150, 1, CancellationToken.None };
+        //    var type = new PrivateType(typeof(CacheResultInterceptor));
+        //    var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest1 });
+        //    var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest2 });
 
-            Assert.NotEqual(result1, result2);
-        }
+        //    Assert.NotEqual(result1, result2);
+        //}
 
         //[Fact]
         //public void GetInvocationSignature_BingDifferentTerm_Works()
@@ -77,22 +77,22 @@ namespace Cloudents.Infrastructure.Test
         //    Assert.NotEqual(result1, result2);
         //}
 
-        [Fact]
-        public void GetInvocationSignature_DifferentLocation_DifferentResult()
-        {
-            var location = new Location(new GeoPoint(41.878f, -87.629f), new Address("Chicago", "IL", "US"), "31.154.39.170", "972");
-            var location2 = new Location(new GeoPoint(42.878f, -86.629f), new Address("Chicago", "IL", "US"), "31.154.39.170", "972");
-            var model1 = new JobProviderRequest("marketing", JobRequestSort.Relevance, null, location, 0);
-            var model2 = new JobProviderRequest("marketing", JobRequestSort.Relevance, null, location2, 0);
-            //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
-            var bookRequest1 = new object[] { model1, CancellationToken.None };
-            var bookRequest2 = new object[] { model2, CancellationToken.None };
+        //[Fact]
+        //public void GetInvocationSignature_DifferentLocation_DifferentResult()
+        //{
+        //    var location = new Location(new GeoPoint(41.878f, -87.629f), new Address("Chicago", "IL", "US"), "31.154.39.170", "972");
+        //    var location2 = new Location(new GeoPoint(42.878f, -86.629f), new Address("Chicago", "IL", "US"), "31.154.39.170", "972");
+        //    var model1 = new JobProviderRequest("marketing", JobRequestSort.Relevance, null, location, 0);
+        //    var model2 = new JobProviderRequest("marketing", JobRequestSort.Relevance, null, location2, 0);
+        //    //IEnumerable<string> term, int imageWidth, int page, CancellationToken token
+        //    var bookRequest1 = new object[] { model1, CancellationToken.None };
+        //    var bookRequest2 = new object[] { model2, CancellationToken.None };
 
-            var type = new PrivateType(typeof(CacheResultInterceptor));
-            var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest1 });
-            var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest2 });
+        //    var type = new PrivateType(typeof(CacheResultInterceptor));
+        //    var result1 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest1 });
+        //    var result2 = type.InvokeStatic("BuildArgument", BindingFlags.Static | BindingFlags.NonPublic, new object[] { bookRequest2 });
 
-            Assert.NotEqual(result1, result2);
-        }
+        //    Assert.NotEqual(result1, result2);
+        //}
     }
 }
