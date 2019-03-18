3b7fa7017c16d2afcd7835c951a23569bfc41828
crow7m <crow7m@gmail.com>
2019-03-18 18:17:08 +0200

    tutor microphone indication

--- Cloudents.Web/ClientApp/components/tutor/tutorService.js
+++ Cloudents.Web/ClientApp/components/tutor/tutorService.js
@@ -79,13 +79,61 @@ const printNetworkQuality = function (networkQualityLevel) {
     }[networkQualityLevel] || '');
 };
 
+const createAudioContext = function () {
 
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    }).then(stream => {
+        const processInput = audioProcessingEvent => {
+            let array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+            let values = 0;
+
+            let length = array.length;
+            for (let i = 0; i < length; i++) {
+                values += (array[i]);
+            }
+
+            let average = values / length;
+
+            //console.log(Math.round(average - 40));
+            let micVolume = document.getElementById('micVolume_indicator');
+            micVolume.style.backgroundColor = 'rgba(66, 224, 113, 0.16)';
+            micVolume.style.height = '6px';
+            micVolume.style.maxWidth = '150px';
+            micVolume.style.width = `${Math.round(average)}px`;
+
+        };
+
+        // Handle the incoming audio stream
+        // Handle the incoming audio stream
+        const audioContext = new AudioContext();
+        const input = audioContext.createMediaStreamSource(stream);
+        const analyser = audioContext.createAnalyser();
+        const scriptProcessor = audioContext.createScriptProcessor();
+
+        // Some analyser setup
+        analyser.smoothingTimeConstant = 0.3;
+        analyser.fftSize = 1024;
+
+        input.connect(analyser);
+        analyser.connect(scriptProcessor);
+        scriptProcessor.connect(audioContext.destination);
+        scriptProcessor.onaudioprocess = processInput;
+
+    }, error => {
+        // Something went wrong, or the browser does not support getUserMedia
+    });
+
+};
 
 const connectToRoom = function(token, options) {
     // disconnect the user from they joined already
     store.dispatch('leaveRoomIfJoined');
     Twilio.connect(token, options)
         .then((room) => {
+                // add microphone indicator
+                createAudioContext();
                 store.dispatch('updateRoomInstance', room);
                 console.log('Successfully joined a Room: ');
                 store.dispatch('updateRoomLoading', false);
