5ddfe935e1f20ecee9258a1d80dd00645eb5f83a
crow7m <crow7m@gmail.com>
2019-03-19 17:07:04 +0200

    defalt microphone

--- Cloudents.Web/ClientApp/components/tutor/tutorHelpers/qualityValidation/qualityValidation.vue
+++ Cloudents.Web/ClientApp/components/tutor/tutorHelpers/qualityValidation/qualityValidation.vue
@@ -65,6 +65,7 @@
             isErorrGettingMedia() {
                 return this.notAvaliableDevices || this.notAllowedDevices
             }
+
         },
         methods: {
             ...mapActions(['updateTestDialogState']),
