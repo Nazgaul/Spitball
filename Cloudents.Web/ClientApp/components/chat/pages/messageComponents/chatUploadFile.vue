32bb1cbda8ffa6dc76d08b44d5119f70d7d4a5cc
Gabriel <gaby.str@gmail.com>
2019-03-26 13:41:53 +0200

    upload file infrastructure

--- Cloudents.Web/ClientApp/components/chat/pages/messageComponents/chatUploadFile.vue
+++ Cloudents.Web/ClientApp/components/chat/pages/messageComponents/chatUploadFile.vue
@@ -2,14 +2,22 @@
     <div class="chat-upload-wrap">
         <!--Upload Image-->
         <div class="chat-input-container align-center justify-center column">
-            <input class="chat-upload"
-                   type="file" name="File Upload"
-                   @change="uploadChatFiles"
-                   id="chat-file"
-                   ref="chatFiles" v-show="false"/>
             <label for="chat-file">
                 <!--<v-icon class="chat-attach-icon">sbf-attach</v-icon>-->
                 <add-file-img></add-file-img>
+                <file-upload
+                id="file-input"
+                :input-id="componentUniqueId"
+                ref="upload"
+                :drop="false"
+                v-model="uploadedFiles"
+                :multiple="false"
+                :post-action="uploadUrl"
+                accept="image/*"
+                :extensions="extensions"
+                @input-file="inputFile"
+                @input-filter="inputFilter"
+              ></file-upload>
             </label>
         </div>
     </div>
@@ -18,11 +26,17 @@
 <script>
     import { mapGetters, mapActions } from 'vuex';
     import addFileImg from '../../../../font-icon/attach.svg';
+    import FileUpload from 'vue-upload-component/src';
     export default {
         name: "chatUploadFile",
-        components: { addFileImg },
+        components: { FileUpload, addFileImg },
         data() {
             return {
+                componentUniqueId: `instance-${this._uid}`,
+                extensions: ['jpeg', 'jpe', 'jpg', 'gif', 'png', 'webp'],
+                uploadUrl: "/api/upload/ask",
+                uploadedFiles: [],
+                uploadedFileNames: [],
             }
         },
         methods: {
@@ -38,7 +52,68 @@
                     isImageType :  file.type && file.type.includes('image')
                 };
                 self.uploadChatFile(objData);
+            },
+        inputFile: function (newFile, oldFile) {
+            let self = this;
+            if (self.uploadedFiles && self.uploadedFiles.length > 1) {
+                return
             }
+            if (newFile && oldFile && !newFile.active && oldFile.active) {
+                // Get response data
+                // console.log('response', newFile.response);
+                if (newFile.xhr) {
+                    //  Get the response status code
+                    // console.log('status', newFile.xhr.status)
+                    if (newFile.xhr.status === 200) {
+                        // console.log('Succesfully uploadede')
+                        //on after successful loading done, emit to parent to add to list
+                        if (newFile.response && newFile.response.files) {
+                            //self.$emit('addFile', newFile.response.files);
+                            let filename = newFile.response.files;
+                            //if max reached replace
+                            if (this.uploadedFileNames && this.uploadedFileNames.length > 1) {
+                                this.uploadedFileNames.splice(-1, 1, filename);
+                                //add
+                            } else {
+                                this.uploadedFileNames = this.uploadedFileNames.concat(filename);
+                            }
+                            this.populatThumbnailBoxes();
+                        }
+                    }
+                }
+            }
+            if (Boolean(newFile) !== Boolean(oldFile) || oldFile.error !== newFile.error) {
+                if (!this.$refs.upload.active) {
+                    this.$refs.upload.active = true
+                }
+            }
+        },
+        inputFilter: function (newFile, oldFile, prevent) {
+            if (newFile && !oldFile) {
+                //prevent adding new files if maximum reached
+                if (this.uploadedFiles.length >= 1) {
+                    return prevent()
+                }
+                // Filter non-supported extensions  both lower and upper case
+                let patt1 = /\.([0-9a-z]+)(?:[\?#]|$)/i;
+                let ext = (`${newFile.name}`.toLowerCase()).match(patt1)[1];
+                let isSupported = this.extensions.includes(ext);
+                if (!isSupported) {
+                    return prevent()
+                }
+                if (newFile && newFile.size === 0) {
+                    return prevent()
+                }
+            }
+            if (newFile && (!oldFile || newFile.file !== oldFile.file)) {
+                // Create a blob field
+                newFile.blob = '';
+                let URL = window.URL || window.webkitURL;
+                if (URL && URL.createObjectURL) {
+                    newFile.blob = URL.createObjectURL(newFile.file);
+                }
+            }
+        }
         },
     }
 </script>
@@ -48,8 +123,20 @@
         display: flex;
         .chat-input-container{
             display: flex;
+            label{
+                position:relative;
+                .file-uploads{
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    label{
+                        cursor: pointer !important;
+                    }
+                }
+            }
             svg{
-                cursor: pointer;
                 vertical-align: middle;
             }
         }
