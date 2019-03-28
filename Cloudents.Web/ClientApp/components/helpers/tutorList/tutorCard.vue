bdeabc5611e64024e771968171be97e8f3c68697
crow7m <crow7m@gmail.com>
2019-03-28 12:53:12 +0200

    contact btn padding

---
+++ Cloudents.Web/ClientApp/components/helpers/tutorList/tutorCard.vue
@@ -0,0 +1,98 @@
+<template>
+    <v-card class="tutor-card-wrap px-3">
+    <v-layout>
+        <v-flex xs6 sm6 md6>
+            <span class="tutor-name font-weight-bold caption">{{tutorData.name}}</span>
+        </v-flex>
+        <v-flex>
+            <user-rank :score="tutorData.score"></user-rank>
+        </v-flex>
+        <v-flex>
+           <span class="d-flex rounded align-center justify-center">
+                <commentIcon class="chat-icon white"></commentIcon>
+           </span>
+        </v-flex>
+    </v-layout>
+    <v-layout>
+        <v-flex>
+                <!--<img src="" alt="">-->
+            <span class="img-placeholder"></span>
+        </v-flex>
+        <v-flex>
+            <span class="tutor-text caption">
+              {{tutorData.text}}
+            </span>
+        </v-flex>
+    </v-layout>
+    <v-layout>
+        <v-flex>
+            <userRating
+                        :rating="3.5"
+                        :starColor="'#43425D'"
+                        :size="'18'"
+                        :rate-num-color="'#43425D'"></userRating>
+        </v-flex>
+        <v-flex>
+            <span class="pricing">
+                <span class="font-weight-medium subheading pricing">{{tutorData.price}}</span>
+                <span class="small-text pricing" v-language:inner>app_currency_dynamic</span>
+                 <span class="pricing small-text">/hour</span>
+            </span>
+        </v-flex>
+    </v-layout>
+    </v-card>
+</template>
+
+<script>
+    import userRank from '../UserRank/UserRank.vue';
+    import userRating from '../../new_profile/profileHelpers/profileBio/bioParts/userRating.vue';
+    import commentIcon from '../../../font-icon/comment-icon.svg';
+    export default {
+        name: "tutorCard",
+        components: {
+            userRank,
+            userRating,
+            commentIcon
+        },
+        data() {
+            return {
+            };
+        },
+        props: {
+            tutorData: {
+            },
+        },
+    };
+</script>
+
+<style lang="less">
+    @import '../../../styles/mixin.less';
+.tutor-card-wrap{
+    //delete when connected with real data
+    .img-placeholder{
+        height: 40px;
+        width: 40px;
+        border-radius: 50%;
+        background-color: @profileTextColor;
+    }
+    .pricing{
+     font-family: @fontOpenSans;
+     color:@purpleLight;
+    }
+    .small-text{
+        font-size: 10px;
+    }
+    .tutor-text{
+        font-family: @fontOpenSans;
+        font-size: 12px;
+        color: @profileTextColor;
+    }
+    .rounded{
+        height: 32px;
+        width: 32px;
+        border-radius: 50%;
+        background-color: @profileTextColor;
+    }
+
+}
+</style>
---
+++ Cloudents.Web/ClientApp/components/helpers/tutorList/tutorList.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-layout column wrap>
+        <v-flex>
+            <span class="subheading">Course Tutors </span>
+        </v-flex>
+        <v-flex>
+            <tutorCard v-for="tutor in 10" :tutorData="tutor"></tutorCard>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import tutorCard from "./tutorCard.vue";
+    export default {
+        name: "tutorList",
+        components: {tutorCard},
+        data() {
+            return {
+                tutor:{
+                    name : 'Jonathan haisenson',
+                    text : 'Course name 1, Course name 2, Course name 3, Course name 4, Course name 5',
+                    price : '100.00',
+                    score: 650
+                }
+            };
+        },
+        computed: {
+            // ...mapGetters([]);
+        },
+    };
+</script>
+
+<style lang="less">
+
+</style>
