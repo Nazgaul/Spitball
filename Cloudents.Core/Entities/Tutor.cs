e51ea69225420388f3655fa71de1675c138862f5
Hadar Keidar <Hadar@cloudents.com>
2019-03-31 09:42:40 +0300

    Review server validation.
    Tutor small fix

--- Cloudents.Core/Entities/Tutor.cs
+++ Cloudents.Core/Entities/Tutor.cs
@@ -4,9 +4,9 @@ namespace Cloudents.Core.Entities
 {
     public class Tutor
     {
-        public Tutor(string bio, long userId) 
+        public Tutor(string bio, RegularUser user) 
         {
-            UserId = userId;
+            User = user;
             Bio = bio;
             Price = 50;
             //Courses = new HashSet<TutorsCourses>();
@@ -18,7 +18,7 @@ namespace Cloudents.Core.Entities
         }
         public virtual string Bio { get; set; }
         public virtual decimal Price { get; protected set; }
-        public virtual long UserId { get; set; }
+        public virtual RegularUser User { get; set; }
         /*public override string Name => RoleName;
         //private readonly ISet<TutorsCourses> _courses = new HashSet<TutorsCourses>();
         //public virtual IReadOnlyCollection<TutorsCourses> Courses => _courses.ToList();
--- Cloudents.Web/Models/ReviewRequest.cs
+++ Cloudents.Web/Models/ReviewRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace Cloudents.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cloudents.Web.Models
 {
     public class ReviewRequest
     {
+        [StringLength(1000)]
+        [Required(ErrorMessage = "Required")]
         public string Review { get; set; }
+        [Range(0, 5)]
+        [Required(ErrorMessage = "Required")]
         public float Rate { get; set; }
+        [Required(ErrorMessage = "Required")]
         public long Tutor { get; set; }
     }
 }
