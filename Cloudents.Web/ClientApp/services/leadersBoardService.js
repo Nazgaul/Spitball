30c9844591674e8f08c3fdb9af57cfbe8b630294
Gabriel <gaby.str@gmail.com>
2019-04-03 12:38:56 +0300

    spitball-722

--- Cloudents.Web/ClientApp/services/leadersBoardService.js
+++ Cloudents.Web/ClientApp/services/leadersBoardService.js
@@ -3,7 +3,8 @@ function LeaderBoardItem(ObjInit) {
     this.name = ObjInit.name || '';
     this.score = ObjInit.score || '';
     this.university = ObjInit.university || '';
-    this.userId = ObjInit.id || 777
+    this.userId = ObjInit.id || 777;
+    this.img = ObjInit.img;
 }
 
 function createLeaderBoardItem(ObjInit){
