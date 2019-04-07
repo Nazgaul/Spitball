e5d2f1cc8ea1b53cf51b3fb2794ebb298365d63e
crow7m <crow7m@gmail.com>
2019-04-07 17:54:57 +0300

    res added

--- Cloudents.Web/ClientApp/store/account.js
+++ Cloudents.Web/ClientApp/store/account.js
@@ -173,7 +173,7 @@ const mutations = {
     },
     updateEditedData(state, newData){
         if(state.isTutorProfile){
-            state.profile.about.bio = newData.bio
+            state.profile.about.bio = newData.bio;
             state.profile.user.name = newData.name;
             state.profile.user.lastName = newData.lastName;
             state.profile.user.description = newData.description
