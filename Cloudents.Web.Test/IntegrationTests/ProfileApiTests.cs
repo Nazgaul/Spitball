521cfe92fad675031f68f434eec69d851ba890e8
Elad Levavi <apolo1805@hotmail.com>
2019-04-14 16:44:45 +0300

    Move profile api tests from user test to profile test

--- Cloudents.Web.Test/IntegrationTests/ProfileApiTests.cs
+++ Cloudents.Web.Test/IntegrationTests/ProfileApiTests.cs
@@ -57,5 +57,67 @@ namespace Cloudents.Web.Test.IntegrationTests
             courses.Should().NotBeNull();
             reviews.Should().NotBeNull();
         }
+
+        [Theory]
+        [InlineData("/api/profile/159039")]
+        [InlineData("/api/profile/160171")]
+        public async Task GetAsync_OK(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            var str = await response.Content.ReadAsStringAsync();
+
+            var d = JObject.Parse(str);
+
+            var uni = d["universityName"]?.Value<string>();
+            var id = d["id"]?.Value<long?>();
+            var name = d["name"]?.Value<string>();
+            var score = d["score"]?.Value<int?>();
+
+            id.Should().BeGreaterThan(0);
+            name.Should().NotBeNull();
+            score.Should().BeGreaterOrEqualTo(0);
+        }
+
+        [Fact]
+        public async Task GetAsync_NotFound()
+        {
+            var client = _factory.CreateClient(new WebApplicationFactoryClientOptions()
+            {
+                AllowAutoRedirect = false
+            });
+
+            var response = await client.GetAsync("api/profile/1");
+
+            //var str = await response.Content.ReadAsStringAsync();
+
+            //var d = JObject.Parse(str);
+
+            //var status = d["status"]?.Value<int?>();
+
+            response.StatusCode.Should().Be(302);
+        }
+
+        [Theory]
+        [InlineData("api/profile/159039/questions")]
+        [InlineData("api/profile/159039/answers")]
+        [InlineData("api/profile/159039/documents")]
+        [InlineData("api/profile/159039/purchaseDocuments")]
+        public async Task GetAsync_UserTabs_OK(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            var str = await response.Content.ReadAsStringAsync();
+
+            JArray obj = JArray.Parse(str);
+
+            var id = obj.Children();
+
+            id.Should().NotBeNull();
+        }
     }
 }
