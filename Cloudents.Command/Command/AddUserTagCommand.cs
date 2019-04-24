942bbcbfe27488559d44f0167d7143b06c379139
Ram <ram.y@outlook.com>
2019-04-24 15:41:06 +0300

    Remove not used code.

--- Cloudents.Command/Command/AddUserTagCommand.cs
+++ Cloudents.Command/Command/AddUserTagCommand.cs
@@ -1,14 +1,14 @@
-﻿namespace Cloudents.Command.Command
-{
-    public class AddUserTagCommand : ICommand
-    {
-        public AddUserTagCommand(long userId, string tag)
-        {
-            UserId = userId;
-            Tag = tag;
-        }
+﻿//namespace Cloudents.Command.Command
+//{
+//    public class AddUserTagCommand : ICommand
+//    {
+//        public AddUserTagCommand(long userId, string tag)
+//        {
+//            UserId = userId;
+//            Tag = tag;
+//        }
 
-        public long UserId { get;  }
-        public string Tag { get; }
-    }
-}
+//        public long UserId { get;  }
+//        public string Tag { get; }
+//    }
+//}
--- Cloudents.Command/CommandHandler/AddUserTagCommandHandler.cs
+++ Cloudents.Command/CommandHandler/AddUserTagCommandHandler.cs
@@ -1,32 +1,32 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-using Cloudents.Command.Command;
-using Cloudents.Core.Entities;
-using Cloudents.Core.Interfaces;
+﻿//using System.Threading;
+//using System.Threading.Tasks;
+//using Cloudents.Command.Command;
+//using Cloudents.Core.Entities;
+//using Cloudents.Core.Interfaces;
 
-namespace Cloudents.Command.CommandHandler
-{
-    public class AddUserTagCommandHandler : ICommandHandler<AddUserTagCommand>
-    {
-        private readonly ITagRepository _tagRepository;
-        private readonly IRepository<RegularUser> _userRepository;
+//namespace Cloudents.Command.CommandHandler
+//{
+//    public class AddUserTagCommandHandler : ICommandHandler<AddUserTagCommand>
+//    {
+//        private readonly ITagRepository _tagRepository;
+//        private readonly IRepository<RegularUser> _userRepository;
 
-        public AddUserTagCommandHandler(ITagRepository tagRepository, IRepository<RegularUser> userRepository)
-        {
-            _tagRepository = tagRepository;
-            _userRepository = userRepository;
-        }
+//        public AddUserTagCommandHandler(ITagRepository tagRepository, IRepository<RegularUser> userRepository)
+//        {
+//            _tagRepository = tagRepository;
+//            _userRepository = userRepository;
+//        }
 
-        public async Task ExecuteAsync(AddUserTagCommand message, CancellationToken token)
-        {
-            var user = await _userRepository.LoadAsync(message.UserId, token);
-            var tag = await _tagRepository.GetOrAddAsync(message.Tag, token);
-            if (user.Tags.Add(tag))
-            {
-                tag.Count++;
-                await _tagRepository.UpdateAsync(tag, token);
-            }
-            await _userRepository.UpdateAsync(user, token);
-        }
-    }
-}
+//        public async Task ExecuteAsync(AddUserTagCommand message, CancellationToken token)
+//        {
+//            var user = await _userRepository.LoadAsync(message.UserId, token);
+//            var tag = await _tagRepository.GetOrAddAsync(message.Tag, token);
+//            if (user.Tags.Add(tag))
+//            {
+//                tag.Count++;
+//                await _tagRepository.UpdateAsync(tag, token);
+//            }
+//            await _userRepository.UpdateAsync(user, token);
+//        }
+//    }
+//}
