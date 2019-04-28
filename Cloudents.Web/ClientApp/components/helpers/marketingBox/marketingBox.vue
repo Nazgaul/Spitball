13d63976dc1a35b429ae1eb03db773095db76492
crow7m <crow7m@gmail.com>
2019-04-28 16:39:57 +0300

    SPITBALL-962 tutor promo box with the link to google web form

--- Cloudents.Web/ClientApp/components/helpers/marketingBox/marketingBox.vue
+++ Cloudents.Web/ClientApp/components/helpers/marketingBox/marketingBox.vue
@@ -3,55 +3,22 @@
         <div class="heading" v-if="$vuetify.breakpoint.smAndDown">
             <span class="heading-text" v-language:inner>marketingBox_title</span>
         </div>
-        <v-card class="main-marketing-content" :class="imageClassABtest"
+        <v-card class="main-marketing-content transparent"  :class="imageClassABtest"
                 :style="{ 'background-image': 'url(' + require(`${imgSrc}`) + ')' }" @click="promotionOpen()">
         </v-card>
-        <sb-dialog :showDialog="marketingReferral"
-                   :popUpType="'marketingPop'"
-                   :content-class="'login-popup'"
-                   :onclosefn="closeRefDialog">
-            <referral-dialog :onclosefn="closeRefDialog"
-                             :isTransparent="false"
-                             :userReferralLink="userReferralLink"
-                             :popUpType="'marketingPop'"></referral-dialog>
-        </sb-dialog>
-
     </div>
 </template>
 
 <script>
     import { mapGetters, mapActions } from 'vuex';
-    import referralDialog from "../../question/helpers/referralDialog/referral-dialog.vue";
     import Base62 from "base62"
-    import SbDialog from "../../wrappers/sb-dialog/sb-dialog.vue"
     import analyticsService from '../../../services/analytics.service'
 
     export default {
         name: "marketingBox",
-        components: {referralDialog, SbDialog},
         data() {
             return {
-                marketingReferral: false,
-                desktop: {
-                    hebrew: {
-                        logedIn: './images/desktop_Hebrew_LogedIn.png',
-                        not_logedIn: './images/desktop_Hebrew_Not_LogedIn.png',
-                    },
-                    english: {
-                        logedIn: './images/desktop_English_LogedIn.png',
-                        not_logedIn: './images/desktop_English_Not_LogedIn.png',
-                    }
-                },
-                mobile: {
-                    hebrew: {
-                        logedIn: './images/mobile_Hebrew_LogedIn.png',
-                        not_logedIn: './images/mobile_Hebrew_Not_LogedIn.png',
-                    },
-                    english: {
-                        logedIn: './images/mobile_English_LogedIn.png',
-                        not_logedIn: './images/mobile_English_Not_LogedIn.png',
-                    }
-                }
+                googleFormUrl: 'https://forms.gle/f5vDdwS3cDNNJUwA7'
             }
         },
         computed: {
@@ -59,53 +26,33 @@
             isIsrael() {
                 return global.lang.toLowerCase() === 'he'
             },
-            isMobile() {
-                return this.$vuetify.breakpoint.xsOnly
-            },
             isLogedIn() {
                 return (this.accountUser != null)
             },
             imgSrc() {
-                let imageSrc = '';
-                let imagesSet = this.$vuetify.breakpoint.xsOnly ? this.mobile : this.desktop;
-                imagesSet = this.isIsrael ? imagesSet.hebrew : imagesSet.english;
-                imageSrc = this.isLogedIn ? imagesSet["logedIn"] : imagesSet["not_logedIn"];
-                return imageSrc
+                return this.isIsrael ? './images/heb_lessons.png' :  './images/eng_lessons.png'
             },
             imageClassABtest() {
                 return this.imgSrc.replace('./images/', '');
             },
-            userReferralLink() {
-                if (!this.isLogedIn) {
-                    return `${global.location.origin}` + "?promo=referral";
-                } else {
-                    return `${global.location.origin}` + "?referral=" + Base62.encode(this.accountUser.id) + "&promo=referral";
-                }
-            },
-
-
         },
         methods: {
             ...mapActions(['changemobileMarketingBoxState']),
-            closeRefDialog() {
-                this.marketingReferral = false
-            },
             promotionOpen() {
                 if (this.isLogedIn) {
-                    analyticsService.sb_unitedEvent('MARKETING_BOX', 'REFER_FRIEND');
+                    analyticsService.sb_unitedEvent('MARKETING_BOX', 'OPEN_TUTOR');
                 } else {
-                    analyticsService.sb_unitedEvent('MARKETING_BOX', 'UPLOAD_DOC');
+                    analyticsService.sb_unitedEvent('MARKETING_BOX', 'GO TO LOGIN');
                 }
-                return this.isLogedIn ? this.openRefDialog() : this.goToRegister();
+                return this.isLogedIn ? this.goToWebForm() : this.goToRegister();
             },
             goToRegister() {
-                this.changemobileMarketingBoxState();
+                // this.changemobileMarketingBoxState();
                 this.$router.push({name: 'registration'});
             },
-            openRefDialog() {
-                this.marketingReferral = true
+            goToWebForm(){
+                global.open(`${this.googleFormUrl}`, '_blank');
             }
-
         },
     }
 </script>
