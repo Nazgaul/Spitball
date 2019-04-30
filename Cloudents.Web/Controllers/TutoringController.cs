cb026543371717012a035fb63688d7eefa00149a
Ram <ram.y@outlook.com>
2019-04-30 12:51:20 +0300

    Redirect tutoring url to india site.

---
+++ Cloudents.Web/Controllers/TutoringController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cloudents.Web.Controllers
+{
+    public class TutoringController : Controller
+    {
+        // GET: /<controller>/
+        [Route("tutoring")]
+        public IActionResult Index()
+        {
+            return Redirect("https://india.spitball.co/tutoring");
+        }
+
+        [Route("tutoring/{id:guid}")]
+        public IActionResult IndexWithId(Guid id)
+        {
+            return Redirect($"https://india.spitball.co/tutoring/{id}");
+        }
+    }
+}
