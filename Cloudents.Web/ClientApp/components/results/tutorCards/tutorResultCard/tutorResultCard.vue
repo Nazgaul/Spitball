a49461782dbde03892739c81c880a15f70210ba7
crow7m <crow7m@gmail.com>
2019-04-30 10:12:58 +0300

    tutor card desktop

--- Cloudents.Web/ClientApp/components/results/tutorCards/tutorResultCard/tutorResultCard.vue
+++ Cloudents.Web/ClientApp/components/results/tutorCards/tutorResultCard/tutorResultCard.vue
@@ -9,45 +9,54 @@
                     <v-flex>
                         <v-layout align-start justify-space-between column fill-height>
                             <v-flex shrink class="pb-3">
-                                <span class="tutor-name font-weight-bold">{{tutorData.name}}</span>
+                                <span class="tutor-name font-weight-bold" v-line-clamp:18="1">{{tutorData.name}}</span>
                             </v-flex>
                             <v-flex grow>
-                                <span class="tutor-about subheading">
+                                <span class="tutor-about subheading" v-line-clamp:18="2">
+                                    Lorem ipsum dolor amet plaid shaman vinyl hashtag lyft stumptown readymade tofu flexitarian.
                                     Lorem ipsum dolor amet plaid shaman vinyl hashtag lyft stumptown readymade tofu flexitarian.
                                 </span>
                             </v-flex>
-                            <v-flex shrink class="text-truncate tutor-courses">
-                                <span class="blue-text subheading">{{tutorData.courses}}</span>
+                            <v-flex shrink class="tutor-courses">
+                                <span class="blue-text subheading" v-line-clamp:18="1">{{tutorData.courses}}</span>
                             </v-flex>
                         </v-layout>
                     </v-flex>
                 </v-layout>
             </div>
-            <div class="section-tutor-price-review ml-1">
-                    <v-layout row wrap>
-                        <v-flex xs12 sm12 md12>
+            <v-layout row wrap align-start justify-start grow
+                      class="price-review-column section-tutor-price-review ml-1">
+                <v-flex xs12 sm12 md12 grow>
+                    <v-flex xs12 sm12 md12 shrink>
                         <span class="pricing">
                         <span class="font-weight-medium headline  pricing">₪{{tutorData.price}}</span>
-                        <span class="pricing caption">/&nbsp;<span >hour</span></span>
+                        <span class="pricing caption">/&nbsp;<span v-language:inner>resultTutor_hour</span></span>
                         </span>
-                        </v-flex>
-                        <v-flex xs12 sm12 md12>
-                            <userRating
-                                    :rating="tutorData.rating"
-                                    :starColor="'#ffca54'"
-                                    :rateNumColor="'#43425D'"
-                                    :size="'20'"
-                                    :rate-num-color="'#43425D'"></userRating>
+                    </v-flex>
+                    <v-flex xs12 sm12 md12 shrink class="pt-2">
+                        <userRating
+                                :rating="tutorData.rating"
+                                :starColor="'#ffca54'"
+                                :rateNumColor="'#43425D'"
+                                :size="'24'"
+                                :rate-num-color="'#43425D'"></userRating>
 
-                        </v-flex>
-                        <v-flex xs12 sm12 md12>
+                    </v-flex>
+                    <v-flex xs12 sm12 md12 class="pt-1" shrink>
                             <span class="blue-text body-2">{{tutorData.reviews}}
-                            <span>Reviews</span>
+                                <span v-if="tutorData.reviews > 1" v-language:inner>resultTutor_reviews_many</span>
+                                <span v-else v-language:inner>resultTutor_review_one</span>
                             </span>
-                        </v-flex>
-                        <v-flex xs12 sm12 md12></v-flex>
-                    </v-layout>
-            </div>
+                    </v-flex>
+                </v-flex>
+
+                <v-flex xs12 sm12 md12 class="d-flex" style="justify-self: flex-end; margin-top: auto">
+                    <v-btn round class="blue-btn rounded elevation-0 ma-0" block
+                           @click="goToTutorProfile(tutorData.userId)">
+                        <span class="font-weight-bold text-capitalize" v-language:inner>resultTutor_btn_view</span>
+                    </v-btn>
+                </v-flex>
+            </v-layout>
         </v-layout>
     </div>
 </template>
@@ -57,7 +66,6 @@
     import userRating from '../../../new_profile/profileHelpers/profileBio/bioParts/userRating.vue';
     import commentIcon from '../../../../font-icon/message-icon.svg';
     import userAvatar from '../../../helpers/UserAvatar/UserAvatar.vue';
-    import chatService from '../../../../services/chatService';
     import { mapActions, mapGetters } from 'vuex';
     import utilitiesService from "../../../../services/utilities/utilitiesService";
 
@@ -78,28 +86,28 @@
         computed: {
             ...mapGetters(['accountUser', 'getConversations']),
             userImageUrl() {
-                if(this.tutorData.image){
-                    return utilitiesService.proccessImageURL(this.tutorData.image, 166, 186)
-                }else{
-                    return './images/placeholder-profile.png'
+                if(this.tutorData.image) {
+                    return utilitiesService.proccessImageURL(this.tutorData.image, 166, 186);
+                } else {
+                    return './images/placeholder-profile.png';
                 }
 
             }
         },
         methods: {
             ...mapActions(['updateLoginDialogState', 'setActiveConversationObj', 'openChatInterface', 'changeFooterActiveTab']),
-            goToTutorProfile(userId){
-                this.$router.push({name:'profile', params:{id: userId}})
+            goToTutorProfile(userId) {
+                this.$router.push({name: 'profile', params: {id: userId}});
             },
-            sendMessage() {
-                if(this.accountUser == null) {
-                    this.updateLoginDialogState(true);
-                } else {
-                    let currentConversationObj = chatService.createActiveConversationObj(this.tutorData);
-                    this.setActiveConversationObj(currentConversationObj);
-                    this.openChatInterface();
-                }
-            }
+            // sendMessage() {
+            //     if(this.accountUser == null) {
+            //         this.updateLoginDialogState(true);
+            //     } else {
+            //         let currentConversationObj = chatService.createActiveConversationObj(this.tutorData);
+            //         this.setActiveConversationObj(currentConversationObj);
+            //         this.openChatInterface();
+            //     }
+            // }
         }
     };
 </script>
@@ -111,7 +119,7 @@
     .tutor-card-wrap-desk {
         width: 100%;
         margin: 0 auto;
-        .section-tutor-info{
+        .section-tutor-info {
             max-width: @cellWidth;
             background-color: @color-white;
             border-radius: 4px;
@@ -121,8 +129,8 @@
                 padding: 12px;
             }
         }
-        .section-tutor-price-review{
-            max-width: 260px;
+        .section-tutor-price-review {
+            width: 260px;
             background-color: @color-white;
             border-radius: 4px;
             padding: 16px;
@@ -131,26 +139,30 @@
                 padding: 12px;
             }
         }
-
-        .tutor-image{
+        .blue-btn {
+            background-color: @colorBlue !important;
+            color: @color-white;
+            height: 42px;
+        }
+        .tutor-image {
             border-radius: 4px;
             width: 166px;
             height: 186px;
         }
-        .tutor-name{
+        .tutor-name {
             opacity: 0.9;
             font-size: 18px;
             letter-spacing: -0.4px;
             color: @textColor;
         }
-        .tutor-about{
+        .tutor-about {
             color: @profileTextColor;
         }
-        .blue-text{
+        .blue-text {
             color: @colorBlue;
         }
-        .tutor-courses{
-            max-width: 180px!important;
+        .tutor-courses {
+            max-width: 180px !important;
         }
     }
 
