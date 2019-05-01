6c26f0007f78a571eacf55f2dde18800239ec2f9
Ram <ram.y@outlook.com>
2019-05-01 15:05:12 +0300

    XXX

--- Cloudents.Web/Models/ReviewRequest.cs
+++ Cloudents.Web/Models/ReviewRequest.cs
@@ -8,7 +8,6 @@ namespace Cloudents.Web.Models
         [Required]
         public Guid RoomId { get; set; }
         [StringLength(1000)]
-        [Required(ErrorMessage = "Required")]
         public string Review { get; set; }
         [Range(1, 5)]
         [Required(ErrorMessage = "Required")]
