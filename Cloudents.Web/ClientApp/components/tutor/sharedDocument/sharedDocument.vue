0ea132c920e2372637e8ada656d4d5ef52970afe
crow7m <crow7m@gmail.com>
2019-05-01 15:03:50 +0300

    SPITBALL-988 Rate A Tutor - Minimum number of stars

--- Cloudents.Web/ClientApp/components/tutor/sharedDocument/sharedDocument.vue
+++ Cloudents.Web/ClientApp/components/tutor/sharedDocument/sharedDocument.vue
@@ -46,7 +46,6 @@
         .iframe-container iframe {
             position: absolute;
             top: 0;
-            /*rtl:ignore*/
             left: 0;
             width: 100%;
             max-width: 1280px;
