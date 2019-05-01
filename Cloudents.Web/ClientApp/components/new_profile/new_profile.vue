0dd1eea9c6e0fce1f31fd640ccad760e4bb19c7b
crow7m <crow7m@gmail.com>
2019-05-01 13:15:43 +0300

    SPITBALL-982 Tutor profile page - Contact me button

--- Cloudents.Web/ClientApp/components/new_profile/new_profile.vue
+++ Cloudents.Web/ClientApp/components/new_profile/new_profile.vue
@@ -11,8 +11,8 @@
 
             <!--TODO HIDDEN FOR NOW-->
             <v-flex  xs12 sm3 md3 :class="{'pl-4': $vuetify.breakpoint.smAndUp}" v-if="isMyProfile || isTutorProfile">
-                <tutorInfoBlock v-if="isTutorProfile"></tutorInfoBlock>
-                <userInfoBlock v-else></userInfoBlock>
+                <tutorInfoBlock v-if="isTutorProfile && !isMyProfile"></tutorInfoBlock>
+                <userInfoBlock v-else-if="!isTutorProfile"></userInfoBlock>
             </v-flex>
         </v-layout>
                 <v-layout v-bind="xsColumn" align-start  justify-start>
