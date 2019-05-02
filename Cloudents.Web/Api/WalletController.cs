495ae7296d51c3edf6255380135add1285f3d744
Ram <ram.y@outlook.com>
2019-05-02 10:21:24 +0300

    Fix issue with swagger.

--- Cloudents.Web/Api/WalletController.cs
+++ Cloudents.Web/Api/WalletController.cs
@@ -197,7 +197,7 @@ namespace Cloudents.Web.Api
         }
 
 
-        [SignInWithToken, Route("/" + UrlConst.GeneratePaymentLink)]
+        [SignInWithToken, Route("/" + UrlConst.GeneratePaymentLink), ApiExplorerSettings(IgnoreApi = true)]
         public async Task<RedirectResult> Payment(CancellationToken token)
         {
             var result = await GenerateLinkAsync(token);
