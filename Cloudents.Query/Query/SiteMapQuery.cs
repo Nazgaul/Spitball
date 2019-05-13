9b6d8e3629494c9b7713ac7ee1c17a40bdea68d8
Ram <ram.y@outlook.com>
2019-05-13 10:22:44 +0300

    Refactoring SiteMapQuery

--- Cloudents.Query/Query/SiteMapQuery.cs
+++ Cloudents.Query/Query/SiteMapQuery.cs
@@ -1,10 +1,49 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Cloudents.Core.DTOs;
+using Cloudents.Core.Entities;
+using Cloudents.Core.Enum;
+using NHibernate;
 
 namespace Cloudents.Query.Query
 {
     public class SiteMapQuery : IQuery<IList<SiteMapCountDto>>
     {
-        
+
+
+        internal sealed class SeoItemCountQueryHandler : IQueryHandler<SiteMapQuery, IList<SiteMapCountDto>>
+        {
+            private readonly IStatelessSession _session;
+
+            public SeoItemCountQueryHandler(QuerySession session)
+            {
+                _session = session.StatelessSession;
+            }
+
+            public async Task<IList<SiteMapCountDto>> GetAsync(SiteMapQuery query, CancellationToken token)
+            {
+                var documentCountFuture = _session.QueryOver<Document>()
+                    .Where(w => w.Status.State == ItemState.Ok)
+                    .ToRowCountQuery().FutureValue<int>();
+
+
+                var questionCountFuture = _session.QueryOver<Question>()
+                    .Where(w => w.Status.State == ItemState.Ok)
+                    .ToRowCountQuery().FutureValue<int>();
+
+
+
+
+                var documentCount = await documentCountFuture.GetValueAsync(token);
+                var questionCount = await questionCountFuture.GetValueAsync(token);
+                return new List<SiteMapCountDto>
+                {
+                    new SiteMapCountDto(SeoType.Document, documentCount),
+                    new SiteMapCountDto(SeoType.Question,questionCount)
+                    // new SiteMapCountDto(SeoType.Flashcard, flashcardCount),
+                };
+            }
+        }
     }
 }
