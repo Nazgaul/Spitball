0566c9b7346e66d79e023544026e23ae2d173276
Ram <ram.y@outlook.com>
2019-05-15 10:45:33 +0300

    Fixes in screen sharing.

--- Cloudents.Web/ClientApp/services/videoStreamService.js
+++ Cloudents.Web/ClientApp/services/videoStreamService.js
@@ -1,5 +1,5 @@
 // const extensionId = 'dhndcoampgbambhkkjkicnibhbndjaop'; // localhost spitball share
- const extensionId = 'jaimgihanebafnbcpckdkilkeoomkpik'; // dev && prod
+const extensionId = 'jaimgihanebafnbcpckdkilkeoomkpik'; // dev && prod
 //  const extensionId = 'chombcfbjenobkieohgkjlmmhehfgomf'; // twillio
 
 export default {
@@ -8,7 +8,7 @@ export default {
     getUserScreen() {
         function isFirefox() {
             let mediaSourceSupport = !!navigator.mediaDevices.getSupportedConstraints().mediaSource;
-            let matchData = navigator.userAgent.match('/Firefox/(d) /');
+            let matchData = navigator.userAgent.match('Firefox\/([0-9]+)\.');
             let firefoxVersion = 0;
             if (matchData && matchData[1]) {
                 firefoxVersion = parseInt(matchData[1], 10);
@@ -19,32 +19,46 @@ export default {
         function isChrome() {
             return 'chrome' in window;
         }
+        function getChromeVersion() {
+            var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+
+            return raw ? parseInt(raw[2], 10) : false;
+        }
 
         function canScreenShare() {
             return isFirefox() || isChrome();
         }
 
         if (!canScreenShare()) {
-            return;
+            return Promise.reject("not supported browser");
         }
         if (isChrome()) {
+
+            if (getChromeVersion() > 72) {
+                //return navigator.mediaDevices.getUserMedia();
+                return navigator.mediaDevices.getDisplayMedia().then(stream => {
+                    return stream.getTracks()[0];
+
+                });
+
+            }
             return new Promise((resolve, reject) => {
                 const request = { sources: ['window', 'screen', 'tab'] };
                 chrome.runtime.sendMessage(extensionId, request, response => {
                     //none installed return error string code
-                    if(!response){
+                    if (!response) {
                         let error = 'noExtension';
                         console.log('Extension not installed');
                         reject(error);
                     }
                     if (response && response.type === 'success') {
-                        resolve({streamId: response.streamId});
+                        resolve({ streamId: response.streamId });
                     } else {
                         reject(new Error('Could not get stream'));
                     }
                 });
-            }).then(response => {
-                return navigator.mediaDevices.getUserMedia({
+            }).then(async response => {
+                const stream = await navigator.mediaDevices.getUserMedia({
                     video: {
                         mandatory: {
                             chromeMediaSource: 'desktop',
@@ -52,12 +66,19 @@ export default {
                         }
                     }
                 });
+                return stream.getVideoTracks()[0];
             });
         } else if (isFirefox()) {
-            return navigator.mediaDevices.getUserMedia({
+
+            let displayMediaOptions = {
                 video: {
-                    mediaSource: 'screen'
-                }
+                    cursor: "never"
+                },
+                audio: false
+            };
+            return navigator.mediaDevices.getDisplayMedia(displayMediaOptions).then(stream => {
+
+                return stream.getTracks()[0];
             });
         }
     },
