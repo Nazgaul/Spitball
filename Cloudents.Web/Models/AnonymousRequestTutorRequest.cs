5f7a11e27a6e2edfb97069c53611a534f375888e
Hadar Keidar <Hadar@cloudents.com>
2019-05-16 15:02:43 +0300

    Added Anonymous tutor request api

---
+++ Cloudents.Web/Models/AnonymousRequestTutorRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cloudents.Web.Models
+{
+    public class AnonymousRequestTutorRequest
+    {
+        public long UserId { get; set; }
+        public string Text { get; set; }
+        public string Course { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string University { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        [MaxLength(4, ErrorMessage = "MaxLength")]
+        public string[] Files { get; set; }
+
+    }
+}
