ec80a71587e276e2b1af7d9284ddb8e4721bd6a0
Ram <ram.y@outlook.com>
2019-05-19 12:04:34 +0300

    Study room event wrong id.

--- Cloudents.Web/EventHandler/WebSocketStudyRoomCreatedEventHandler.cs
+++ Cloudents.Web/EventHandler/WebSocketStudyRoomCreatedEventHandler.cs
@@ -13,25 +13,22 @@ namespace Cloudents.Web.EventHandler
     public class WebSocketStudyRoomCreatedEventHandler : IEventHandler<StudyRoomCreatedEvent>
     {
         private readonly IHubContext<SbHub> _hubContext;
-        private readonly IChatRoomRepository _chatRoomRepository;// This is ok for now because we are still inside session
 
-        public WebSocketStudyRoomCreatedEventHandler(IHubContext<SbHub> hubContext, IChatRoomRepository chatRoomRepository)
+        public WebSocketStudyRoomCreatedEventHandler(IHubContext<SbHub> hubContext)
         {
             _hubContext = hubContext;
-            _chatRoomRepository = chatRoomRepository;
         }
 
         public async Task HandleAsync(StudyRoomCreatedEvent eventMessage, CancellationToken token)
         {
             
             var studyRoom = eventMessage.StudyRoom;
-            var chatRoom = await _chatRoomRepository.GetChatRoomAsync(studyRoom.Identifier, token);
             var message = new SignalRTransportType(SignalRType.StudyRoom,
                 SignalRAction.Add, new
                 {
                     id = studyRoom.Id,
                     userId = studyRoom.Tutor.Id,
-                    conversationId = chatRoom.Id
+                    conversationId = studyRoom.Identifier
                 });
             await _hubContext.Clients.Users(studyRoom.Users.Select(s => s.User.Id.ToString()).ToList()).SendAsync(SbHub.MethodName, message, token);
         }
