15ae9fb1d0e41dd897d9a88894e5ab474ee01ae6
Hadar Keidar <Hadar@cloudents.com>
2019-05-20 10:54:41 +0300

    Added phone number to user search

--- Cloudents.Query/Query/Admin/AdminUserDetailsQuery.cs
+++ Cloudents.Query/Query/Admin/AdminUserDetailsQuery.cs
@@ -1,13 +1,68 @@
 ﻿using Cloudents.Core.DTOs.Admin;
+using Dapper;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Cloudents.Query.Query.Admin
 {
     public class AdminUserDetailsQuery : IQuery<UserDetailsDto>
     {
-        public AdminUserDetailsQuery(long userId)
+        public AdminUserDetailsQuery(string userId)
         {
             UserId = userId;
         }
-        public long UserId { get; }
+        private string UserId { get; }
+
+        internal sealed class AdminUserDetailsQueryHandler : IQueryHandler<AdminUserDetailsQuery, UserDetailsDto>
+        {
+
+            private readonly DapperRepository _dapper;
+
+
+            public AdminUserDetailsQueryHandler(DapperRepository dapper)
+            {
+                _dapper = dapper;
+            }
+
+            public async Task<UserDetailsDto> GetAsync(AdminUserDetailsQuery query, CancellationToken token)
+            {
+                var sql = @"select U.Id, U.Name, Email, PhoneNumberHash as PhoneNumber, Un.Name as University, U.Country, U.Score, U.FraudScore, 
+		(select count(1) from sb.[Transaction] T where  U.Id = T.[User_id] and T.[Action] = 'ReferringUser')  as ReferredCount,
+		U.Balance, 
+	        case when U.LockOutEnd is null or U.LockOutEnd < getutcdate() then 1
+	        else 0 end as IsActive,
+			case when U.LockoutEnd is null then 0
+			else 1 end as WasSuspended,
+			(select min(Created) from sb.[Transaction] where [Action] = 'SignUp' and User_id = U.Id) as Joined,
+			U.PhoneNumberConfirmed,
+			U.EmailConfirmed,
+            U.LastOnline,
+			case when T.id is not null then 1 else 0 end as IsTutor
+                        from sb.[User] U
+                        left join sb.University Un
+	                        on U.UniversityId2 = Un.Id
+						left join sb.Tutor T
+							on U.Id = T.Id
+                        where U.Id = @Id or U.Email = @Email or U.PhoneNumberHash = @Email
+            group by U.Id, U.Name, Email, PhoneNumberHash, Un.Name, U.Country, U.Score, U.FraudScore, U.Balance, 
+            case when U.LockOutEnd is null or U.LockOutEnd < getutcdate() then 1
+	            else 0 end,
+			case when U.LockoutEnd is null then 0
+			else 1 end,
+			U.PhoneNumberConfirmed,
+			U.EmailConfirmed, U.LastOnline,case when T.id is not null then 1 else 0 end";
+
+                using (var connection = _dapper.OpenConnection())
+                {
+                    long.TryParse(query.UserId, out var tmpId);
+                    return await connection.QuerySingleOrDefaultAsync<UserDetailsDto>(sql,
+                        new
+                        {
+                            id = tmpId,
+                            email = query.UserId
+                        });
+                }
+            }
+        }
     }
 }
