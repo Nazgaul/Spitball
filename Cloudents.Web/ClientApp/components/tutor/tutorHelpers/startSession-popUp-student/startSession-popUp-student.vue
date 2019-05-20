e5be692199d32282ced7779d3f3f23be89768296
crow7m <crow7m@gmail.com>
2019-05-20 15:53:25 +0300

    res added

--- Cloudents.Web/ClientApp/components/tutor/tutorHelpers/startSession-popUp-student/startSession-popUp-student.vue
+++ Cloudents.Web/ClientApp/components/tutor/tutorHelpers/startSession-popUp-student/startSession-popUp-student.vue
@@ -1,38 +1,86 @@
 <template>
-    <v-layout row wrap>
-        <v-flex xs12 sm12 md12>
-            <v-icon>sbf-close</v-icon>
-        </v-flex>
-        <v-flex xs12 sm12 md12>
-            <userAvatar :user-name="user.name"  :userImageUrl="user.image" :user-id="user.id" ></userAvatar>
-        </v-flex>
-        <v-flex xs12 sm12 md12>
-            <span class="subheading font-weight-bold">Your tutor, Tony Stark</span>
-            <span class="subheading">has entered the room</span>
-        </v-flex>
-        <v-flex xs12 sm12 md12>
-            <v-btn class="start-session-btn">
-                <timerIcon class="timer-icon"></timerIcon>
-                <span class="text-uppercase">Accept and start</span>
-            </v-btn>
-        </v-flex>
-    </v-layout>
+    <div class="student-start-wrap pb-5">
+        <v-layout row class="pt-2">
+            <v-flex xs12 sm12 md12 class="text-xs-right px-3">
+                <v-icon class="caption">sbf-close</v-icon>
+            </v-flex>
+        </v-layout>
+        <v-layout column align-center>
+            <v-flex xs12 sm12 md12 class="pt-2">
+                <userAvatar :user-name="studentName"  :userImageUrl="studentImage" :user-id="studentId" :size="'58'" ></userAvatar>
+            </v-flex>
+            <v-flex xs12 sm12 md12 class="pt-12">
+                <span class="subheading font-weight-bold" v-language:inner>tutor_start_dialog_your_student</span>
+                <span class="subheading font-weight-bold">Tony Stark</span>
+            </v-flex>
+            <v-flex xs12 sm12 md12 class="pt-2">
+                <span class="subheading" v-language:inner>tutor_entered_room</span>
+            </v-flex>
+            <v-flex xs12 sm12 md12 class="pt-4">
+                <button class="start-session-btn elevation-0 align-center justify-center">
+                    <timerIcon class="timer-icon mr-2"></timerIcon>
+                    <span class="text-uppercase" v-language:inner>tutor_stream_btn_start_session</span>
+                </button>
+            </v-flex>
+        </v-layout>
+    </div>
 </template>
 
 <script>
+    import {mapGetters, mapActions} from 'vuex';
     import userAvatar from '../../../helpers/UserAvatar/UserAvatar.vue';
     import timerIcon from '../../images/timer.svg';
     export default {
         name: "startSession-popUp-student",
         components: {userAvatar, timerIcon},
         data() {
-            return {
-
-            };
+            return {};
+        },
+        computed: {
+            ...mapGetters(['getStudyRoomData']),
+            tutorName() {
+                return this.getStudyRoomData.tutorName;
+            },
+            tutorImage(){
+                return this.getStudyRoomData.tutorImage;
+            },
+            tutorId(){
+                return this.getStudyRoomData.tutorId;
+            }
+        },
+        methods: {
+            ...mapActions(['updateStudentStartDialog']),
+            closeDialog() {
+                this.updateStudentStartDialog(false)
+            }
         },
     };
 </script>
 
 <style lang="less">
-@import '../../../../styles/mixin.less';
+    @import '../../../../styles/mixin.less';
+
+    .pt-12{
+        padding-top: 12px;
+    }
+    .student-start-wrap{
+        @greenBtnBackground: #00d983;
+        background: @color-white;
+        border-radius: 4px;
+        box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+        width: 356px;
+        .start-session-btn{
+            display: flex;
+            height: 48px;
+            width: 210px;
+            color: @color-white;
+            background-color:  @greenBtnBackground;
+            border-radius: 4px;
+            .timer-icon{
+                fill: @color-white;
+                max-width: 24px;
+            }
+        }
+    }
+
 </style>
