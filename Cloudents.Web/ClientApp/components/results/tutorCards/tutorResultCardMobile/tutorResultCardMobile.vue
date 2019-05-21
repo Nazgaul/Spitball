e4f45f92aa3d7904163096c91368230c9fe6ae65
crow7m <crow7m@gmail.com>
2019-05-21 16:12:50 +0300

    strike throgh disply table

--- Cloudents.Web/ClientApp/components/results/tutorCards/tutorResultCardMobile/tutorResultCardMobile.vue
+++ Cloudents.Web/ClientApp/components/results/tutorCards/tutorResultCardMobile/tutorResultCardMobile.vue
@@ -24,11 +24,13 @@
                                 <span class="subheading font-weight-bold">₪{{tutorData.price}}</span>
                             <span class="pricing caption" v-language:inner>resultTutor_hour</span>
                         </span>
-                                        <v-flex shrink v-if="showStriked" class="strike-through ">
-                                            <span class="striked-price">₪{{tutorData.price}}</span>
-                                            <span class="pricing striked-hour">
+                                        <v-flex shrink v-if="showStriked" class="strike-through">
+
+                                                <span class="striked-price">₪{{tutorData.price}}</span>
+                                                <span class="pricing striked-hour">
                             <span v-language:inner>resultTutor_hour</span>
                         </span>
+
                                         </v-flex>
                                     </v-flex>
 
@@ -116,7 +118,7 @@
                 //text-decoration: line-through; //will not work cause of different font sizes
                 position: relative;
                 color: @colorBlackNew;
-                display: flex;
+                display: table;
                 .striked-price{
                     font-size: 12px;
                 }
@@ -126,7 +128,6 @@
                 &:after {
                     content: '';
                     width: 100%;
-                    max-width: 56px;
                     border-bottom: solid 1px @colorBlackNew;
                     position: absolute;
                     left: 0;
@@ -206,10 +207,10 @@
             //text-decoration: line-through; //will not work cause of different font sizes
             font-size: 14px;
             position: relative;
+            display: table;
             &:after {
                 content: '';
                 width: 100%;
-                max-width: 86px;
                 border-bottom: solid 1px @textColor;
                 position: absolute;
                 left: 0;
