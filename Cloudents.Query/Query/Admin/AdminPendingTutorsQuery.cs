681bbf8190592bb86e8e3af012bee83156acce4e
Hadar Keidar <Hadar@cloudents.com>
2019-05-27 13:24:37 +0300

    fixed AdminPendingTutorsQuery

--- Cloudents.Query/Query/Admin/AdminPendingTutorsQuery.cs
+++ Cloudents.Query/Query/Admin/AdminPendingTutorsQuery.cs
@@ -1,10 +1,6 @@
 ﻿using Cloudents.Core.DTOs.Admin;
-using Cloudents.Core.Entities;
-using Cloudents.Core.Enum;
-using NHibernate;
-using NHibernate.Linq;
+using Dapper;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,27 +10,29 @@ namespace Cloudents.Query.Query.Admin
     {
         internal sealed class AdminPendingTutorsQueryHandler : IQueryHandler<AdminPendingTutorsQuery, IEnumerable<PendingTutorsDto>>
         {
-            private readonly IStatelessSession _session;
-            public AdminPendingTutorsQueryHandler(QuerySession session)
+            private readonly DapperRepository _dapper;
+            public AdminPendingTutorsQueryHandler(DapperRepository dapper)
             {
-                _session = session.StatelessSession;
+                _dapper = dapper;
             }
 
             public async Task<IEnumerable<PendingTutorsDto>> GetAsync(AdminPendingTutorsQuery query, CancellationToken token)
             {
-                return await _session.Query<RegularUser>()
-                    .Fetch(f => f.Tutor)
-                    .Where(w => w.Tutor.State == ItemState.Pending)
-                    .Select(s => new PendingTutorsDto
-                    {
-                        Id = s.Id,
-                        FirstName = s.FirstName,
-                        LastName = s.LastName,
-                        Bio = s.Tutor.Bio,
-                        Price = s.Tutor.Price,
-                        Email = s.Email,
-                        Created = s.Tutor.Created
-                    }).ToListAsync();
+
+                const string sql = @"select u.Id, u.FirstName, u.LastName, u.Email, t.Bio, t.Price, t.Created, STRING_AGG(uc.CourseId, ', ') as Courses
+                                    from sb.[User] u
+                                    join sb.Tutor t
+	                                    on u.Id = t.Id
+                                    left join sb.UsersCourses uc
+	                                    on u.Id = uc.UserId and CanTeach = 1
+                                    where t.State = 'Pending'
+                                    group by u.Id, u.FirstName, u.LastName, u.Email, t.Bio, t.Price, t.Created";
+                using (var connection = _dapper.OpenConnection())
+                {
+                    var res = await connection.QueryAsync<PendingTutorsDto>(sql);
+                    return res;
+                }
+               
             }
         }
     }
