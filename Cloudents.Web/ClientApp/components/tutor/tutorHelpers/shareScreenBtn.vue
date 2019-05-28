fcfe0f57df87ba43132e56a6ba4ad33270ebe033
crow7m <crow7m@gmail.com>
2019-05-28 13:49:50 +0300

    design fixes done

--- Cloudents.Web/ClientApp/components/tutor/tutorHelpers/shareScreenBtn.vue
+++ Cloudents.Web/ClientApp/components/tutor/tutorHelpers/shareScreenBtn.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="share-screen-btn-wrap">
-        <v-flex>
+        <v-flex class="text-xs-center">
             <!--Select-->
             <div v-if="!isSharing" >
-                <v-tooltip left >
+                <v-tooltip top >
                     <template v-slot:activator="{on}">
                         <!--keep this div, due to tooltip not appearing on disabled btn bug of vuetify-->
                         <div v-on="on" >
@@ -169,7 +169,7 @@
 <style lang="less">
     .share-screen-btn-wrap {
         .outline-btn-share {
-            display: flex;
+            display: inline-flex;
             align-items: center;
             justify-content: space-between;
             padding: 8px 12px;
