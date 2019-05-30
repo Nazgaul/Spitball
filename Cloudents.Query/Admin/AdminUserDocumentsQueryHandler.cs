dea8451aaf09d204b5d126db67c78163675be188
Ram <ram.y@outlook.com>
2019-05-30 15:46:08 +0300

    Change to query from dapper to nhibernate

--- Cloudents.Query/Admin/AdminUserDocumentsQueryHandler.cs
+++ Cloudents.Query/Admin/AdminUserDocumentsQueryHandler.cs
@@ -1,44 +1,44 @@
 ﻿using Cloudents.Core.DTOs.Admin;
 using Cloudents.Query.Query.Admin;
-using Dapper;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Cloudents.Core.Entities;
+using NHibernate;
+using NHibernate.Linq;
 
 namespace Cloudents.Query.Admin
 {
     public class AdminUserDocumentsQueryHandler : IQueryHandler<AdminUserDocumentsQuery, IEnumerable<UserDocumentsDto>>
     {
-        private readonly DapperRepository _dapper;
+        private readonly IStatelessSession _session;
 
 
-        public AdminUserDocumentsQueryHandler(DapperRepository dapper)
+        public AdminUserDocumentsQueryHandler(QuerySession session)
         {
-            _dapper = dapper;
+            _session = session.StatelessSession;
         }
-        private const int PageSize = 200;
+        private const int PageSize = 50;
 
         public async Task<IEnumerable<UserDocumentsDto>> GetAsync(AdminUserDocumentsQuery query, CancellationToken token)
         {
-            const string sql = @"select D.Id, D.Name, D.CreationTime as Created, U.Name as University, D.CourseName as Course, D.Price, D.[state]
-                from sb.Document D
-                join sb.University U
-	                on D.UniversityId = U.Id
-                where UserId = @Id
-				order by 1
-                 OFFSET @PageSize * @PageNumber ROWS
-                 FETCH NEXT @PageSize ROWS ONLY;";
 
-            using (var connection = _dapper.OpenConnection())
-            {
-                return await connection.QueryAsync<UserDocumentsDto>(sql,
-                    new
-                    {
-                        id = query.UserId,
-                        PageNumber = query.Page,
-                        PageSize
-                    });
-            }
+            return await _session.Query<Document>()
+                  .Fetch(f => f.University)
+                  .Where(w => w.User.Id == query.UserId)
+                  .Select(s => new UserDocumentsDto
+                  {
+                      Course = s.Course.Id,
+                      Id = s.Id,
+                      Name = s.Name,
+                      University = s.University.Name,
+                      State = s.Status.State,
+                      Price = s.Price,
+                      Created = s.TimeStamp.CreationTime,
+
+                  }).OrderBy(o => o.Id).Take(PageSize).Skip(PageSize * query.Page).ToListAsync(token);
+
         }
     }
 }
