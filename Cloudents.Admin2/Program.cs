28e3ecad68e339a648d762afd8333051d3323058
Elad Levavi <apolo1805@hotmail.com>
2019-06-02 14:10:21 +0300

    Add automatic version increment

--- Cloudents.Admin2/Program.cs
+++ Cloudents.Admin2/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System.Reflection;
 
+
+[assembly: AssemblyVersion("1.0.9.*")]
 namespace Cloudents.Admin2
 {
     public class Program
