307993d491a6c28f0f8a95d3155812a0c713e1c2
Maor Barel <maorrbarel@gmail.com>
2019-06-10 12:02:17 +0300

    newLandingPage design headline

--- Cloudents.Web/ClientApp/components/landingPage/components/findTutorComponents/FindTutor-headlineSection.vue
+++ Cloudents.Web/ClientApp/components/landingPage/components/findTutorComponents/FindTutor-headlineSection.vue
@@ -6,7 +6,6 @@
       <button @click="goToTutorPage" v-language:inner="'landingPage_main_btn'"></button>
     </v-flex>
     <v-flex class="landing-headeline-section-img-container" hidden-md-and-down lg6>
-        <img src="./images/FindTutor_headeline.png" alt>
     </v-flex>
   </v-layout>
 </template>
@@ -30,7 +29,15 @@ export default {
 .landing-headeline-section-container{
     .landing-headeline-section-text-container{
         background-color: #facb57;
-        padding: 96px 84px 0 258px;
+        padding: 96px 84px 40px 258px;
+        @media (max-width: 1600px){
+            padding: 96px 40px 40px 258px;
+
+        }
+        @media (max-width: 1400px){
+            padding: 96px 50px 40px 150px;
+
+        }
         @media (max-width: @screen-md) {
             padding: 55px 20px 40px 20px;
         }
@@ -49,6 +56,9 @@ export default {
             font-size: 28px;
             font-weight: bold;
             color: #fff;
+            @media (max-width: 1600px){
+                margin: 30px 0 42px 0;
+            }
             @media (max-width: @screen-md){
                 font-size: 16px;
                 font-weight: bolder;
@@ -67,9 +77,9 @@ export default {
         }
     } 
     .landing-headeline-section-img-container{
-        img{
-            height:100%;
-        }
+        background-image: url('./images/FindTutor_headeline.png');
+        background-position: center center;
+        background-size: cover;
     }
 }
 </style>
