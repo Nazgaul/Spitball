cd4b7dafde886175b3186b5eaf4ffd623d6ab448
Hadar Keidar <Hadar@cloudents.com>
2019-06-16 10:26:54 +0300

    Added Admin add short url api

---
+++ Cloudents.Admin2/Api/AdminShortUrlController.cs
@@ -0,0 +1,33 @@
+﻿using Cloudents.Admin2.Models;
+using Cloudents.Command;
+using Cloudents.Command.Command;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cloudents.Admin2.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminShortUrlController : ControllerBase
+    {
+        private readonly ICommandBus _commandBus;
+        public AdminShortUrlController(ICommandBus commandBus)
+        {
+            _commandBus = commandBus;
+        }
+
+
+        [HttpPost("url")]
+        public async Task<ActionResult> ApproveQuestionAsync([FromBody] AddShortUrlRequest model, CancellationToken token)
+        {
+            var command = new CreateShortUrlCommand(model.Identifier, model.Destination, model.Expiration);
+            await _commandBus.DispatchAsync(command, token);
+
+            return Ok();
+        }
+    }
+}
