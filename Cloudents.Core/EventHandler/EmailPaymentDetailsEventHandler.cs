4edc9051a1fd5c9123adcf6f0a531c37424709fd
Ram <ram.y@outlook.com>
2019-06-18 10:47:11 +0300

    Revert "Payme is back"
    This reverts commit 1400c57d8222dac8dc7057f893b3c706e7079f29.
    
    # Conflicts:
    #	Cloudents.Core/EventHandler/EmailPaymentDetailsEventHandler.cs

--- Cloudents.Core/EventHandler/EmailPaymentDetailsEventHandler.cs
+++ Cloudents.Core/EventHandler/EmailPaymentDetailsEventHandler.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using Cloudents.Core.Event;
 using Cloudents.Core.Interfaces;
-using Cloudents.Core.Message.Email;
 using Cloudents.Core.Storage;
 
 namespace Cloudents.Core.EventHandler
@@ -17,9 +16,10 @@ namespace Cloudents.Core.EventHandler
             _serviceBusProvider = serviceBusProvider;
         }
 
-        public async Task HandleAsync(StudyRoomCreatedEvent eventMessage, CancellationToken token)
+        public  Task HandleAsync(StudyRoomCreatedEvent eventMessage, CancellationToken token)
         {
-            await _serviceBusProvider.InsertMessageAsync(new StudentPaymentMessage(eventMessage.StudyRoom.Id), token);
+            return Task.CompletedTask;
+         //   await _serviceBusProvider.InsertMessageAsync(new StudentPaymentMessage(eventMessage.StudyRoom.Id), token);
         }
     }
 }
