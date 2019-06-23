8a583847185747cd984aef3f790f1d4260357290
Gabriel <gaby.str@gmail.com>
2019-06-23 15:13:02 +0300

    insight service added

--- Cloudents.Web/ClientApp/services/insightService.js
+++ Cloudents.Web/ClientApp/services/insightService.js
@@ -9,16 +9,16 @@ function createEventName(type, name){
 }
 
 const track={
-    exception: function(exception, handledAt, properties, measurements, severityLevel){
-    /*
-    Gaby: use this only in a TRY CATCH expression otherwise use the track Event function
-    exception - 	An Error from a catch clause.
-    handledAt - Deprecated. This argument is ignored. Please pass null.
-    properties - Map of string to string: Additional data used to filter exceptions in the portal. Defaults to empty.
-    measurements - Map of string to number: Metrics associated with this page, displayed in Metrics Explorer on the portal. Defaults to empty.
-    severityLevel - Supported values: SeverityLevel.ts
-    */
-
+    exception: function(exception, handledAt = null, properties, measurements, severityLevel){
+        /*
+        Gaby: use this only in a TRY CATCH expression otherwise use the track Event function
+        exception - An Error from a catch clause.
+        handledAt - Deprecated. This argument is ignored. Please pass null.
+        properties - Map of string to string: Additional data used to filter exceptions in the portal. Defaults to empty.
+        measurements - Map of string to number: Metrics associated with this page, displayed in Metrics Explorer on the portal. Defaults to empty.
+        severityLevel - Supported values: SeverityLevel.ts
+        */
+        global.appInsights.trackException(exception, properties, measurements, severityLevel);
     },
     event: function(type = EVENT_TYPES.LOG, name, properties, measurements){
         //type defines if the event is ERROR or TRACK
@@ -32,5 +32,6 @@ const track={
 }
 
 export default{
-    track
+    track,
+    EVENT_TYPES
 }
