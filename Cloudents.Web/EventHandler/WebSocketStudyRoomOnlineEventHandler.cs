f1cd405c4c743d84aecfcd5e68f3b0136effd447
Ram <ram.y@outlook.com>
2019-06-24 16:11:24 +0300

    Some changes to support reconnect to study room on client.

--- Cloudents.Web/EventHandler/WebSocketStudyRoomOnlineEventHandler.cs
+++ Cloudents.Web/EventHandler/WebSocketStudyRoomOnlineEventHandler.cs
@@ -1,36 +1,36 @@
-﻿using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Cloudents.Core;
-using Cloudents.Core.Event;
-using Cloudents.Core.Interfaces;
-using Cloudents.Web.Hubs;
-using Microsoft.AspNetCore.SignalR;
+﻿//using System.Linq;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Cloudents.Core;
+//using Cloudents.Core.Event;
+//using Cloudents.Core.Interfaces;
+//using Cloudents.Web.Hubs;
+//using Microsoft.AspNetCore.SignalR;
 
-namespace Cloudents.Web.EventHandler
-{
-    public class WebSocketStudyRoomOnlineEventHandler : IEventHandler<StudyRoomOnlineChangeEvent>
-    {
-        private readonly IHubContext<StudyRoomHub> _hubContext;
+//namespace Cloudents.Web.EventHandler
+//{
+//    public class WebSocketStudyRoomOnlineEventHandler : IEventHandler<StudyRoomOnlineChangeEvent>
+//    {
+//        private readonly IHubContext<StudyRoomHub> _hubContext;
 
-        public WebSocketStudyRoomOnlineEventHandler(IHubContext<StudyRoomHub> hubContext)
-        {
-            _hubContext = hubContext;
-        }
+//        public WebSocketStudyRoomOnlineEventHandler(IHubContext<StudyRoomHub> hubContext)
+//        {
+//            _hubContext = hubContext;
+//        }
 
-        public async Task HandleAsync(StudyRoomOnlineChangeEvent eventMessage, CancellationToken token)
-        {
-            var studyUser = eventMessage.StudyUser;
-            var studyRoom = studyUser.Room;
-            var message = new SignalRTransportType(SignalRType.StudyRoom,
-                SignalRAction.Update, new
-                {
-                    onlineCount = studyRoom.Users.Count(f => f.Online),
-                    totalOnline = studyRoom.Users.Count
-                });
+//        public async Task HandleAsync(StudyRoomOnlineChangeEvent eventMessage, CancellationToken token)
+//        {
+//            var studyUser = eventMessage.StudyUser;
+//            var studyRoom = studyUser.Room;
+//            var message = new SignalRTransportType(SignalRType.StudyRoom,
+//                SignalRAction.Update, new
+//                {
+//                    onlineCount = studyRoom.Users.Count(f => f.Online),
+//                    totalOnline = studyRoom.Users.Count
+//                });
 
 
-            await _hubContext.Clients.Group(studyRoom.Id.ToString()).SendAsync(SbHub.MethodName, message, token);
-        }
-    }
-}
+//            await _hubContext.Clients.Group(studyRoom.Id.ToString()).SendAsync(SbHub.MethodName, message, token);
+//        }
+//    }
+//}
