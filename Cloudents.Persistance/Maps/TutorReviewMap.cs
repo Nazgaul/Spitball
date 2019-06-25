312e669c9bc2437fd37b09ffa17ceacdd82cb2ab
Ram <ram.y@outlook.com>
2019-06-25 12:58:28 +0300

    Allow review all the time

--- Cloudents.Persistance/Maps/TutorReviewMap.cs
+++ Cloudents.Persistance/Maps/TutorReviewMap.cs
@@ -12,7 +12,7 @@ namespace Cloudents.Persistence.Maps
             Id(x => x.Id).GeneratedBy.GuidComb();
             References(x => x.User).Not.Nullable().Column("UserId");
             References(x => x.Tutor).Not.Nullable().Column("TutorId");
-            References(x => x.Room).Not.Nullable().Column("RoomId");
+            //References(x => x.Room).Not.Nullable().Column("RoomId");
             Map(x => x.DateTime).Not.Nullable();
             Map(x => x.Review).Length(1000);
             Map(x => x.Rate).Not.Nullable();
