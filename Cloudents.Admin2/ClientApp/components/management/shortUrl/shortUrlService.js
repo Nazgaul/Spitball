b8651ec336fef696d036b3fc7d0969b761b930fc
idanKal <idankallmann_91@hotmail.com>
2019-06-26 15:17:11 +0300

    impl shorthand url

--- Cloudents.Admin2/ClientApp/components/management/shortUrl/shortUrlService.js
+++ Cloudents.Admin2/ClientApp/components/management/shortUrl/shortUrlService.js
@@ -1,16 +1,24 @@
 import { connectivityModule } from '../../../services/connectivity.module';
 
+function ShortHandUrl(objInit) {
+    this.identifier = objInit.identifier;
+    this.destination = objInit.destination;
+    this.expiration = objInit.expiration ? new Date(objInit.expiration).toISOString().substr(0, 10) : 'indefinite'
+}
+
+function createShortHandUrl(objInit) {
+    return new ShortHandUrl(objInit);
+}
 
 const addUrl = function (item) {
-    return connectivityModule.http.post(`AdminShortUrl/url`, { "Destination": item.destination, "Identifier": item.identifier, "Expiration": item.date })
+    return connectivityModule.http.post(`AdminShortUrl/url`, { "destination": item.destination, "identifier": item.identifier, "expiration": item.date })
         .then((resp) => {
-            return Promise.resolve(resp);
+            return createShortHandUrl(resp);
         }, (error) => {
-            return Promise.reject(error);
+            return error;
         });
 };
 
-export
-{
+export {
     addUrl
 }
