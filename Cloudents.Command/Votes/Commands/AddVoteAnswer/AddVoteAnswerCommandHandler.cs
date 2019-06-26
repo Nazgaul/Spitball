886f0c58924329c4afc58086e0c6f8c8f81b668e
Ram <ram.y@outlook.com>
2019-06-26 16:09:16 +0300

    Fix compile issue.

--- Cloudents.Command/Votes/Commands/AddVoteAnswer/AddVoteAnswerCommandHandler.cs
+++ Cloudents.Command/Votes/Commands/AddVoteAnswer/AddVoteAnswerCommandHandler.cs
@@ -26,81 +26,11 @@ namespace Cloudents.Command.Votes.Commands.AddVoteAnswer
             var answer = await _repository.LoadAsync(message.AnswerId, token);
             answer.Vote(message.VoteType,user);
            await _repository.UpdateAsync(answer, token);
-            //if (answer.State != ItemState.Ok)
-            //{
-            //    throw new NotFoundException();
-            //}
-            //await ValidateAsync(user, answer, token);
-            //var vote = await GetVoteAsync(user.Id, answer.Id, token);
-
-            //if (vote == null && message.VoteType == VoteType.None)
-            //{
-            //    throw new ArgumentException();
-            //}
-            //if (vote == null)
-            //{
-            //    vote = CreateVote(user, answer, message.VoteType);
-            //    await _voteRepository.AddAsync(vote, token);
-
-            //    answer.VoteCount += (int)vote.VoteType;
-
-            //    if (answer.VoteCount < VotesToFlag)
-            //    {
-            //        answer.Flag("Too many down vote", null);
-            //    }
-            //    return;
-            //}
-
-            //if (message.VoteType == VoteType.None)
-            //{
-            //    answer.VoteCount -= (int)vote.VoteType;
-            //    if (answer.VoteCount < VotesToFlag)
-            //    {
-            //        answer.Flag("Too many down vote", null);
-            //        // _eventStore.Add(new ItemFlaggedEvent(question));
-            //    }
-            //    await _voteRepository.DeleteAsync(vote, token);
-            //    return;
-            //}
-
-            //answer.VoteCount -= (int)vote.VoteType;
-            //answer.VoteCount += (int)message.VoteType;
-            //if (answer.VoteCount < VotesToFlag)
-            //{
-            //    answer.Flag("Too many down vote", null);
-            //    // _eventStore.Add(new ItemFlaggedEvent(question));
-            //}
-            //vote.VoteType = message.VoteType;
-
-            //await _voteRepository.UpdateAsync(vote, token);
+          
 
 
         }
 
-        //public const int VotesToFlag = -2;
-
-        //protected Vote CreateVote(RegularUser user, Answer question, VoteType vote)
-        //{
-        //    return new Vote(user, question, vote);
-        //}
-
-        //protected Task<Vote> GetVoteAsync(long userId, Guid id, CancellationToken token)
-        //{
-        //   return _voteRepository.GetVoteAnswerAsync(userId, id, token);
-        //}
-
-        //protected async Task ValidateAsync(User user, Answer answer, CancellationToken token)
-        //{
-        //    if (answer.User.Id == user.Id)
-        //    {
-        //        throw new UnauthorizedAccessException("you cannot vote you own answer");
-        //    }
-
-        //    var answerExists = await _repository.GetUserAnswerInQuestion(answer.Question.Id, user.Id, token);
-        //    if (answerExists != null)
-        //    {
-        //        throw new UnauthorizedAccessException("you cannot vote if you gave answer");
-        //    }
-        //}
+       
     }
 }
