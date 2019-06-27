b24018a289566d54e7623236c3f7b00b56d4bd9f
Gabriel <gaby.str@gmail.com>
2019-06-27 16:04:02 +0300

    test added

---
+++ Cloudents.Web/ClientApp/store/tests/userOnlineStatus.spec.js
@@ -0,0 +1,14 @@
+import userOnlineStatus from '../userOnlineStatus';
+import userOnlineStatusService from './../../services/userOnlineStatusService';
+
+describe('STORE_userOnlineStatus_GETTER_getUserStatus', ()=>{
+    it('should return user status', ()=>{
+        const userStatusObj = userOnlineStatusService.createUserStatus({id: 12345, online: true});
+        const userStatus = {[userStatusObj.id]: userStatusObj.online};
+        const result = {
+            12345: true
+        };
+        const state = {userStatus};
+        expect(userOnlineStatus.getters.getUserStatus(state)).toEqual(result);
+    })
+})
