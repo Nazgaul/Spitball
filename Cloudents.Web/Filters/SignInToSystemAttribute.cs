6f1acc12dd9f53c5146a5c037fe7f69ad8124e6b
Ram <ram.y@outlook.com>
2019-06-27 12:50:49 +0300

    Sign in fix.

--- Cloudents.Web/Filters/SignInToSystemAttribute.cs
+++ Cloudents.Web/Filters/SignInToSystemAttribute.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 
 namespace Cloudents.Web.Filters
@@ -37,28 +38,71 @@ namespace Cloudents.Web.Filters
             public override async Task OnActionExecutionAsync(ActionExecutingContext context,
                 ActionExecutionDelegate next)
             {
-
-
                 var token = context.HttpContext.Request.Query["token"].ToString();
 
                 if (!string.IsNullOrEmpty(token))
                 {
-                    var user = context.HttpContext.User;
-                    if (_signInManager.IsSignedIn(user))
+                    var claimUser = context.HttpContext.User;
+                    if (_signInManager.IsSignedIn(claimUser))
                     {
                         context.Result = new RedirectResult(GetUrlWithoutTokenQuery(context));
                         return;
                     }
 
-                    Enum.TryParse(context.HttpContext.Request.Query["channel"].ToString(), out CommunicationChannel c);
-                    var result = await SignInUserAsync(token,c);
+                    Enum.TryParse(context.HttpContext.Request.Query["channel"].ToString(), out CommunicationChannel communicationChannel);
 
-                    if (result && context.Controller is Controller controller)
+                    try
                     {
-                        controller.ViewBag.Auth = true;
+                        var userId = _dataProtect.Unprotect(token);
+                        var user = await _userManager.FindByIdAsync(userId);
+                        if (user == null)
+                        {
+                            context.Result = new RedirectResult(GetUrlWithoutTokenQuery(context));
+                            return;
+                        }
+
+                        switch (communicationChannel)
+                        {
+                            case CommunicationChannel.None:
+                                break;
+                            case CommunicationChannel.Phone:
+                                user.PhoneNumberConfirmed = true;
+                                await _userManager.UpdateAsync(user);
+                                break;
+                            case CommunicationChannel.Email:
+                                user.EmailConfirmed = true;
+                                await _userManager.UpdateAsync(user);
+                                break;
+                        }
+
+                        if (user.PasswordHash == null && !_userManager.SupportsUserLogin)
+                        {
+                            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                            code = UrlEncoder.Default.Encode(code);
+
+                            if (context.Controller is Controller controller)
+                            {
+                                var link = controller.Url.Link("ResetPassword", new { user.Id, code });
+                                context.Result = new RedirectResult(link);
+                                return;
+                            }
+                            //Need to change user to enter password
+                        }
+
+                        await _signInManager.SignInAsync(user, false);
+
                     }
+                    catch (CryptographicException ex)
+                    {
+                        //We just log the exception. user open the email too later and we can't sign it.
+                        //If we see this persist then maybe we need to increase the amount of time
+                        _logger.Exception(ex);
+                    }
+                    //var result = await SignInUserAsync(token,c);
 
 
+
+                    //var link = Url.Link("ResetPassword", new { user.Id, code });
                     var url = GetUrlWithoutTokenQuery(context);
 
                     context.Result = new RedirectResult(url);
@@ -81,45 +125,58 @@ namespace Cloudents.Web.Filters
                 return url;
             }
 
-            private async Task<bool> SignInUserAsync(string code, CommunicationChannel communicationChannel)
-            {
-                try
-                {
+            //private async Task<string> SignInUserAsync(string code, CommunicationChannel communicationChannel)
+            //{
+            //    try
+            //    {
 
-                    var userId = _dataProtect.Unprotect(code);
-                    var user = await _userManager.FindByIdAsync(userId);
-                    if (user == null)
-                    {
-                        return false;
-                    }
+            //        var userId = _dataProtect.Unprotect(code);
+            //        var user = await _userManager.FindByIdAsync(userId);
+            //        if (user == null)
+            //        {
+            //            return false;
+            //        }
 
-                    var updateOccur = false;
-                    switch (communicationChannel)
-                    {
-                        case CommunicationChannel.None:
-                            break;
-                        case CommunicationChannel.Phone:
-                            user.PhoneNumberConfirmed = true;
-                            break;
-                        case CommunicationChannel.Email:
-                            user.EmailConfirmed = true;
-                            break;
-                    }
 
-                    //TODO: we need to take care verified the phone number /or email
-                    //TODO: need to redirect to create password page.
-                    //ViewBag.Auth = true;
-                    await _signInManager.SignInAsync(user, false);
-                    return true;
-                }
-                catch (CryptographicException ex)
-                {
-                    //We just log the exception. user open the email too later and we can't sign it.
-                    //If we see this persist then maybe we need to increase the amount of time
-                    _logger.Exception(ex);
-                }
-                return false;
-            }
+
+            //        switch (communicationChannel)
+            //        {
+            //            case CommunicationChannel.None:
+            //                break;
+            //            case CommunicationChannel.Phone:
+            //                user.PhoneNumberConfirmed = true;
+            //                await _userManager.UpdateAsync(user);
+            //                break;
+            //            case CommunicationChannel.Email:
+            //                user.EmailConfirmed = true;
+            //                await _userManager.UpdateAsync(user);
+            //                break;
+            //        }
+
+            //        if (user.PasswordHash == null && !_userManager.SupportsUserLogin)
+            //        {
+            //            var code2 = await _userManager.GeneratePasswordResetTokenAsync(user);
+            //            code2 = UrlEncoder.Default.Encode(code);
+
+            //            //Need to change user to enter password
+            //        }
+
+            //        //TODO: we need to take care verified the phone number /or email
+            //        //TODO: need to redirect to create password page.
+            //        //ViewBag.Auth = true;
+
+            //        await _signInManager.SignInAsync(user, false);
+            //        return true;
+            //    }
+            //    catch (CryptographicException ex)
+            //    {
+            //        //We just log the exception. user open the email too later and we can't sign it.
+            //        //If we see this persist then maybe we need to increase the amount of time
+            //        _logger.Exception(ex);
+            //    }
+            //    return false;
+            //}
         }
+        
     }
 }
