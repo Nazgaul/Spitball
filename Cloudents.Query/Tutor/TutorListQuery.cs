6e5dcba3683dd0536edee214a2e0b14610c4476f
Hadar Keidar <Hadar@cloudents.com>
2019-07-08 10:40:51 +0300

    Simplify TutorListQuery SQL

--- Cloudents.Query/Tutor/TutorListQuery.cs
+++ Cloudents.Query/Tutor/TutorListQuery.cs
@@ -28,79 +28,41 @@ namespace Cloudents.Query.Tutor
                 _dapperRepository = dapperRepository;
             }
 
-
+            //TODO: review query 
             public async Task<IEnumerable<TutorListDto>> GetAsync(TutorListQuery query, CancellationToken token)
             {
-                const string sql1 = @"select distinct U.Id as UserId, U.Name, U.Image, 
+                const string sql = @"select distinct U.Id as UserId, U.Name, U.Image, 
 (select STRING_AGG(dt.CourseId, ', ') FROM(select top 10 courseId
 from sb.UsersCourses dt where u.Id = dt.UserId and dt.CanTeach = 1) dt) as courses,
 T.Price,
-T.Bio,x.*
+T.Bio,
+case when uc.CourseId in (select CourseId from sb.UsersCourses where UserId = @UserId or @UserId = 0) then 2
+when c.SubjectId in (Select subjectId  from sb.UsersCourses where UserId = @UserId or @UserId = 0) then 1
+else 0 end as t,
+x.*
 	                      
                         from sb.[user] U
                         join sb.Tutor T
 	                        on U.Id = T.Id
-						join sb.UsersCourses uc on u.Id = uc.UserId and uc.CanTeach = 1
+						left join sb.UsersCourses uc on u.Id = uc.UserId and uc.CanTeach = 1
 						and  uc.CourseId in (select CourseId from sb.UsersCourses where UserId = @UserId or @UserId = 0)
+						left join sb.Course c on uc.CourseId = c.Name
+						
+cross apply (select avg(Rate) as Rate, count(1) as ReviewsCount from sb.TutorReview where TutorId = T.Id) as x
+where (t.Id <> @UserId or @UserId = 0) 
                         and T.State = 'Ok'
-                        and (U.Country = @Country or @Country is null)
-cross apply (select avg(Rate) as Rate, count(1) as ReviewsCount from sb.TutorReview where TutorId = T.Id) as x
-where t.Id <> @UserId or @UserId = 0
-order by Rate desc
-OFFSET 0 ROWS
-FETCH NEXT 20 ROWS ONLY;";
-
-
-                const string sql2 = @"select distinct U.Id as UserId, U.Name, U.Image, 
-(select STRING_AGG(dt.CourseId, ', ') FROM(select top 10 courseId
-from sb.UsersCourses dt where u.Id = dt.UserId and dt.CanTeach = 1) dt) as courses,
-T.Price, 
-T.Bio,x.*
-	                     
-                        from sb.[user] U
-                        join sb.Tutor T
-	                        on U.Id = T.Id
-						join sb.UsersCourses uc on u.Id = uc.UserId and uc.CanTeach = 1
-						join sb.Course c on uc.CourseId = c.Name
-						and c.SubjectId in (Select subjectId  from sb.UsersCourses where UserId = @UserId or @UserId = 0)
-						and T.State = 'Ok'
-						and (U.Country = @Country or @Country is null)
-cross apply (select avg(Rate) as Rate, count(1) as ReviewsCount from sb.TutorReview where TutorId = T.Id) as x
-where t.Id <> @UserId or @UserId = 0
-order by Rate desc
-OFFSET 0 ROWS
-FETCH NEXT 20 ROWS ONLY;";
-
-
-                const string sql3 = @"select distinct U.Id as UserId, U.Name, U.Image, 
-(select STRING_AGG(dt.CourseId, ', ') FROM(select top 10 courseId
-from sb.UsersCourses dt where u.Id = dt.UserId and dt.CanTeach = 1) dt) as courses,
-T.Price, 
-T.Bio,x.*
-	                       
-                        from sb.[user] U
-                        join sb.Tutor T
-	                        on U.Id = T.Id
-						and T.State = 'Ok'
-						and (U.Country = @Country or @Country is null)
-cross apply (select avg(Rate) as Rate, count(1) as ReviewsCount from sb.TutorReview where TutorId = T.Id) as x
-where t.Id <> @UserId or @UserId = 0
-order by Rate desc
+                        and (U.Country = @Country or @Country is null) 
+order by t desc, Rate desc
 OFFSET 0 ROWS
 FETCH NEXT 20 ROWS ONLY;";
 
 
                 using (var conn = _dapperRepository.OpenConnection())
                 {
-                    using (var multi = await conn.QueryMultipleAsync(sql1 + sql2 + sql3, new { query.UserId, query.Country }))
-                    {
-                        var t1 = await multi.ReadAsync<TutorListDto>();
-                        var t2 = await multi.ReadAsync<TutorListDto>();
-                        var t3 = await multi.ReadAsync<TutorListDto>();
-
-                        return t1.Union(t2).Union(t3).Distinct(TutorListDto.UserIdComparer).Take(20);
-                    }
-
+                    var retVal = await conn.QueryAsync<TutorListDto>(sql, new { query.UserId, query.Country });
+                   
+                    return retVal.Take(20);
+                    
                 }
             }
         }
