2932c1bfd2c54dd8b1d29d02fd34a0346e1a4bd5
Ram <ram.y@outlook.com>
2019-07-08 16:03:48 +0300

    Added system text message.

--- Cloudents.Core/Entities/ChatTextMessage.cs
+++ Cloudents.Core/Entities/ChatTextMessage.cs
@@ -19,4 +19,16 @@ namespace Cloudents.Core.Entities
         public virtual string Message { get; protected set; }
 
     }
+
+    public class SystemTextMessage : ChatTextMessage
+    {
+        protected SystemTextMessage()
+        {
+
+        }
+
+        public SystemTextMessage(User user, string message, ChatRoom room) : base(user, message, room)
+        {
+        }
+    }
 }
