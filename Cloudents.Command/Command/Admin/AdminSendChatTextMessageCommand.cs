7fb46dfb4040c5aba76477693a3d5114f0798680
Hadar Keidar <Hadar@cloudents.com>
2019-07-09 13:17:30 +0300

    Created AdminSendChatTextMessageCommand for SystemTextMessage

---
+++ Cloudents.Command/Command/Admin/AdminSendChatTextMessageCommand.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Cloudents.Command.Command.Admin
+{
+    public class AdminSendChatTextMessageCommand : ICommand
+    {
+        public AdminSendChatTextMessageCommand(string message, long userSendingId, long toUsersId)
+        {
+            Message = message;
+            UserSendingId = userSendingId;
+            ToUsersId = toUsersId;
+        }
+        public string Message { get; }
+        public long UserSendingId { get; }
+        public long ToUsersId { get; }
+    }
+}
