0c7726bb2686a7696edc0ee1d3a5bf40e02af606
Hadar Keidar <Hadar@cloudents.com>
2019-08-05 12:00:01 +0300

    fixed AdminUserMap.
    Made Email unique and Country Nullable

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -479,13 +479,14 @@ namespace ConsoleApp
         private static async Task HadarMethod()
         {
             //ResourcesMaintenance.GetOrphanedResources();
-            var queryBus = _container.Resolve<IQueryBus>();
-            var query = new TutorListQuery(159039, "IL",0);
-            var test = await queryBus.QueryAsync(query, default);
-            foreach (var item in test)
-            {
-                Console.WriteLine(item.UserId);
-            }
+            var uoc = _container.Resolve<IUnitOfWork>();
+            //var queryBus = _container.Resolve<IQueryBus>();
+            //var query = new TutorListQuery(159039, "IL",0);
+            //var test = await queryBus.QueryAsync(query, default);
+            //foreach (var item in test)
+            //{
+            //    Console.WriteLine(item.UserId);
+            //}
             //var provider = _container.Resolve<IMondayProvider>();
             //await provider.UpdateTextRecordAsync(244705486, "text9", "רופין", default);
             //await provider.UpdateTextRecordAsync(244705486, "_____________1",
