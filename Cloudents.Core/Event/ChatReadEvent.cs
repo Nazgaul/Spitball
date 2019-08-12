9419efa4c5d2ca381dd12baca9596d6453b2a64a
Ram <ram@cloudents.com>
2019-08-12 09:16:11 +0300

    Added signalr event on unread message.

---
+++ Cloudents.Core/Event/ChatReadEvent.cs
@@ -0,0 +1,15 @@
+﻿using Cloudents.Core.Entities;
+using Cloudents.Core.Interfaces;
+
+namespace Cloudents.Core.Event
+{
+    public class ChatReadEvent : IEvent
+    {
+        public ChatReadEvent(ChatUser chatUser)
+        {
+            ChatUser = chatUser;
+        }
+
+        public ChatUser ChatUser { get; }
+    }
+}
