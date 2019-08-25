e9f12ddd95d97523145dcbe04f83736ee453a6b0
Ram <ram@cloudents.com>
2019-08-25 17:40:11 +0300

    Fix issue with height of preview.

--- Cloudents.Web/ClientApp/components/document/mainDocument/mainDocument.vue
+++ Cloudents.Web/ClientApp/components/document/mainDocument/mainDocument.vue
@@ -275,8 +275,7 @@ export default {
                 if (this.$vuetify.breakpoint.width === 375) {
                     this.imgWidth = 375
                 }           
-                //this.imgHeight = this.imgWidth / 0.707;        
-                this.imgHeight = 1100;
+                this.imgHeight = this.imgWidth / 0.707;        
                 let result = this.document.preview.map(preview => {                    
                     return utillitiesService.proccessImageURL(preview, this.imgWidth, Math.ceil(this.imgHeight), 'pad')
                 })
