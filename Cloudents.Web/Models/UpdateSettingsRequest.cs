ed34a413f88c36bd34bd6096758360339e65af12
Ram <ram@cloudents.com>
2019-08-27 12:02:35 +0300

    Added minimum length

--- Cloudents.Web/Models/UpdateSettingsRequest.cs
+++ Cloudents.Web/Models/UpdateSettingsRequest.cs
@@ -6,14 +6,14 @@ namespace Cloudents.Web.Models
     public class UpdateSettingsRequest
     {
         [Required]
-        [StringLength(255, ErrorMessage = "StringLength")]
+        [StringLength(255,MinimumLength = 2, ErrorMessage = "StringLength")]
         public string FirstName { get; set; }
-        //[Required]
+        [StringLength(255,MinimumLength = 2, ErrorMessage = "StringLength")]
         public string LastName { get; set; }
-        [StringLength(255, ErrorMessage = "StringLength")]
+        [StringLength(255,MinimumLength = 15, ErrorMessage = "StringLength")]
         public string Description { get; set; }
 
-        [StringLength(1000, ErrorMessage = "StringLength")]
+        [StringLength(1000, MinimumLength = 15, ErrorMessage = "StringLength")]
         public string Bio { get; set; }
 
         [Range(Tutor.MinimumPrice, Tutor.MaximumPrice)]
