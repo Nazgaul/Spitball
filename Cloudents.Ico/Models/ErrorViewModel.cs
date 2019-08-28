a3083fd2abc893ceef61e9ac0d106a03e00a4e72
Ram <ram@cloudents.com>
2019-08-28 15:16:01 +0300

    Remove cloudents.ico

--- Cloudents.Ico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cloudents.Ico.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
