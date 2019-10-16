181a78785fa502273bd1f05c08d46eb4664d54c7
Ram <ram@cloudents.com>
2019-10-16 16:08:14 +0300

    Vue cli starter

---
+++ Cloudents.Web2/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Cloudents.VueCli
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+             .ConfigureWebHostDefaults(webBuilder =>
+             {
+                 webBuilder.ConfigureKestrel(serverOptions =>
+                 {
+                     // Set properties and call methods on options
+                 })
+                 .UseStartup<Startup>();
+             });
+    }
+}
