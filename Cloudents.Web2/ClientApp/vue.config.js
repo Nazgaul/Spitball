5d4cffde66ba343f376e4f849e0cfef9271aae5d
Gabriel Sternberg <gaby.str@gmail.com>
2019-10-17 17:03:25 +0300

    skeleton

--- Cloudents.Web2/ClientApp/vue.config.js
+++ Cloudents.Web2/ClientApp/vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  },
   "transpileDependencies": [
     "vuetify"
   ]
