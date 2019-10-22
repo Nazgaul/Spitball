cd91d401bce9b7fbfa0c88e7bf887ccdd1608f21
gaby <gaby@cloudents.com>
2019-10-22 12:57:14 +0000

    Merged PR 114: Product Backlog Item #850: wix answers for frymo
    Product Backlog Item #850: wix answers for frymo
    
    Related work items: #850

--- Cloudents.Web/ClientApp/services/satelliteService.js
+++ Cloudents.Web/ClientApp/services/satelliteService.js
@@ -1,14 +1,20 @@
 ﻿// import { help } from './resources';
 import { connectivityModule } from "./connectivity.module";
 import {LanguageService} from './language/languageService';
-
+import store from '../store/index';
 let cacheControl = `?v=${global.version}&l=${global.lang}`;
+const isFrymo = store.getters['isFrymo'];
+
+function websitePrefix(){
+    return `${isFrymo ? 'frymo.com':'spitball.co'}`
+}
+
 const sattelites = {
     about:{
         title: LanguageService.getValueByKey('tutorListLanding_footer_links_about'), 
         icon: 'sbf-about',
         url:{
-            en:"https://help.spitball.co/en/about-us",
+            en:`https://help.${websitePrefix()}/en/about-us`,
             he:"https://help.spitball.co/he/article/%D7%94%D7%9B%D7%9C-%D7%A2%D7%9C%D7%99%D7%A0%D7%95"
         } 
     },
@@ -16,7 +22,7 @@ const sattelites = {
         title: LanguageService.getValueByKey('tutorListLanding_footer_links_feedback'),
         
         url:{
-            en:"https://help.spitball.co/en/article/how-to-contact-us",
+            en:`https://help.${websitePrefix()}/en/article/how-to-contact-us`,
             he:"https://help.spitball.co/he/contact"
         } 
     },
@@ -24,7 +30,7 @@ const sattelites = {
         title: LanguageService.getValueByKey('tutorListLanding_footer_links_terms'),
         icon: 'sbf-terms',
         url:{
-            en:"https://help.spitball.co/en/article/terms-of-service",
+            en:`https://help.${websitePrefix()}/en/article/terms-of-service`,
             he:"https://help.spitball.co/en/article/terms-of-service"
         } 
     },
@@ -32,7 +38,7 @@ const sattelites = {
         title: LanguageService.getValueByKey('tutorListLanding_footer_links_privacy'),
         icon: 'sbf-privacy',
         url:{
-            en:"https://help.spitball.co/en/article/privacy-policy",
+            en:`https://help.${websitePrefix()}/en/article/privacy-policy`,
             he:"https://help.spitball.co/en/article/privacy-policy"
         } 
     },
@@ -40,14 +46,14 @@ const sattelites = {
         title: LanguageService.getValueByKey('tutorListLanding_footer_links_faq'),
         icon: 'sbf-help',
          url:{
-            en:"https://help.spitball.co/en/faq",
+            en:`https://help.${websitePrefix()}/en/faq`,
             he:"https://help.spitball.co/he/%D7%A9%D7%90%D7%9C%D7%95%D7%AA-%D7%A0%D7%A4%D7%95%D7%A6%D7%95%D7%AA"
         } 
     },
     contact:{
         title: LanguageService.getValueByKey('tutorListLanding_footer_links_contact'),
          url:{
-            en:"https://help.spitball.co/en/article/how-to-contact-us",
+            en:`https://help.${websitePrefix()}/en/article/how-to-contact-us`,
             he:"https://help.spitball.co/he/contact"
         } 
     },
@@ -58,7 +64,15 @@ export default {
         if(!sattelites[name]){
             console.error(`no satellite name ${name} found`);
         }
-        console.log(sattelites[name].url[language]);
-        return sattelites[name].url[language];
+
+        let satteliteUrl = sattelites[name].url[language];
+        if(!!satteliteUrl){
+            return satteliteUrl;
+        }else{
+            //get default english URL's by default
+            console.error(`no satellite url for ${language} exists, will bring default EN url instead`);
+            return sattelites[name].url['en'];
+        }
+        
     },
 }
