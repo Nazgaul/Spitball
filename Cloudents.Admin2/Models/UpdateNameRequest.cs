09fecd1eb91f36b294ed19f476a3ccd8cbea1a24
Hadar Keidar <hadar@cloudents.com>
2019-10-24 08:11:05 +0000

    Merged PR 123: PRODUCT BACKLOG ITEM 421
    PRODUCT BACKLOG ITEM 421
    
    added string length validation for Admin change name on first and lest name
    
    Related work items: #421

--- Cloudents.Admin2/Models/UpdateNameRequest.cs
+++ Cloudents.Admin2/Models/UpdateNameRequest.cs
@@ -1,10 +1,17 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Cloudents.Admin2.Models
 {
     public class UpdateNameRequest
     {
+        [Required]
         public long UserId { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "StringLength")]
         public string FirstName{ get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 2, ErrorMessage = "StringLength")]
         public string LastName { get; set; }
     }
 }
