78a54580479fe2d2d6a181cd6ecc1ea5d2b501a4
Hadar Keidar <hadar@cloudents.com>
2019-11-19 11:06:18 +0200

    started to make integration tests for feed controller with filter

---
+++ Cloudents.Web.Test/IntegrationTests/FeedApiTests.cs
@@ -0,0 +1,51 @@
+﻿using Cloudents.Core.DTOs;
+using Cloudents.Core.Enum;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cloudents.Web.Test.IntegrationTests
+{
+    [Collection(SbWebApplicationFactory.WebCollection)]
+    public class FeedApiTests
+    {
+        private readonly System.Net.Http.HttpClient _client;
+        public FeedApiTests(SbWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions()
+            {
+                AllowAutoRedirect = false
+            });
+            _client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
+        }
+
+        [Theory]
+        [InlineData("/api/feed?page=0", FeedType.Document)]
+        [InlineData("/api/feed?page=0", FeedType.Video)]
+        [InlineData("/api/feed?page=0", FeedType.Tutor)]
+        [InlineData("/api/feed?page=0", null)]
+        public async Task AggregateAllCoursesAsync_Ok(string url, FeedType? filter)
+        {
+            if (filter != null)
+            {
+                url += $"&filter={filter}";
+            }
+
+            var response = await _client.GetAsync(url);
+            var str = await response.Content.ReadAsStringAsync();
+
+            var d = JObject.Parse(str);
+            var result = d["result"]?.Value<JArray>();
+
+            result.Should().NotBeNull();
+            result.Should().OnlyHaveUniqueItems();
+            result.Should().HaveCount(21);
+        }
+    }
+}
