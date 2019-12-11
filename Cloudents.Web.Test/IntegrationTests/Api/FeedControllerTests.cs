66594c059ac91bf49f10b708373021c4ca22931b
Hadar Keidar <hadar@cloudents.com>
2019-12-11 16:54:39 +0200

    integration tests for all FeedController

--- Cloudents.Web.Test/IntegrationTests/Api/FeedControllerTests.cs
+++ Cloudents.Web.Test/IntegrationTests/Api/FeedControllerTests.cs
@@ -41,6 +41,53 @@ namespace Cloudents.Web.Test.IntegrationTests.Api
 
             //str.Should().BeEmpty();
         }
+
+        [Theory]
+        [InlineData(false, 0)]
+        [InlineData(true, 0)]
+        [InlineData(false, 10000)]
+        [InlineData(true, 10000)]
+        public async Task GetAsync_AggregateAllCourses_Ok(bool logIn, int page)
+        {
+            if (logIn)
+            {
+                await _client.LogInAsync();
+            }
+            var response = await _client.GetAsync($"api/feed?page={page}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Theory]
+        [InlineData("econ", false, 0)]
+        [InlineData("econ", true, 0)]
+        [InlineData("this is a long term without results", false, 0)]
+        [InlineData("this is a long term without results", true, 0)]
+        public async Task GetAsync_Search_WithTerm_Ok(string term, bool logIn, int page)
+        {
+            if (logIn)
+            {
+                await _client.LogInAsync();
+            }
+            var response = await _client.GetAsync($"api/feed?term={term}&page={page}");
+            response.EnsureSuccessStatusCode();
+        }
+
+
+        [Theory]
+        [InlineData("econ", "Economics", false, 0)]
+        [InlineData("econ", "Economics", true, 0)]
+        [InlineData("this is a long term without results", "nice123", false, 0)]
+        [InlineData("this is a long term without results", "nice123", true, 0)]
+        public async Task GetAsync_Search_WithTermAndCourse_Ok(string term, string course, bool logIn, int page)
+        {
+            if (logIn)
+            {
+                await _client.LogInAsync();
+            }
+            var response = await _client.GetAsync($"api/feed?term={term}&page={page}&course={course}");
+            response.EnsureSuccessStatusCode();
+        }
+
         private UriBuilder _uri = new UriBuilder()
         {
             Path = "api/document"
