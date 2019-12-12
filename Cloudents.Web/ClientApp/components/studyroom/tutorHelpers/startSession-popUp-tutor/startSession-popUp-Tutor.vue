7fcd1182829e15657acfff8a7e244c44f4b4c2b1
Gabriel <gaby.str@gmail.com>
2019-12-12 14:19:34 +0200

    ff

--- Cloudents.Web/ClientApp/components/studyroom/tutorHelpers/startSession-popUp-tutor/startSession-popUp-Tutor.vue
+++ Cloudents.Web/ClientApp/components/studyroom/tutorHelpers/startSession-popUp-tutor/startSession-popUp-Tutor.vue
@@ -10,7 +10,7 @@
             <v-progress-circular v-if="isStudentImage && studentImage" :width="2" indeterminate v-bind:size="35" color="#514f7d"/>
             <userAvatar v-else :user-name="studentName"  :userImageUrl="studentImage" :user-id="studentId" :size="'58'"/>
         </v-flex>
-        <v-flex xs12  class="pt-12">
+        <v-flex xs12 v-if="!sessionFinished" class="pt-12">
             <span class="subheading font-weight-bold" v-language:inner>tutor_start_dialog_your_student</span>
             <span class="subheading font-weight-bold">&nbsp;{{studentName}}</span>
         </v-flex>
