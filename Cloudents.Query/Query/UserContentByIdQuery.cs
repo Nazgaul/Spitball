ed32a5c7cbbbfef73ff3c29edd717859f6503c78
Maor Barel <maor@cloudents.com>
2019-12-17 14:23:09 +0200

    merge

--- Cloudents.Query/Query/UserContentByIdQuery.cs
+++ Cloudents.Query/Query/UserContentByIdQuery.cs
@@ -1,11 +1,10 @@
 ﻿using Cloudents.Core.DTOs;
 using Cloudents.Core.Entities;
+using Cloudents.Core.Enum;
 using NHibernate;
 using NHibernate.Linq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
