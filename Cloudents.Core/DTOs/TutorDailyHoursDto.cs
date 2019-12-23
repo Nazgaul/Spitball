4775ffe54c1a9650122f28b8326718f9debfe3c2
Hadar Keidar <hadar@cloudents.com>
2019-12-23 17:22:16 +0200

    Created API for tutor to see the hours he have in the system

---
+++ Cloudents.Core/DTOs/TutorDailyHoursDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cloudents.Core.DTOs
+{
+    public class TutorDailyHoursDto
+    {
+        public DayOfWeek WeekDay { get; set; }
+        public TimeSpan From { get; set; }
+        public TimeSpan To { get; set; }
+    }
+}
