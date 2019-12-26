274048aa2bc0b036e6200e9c0b7d4a15c90447b9
Hadar Keidar <hadar@cloudents.com>
2019-12-26 16:09:22 +0200

    fixed ReviewsCount on TutorListQuery

--- Cloudents.Query/Tutor/TutorListQuery.cs
+++ Cloudents.Query/Tutor/TutorListQuery.cs
@@ -33,8 +33,8 @@ namespace Cloudents.Query.Tutor
             //TODO: review query 
             public async Task<ListWithCountDto<TutorCardDto>> GetAsync(TutorListQuery query, CancellationToken token)
             {
-                const string sql = @"Select 'Tutor' as 'Type', rt.Id as UserId, rt.Name as 'Name', rt.Image as 'Image', rt.Courses, rt.Subjects, rt.Price,
-rt.Rate, rt.RateCount, rt.Bio, rt.University, rt.Lessons, rt.Country, rt.SubsidizedPrice, rt.Rating 
+                const string sql = @"Select rt.Id as UserId, rt.Name as 'Name', rt.Image as 'Image', rt.Courses, rt.Subjects, rt.Price,
+rt.Rate, rt.RateCount as ReviewsCount, rt.Bio, rt.University, rt.Lessons, rt.Country, rt.SubsidizedPrice
 from sb.ReadTutor rt
 where rt.Country = coalesce(@country, (select country from sb.[user] where Id = @userId))
 and rt.Id != @userId
@@ -46,10 +46,10 @@ CASE
 				select uc2.CourseId from sb.UsersCourses uc2 where uc2.UserId = @userid) THEN 2
    WHEN exists (
 				select c.subjectid from sb.UsersCourses uc  join sb.Course c on uc.CourseId = c.Name
-				where rt.Id = uc.UserId and uc.CanTeach = 1 --and SubjectId is not null
+				where rt.Id = uc.UserId and uc.CanTeach = 1
    intersect
 				select c2.SubjectId from sb.UsersCourses uc2 join sb.Course c2 on uc2.CourseId = c2.Name 
-				where uc2.UserId = @userid --and SubjectId is not null
+				where uc2.UserId = @userid
 				) THEN 1
    else 0
    end  desc, rt.Rating desc
