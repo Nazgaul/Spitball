419d86799fd58cca74bdc774621c7c6f760de3d1
Ram <ram@cloudents.com>
2019-12-29 11:55:14 +0200

    Fixes in seo.

--- Cloudents.Web/Api/QuestionController.cs
+++ Cloudents.Web/Api/QuestionController.cs
@@ -128,17 +128,10 @@ namespace Cloudents.Web.Api
         public async Task<IActionResult> FlagAsync([FromBody] FlagQuestionRequest model, CancellationToken token)
         {
             var userId = _userManager.GetLongUserId(User);
-            //try
-            //{
             var command = new FlagQuestionCommand(userId, model.Id, model.FlagReason);
             await _commandBus.DispatchAsync(command, token);
             return Ok();
-            //}
-            //catch (NoEnoughScoreException)
-            //{
-            //    ModelState.AddModelError(nameof(AddVoteDocumentRequest.Id), _localizer["VoteNotEnoughScore"]);
-            //    return BadRequest(ModelState);
-            //}
+            
         }
 
 
