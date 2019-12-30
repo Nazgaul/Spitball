5c554b626b2085fad9f312d07c296336529e1298
Ram <ram@cloudents.com>
2019-12-30 15:03:24 +0200

    Remove request body Initialize

--- Cloudents.Web/Services/RequestBodyInitializer.cs
+++ Cloudents.Web/Services/RequestBodyInitializer.cs
@@ -1,54 +1,54 @@
-﻿using Microsoft.ApplicationInsights.Channel;
-using Microsoft.ApplicationInsights.DataContracts;
-using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Http.Internal;
-using System;
-using System.IO;
+﻿//using Microsoft.ApplicationInsights.Channel;
+//using Microsoft.ApplicationInsights.DataContracts;
+//using Microsoft.ApplicationInsights.Extensibility;
+//using Microsoft.AspNetCore.Http;
+////using Microsoft.AspNetCore.Http.Internal;
+//using System;
+//using System.IO;
 
-namespace Cloudents.Web.Services
-{
-    public class RequestBodyInitializer : ITelemetryInitializer
-    {
-        private readonly IHttpContextAccessor _httpContextAccessor;
+//namespace Cloudents.Web.Services
+//{
+//    public class RequestBodyInitializer : ITelemetryInitializer
+//    {
+//        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public RequestBodyInitializer(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
+//        public RequestBodyInitializer(IHttpContextAccessor httpContextAccessor)
+//        {
+//            _httpContextAccessor = httpContextAccessor;
+//        }
 
-        public void Initialize(ITelemetry telemetry)
-        {
-            if (!(telemetry is RequestTelemetry requestTelemetry) ||
-                (_httpContextAccessor.HttpContext.Request.Method != HttpMethods.Post &&
-                 _httpContextAccessor.HttpContext.Request.Method != HttpMethods.Put) ||
-                !_httpContextAccessor.HttpContext.Request.Body.CanRead) return;
-            const string jsonBody = "JsonBody";
+//        public void Initialize(ITelemetry telemetry)
+//        {
+//            if (!(telemetry is RequestTelemetry requestTelemetry) ||
+//                (_httpContextAccessor.HttpContext.Request.Method != HttpMethods.Post &&
+//                 _httpContextAccessor.HttpContext.Request.Method != HttpMethods.Put) ||
+//                !_httpContextAccessor.HttpContext.Request.Body.CanRead) return;
+//            const string jsonBody = "JsonBody";
 
-            if (requestTelemetry.Properties.ContainsKey(jsonBody))
-            {
-                return;
-            }
+//            if (requestTelemetry.Properties.ContainsKey(jsonBody))
+//            {
+//                return;
+//            }
 
-            //Allows re-usage of the stream
-            try
-            {
-                _httpContextAccessor.HttpContext.Request.EnableBuffering();
+//            //Allows re-usage of the stream
+//            try
+//            {
+//                _httpContextAccessor.HttpContext.Request.EnableBuffering();
 
-                if (_httpContextAccessor.HttpContext.Request.Body.CanRead)
-                {
-                    using (var stream = new StreamReader(_httpContextAccessor.HttpContext.Request.Body))
-                    {
-                        var body = stream.ReadToEndAsync().Result;
-                        _httpContextAccessor.HttpContext.Request.Body.Seek(0, SeekOrigin.Begin);
-                        requestTelemetry.Properties.Add(jsonBody, body);
-                    }
-                }
-            }
-            catch (ObjectDisposedException)
-            {
-                //Do nothing
-            }
-        }
-    }
-}
+//                if (_httpContextAccessor.HttpContext.Request.Body.CanRead)
+//                {
+//                    using (var stream = new StreamReader(_httpContextAccessor.HttpContext.Request.Body))
+//                    {
+//                        var body = stream.ReadToEndAsync().Result;
+//                        _httpContextAccessor.HttpContext.Request.Body.Seek(0, SeekOrigin.Begin);
+//                        requestTelemetry.Properties.Add(jsonBody, body);
+//                    }
+//                }
+//            }
+//            catch (ObjectDisposedException)
+//            {
+//                //Do nothing
+//            }
+//        }
+//    }
+//}
