a2f696a15dadfc2bcb5241aea30a1cc5917fa11e
idanKal <idankallmann_91@hotmail.com>
2020-01-02 15:04:44 +0200

    Bug #1898: Courses Page - Vuetify 2 differents

--- Cloudents.Web/ClientApp/components/courses/helpers/finishBtn.vue
+++ Cloudents.Web/ClientApp/components/courses/helpers/finishBtn.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-btn sel="finished_courses" @click="finish()" round-outline class="sb-round sb-outline px-4 elevation-0">
-        <span class="caption font-weight-bold text-capitalize" v-language:inner>courses_btn_finished</span>
+    <v-btn sel="finished_courses" @click="finish()" round-outline class="sb-round sb-outline px-6 elevation-0">
+        <span class="font-weight-bold text-capitalize" v-language:inner>courses_btn_finished</span>
     </v-btn>
 </template>
 
