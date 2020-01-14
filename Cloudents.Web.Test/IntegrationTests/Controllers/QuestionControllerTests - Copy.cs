f8d84b8431651f1d5206f01ea75c273c01d9e6ff
Ram <ram@cloudents.com>
2020-01-14 14:34:19 +0200

    Added integration test to controllers.

---
+++ Cloudents.Web.Test/IntegrationTests/Controllers/QuestionControllerTests - Copy.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cloudents.Web.Test.IntegrationTests.Controllers
+{
+    [Collection(SbWebApplicationFactory.WebCollection)]
+    public class ProfileControllerTests
+    {
+        private readonly System.Net.Http.HttpClient _client;
+
+        public ProfileControllerTests(SbWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions()
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+
+        [Theory]
+        [InlineData("en")]
+        [InlineData("he")]
+        [InlineData("en-IN")]
+        public async Task GetAsync_Profile_OKAsync(string culture)
+        {
+
+            var response = await _client.GetAsync($"profile/638/Ram%20Yaari?culture={culture}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
