40431f0c856d99dc46904a97557db6d1784e78b6
Hadar Keidar <hadar@cloudents.com>
2020-01-15 11:38:51 +0200

    Made Admin delete document support multiple ids

--- Cloudents.Admin2/ClientApp/components/document/documentComponents/documentDelete/documentDeleteService.js
+++ Cloudents.Admin2/ClientApp/components/document/documentComponents/documentDelete/documentDeleteService.js
@@ -1,7 +1,19 @@
 import {connectivityModule} from '../../../../services/connectivity.module'
 
-const deleteDocument = function(id) {
-	const path = `AdminDocument/${id}`;
+
+function setIdQuery(ids){
+    if(!ids) return "";
+    let query = "";
+    ids.forEach(function(id, index){
+        query += index === 0 ? `?id=${id}` : `&id=${id}`;
+    });
+    return query;
+}
+
+
+const deleteDocument = function(ids) {
+    let idsQueryString = setIdQuery(ids);
+	const path = `AdminDocument` + idsQueryString;;
 	return connectivityModule.http.delete(path).then((resp)=>{
         return Promise.resolve(resp);
     }, (err)=>{
