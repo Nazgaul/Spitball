f9d368c4d05de0b6910601b9273ef455b29aa14c
Hadar Keidar <hadar@cloudents.com>
2020-01-19 11:21:02 +0200

    Added filter by course and documentType to Profile.GetDocumentsAsync

--- Cloudents.Query/UserDocumentsQueryHandler.cs
+++ Cloudents.Query/UserDocumentsQueryHandler.cs
@@ -23,43 +23,57 @@ namespace Cloudents.Query
             var r = _session.Query<Document>()
                 .Fetch(f => f.User)
                 .ThenFetch(f => f.University)
-                .Where(w => w.User.Id == query.Id && w.Status.State == ItemState.Ok)
-                .Select(s => new DocumentFeedDto()
+                .Where(w => w.User.Id == query.Id && w.Status.State == ItemState.Ok);
+
+            var count = _session.Query<Document>().Where(w => w.User.Id == query.Id && w.Status.State == ItemState.Ok);
+
+            if (query.DocumentType != null)
+            {
+                r = r.Where(w => w.DocumentType == query.DocumentType);
+                count = count.Where(w => w.DocumentType == query.DocumentType);
+            }
+            if (!string.IsNullOrEmpty(query.Course))
+            {
+                r = r.Where(w => w.Course.Id == query.Course);
+                count = count.Where(w => w.Course.Id == query.Course);
+            }
+
+            var result = r.Select(s => new DocumentFeedDto()
+            {
+                Id = s.Id,
+                User = new DocumentUserDto
                 {
-                    Id = s.Id,
-                    User = new DocumentUserDto
-                    {
-                        Id = s.User.Id,
-                        Name = s.User.Name,
-                        Image = s.User.ImageName,
-                    },
-                    DateTime = s.TimeStamp.UpdateTime,
-                    Course = s.Course.Id,
-                    Title = s.Name,
-                    Views = s.Views,
-                    Downloads = s.Downloads,
-                    University = s.User.University.Name,
-                    Snippet = s.Description ?? s.MetaContent,
-                    Price = s.Price,
-                    Vote = new VoteDto
-                    {
-                        Votes = s.VoteCount
-                    },
-                    DocumentType = s.DocumentType ?? DocumentType.Document,
-                    Duration = s.Duration,
-                    Purchased = _session.Query<DocumentTransaction>().Count(x => x.Document.Id == s.Id && x.Action == TransactionActionType.SoldDocument)
+                    Id = s.User.Id,
+                    Name = s.User.Name,
+                    Image = s.User.ImageName,
+                },
+                DateTime = s.TimeStamp.UpdateTime,
+                Course = s.Course.Id,
+                Title = s.Name,
+                Views = s.Views,
+                Downloads = s.Downloads,
+                University = s.User.University.Name,
+                Snippet = s.Description ?? s.MetaContent,
+                Price = s.Price,
+                Vote = new VoteDto
+                {
+                    Votes = s.VoteCount
+                },
+                DocumentType = s.DocumentType ?? DocumentType.Document,
+                Duration = s.Duration,
+                Purchased = _session.Query<DocumentTransaction>().Count(x => x.Document.Id == s.Id && x.Action == TransactionActionType.SoldDocument)
 
-                }
-                ).OrderByDescending(o => o.DateTime)
-                .Take(query.PageSize).Skip(query.Page * query.PageSize).ToFuture();
+            }
+            ).OrderByDescending(o => o.DateTime)
+            .Take(query.PageSize).Skip(query.Page * query.PageSize).ToFuture();
 
-            var count = _session.Query<Document>().Where(w => w.User.Id == query.Id && w.Status.State == ItemState.Ok).ToFuture();
+           var countFuture = count.ToFuture();
 
 
             return new ListWithCountDto<DocumentFeedDto>()
             {
-                Result = await r.GetEnumerableAsync(token),
-                Count = (await count.GetEnumerableAsync(token)).Count()
+                Result = await result.GetEnumerableAsync(token),
+                Count = (await countFuture.GetEnumerableAsync(token)).Count()
             };
         }
     }
