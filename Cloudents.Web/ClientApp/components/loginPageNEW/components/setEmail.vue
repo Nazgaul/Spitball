efcfb7c0432bae64f81e9c782de911b32f230b06
idanKal <idankallmann_91@hotmail.com>
2020-01-23 16:55:24 +0200

    fix login style width

--- Cloudents.Web/ClientApp/components/loginPageNEW/components/setEmail.vue
+++ Cloudents.Web/ClientApp/components/loginPageNEW/components/setEmail.vue
@@ -65,11 +65,13 @@ export default {
 @import '../../../styles/mixin.less';
 @import '../../../styles/colors.less';
 .setEmail {
+    width: 400px;
 
       @media (max-width: @screen-xs) {
         display: flex;
         flex-direction: column;
         align-items: center;
+        width: auto;
       }
   p {
     .responsive-property(font-size, 28px, null, 22px);
