e43ad8c6fd0e7010dbff7810c4a1592b9b70ee47
idanKal <idankallmann_91@hotmail.com>
2020-01-23 13:59:51 +0200

    add component wrapper for dialog

---
+++ Cloudents.Web/ClientApp/components/pages/global/dialogInjection/dialogInjection.vue
@@ -0,0 +1,26 @@
+<template>
+    <component v-if="query" :is="query"></component>
+</template>
+
+<script>
+
+const becomeTutor = () => import('../../../becomeTutor/becomeTutor.vue');
+
+export default {
+    components: {
+        becomeTutor
+    },
+    data: () => ({
+        query: ''
+    }),
+    watch: {
+        "$route.query.dialog"(query) {
+            this.query = query ? query : '';
+        }
+    }
+}
+</script>
+
+<style lang="less">
+
+</style>
