22edaea2a0f975ab9055fa2a8475cfc425ad1f76
Maor Barel <maor@cloudents.com>
2020-01-23 11:43:21 +0200

    done

--- Cloudents.Web/ClientApp/services/Api/homePage.js
+++ Cloudents.Web/ClientApp/services/Api/homePage.js
@@ -5,5 +5,10 @@ const BASE_URL = '/homePage';
 export default {
    get: {
       banner: () => connectivityModule.http.get(`${BASE_URL}/banner`),
+      stats: () => connectivityModule.http.get(`${BASE_URL}`),
+      subjects: (params) => connectivityModule.http.get(`${BASE_URL}/subjects`,params),
+      reviews: (params) => connectivityModule.http.get(`${BASE_URL}/reviews`,params),
+      items: (params) => connectivityModule.http.get(`${BASE_URL}/documents`,params),
+      tutors: (params) => connectivityModule.http.get(`${BASE_URL}/tutors`,params),
    },
 }
