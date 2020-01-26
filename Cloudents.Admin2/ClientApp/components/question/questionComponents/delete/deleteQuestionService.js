eb07355e7d37590130eb556dede8bea58a63c2e4
Hadar Keidar <hadar@cloudents.com>
2020-01-26 13:17:18 +0200

    fixed BUG 2187
    Admin Site - Deleting a pending document  and deleting  flagged docs

--- Cloudents.Admin2/ClientApp/components/question/questionComponents/delete/deleteQuestionService.js
+++ Cloudents.Admin2/ClientApp/components/question/questionComponents/delete/deleteQuestionService.js
@@ -1,24 +1,15 @@
 import {connectivityModule} from '../../../../services/connectivity.module'
 
 
-    function setIdQuery(ids){
-        if(!ids) return "";
-        let query = "";
-        ids.forEach(function(id, index){
-            query += index === 0 ? `?id=${id}` : `&id=${id}`;
-        });
-        return query;
-    }
-
-    export const deleteQuestion =(ids)=>{
-        let idsQueryString = setIdQuery(ids);
-        let url = 'AdminQuestion' + idsQueryString;
-        return connectivityModule.http.delete(`${url}`)
-        .then((resp)=>{
-            console.log(resp, 'success deleted');
-            return Promise.resolve(resp);
-        }, (error)=>{
-            console.log(error, 'error deleted');
-            return Promise.reject(error);
-        });
-    };
+export const deleteQuestion =(ids)=>{
+    let idstoDelete = {'id': ids};
+    let url = 'AdminQuestion'
+    return connectivityModule.http.delete(url, idstoDelete)
+    .then((resp)=>{
+        console.log(resp, 'success deleted');
+        return Promise.resolve(resp);
+    }, (error)=>{
+        console.log(error, 'error deleted');
+        return Promise.reject(error);
+    });
+};
