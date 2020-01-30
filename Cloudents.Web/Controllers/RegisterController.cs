57fad6bf074e3ff020763a0b74cc3d738b0d7107
Ram <ram@cloudents.com>
2020-01-30 16:39:52 +0200

    xx

--- Cloudents.Web/Controllers/RegisterController.cs
+++ Cloudents.Web/Controllers/RegisterController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
-using WebMarkupMin.Core;
 
 namespace Cloudents.Web.Controllers
 {
@@ -31,7 +30,7 @@ namespace Cloudents.Web.Controllers
         [Route("signin/{page?}", Name = Signin)]
 
 
-        public async Task<IActionResult> IndexAsync(string page, /*NextStep? step,*/ CancellationToken token)
+        public async Task<IActionResult> IndexAsync(string page, CancellationToken token)
         {
 
             //return View("Index");
