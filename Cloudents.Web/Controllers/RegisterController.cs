f5feb8d91e788d6de368f4509f9cf2b86232f329
Ram <ram@cloudents.com>
2020-01-30 16:39:29 +0200

    Bring back restriction

--- Cloudents.Web/Controllers/RegisterController.cs
+++ Cloudents.Web/Controllers/RegisterController.cs
@@ -34,11 +34,11 @@ namespace Cloudents.Web.Controllers
         public async Task<IActionResult> IndexAsync(string page, /*NextStep? step,*/ CancellationToken token)
         {
 
-            return View("Index");
-            if (User.Identity.IsAuthenticated)
-            {
-                return Redirect("/feed");
-            }
+            //return View("Index");
+            //if (User.Identity.IsAuthenticated)
+            //{
+            //    return Redirect("/feed");
+            //}
 
 
 
