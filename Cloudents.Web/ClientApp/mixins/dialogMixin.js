772788891d59b57d2d4a6151cdf774cc85295c16
idanKal <idankallmann_91@hotmail.com>
2020-02-03 16:10:42 +0200

    fix lint error

--- Cloudents.Web/ClientApp/mixins/dialogMixin.js
+++ Cloudents.Web/ClientApp/mixins/dialogMixin.js
@@ -2,7 +2,7 @@ export default {
     methods: {
         closeDialog() {
             let { dialog, ...query } = this.$route.query;
-            
+            console.log(dialog); // need to keep for destructure query object for eslint error
             this.$router.replace({query});
         }
     }
