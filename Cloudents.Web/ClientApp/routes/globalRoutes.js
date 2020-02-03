688637437b905b17e8704f4d014dd3ab18855027
Ram <ram@cloudents.com>
2020-02-03 14:23:57 +0200

    Remove more lazy component

--- Cloudents.Web/ClientApp/routes/globalRoutes.js
+++ Cloudents.Web/ClientApp/routes/globalRoutes.js
@@ -1,12 +1,6 @@
-import notFound from  "./../components/notFound2.vue";
-
 export const globalRoutes = [
     {
         path:'*',
-        component: notFound// lazyComponent("notFound")
-        // redirect : () => {
-        //     debugger;
-        //   //  window.location = "/error/notfound?client=true";
-        // }
+        component: () => import(`./../components/notFound2.vue`) 
     }
 ]
--- Cloudents.Web/ClientApp/routes/itemRoutes.js
+++ Cloudents.Web/ClientApp/routes/itemRoutes.js
@@ -1,11 +1,11 @@
-import {lazyComponent,staticComponents} from './routesUtils.js';
+import {staticComponents} from './routesUtils.js';
 
 export const itemRoutes = [
     {
         path: "/document/:courseName/:name/:id",
         name: "document",
         components: {
-            default: lazyComponent('pages/itemPage/item'),
+            default: () => import(`../components/pages/itemPage/item.vue`),
             ...staticComponents(['banner', 'header', 'sideMenu'])
         },
         props: {
