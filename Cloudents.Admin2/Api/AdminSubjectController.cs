738153c9c9ba11fd96904867987c50ea57db5c8c
Hadar Keidar <hadar@cloudents.com>
2020-02-05 11:55:43 +0200

    Added Get subjects in AdminSubjectController

---
+++ Cloudents.Admin2/Api/AdminSubjectController.cs
@@ -0,0 +1,40 @@
+﻿using Cloudents.Command;
+using Cloudents.Core.DTOs.Admin;
+using Cloudents.Query;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cloudents.Core.Extension;
+
+namespace Cloudents.Admin2.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AdminSubjectController : ControllerBase
+    {
+        private readonly ICommandBus _commandBus;
+        private readonly IQueryBus _queryBus;
+
+
+        public AdminSubjectController(ICommandBus commandBus, IQueryBus queryBus)
+        {
+            _commandBus = commandBus;
+            _queryBus = queryBus;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<SubjectDto>> GetAsync(CancellationToken token)
+        {
+            if (User.GetCountryClaim() != null)
+            {
+                return null;
+            }
+            var query = new SubjectsTranslationQuery();
+        }
+    }
+}
