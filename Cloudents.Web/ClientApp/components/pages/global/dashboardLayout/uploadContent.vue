aa2a7732b79b7e2707d6dbe497262d327890d576
idanKal <idankallmann_91@hotmail.com>
2020-02-09 10:58:25 +0200

    Task #2399: Upload content

---
+++ Cloudents.Web/ClientApp/components/pages/global/dashboardLayout/uploadContent.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-row class="uploadContent mb-2 mb-sm-4" dense>
+        <v-col class="text-center uploadCol pb-sm-0">
+            <uploadIcon class="mb-3 uploadIcon" />
+            <div class="text1">Upload content</div>
+            <div class="text2 mb-3">Earn money by selling education content </div>
+            <v-btn class="uploadBtn" color="#4c59ff" outlined rounded>Upload</v-btn>
+        </v-col>
+    </v-row>
+</template>
+<script>
+import uploadIcon from './images/uploadIcon.svg'
+export default {
+  name: "",
+  components: {
+    uploadIcon
+  },
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="less">
+@color: #43425d;
+
+.uploadContent {
+  padding: 16px 12px;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.15);
+  border-radius: 8px;
+  background: white;
+
+  @media (max-width: 599px) {
+    box-shadow: none;
+  }
+
+  .uploadCol {
+    color: @color;
+    .uploadIcon {
+      width: 240px;
+    }
+    .text1 {
+      font-size: 16px;
+      font-weight: 600;
+    }
+    .text2 {
+      font-size: 12px;
+    }
+    .uploadBtn {
+      text-transform: initial;
+      font-weight: 600;
+      min-width: 120px;
+      letter-spacing: normal;
+    }
+  }
+}
+</style>
