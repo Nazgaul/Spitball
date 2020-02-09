61dd6cfcfc460fe16142d2c62e92ae50d60f4b6a
idanKal <idankallmann_91@hotmail.com>
2020-02-09 16:06:49 +0200

    Task #2400: Tips From Spitball

--- Cloudents.Web/ClientApp/components/pages/global/dashboardLayout/spitballTips.vue
+++ Cloudents.Web/ClientApp/components/pages/global/dashboardLayout/spitballTips.vue
@@ -1,11 +1,22 @@
 <template>
     <v-row class="spitballTips" dense>
-        <v-col cols="12" class="pa-0 d-flex justify-space-between">
-            <div class="tipTitle">Tips from spitball blog</div>
-            <router-link class="seeALl" :to="{ name: 'routeName'}">see all</router-link>
+        <v-col cols="12" class="pa-0 mb-4 d-flex justify-space-between">
+            <div class="tipTitle">{{$t('dashboard_spitball_tips')}}</div>
+            <router-link class="seeAll" :to="{ name: 'routeName'}">{{$t('dashboard_see_all')}}</router-link>
         </v-col>
-        <v-col cols="12">
-          <!-- ... -->
+        <v-col class="tipsList d-flex" cols="12">
+            <div class="tipsListBox" v-for="(tip, index) in tips" :key="index">
+                <div class="top">
+                    <img src="./images/group-16.png" alt="" >
+                </div>
+                <div class="bottom">
+                    <div class="text mb-3">{{tip.text}}</div>
+                    <div class="nameDate d-flex justify-space-between">
+                        <div class="name">{{tip.name}}</div>
+                        <div class="date">{{$d(tip.date, 'short')}}</div>
+                    </div>
+                </div>
+            </div>
         </v-col>
     </v-row>
 </template>
@@ -13,7 +24,26 @@
 export default {
   name: "spitballTips",
   data: () => ({
-
+    tips: [
+      {
+        image: './images/group-16.png',
+        text: 'Learn to Succeed: Tips to help you speak English Learn to Succeed: Tips to help you speak English',
+        name: 'Keren tzor',
+        date: new Date()
+      },
+      {
+        image: './images/group-16.png',
+        text: 'Learn to Succeed: Tips to help you speak English',
+        name: 'Keren tzor',
+        date: new Date()
+      },
+      {
+        image: './images/group-16.png',
+        text: 'Learn to Succeed: Tips to help you speak English Learn to Succeed: Tips to help you speak English Learn to Succeed: Tips to help you speak English Learn to Succeed: Tips to help you speak English',
+        name: 'Keren tzor',
+        date: new Date()
+      },
+    ]
   })
 }
 </script>
@@ -35,8 +65,46 @@ export default {
     font-weight: 600;
     font-size: 16px;
   }
-  .seeALl {
+  .seeAll {
     color: #4c59ff;
+  }
+  .tipsList {
+    .tipsListBox {
+      flex: 1;
+      margin: 0 10px;
+      &:first-child {
+        margin-left: 0;
+      }
+      &:last-child {
+        margin-right: 0;
+      }
+      .top {
+        display: flex;
+        img {
+          width: 100%;
+        }
+      }
+      .bottom {
+        border: 1px solid #dddddd;
+        border-top: 0;
+        padding: 8px 10px;
+        .text {
+          color: @global-purple;
+          font-size: 13px;
+          .giveMeEllipsis(2, 18);
+        }
+        .nameDate {
+          color: #a0a0a0;
+          font-size: 12px;
+          .name {
+            color: @global-purple;
+          }
+          .date {
+
+          }
+        }
+      }
+    }
   }
 }
 </style>
