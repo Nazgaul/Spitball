96dd5b89051ae98bff986da6eaca9b6c525ee3eb
idanKal <idankallmann_91@hotmail.com>
2020-02-09 12:47:59 +0200

    add resource to marketingTools

--- Cloudents.Web/ClientApp/components/pages/global/dashboardLayout/marketingTools.vue
+++ Cloudents.Web/ClientApp/components/pages/global/dashboardLayout/marketingTools.vue
@@ -1,22 +1,22 @@
 <template>
     <v-row class="text-center marketingTools mb-2 mb-sm-4 px-0 py-0 px-sm-4 py-sm-3" align="end" dense>
         <v-col cols="12" class="marketingCol marketingTitle d-none d-sm-block pa-0 mb-4">
-            Marketing Tools
+            {{$t('dashboard_marketing_title')}}
         </v-col>
         <v-col cols="12" sm="6" class="marketingCol social mb-2 mb-sm-0 pb-sm-0">
             <shareIcon class="mb-3 icon" />
             <div class="bottom">
-                <div class="text1">Share a social post</div>
-                <div class="text2 mb-3">Stand out on Facebook & Instagram</div>
-                <v-btn class="marketingbtn" color="#4c59ff" outlined rounded>Let’s go</v-btn>
+                <div class="text1">{{$t('dashboard_share_post')}}</div>
+                <div class="text2 mb-3">{{$t('dashboard_stand_out')}}</div>
+                <v-btn class="marketingbtn" color="#4c59ff" outlined rounded>{{$t('dashboard_lets_go')}}</v-btn>
             </div>
         </v-col>
         <v-col cols="12" sm="6" class="marketingCol pb-sm-0">
           <couponIcon class="mb-3 icon" />
           <div class="bottom">
-              <div class="text1">Create a coupon</div>
-              <div class="text2 mb-3">Create your own costume coupon</div>
-              <v-btn class="marketingbtn" color="#4c59ff" outlined rounded>Get started</v-btn>
+              <div class="text1">{{$t('dashboard_create_coupon')}}</div>
+              <div class="text2 mb-3">{{$t('dashboard_costume_coupon')}}</div>
+              <v-btn class="marketingbtn" color="#4c59ff" outlined rounded>{{$t('dashboard_get_started')}}</v-btn>
           </div>
         </v-col>
     </v-row>
@@ -30,10 +30,7 @@ export default {
   components: {
     shareIcon,
     couponIcon
-  },
-  data: () => ({
-
-  })
+  }
 }
 </script>
 <style lang="less">
