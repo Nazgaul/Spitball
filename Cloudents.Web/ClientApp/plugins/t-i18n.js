b62a80bd24c8f5d060b5263db14158b731653136
idanKal <idankallmann_91@hotmail.com>
2020-02-10 13:02:12 +0200

    clean comment

--- Cloudents.Web/ClientApp/plugins/t-i18n.js
+++ Cloudents.Web/ClientApp/plugins/t-i18n.js
@@ -48,37 +48,21 @@ const dateTimeFormats = {
     short: {
       year: 'numeric', month: 'short', day: 'numeric'
     },
-    // long: {
-    //   year: 'numeric', month: 'short', day: 'numeric',
-    //   weekday: 'short', hour: 'numeric', minute: 'numeric'
-    // }
   },
   'en-IL': {
     short: {
       year: 'numeric', month: 'short', day: 'numeric'
     },
-    // long: {
-    //   year: 'numeric', month: 'short', day: 'numeric',
-    //   weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
-    // }
   },
   'he-IL': {
     short: {
       year: 'numeric', month: 'short', day: 'numeric'
     },
-    // long: {
-    //   year: 'numeric', month: 'short', day: 'numeric',
-    //   weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
-    // }
   },
   'en-IN': {
     short: {
       year: 'numeric', month: 'short', day: 'numeric'
     },
-    // long: {
-    //   year: 'numeric', month: 'short', day: 'numeric',
-    //   weekday: 'short', hour: 'numeric', minute: 'numeric', hour12: true
-    // }
   }
 }
 
