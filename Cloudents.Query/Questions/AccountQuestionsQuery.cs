90bc286a0346baeee603ec2c923a48b879d98589
Elad Levavi <elad@cloudents.com>
2020-02-12 17:08:14 +0200

    Fix unit test (paramet name)

--- Cloudents.Query/Questions/AccountQuestionsQuery.cs
+++ Cloudents.Query/Questions/AccountQuestionsQuery.cs
@@ -51,7 +51,7 @@ namespace Cloudents.Query.Questions
                                 join sb.[user] u
 	                                on u.Id = q.UserId
                                 ,cte
-                                where not exists (select Id from sb.Answer where QuestionId = q.Id and State = 'Ok'and UserId = :userId) 
+                                where not exists (select Id from sb.Answer where QuestionId = q.Id and State = 'Ok'and UserId = :userid) 
                                 and q.Updated > GETUTCDATE() - 182
                                 and q.State = 'Ok'
                                 order by
