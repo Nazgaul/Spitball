13e762a4eb2711515f0609bf16a2795e837a9c02
idanKal <idankallmann_91@hotmail.com>
2020-02-24 14:39:14 +0200

    layout fix

--- Cloudents.Web/ClientApp/components/pages/dashboardPage/dashboardTeacher/uploadContent.vue
+++ Cloudents.Web/ClientApp/components/pages/dashboardPage/dashboardTeacher/uploadContent.vue
@@ -23,7 +23,8 @@ export default {
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.15);
   border-radius: 8px;
   background: white;
-
+  width: 100%;
+  margin: 0 auto;
   @media (max-width: @screen-xs) {
     box-shadow: none;
   }
