08c167e52610d132dde9b97b6ebe166dfa9dd97c
idanKal <idankallmann_91@hotmail.com>
2020-02-25 09:59:16 +0200

    add skeleton loader to promote template

--- Cloudents.Web/ClientApp/components/pages/marketing/promote/promoteTemplate.vue
+++ Cloudents.Web/ClientApp/components/pages/marketing/promote/promoteTemplate.vue
@@ -1,25 +1,28 @@
 <template>
     <div class="promoteTemplate">
-        <div class="top  d-none d-sm-flex align-center">
+        <div class="top d-none d-md-flex align-center">
             <div class="topWrap">
                 <div class="textWrap mr-4">
                   <div class="text1">{{$t('promote_template_title1')}}</div>
                   <div class="text2">{{$t('promote_template_title2')}}</div>
                 </div>
             </div>
-            <div class="bottomWrap text-center" >
-                <img class="img" :src="$proccessImageUrl('https://spitball-function-dev2.azurewebsites.net/api/share/document/50997?theme=1', 422, 220 )" alt="">
+            <div class="bottomWrap text-center">
+                <v-skeleton-loader type="image" width="422" v-if="loading"></v-skeleton-loader>
+
+                <img class="img" @load="onLoad" v-else :src="$proccessImageUrl('https://spitball-function-dev2.azurewebsites.net/api/share/document/50997?theme=1', 422, 220)" alt="">
                 <div class="btnWrap">
                   <v-btn class="useBtn" @click="useTemplate(recommended)" height="34" rounded outlined color="#4c59ff">{{$t('promote_use_template')}}</v-btn>
                 </div>
             </div>
         </div>
 
-        <div class="">{{$t('promote_choose_template')}}</div>
+        <div class="centerTitle">{{$t('promote_choose_template')}}</div>
 
         <div class="bottom">
           <div v-for="n in 3" class="bottomWrap text-center" :key="n">
-              <img class="img" :src="$proccessImageUrl(`https://spitball-function-dev2.azurewebsites.net/api/share/document/50997?theme=${n+1}`, 292, 150)" alt="">
+              <v-skeleton-loader type="image" width="292" v-if="loading"></v-skeleton-loader>
+              <img class="img" @load="onLoad" :src="$proccessImageUrl(`https://spitball-function-dev2.azurewebsites.net/api/share/document/50997?theme=${n+1}`, 292, 150)" alt="">
               <div class="btnWrap">
                 <v-btn class="useBtn" @click="useTemplate(template)" height="34" rounded outlined color="#4c59ff">
                     <span class="text-truncate">{{$t('promote_use_template')}}</span>
@@ -54,12 +57,16 @@ export default {
   data() {
     return {
       selected: 0,
+      loading: true,
     }
   },
   methods: {
     useTemplate(template) {
       this.selected = template.id
       this.$emit('selectedTemplate', template);
+    },
+    onLoad() {
+      this.loading = false
     }
   }
 }
@@ -85,7 +92,11 @@ export default {
       }
     }
   }
-
+  .centerTitle {
+    color: @global-purple;
+    font-size: 18px;
+    margin-bottom: 14px;
+  }
   .bottom {
     display: grid;
     grid-template-columns: repeat(auto-fill, 292px);
@@ -96,13 +107,15 @@ export default {
   }
 
   .bottomWrap {
-    width: max-content;
+    // min-width: 100%;
     @media (max-width: @screen-xs) {
-      width: 100%;
+      // width: 100%;
     }
     .img {
       vertical-align: bottom;
-      width: 100%;
+      @media (max-width: @screen-xs) {
+        width: 100%;
+      }
     }
   }
 
