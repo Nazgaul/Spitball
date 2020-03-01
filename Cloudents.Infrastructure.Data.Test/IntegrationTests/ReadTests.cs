441372ecde8fbc64101f77246173ce6993d71447
Ram <ram@cloudents.com>
2020-03-01 13:15:15 +0200

    Some fixes.

--- Cloudents.Infrastructure.Data.Test/IntegrationTests/ReadTests.cs
+++ Cloudents.Infrastructure.Data.Test/IntegrationTests/ReadTests.cs
@@ -106,10 +106,13 @@ namespace Cloudents.Infrastructure.Data.Test.IntegrationTests
             var query = new DocumentFeedWithFilterQuery(page, userId, filter, country, course, pageSize);
             var result = (await fixture.QueryBus.QueryAsync(query, default)).ToList();
             //result.Should().NotBeNullOrEmpty();
-            result.Should().OnlyContain(c => c.DocumentType == DocumentType.Video);
-            if (!string.IsNullOrEmpty(course))
+            if (result.Count > 0)
             {
-                result.Should().OnlyContain(c => c.Course == course);
+                result.Should().OnlyContain(c => c.DocumentType == DocumentType.Video);
+                if (!string.IsNullOrEmpty(course))
+                {
+                    result.Should().OnlyContain(c => c.Course == course);
+                }
             }
         }
 
