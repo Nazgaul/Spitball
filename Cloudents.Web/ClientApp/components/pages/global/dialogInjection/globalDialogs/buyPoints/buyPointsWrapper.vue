c242fd0d26ce84525252a198775c9f009523b73c
Maor Barel <maor@cloudents.com>
2020-03-04 12:45:20 +0200

    v1 need to add stash

---
+++ Cloudents.Web/ClientApp/components/pages/global/dialogInjection/globalDialogs/buyPoints/buyPointsWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-dialog :value="true" persistent max-width="840px" :content-class="selectedBuyPointsClass">
+       <component :is="selectedBuyPointsComponent"></component>
+    </v-dialog>
+</template>
+
+<script>
+import paymentService from '../../../../../../services/payment/paymentService.js';
+import buyPointsFrymo from '../../../../../dialogs/buyTokenFrymo/buyTokenFrymo.vue';
+import buyPoints from '../../../../../dialogs/buyTokens/buyTokens.vue';
+
+export default {
+   name:'buyPointsWrapper',
+   components:{
+      buyPointsFrymo,
+      buyPoints
+   },
+   computed: {
+      selectedBuyPointsComponent(){
+         return paymentService.getBuyPointsComponent()
+      },
+      selectedBuyPointsClass(){
+         return !this.$store.getters.isFrymo ? 'buy-tokens-popup' : 'buy-tokens-frymo-popup';
+      }
+   },
+
+}
+</script>
