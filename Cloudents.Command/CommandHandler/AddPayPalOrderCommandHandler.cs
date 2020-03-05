8feb0e23ce6b82ea9fc8dce45614d9922a44d01b
Hadar Keidar <hadar@cloudents.com>
2020-03-05 11:52:45 +0200

    xxx

--- Cloudents.Command/CommandHandler/AddPayPalOrderCommandHandler.cs
+++ Cloudents.Command/CommandHandler/AddPayPalOrderCommandHandler.cs
@@ -9,14 +9,20 @@ namespace Cloudents.Command.CommandHandler
     public class AddPayPalOrderCommandHandler : ICommandHandler<AddPayPalOrderCommand>
     {
         private readonly IRepository<StudyRoomSession> _studyRoomSessionRepository;
-        public AddPayPalOrderCommandHandler(IRepository<StudyRoomSession> studyRoomSessionRepository)
+        private readonly IRepository<StudyRoom> _studyRoomRepository;
+        public AddPayPalOrderCommandHandler(IRepository<StudyRoomSession> studyRoomSessionRepository,
+            IRepository<StudyRoom> studyRoomRepository)
         {
             _studyRoomSessionRepository = studyRoomSessionRepository;
+            _studyRoomRepository = studyRoomRepository;
         }
 
         public async Task ExecuteAsync(AddPayPalOrderCommand message, CancellationToken token)
         {
-            var session = await _studyRoomSessionRepository.LoadAsync(message.RoomId, token);
+            var room = await _studyRoomRepository.LoadAsync(message.RoomId, token);
+            var session = room.GetCurrentSession();
+           
+            //var session = await _studyRoomSessionRepository.LoadAsync(message.RoomId, token);
             var payPal = new PayPal(message.Token);
             session.SetPyment(payPal);
             await _studyRoomSessionRepository.UpdateAsync(session, token);
