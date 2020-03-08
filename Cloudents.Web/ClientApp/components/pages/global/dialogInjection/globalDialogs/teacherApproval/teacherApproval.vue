1a16ae7778c40bc0cf89aa643ce5b9efcdae04e1
idanKal <idankallmann_91@hotmail.com>
2020-03-08 17:02:06 +0200

    changes in dialog

--- Cloudents.Web/ClientApp/components/pages/global/dialogInjection/globalDialogs/teacherApproval/teacherApproval.vue
+++ Cloudents.Web/ClientApp/components/pages/global/dialogInjection/globalDialogs/teacherApproval/teacherApproval.vue
@@ -1,51 +1,41 @@
 <template>
-   <v-dialog :value="true" persistent :maxWidth="'840'" :content-class="'teacherApproval'" :fullscreen="$vuetify.breakpoint.xsOnly">
-       <div class="pa-3">
-            <div>{{$t('teacherApproval_title')}}</div>
-            <v-alert type="error" icon="mdi-firework">sadssdas</v-alert>
-       </div>
-        <v-row class="ma-0">
-            <v-col cols="6">
-                <v-col>{{$t('teacherApproval_date')}} <b>{{$d(new Date(item.date))}}</b></v-col>
-                <v-col>{{$t('teacherApproval_student')}}  <b>{{item.name}}</b></v-col>
-                <v-col>{{$t('teacherApproval_time_record')}}  <b>{{item.duration | sessionDuration}} </b></v-col>
-            </v-col>
-            <v-col cols="6" class="mb-4">
-                <template>
-                    <v-col>{{$t('teacherApproval_coupon_used')}} <b>{{item.price}}</b></v-col>
-                    <v-col>{{$t('teacherApproval_coupon_payee')}} <b>{{item.price}}</b></v-col>
-                    <v-col>{{$t('teacherApproval_coupon_payout')}} <b>{{$n(item.price, 'currency')}}</b></v-col>
-                </template>
-            </v-col>
-            
-            <v-col cols="12" sm="3">
-                <v-text-field :label="$t('teacherApproval_hourly_price')"></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-                <v-text-field :label="$t('teacherApproval_session_duration')" :value="item.duration | sessionDuration"></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-                <v-text-field :label="$t('teacherApproval_coupon_discount')"></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="3">
-                <v-text-field :label="$t('teacherApproval_total_session')" ></v-text-field>
-            </v-col>
+   <v-dialog :value="true" persistent :maxWidth="'580'" :content-class="'teacherApproval'" :fullscreen="$vuetify.breakpoint.xsOnly">
+       <!-- <div class="">
+       </div> -->
+        <div class="pa-4 text-center">
+            <div class="mainTitle text-center mb-3">{{$t('teacherApproval_title')}}</div>
+            <v-alert type="error" icon="mdi-firework">{{$t('teacherApproval_error')}}</v-alert>
+            <div>
+                <div>{{$t('teacherApproval_date')}} <b>{{$d(new Date(item.date))}}</b></div>
+                <div>{{$t('teacherApproval_student')}}  <b>{{item.name}}</b></div>
 
-            <v-row class="ma-0 pa-3">
-                <v-btn width="120" outlined color="#5A66FF">{{$t('teacherApproval_need_help')}}</v-btn>
+                <div>{{$t('teacherApproval_session_duration')}} <b>{{item.duration | sessionDuration}}</b></div>
+                <div>{{$t('teacherApproval_lesson_per_hour')}} <b>{{}}</b></div>
+                <div>{{$t('teacherApproval_coupon_discount')}} <b>{{}}</b></div>
+                <v-divider></v-divider>
+                <div>{{$t('teacherApproval_total_session')}} <b>{{}}</b></div>
+            </div>
+
+
+            <div class="d-flex">
+                <v-btn icon color="#5A66FF" :ripples="false" depressed><needHelpIcon/></v-btn>
                 <v-spacer></v-spacer>
-                <v-btn width="120" text v-closeDialog>{{$t('teacherApproval_cancel')}}</v-btn>
-                <v-btn width="120" color="#5A66FF">{{$t('teacherApproval_approve')}}</v-btn>
-            </v-row>
-        </v-row>
+                <v-btn width="140" height="40" color="#4452fc" rounded outlined v-closeDialog>{{$t('teacherApproval_btn_cancel')}}</v-btn>
+                <v-btn width="140" height="40" color="#4452fc" rounded class="white--text" depressed>{{$t('teacherApproval_btn_approve')}}</v-btn>
+            </div>
+        </div>
     </v-dialog>
 </template>
 
 <script>
 // import { mapGetters } from 'vuex';
 // import * as routeNames from '../../../../../../routes/routeNames.js';
+import needHelpIcon from './need_help.svg'
 export default {
     name: 'teacherApproval',
+    components: {
+        needHelpIcon
+    },
     data() {
         return {
             item: null
@@ -54,9 +44,7 @@ export default {
     computed: {
     },
     created() {
-        debugger
         this.item = this.$route.params || null
-        
     }
 }
 </script>
@@ -66,5 +54,11 @@ export default {
 @import '../../../../../../styles/mixin.less';
 .teacherApproval{
     background: #fff;
+
+    .mainTitle {
+        color: @global-purple;
+        font-size: 20px;
+        font-weight: 600;
+    }
 }
 </style>
