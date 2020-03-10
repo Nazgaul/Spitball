70cf3d93217b711e434193aba7b618472589d0de
Ram <ram@cloudents.com>
2020-03-10 15:17:12 +0200

    Working on analytics event handler.

---
+++ Cloudents.Core/EventHandler/AnalyticsEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cloudents.Core.Event;
+using Cloudents.Core.Interfaces;
+
+namespace Cloudents.Core.EventHandler
+{
+    public class AnalyticsEventHandler : IEventHandler<TutorApprovedEvent>,
+        IEventHandler<StudyRoomSessionCreatedEvent>
+    {
+        private readonly IGoogleAnalytics _googleAnalytics;
+
+        public AnalyticsEventHandler(IGoogleAnalytics googleAnalytics)
+        {
+            _googleAnalytics = googleAnalytics;
+        }
+
+        public Task HandleAsync(TutorApprovedEvent eventMessage, CancellationToken token)
+        {
+            return _googleAnalytics.TrackEventAsync("Tutor funnel", "Tutor approved", eventMessage.UserId.ToString(), token);
+        }
+
+        public async Task HandleAsync(StudyRoomSessionCreatedEvent eventMessage, CancellationToken token)
+        {
+           //eventMessage.StudyRoomSession.StudyRoom.Identifier
+        }
+    }
+}
