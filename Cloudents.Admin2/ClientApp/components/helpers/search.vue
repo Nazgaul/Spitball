bccdba018e23d3ac71bb6a114df1f849d0b1bb7f
Hadar Keidar <hadar@cloudents.com>
2020-03-12 15:06:59 +0200

    TASK 2703

--- Cloudents.Admin2/ClientApp/components/helpers/search.vue
+++ Cloudents.Admin2/ClientApp/components/helpers/search.vue
@@ -28,7 +28,7 @@
                              @click="setCallback(field)">
 
                     <v-list-tile-content>
-                        <v-list-tile-title v-text="field.name"></v-list-tile-title>
+                        <v-list-tile-title> {{field.name}} | {{field.country}}</v-list-tile-title>
                         <!--<v-list-tile-sub-title v-text="field.key"></v-list-tile-sub-title>-->
                     </v-list-tile-content>
                 </v-list-tile>
