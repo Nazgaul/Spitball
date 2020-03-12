0cb45f433cfc0441cb73c0c6ce37776e94a7d184
idanKal <idankallmann_91@hotmail.com>
2020-03-12 11:49:28 +0200

    fix

--- Cloudents.Web/ClientApp/plugins/componentPlugin.js
+++ Cloudents.Web/ClientApp/plugins/componentPlugin.js
@@ -2,7 +2,7 @@ export default () => {
   return store => {
     store.subscribe((mutation) => {
         if(mutation.type === 'updateUser') {
-            if(!store.getters.getPendingPayment > 0) {
+            if(store.getters.getPendingPayment > 0) {
                 store.commit('setToaster', 'linkToaster');
             }
         }
