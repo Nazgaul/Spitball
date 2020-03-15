ff01a678e8c79d4b332cbd295dc3fb96f7380dfb
idanKal <idankallmann_91@hotmail.com>
2020-03-15 12:40:01 +0200

    add remove toaster when eneter whiteboard and bring back when leave whiteboard

--- Cloudents.Web/ClientApp/components/studyroom/tutor.vue
+++ Cloudents.Web/ClientApp/components/studyroom/tutor.vue
@@ -626,6 +626,7 @@ watch: {
     document.addEventListener("fullscreenchange",this.closeFullScreen);
   },
   destroyed(){
+    this.$store.commit('setToaster', 'linkToaster') 
     global.onbeforeunload = function() { };
   },
   beforeDestroy(){
@@ -658,6 +659,7 @@ watch: {
     storeService.registerModule(this.$store,'codeEditor_store',codeEditor_store);
   },
   async created() {
+    this.$store.commit('setToaster', '')
     this.userId = this.accountUser?.id || 'GUEST';
     if (!studyroomSettingsUtils.isBrowserSupport()) {
       this.$nextTick(()=>{
