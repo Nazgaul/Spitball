2b89a2a8169e5b49dde6e02f59d5c964aff7fac0
Ram <ram@cloudents.com>
2020-03-22 10:07:54 +0200

    Fixed AB#2778 Fix profile courses query

--- Cloudents.Query/Users/UserProfileQuery.cs
+++ Cloudents.Query/Users/UserProfileQuery.cs
@@ -106,7 +106,7 @@ and uc.tutorId =  :profileId";
 
 
                 var userCoursesFuture = _session.Query<UserCourse>()
-                    .Where(w => w.User.Id == query.UserId)
+                    .Where(w => w.User.Id == query.Id)
                     .Take(20)
                     .Select(s => s.Course.Id).ToFuture();
 
