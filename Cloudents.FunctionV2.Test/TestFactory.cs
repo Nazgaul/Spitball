59574827e31d9e7bc7444b8c1b768a7f603bedac
Ram <ram@cloudents.com>
2020-03-24 14:58:33 +0200

    nuget package update

--- Cloudents.FunctionV2.Test/TestFactory.cs
+++ Cloudents.FunctionV2.Test/TestFactory.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Primitives;
@@ -20,23 +19,23 @@ namespace Cloudents.FunctionsV2.Test
             };
         }
 
-        private static Dictionary<string, StringValues> CreateDictionary(string key, string value)
-        {
-            var qs = new Dictionary<string, StringValues>
-            {
-                { key, value }
-            };
-            return qs;
-        }
+        //private static Dictionary<string, StringValues> CreateDictionary(string key, string value)
+        //{
+        //    var qs = new Dictionary<string, StringValues>
+        //    {
+        //        { key, value }
+        //    };
+        //    return qs;
+        //}
 
-        public static DefaultHttpRequest CreateHttpRequest(string queryStringKey, string queryStringValue)
-        {
-            var request = new DefaultHttpRequest(new DefaultHttpContext())
-            {
-                Query = new QueryCollection(CreateDictionary(queryStringKey, queryStringValue))
-            };
-            return request;
-        }
+        //public static DefaultHttpRequest CreateHttpRequest(string queryStringKey, string queryStringValue)
+        //{
+        //    var request = new DefaultHttpRequest(new DefaultHttpContext())
+        //    {
+        //        Query = new QueryCollection(CreateDictionary(queryStringKey, queryStringValue))
+        //    };
+        //    return request;
+        //}
 
         public static ILogger CreateLogger(LoggerTypes type = LoggerTypes.Null)
         {
