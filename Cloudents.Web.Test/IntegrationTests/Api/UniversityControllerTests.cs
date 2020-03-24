78c6ec27c923b32afc5f82b75ce75ecfc841ef48
Ram <ram@cloudents.com>
2020-03-24 14:18:07 +0200

    Remove unused unit tests

--- Cloudents.Web.Test/IntegrationTests/Api/UniversityControllerTests.cs
+++ Cloudents.Web.Test/IntegrationTests/Api/UniversityControllerTests.cs
@@ -1,34 +1,34 @@
-﻿using System.Net;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Xunit;
+﻿//using System.Net;
+//using System.Threading.Tasks;
+//using FluentAssertions;
+//using Xunit;
 
-namespace Cloudents.Web.Test.IntegrationTests.Api
-{
-    [Collection(SbWebApplicationFactory.WebCollection)]
-    public class UniversityControllerTests //: IClassFixture<SbWebApplicationFactory>
-    {
-        private readonly System.Net.Http.HttpClient _client;
+//namespace Cloudents.Web.Test.IntegrationTests.Api
+//{
+//    [Collection(SbWebApplicationFactory.WebCollection)]
+//    public class UniversityControllerTests //: IClassFixture<SbWebApplicationFactory>
+//    {
+//        private readonly System.Net.Http.HttpClient _client;
 
-        public UniversityControllerTests(SbWebApplicationFactory factory)
-        {
-            _client = factory.CreateClient();
-        }
+//        public UniversityControllerTests(SbWebApplicationFactory factory)
+//        {
+//            _client = factory.CreateClient();
+//        }
         
 
-        [Theory]
-        [InlineData("/api/university")]
-        [InlineData("api/university?term=אוני")]
-        [InlineData("api/university?term=uni")]
-        [InlineData("api/university?term=univer&page=1")]
-        [InlineData("api/university?term=univer&page=0&country=&7BIN%7D")]
-        public async Task GetAsync_University_OKAsync(string uri)
-        {
-            var response = await _client.GetAsync(uri);
+//        [Theory]
+//        [InlineData("/api/university")]
+//        [InlineData("api/university?term=אוני")]
+//        [InlineData("api/university?term=uni")]
+//        [InlineData("api/university?term=univer&page=1")]
+//        [InlineData("api/university?term=univer&page=0&country=&7BIN%7D")]
+//        public async Task GetAsync_University_OKAsync(string uri)
+//        {
+//            var response = await _client.GetAsync(uri);
 
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            var str = await response.Content.ReadAsStringAsync();
-            str.IsValidJson().Should().BeTrue();
-        }
-    }
-}
+//            response.StatusCode.Should().Be(HttpStatusCode.OK);
+//            var str = await response.Content.ReadAsStringAsync();
+//            str.IsValidJson().Should().BeTrue();
+//        }
+//    }
+//}
