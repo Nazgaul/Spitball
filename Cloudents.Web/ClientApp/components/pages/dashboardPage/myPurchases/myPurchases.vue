a297654b1d30398d4c5fd78ecf1df3f6b417684f
Maor Barel <maor@cloudents.com>
2020-03-26 18:00:48 +0200

    add padding

--- Cloudents.Web/ClientApp/components/pages/dashboardPage/myPurchases/myPurchases.vue
+++ Cloudents.Web/ClientApp/components/pages/dashboardPage/myPurchases/myPurchases.vue
@@ -138,10 +138,10 @@ export default {
       // box-shadow: 0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12)!important;
    }
    tr{
-      }
+      height:54px;
+   }
    td{
-      // border-bottom: thin solid rgba(0, 0, 0, 0.12) !important;
-      // border: none !important;
+      border: none !important;
    }
    td:first-child {
       width:1%;
