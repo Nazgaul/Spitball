0fb999644dce585cebb76e5e98773fc26e9bd3be
Maor Barel <maor@cloudents.com>
2020-03-26 18:01:23 +0200

    add padding

--- Cloudents.Web/ClientApp/components/pages/dashboardPage/myContent/myContent.vue
+++ Cloudents.Web/ClientApp/components/pages/dashboardPage/myContent/myContent.vue
@@ -188,6 +188,12 @@ export default {
       line-height: 1.3px;
       // box-shadow: 0 2px 1px -1px rgba(0,0,0,.2),0 1px 1px 0 rgba(0,0,0,.14),0 1px 3px 0 rgba(0,0,0,.12)!important;
    }
+   tr{
+      height:54px;
+   }
+   td{
+      border: none !important;
+   }
    td:first-child {
       width:1%;
       white-space: nowrap;
