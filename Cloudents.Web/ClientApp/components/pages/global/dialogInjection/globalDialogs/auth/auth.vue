74d00e90b0ca24d453484f2c0a1b2cd34faaed7a
idan <idan@cloudents.com>
2020-03-28 21:39:04 +0300

    set up auth dialog

---
+++ Cloudents.Web/ClientApp/components/pages/global/dialogInjection/globalDialogs/auth/auth.vue
@@ -0,0 +1,38 @@
+<template>
+    <component :is="component" :component="component"></component>
+</template>
+
+<script>
+
+const login = () => import('./login/login2.vue')
+const register = () => import('./register/register.vue')
+
+export default {
+    components: {
+        login,
+        register
+    },
+    data() {
+        return {
+            component: '',
+        }
+    },
+    watch: {
+        "$store.getters.getLoginDialog"(val) {
+            this.openDialog(val, 'login')
+        },
+        "$store.getters.getRegisterDialog"(val) {
+            this.openDialog(val, 'register')
+        },
+    },
+    methods: {
+        openDialog(val, name) {
+            if(val) {
+                this.component = name
+                return
+            }
+            this.component = ''
+        }
+    },
+};
+</script>
