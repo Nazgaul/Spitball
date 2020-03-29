d8711cb19a0158702ba013d37cf718a2614eccff
idanKal <idankallmann_91@hotmail.com>
2020-03-29 11:37:37 +0300

    commit

--- Cloudents.Web/ClientApp/services/registrationService2.js
+++ Cloudents.Web/ClientApp/services/registrationService2.js
@@ -42,14 +42,13 @@ export default {
     googleRegistration, // ok
     emailLogin: logObj => authInstance.post("/LogIn", logObj), // ok
     emailRegistration: regObj => authInstance.post("/Register", regObj), // ok
-    resetPassword: email => authInstance.post("/ForgotPassword", email), // ok
-    emailForgotPasswordResend: () => authInstance.post("/ForgotPassword/resend"), // not ok ?? not sure if need it anymore @idan
+    // resetPassword: email => authInstance.post("/ForgotPassword", email), // ok
+    // emailForgotPasswordResend: () => authInstance.post("/ForgotPassword/resend"), // not ok ?? not sure if need it anymore @idan ///// signin/forgot-password
+    // updatePassword: (password, id, code) => authInstance.post("/ForgotPassword/reset", {id, code, password}), // not ok // what about change password
     smsRegistration: smsObj => authInstance.post("/sms", smsObj), // ok
     voiceConfirmation: () => authInstance.post("/sms/call"), // ok
     smsCodeVerification: data => authInstance.post("/sms/verify", data), // ok
-    updatePassword: (password, confirmPassword, id, code) => authInstance.post("ForgotPassword/reset", {id, code, password}), // not ok // what about change password
+    getLocalCode: () => authInstance.get("/sms/code"), // ok
     // emailResend: () => authInstance.post("Register/resend"),
     // resendCode: () => authInstance.post("/sms/resend"),
-    getLocalCode: () => authInstance.get("/sms/code"), // ok
-    validateEmail: email => authInstance.get(`/LogIn/ValidateEmail`, { params: { email } }), // not ok ?? not sure if need it anymore @idan
 }
