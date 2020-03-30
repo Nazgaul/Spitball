ceb75e1d0147b9af8a4a3f25d8aa9980f129a42e
Ram <ram@cloudents.com>
2020-03-30 12:08:45 +0300

    Change read chat api

--- Cloudents.Command/CommandHandler/ResetUnreadInChatCommandHandler.cs
+++ Cloudents.Command/CommandHandler/ResetUnreadInChatCommandHandler.cs
@@ -24,14 +24,14 @@ namespace Cloudents.Command.CommandHandler
 
         public async Task ExecuteAsync(ResetUnreadInChatCommand message, CancellationToken token)
         {
-            var users = message.ToUsersId.ToList();
-            users.Add(message.UserSendingId);
-            var chatRoom = await _chatRoomRepository.GetChatRoomAsync(users, token);
+           // var users = message.ToUsersId.ToList();
+          //  users.Add(message.UserSendingId);
+            var chatRoom = await _chatRoomRepository.GetChatRoomAsync(message.Identifier, token);
             if (chatRoom == null)
             {
                 throw new NullReferenceException("no such room");
             }
-            var user = chatRoom.Users.First(f => f.User.Id == message.UserSendingId);
+            var user = chatRoom.Users.Single(f => f.User.Id == message.UserSendingId);
             user.ResetUnread();
             await _chatUserRepository.UpdateAsync(user, token);
         }
