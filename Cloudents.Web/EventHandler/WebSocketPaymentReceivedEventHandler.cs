9ffb8e27e180b0b74b9f352223b2ea0b84c61b2f
Ram <ram@cloudents.com>
2020-03-30 15:28:37 +0300

    Fix payment generating jwt token for everyone in the room

--- Cloudents.Web/EventHandler/WebSocketPaymentReceivedEventHandler.cs
+++ Cloudents.Web/EventHandler/WebSocketPaymentReceivedEventHandler.cs
@@ -3,8 +3,6 @@ using Cloudents.Core.Event;
 using Cloudents.Core.Interfaces;
 using Cloudents.Web.Hubs;
 using Microsoft.AspNetCore.SignalR;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,30 +11,27 @@ namespace Cloudents.Web.EventHandler
     public class WebSocketPaymentReceivedEventHandler : IEventHandler<StudentPaymentReceivedEvent>
     {
         private readonly IHubContext<SbHub> _hubContext;
-        private readonly IHubContext<StudyRoomHub> _studyRoomHubContext;
 
-        public WebSocketPaymentReceivedEventHandler(IHubContext<SbHub> hubContext, IHubContext<StudyRoomHub> studyRoomHub)
+        public WebSocketPaymentReceivedEventHandler(IHubContext<SbHub> hubContext
+            )
         {
             _hubContext = hubContext;
-            _studyRoomHubContext = studyRoomHub;
         }
 
         public async Task HandleAsync(StudentPaymentReceivedEvent eventMessage, CancellationToken token)
         {
 
-            var list = new List<Task>();
             var message = new SignalRTransportType(SignalRType.User,
                 SignalREventAction.PaymentReceived, new object());
-            foreach (var roomId in eventMessage.User.StudyRooms.Select(s => s.Room.Id))
-            {
-                var t = _studyRoomHubContext.Clients.Group(roomId.ToString()).SendAsync(SbHub.MethodName, message, token);
-                list.Add(t);
-            }
+            //foreach (var roomId in eventMessage.User.StudyRooms.Select(s => s.Room.Id))
+            //{
+            //    var t = _studyRoomHubContext.Clients.Group(roomId.ToString())
+            //        .SendAsync(SbHub.MethodName, message, token);
+            //    list.Add(t);
+            //}
 
-            var t1 = _hubContext.Clients.User(eventMessage.User.Id.ToString())
+            await _hubContext.Clients.User(eventMessage.User.Id.ToString())
                 .SendAsync(SbHub.MethodName, message, token);
-            list.Add(t1);
-            await Task.WhenAll(list);
         }
     }
 }
