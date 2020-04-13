c24aa7d97f7ac4f87d4f0b7f9d1b1dcb2be5f091
idan <idan@cloudents.com>
2020-04-14 00:25:09 +0300

    new audio video settng dialog

---
+++ Cloudents.Web/ClientApp/components/studyroom/tutorHelpers/studyRoomSettingsDialog/studyRoomAudioVideoDialog.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-dialog :value="true" max-width="680" content-class="studyRoomAudioVideoDialog" @click:outside="$emit('updateSettingDialogState', false)">
+settingState
+    </v-dialog>
+</template>
+<script>
+export default {
+    name: 'studyRoomAudioVideoDialog',
+}
+</script>
+<style lang="less" scoped>
+    .studyRoomAudioVideoDialog {
+        background: #fff;
+    }
+</style>
