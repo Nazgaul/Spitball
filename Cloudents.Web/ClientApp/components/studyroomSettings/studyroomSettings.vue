41a8c83609d061756bddebd752e03701314a24b4
Maor Barel <maor@cloudents.com>
2020-04-26 17:24:53 +0300

    cleanv2

--- Cloudents.Web/ClientApp/components/studyroomSettings/studyroomSettings.vue
+++ Cloudents.Web/ClientApp/components/studyroomSettings/studyroomSettings.vue
@@ -1,38 +1,15 @@
-
-<template>
-  <div>
-      <component :nextStep="nextStep" :is="currentStep" />
-  </div>
-</template>
-
 <script>
 // TODO: clean this file @idan to @maor
 import {mapGetters, mapActions} from 'vuex';
 
-import studyroomSettingsUtils from './studyroomSettingsUtils';
-// import logo from '../../components/app/logo/logo-spitball.svg';
-import watchRecordedStep from './components/watchRecordedStep.vue';
-//import enableScreenStep from './components/enableScreenStep.vue';
-
-
-
-
 export default {
-  components:{
-    watchRecordedStep,
-  },
+
   data(){
     return{
       currentStep: null,
       stepHistory: [],
       currentPageIndex: 0,
       nextStepName: '',
-    }
-  },
-  props: {
-    id: {
-      type: String,
-      default: ''
     }
   },
   computed:{
@@ -77,6 +54,8 @@ export default {
     };
 
     this.setVisitedSettingPage(true);
+
+
     await studyroomSettingsUtils.validateUserMedia(true, true);
     let firstPage = studyroomSettingsUtils.determinFirstPage();
     if(firstPage.type === "studyRoom"){
