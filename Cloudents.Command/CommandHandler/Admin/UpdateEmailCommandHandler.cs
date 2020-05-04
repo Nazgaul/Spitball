d24d68da94a810ffdf862c78cae8fc728bf89c52
Ram <ram@cloudents.com>
2020-05-04 10:36:56 +0300

    Added mail validation in admin

--- Cloudents.Command/CommandHandler/Admin/UpdateEmailCommandHandler.cs
+++ Cloudents.Command/CommandHandler/Admin/UpdateEmailCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Cloudents.Command.Command.Admin;
+﻿using System;
+using Cloudents.Command.Command.Admin;
 using Cloudents.Core.Interfaces;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,13 +9,20 @@ namespace Cloudents.Command.CommandHandler.Admin
     public class UpdateEmailCommandHandler : ICommandHandler<UpdateEmailCommand>
     {
         private readonly IRegularUserRepository _repository;
-        public UpdateEmailCommandHandler(IRegularUserRepository repository)
+        private readonly IMailProvider _mailProvider;
+        public UpdateEmailCommandHandler(IRegularUserRepository repository, IMailProvider mailProvider)
         {
             _repository = repository;
+            _mailProvider = mailProvider;
         }
         public async Task ExecuteAsync(UpdateEmailCommand message, CancellationToken token)
         {
-            var user = await _repository.GetAsync(message.UserId, token);
+            var result = await _mailProvider.ValidateEmailAsync(message.Email, token);
+            if (!result)
+            {
+                throw new ArgumentException();
+            }
+            var user = await _repository.LoadAsync(message.UserId, token);
             user.ChangeEmail(message.Email);
             await _repository.UpdateAsync(user, token);
         }
