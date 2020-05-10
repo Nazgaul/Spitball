c31e95fb663fb893b14ceea4eaaf021f3c671d48
Maor Barel <maor@cloudents.com>
2020-05-10 17:41:25 +0300

    tools without calc

--- Cloudents.Web/ClientApp/components/studyroom/windows/canvas/whiteBoardFloatingTools.vue
+++ Cloudents.Web/ClientApp/components/studyroom/windows/canvas/whiteBoardFloatingTools.vue
@@ -285,7 +285,7 @@ export default {
 <style lang="less">
 .whiteBoardFloatingTools{
    position: absolute;
-   left: ~"calc(100% / 3)";
+   // left: ~"calc(100% / 3)";
    margin-top: 14px;
    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.1);
    .nav-action{
