a8066ef1e645c665e0bc3e1607d3368be15ac3a0
idanKal <idankallmann_91@hotmail.com>
2020-05-11 12:29:19 +0300

    remove sbf-carousel to slide group

--- Cloudents.Web/ClientApp/components/landingPage/components/tutorsSection.vue
+++ Cloudents.Web/ClientApp/components/landingPage/components/tutorsSection.vue
@@ -1,46 +1,44 @@
 <template>
   <div class="tutorsSections">
-    <h1 class="ts-title" v-language:inner="'homePage_ts_title'" />
-    <h2 class="ts-subtitle" v-language:inner="'homePage_ts_subtitle'" />
-    <div class="tutorsCarousel">
-      <sbCarousel :slideStep="5" @select="enterTutorCard" v-if="tutorList.length" :arrows="!$vuetify.breakpoint.xsOnly">
-        <tutorCard :fromCarousel="true" v-for="(tutor, index) in tutorList" :tutor="tutor" :key="index"/>
-      </sbCarousel>
-    </div>
+    <h1 class="ts-title" v-t="'homePage_ts_title'"></h1>
+    <h2 class="ts-subtitle" v-t="'homePage_ts_subtitle'"></h2>
+    <v-slide-group
+      class="tutorSlider"
+      :prev-icon="$vuetify.icons.prev"
+      :next-icon="$vuetify.icons.next"
+      :show-arrows="!$vuetify.breakpoint.xsOnly"
+    >
+      <v-slide-item v-for="(tutor, index) in tutorList" :key="index">
+          <tutorCard :tutor="tutor" :key="index" />
+      </v-slide-item>
+    </v-slide-group>
   </div>
 </template>
 
 <script>
-const sbCarousel = () => import(/* webpackChunkName: "sbCarousel" */"../../sbCarousel/sbCarousel.vue");
 import tutorCard from "../../carouselCards/tutorCard.vue"
-import { mapActions, mapGetters } from "vuex";
+
 export default {
-  components: {sbCarousel,tutorCard},
-  computed: {
-    ...mapGetters(["getHPTutors"]),
-    tutorList() {
-      return this.getHPTutors;
+  components: { tutorCard },
+  data() {
+    return {
+      isRtl: global.isRtl
     }
   },
-  methods: {
-    ...mapActions(["updateHPTutors"]),
-    enterTutorCard(vueElm){
-      if(vueElm.enterProfilePage){
-        vueElm.enterProfilePage();
-      }else{
-        vueElm.$parent.enterProfilePage();
-      }
-      
+  computed: {
+    tutorList() {
+      return this.$store.getters.getHPTutors;
     }
   },
   created() {
-    this.updateHPTutors();
+    this.$store.dispatch('updateHPTutors');
   }
-};
+}
 </script>
 
 <style lang="less">
 @import "../../../styles/mixin.less";
+@import "../../../styles/colors.less";
 
 .tutorsSections {
   .responsiveLandingPage(1354px, 80px);
@@ -55,11 +53,7 @@ export default {
     }
     font-size: 24px;
     font-weight: bold;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #43425d;
+    color: @global-purple;
   }
   .ts-subtitle {
     @media (max-width: @screen-xs) {
@@ -69,21 +63,30 @@ export default {
     margin: 10px 0 15px 0;
     font-size: 16px;
     font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    color: #43425d;
+    color: @global-purple;
   }
-  .tutorsCarousel {
-    width: 100%;
-
-    .sbCarousel_btn {
-      i {
-        // transform: scaleX(1)/*rtl:scaleX(-1)*/; 
-        color: rgb(68, 82, 252) !important;
-        font-size: 18px !important;
-      }
+}
+.tutorSlider {
+  .v-slide-group__next {
+    right: -20px;
+  }
+  .v-slide-group__prev {
+    left: -10px;
+  }
+  .v-slide-group__prev, .v-slide-group__next {
+    height: 50px !important;
+    width: 50px !important;
+    background: #fff;
+    position: absolute;
+    top: calc(50% - 20px);
+    z-index: 11;
+    border-radius: 50%;
+    box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14), 0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+  
+    i {
+      transform: scaleX(1)/*rtl:scaleX(-1)*/; 
+      font-size: 18px !important;
+      color: #4c59ff !important;
     }
   }
 }
