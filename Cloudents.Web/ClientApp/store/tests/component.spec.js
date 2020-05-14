5afd5928b237e3209a415f5a0a24cd88302b7eb0
idanKal <idankallmann_91@hotmail.com>
2020-05-14 10:00:52 +0300

    jest test

---
+++ Cloudents.Web/ClientApp/store/tests/component.spec.js
@@ -0,0 +1,13 @@
+import { mutations } from '../Toaster'
+import { setComponent } from mutations
+console.log(mutations);
+
+describe('set component mutations', () => {
+    it('Receive', () => {
+        const state = ''
+
+        setComponent('xxx')
+
+        expect(state.isEmpty()).toBe(false)
+    })
+})
