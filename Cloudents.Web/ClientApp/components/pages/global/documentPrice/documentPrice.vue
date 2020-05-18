89b22ffc83db3b70d453b8020fe239f49d84e008
idanKal <idankallmann_91@hotmail.com>
2020-05-18 16:29:55 +0300

    fix itemCard resource

--- Cloudents.Web/ClientApp/components/pages/global/documentPrice/documentPrice.vue
+++ Cloudents.Web/ClientApp/components/pages/global/documentPrice/documentPrice.vue
@@ -1,6 +1,6 @@
 <template functional>
     <div class="documentPrice">
         <v-icon class="docIcon" color="#69687d" v-if="props.price > 0 || props.isSubscribed" size="20">{{parent.$vuetify.icons.values.lock}}</v-icon>
-        <span class="docFree" v-else v-t="'resultNote_free'"></span>
+        <span v-else>{{parent.$tc('itemCardCarousel_pts', 0)}}</span>
     </div>
 </template>
