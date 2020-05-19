8b880363e19e312b9a27194941672a2524f0a422
Elad Levavi <elad@cloudents.com>
2020-05-19 16:00:47 +0300

    Add some unit testing

---
+++ Cloudents.Core.Test/Entities/AnswerTests.cs
@@ -0,0 +1,21 @@
+﻿using Cloudents.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Cloudents.Core.Test.Entities
+{
+    public class AnswerTests
+    {
+        [Theory]
+        [InlineData('a', 0)]
+        [InlineData('a', 14)]
+        [InlineData('a', 551)]
+        public void IllegalCharsAnswerTests(char c, int num)
+        {
+            string str = new string(c, num);
+            Assert.Throws<ArgumentException>(() => new Answer(null, str, null));
+        }
+    }
+}
