05bc8b16be653cb9f8735286bc48e9ebc05cae01
Ram <ram@cloudents.com>
2020-05-24 09:28:10 +0300

    #3509 Bug 3509: Issue with www.spitball.co POST Sms/SetUserPhoneNumber

--- Cloudents.Persistance/Maps/RegularUserMap.cs
+++ Cloudents.Persistance/Maps/RegularUserMap.cs
@@ -77,7 +77,8 @@ namespace Cloudents.Persistence.Maps
                 .Cascade.AllDeleteOrphan()
                 .KeyColumn("UserId").Inverse();
 
-            HasOne(x => x.Tutor).Cascade.None().LazyLoad(Laziness.NoProxy);
+            //On sms api - we got  No persister for: UserProxyForFieldInterceptor if we put no Proxy on lazy type
+            HasOne(x => x.Tutor).Cascade.None()/*.LazyLoad(Laziness.NoProxy)*/;
 
             HasMany(x => x.UserCoupon).Access.CamelCaseField(Prefix.Underscore)
                 .Cascade.AllDeleteOrphan()
