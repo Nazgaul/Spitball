e6a408f20a7d770932a49d8ee1a739c5428c5581
Ram <ram@cloudents.com>
2020-05-24 12:12:09 +0300

    #3371 Bug 3371: brian@thegreatness.studio is a valid email

--- Cloudents.Infrastructure/Mail/MailProvider.cs
+++ Cloudents.Infrastructure/Mail/MailProvider.cs
@@ -41,7 +41,7 @@ namespace Cloudents.Infrastructure.Mail
 
             var result = s.ToJsonReader<Rootobject>();
 
-            return result.Result.Score > 0.44;
+            return result.Result.Score >= 33.75;
             //return result.Result.Verdict == "Valid";
            
         }
