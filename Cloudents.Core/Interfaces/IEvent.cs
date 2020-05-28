f57d1ab649a0c1747a5075264e7b437af4cc363e
Ram <ram@cloudents.com>
2020-05-28 08:22:05 +0300

    Some move codes.

--- Cloudents.Core/Interfaces/IEvent.cs
+++ Cloudents.Core/Interfaces/IEvent.cs
@@ -1,31 +1,8 @@
-﻿using System;
-
+﻿
 namespace Cloudents.Core.Interfaces
 {
     public interface IEvent
     {
 
-    }
-
-    public interface IBinarySerializer
-    {
-        byte[] Serialize(object o);
-        T Deserialize<T>(byte[] sr);
-    }
-
-    public interface IJsonSerializer
-    {
-        string Serialize(object o);
-
-        T Deserialize<T>(string sr);
-
-        T Deserialize<T>(string sr, JsonConverterTypes types);
-    }
-
-    [Flags]
-    public enum JsonConverterTypes
-    {
-        None = 0x0,
-        TimeSpan = 0x1
     }
 }
