a2b6e3e2af97ea0f53d5085fa49189366cbcb1a3
idan <idan@cloudents.com>
2020-05-31 10:50:53 +0300

    reroll new dashboard

--- Cloudents.Web/ClientApp/components/pages/global/analyticOverview/analyticOverview.vue
+++ Cloudents.Web/ClientApp/components/pages/global/analyticOverview/analyticOverview.vue
@@ -132,7 +132,7 @@ export default {
     padding: 16px;
     background: white;
     //need to put it for feed page
-    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.15);
+    // box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.15);
     border-radius: 8px;
     width: 100%;
     margin: 0 auto;
