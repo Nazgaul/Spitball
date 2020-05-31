4f8278d78e4233ab6d37e2642b0f3682f38d2a1a
idan <idan@cloudents.com>
2020-05-31 11:13:46 +0300

    change name

--- Cloudents.Web/ClientApp/components/pages/dashboardPage/dashboardTeacher/dashboard.vue
+++ Cloudents.Web/ClientApp/components/pages/dashboardPage/dashboardTeacher/dashboard.vue
@@ -2,7 +2,7 @@
   <div class="dashboardMain mr-md-6">
     <dashboardTutorActions />
     <dashboardNotifications />
-    <marketingTools />
+    <dashboardTools />
     <spitballTips />
   </div>
 </template>
@@ -10,14 +10,14 @@
 <script>
 const dashboardTutorActions = () => import('./dashboardTutorActions.vue');
 const dashboardNotifications = () => import('./dashboardNotifications.vue');
-const marketingTools = () => import('./marketingTools.vue');
+const dashboardTools = () => import('./dashboardTools.vue');
 const spitballTips = () => import('./spitballTips.vue');
 
 export default {
   components: {
     dashboardTutorActions,
     dashboardNotifications,
-    marketingTools,
+    dashboardTools,
     spitballTips,
   }
 }
