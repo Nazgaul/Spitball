92b642ba4dbbfb9bbfc28a860c006c7f970cbb86
idan <idan@cloudents.com>
2020-05-31 15:21:21 +0300

    comment coupon route

--- Cloudents.Web/ClientApp/routes/dashboardRoutes.js
+++ Cloudents.Web/ClientApp/routes/dashboardRoutes.js
@@ -137,27 +137,27 @@ export const dashboardRoutes = [
 
         },
     },
-    {
-        path: "/my-coupons",
-        name: routeName.MyCoupons,
-        components: {
-            default: () => import('../components/pages/dashboardPage/myCoupons/myCoupons.vue'),
-            ...staticComponents(['banner', 'header', 'sideMenu']),
-        },
-        beforeEnter: (to, from, next) => {
-            if(store.getters.getIsTeacher){
-                next()
-                return
-            }
-            // Redirect to root
-            next('/')
-        },
-        meta: {
-            showMobileFooter: true,
-            requiresAuth: true,
+    // {
+    //     path: "/my-coupons",
+    //     name: routeName.MyCoupons,
+    //     components: {
+    //         default: () => import('../components/pages/dashboardPage/myCoupons/myCoupons.vue'),
+    //         ...staticComponents(['banner', 'header', 'sideMenu']),
+    //     },
+    //     beforeEnter: (to, from, next) => {
+    //         if(store.getters.getIsTeacher){
+    //             next()
+    //             return
+    //         }
+    //         // Redirect to root
+    //         next('/')
+    //     },
+    //     meta: {
+    //         showMobileFooter: true,
+    //         requiresAuth: true,
 
-        }
-    },
+    //     }
+    // },
     {
         path: "/wallet",
         redirect: { name: routeName.MySales }
