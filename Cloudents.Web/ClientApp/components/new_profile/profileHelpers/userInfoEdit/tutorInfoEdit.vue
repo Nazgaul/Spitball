4f56e126f87427baeb29bf2d2cd7c8795ff38356
idanKal <idankallmann_91@hotmail.com>
2020-06-04 09:48:21 +0300

    reroll price tutorInfoEdit

--- Cloudents.Web/ClientApp/components/new_profile/profileHelpers/userInfoEdit/tutorInfoEdit.vue
+++ Cloudents.Web/ClientApp/components/new_profile/profileHelpers/userInfoEdit/tutorInfoEdit.vue
@@ -8,7 +8,7 @@
                 </v-flex>
             </v-layout>
             <v-layout class="px-3 mt-4" wrap>
-                <v-flex xs12 sm6  :class="{'pr-2': $vuetify.breakpoint.smAndUp}">
+                <v-flex xs12 sm4  :class="{'pr-2': $vuetify.breakpoint.smAndUp}">
                     <v-layout column>
                         <v-flex xs12 sm6  class="pl-2 mb-2">
                             <span class="subtitle" v-t="'profile_personal_details'"></span>
@@ -24,7 +24,7 @@
                         </v-flex>
                     </v-layout>
                 </v-flex>
-                <v-flex xs12 sm6 :class="[ $vuetify.breakpoint.xsOnly ? 'mt-2 mr-0' : 'pr-2']">
+                <v-flex xs12 sm4 :class="[ $vuetify.breakpoint.xsOnly ? 'mt-2 mr-0' : 'pr-2']">
                     <v-layout column>
                         <v-flex v-if="$vuetify.breakpoint.smAndUp" xs12 sm6  class="mb-2 pl-2" grow>
                             <span class="subtitle" style="visibility: hidden">hidden</span>
@@ -40,7 +40,7 @@
                         </v-flex>
                     </v-layout>
                 </v-flex>
-                <!-- <v-flex xs12 sm4 :class="{'mt-4': $vuetify.breakpoint.xsOnly}" v-if="!isFrymo">
+                <v-flex xs12 sm4 :class="{'mt-4': $vuetify.breakpoint.xsOnly}" v-if="!isFrymo">
                     <v-layout column>
                         <v-flex xs12 sm6  class="mb-2 pl-2">
                             <span class="subtitle" v-t="'profile_pricing'"></span>
@@ -48,7 +48,7 @@
                         <v-flex>
                             <v-text-field 
                                         :rules="[rules.required, rules.minimum, rules.maximum,rules.integer]"
-                                        :label="priceLabel"
+                                        :label="$t('profile_price_label')"
                                         v-model="price"
                                         outlined
                                         :prefix="accountUser.currencySymbol"
@@ -58,7 +58,7 @@
                             ></v-text-field>
                         </v-flex>
                     </v-layout>
-                </v-flex> -->
+                </v-flex>
             </v-layout>
 
             <v-layout class="px-3" column :class="[$vuetify.breakpoint.xsOnly ? 'mt-4' : '']">
@@ -117,17 +117,19 @@
                 editedLastName: '',
                 rules: {
                     required: (value) => validationRules.required(value),
+                    minimum: (value) => validationRules.minVal(value, 35),
                     maximum: (value) => validationRules.maxVal(value, 1000),
                     maximumChars: (value) => validationRules.maximumChars(value, 1000),
                     minimumChars: (value) => validationRules.minimumChars(value, 2),
-                    descriptionMinChars: (value) => validationRules.minimumChars(value, 15)
+                    descriptionMinChars: (value) => validationRules.minimumChars(value, 15),
+                    integer: (value) => validationRules.integer(value)
                 },
                 valid: false,
                 btnLoading: false
             };
         },
         computed: {
-            ...mapGetters(['getProfile','accountUser']),
+            ...mapGetters(['getProfile','accountUser', 'isFrymo']),
             bio: {
                 get() {
                     return this.getProfile.user.tutorData.bio
@@ -136,14 +138,14 @@
                     this.editedBio = newVal;
                 }
             },
-            // price: {
-            //     get() {
-            //         return this.getProfile.user.tutorData.price;
-            //     },
-            //     set(newVal) {
-            //         this.editedPrice = newVal;
-            //     }
-            // },
+            price: {
+                get() {
+                    return this.getProfile.user.tutorData.price;
+                },
+                set(newVal) {
+                    this.editedPrice = newVal;
+                }
+            },
             firstName: {
                 get() {
                     return this.getProfile.user.firstName;
@@ -176,14 +178,14 @@
                 if(this.$refs.formTutor.validate()) {
                     let firstName = this.editedFirstName || this.firstName;
                     let lastName = this.editedLastName || this.lastName;
-                    // let price = this.editedPrice || this.price;
+                    let price = this.editedPrice || this.price;
                     let bio = this.editedBio || this.bio;
                     let description = this.editedDescription || this.description;
                     let editsData = {
                         name: `${firstName} ${lastName}`,
                         lastName,
                         firstName,
-                        // price,
+                        price,
                         bio,
                         description,
                     };
@@ -191,7 +193,7 @@
                     let serverFormat = {
                         firstName,
                         lastName,
-                        // price,
+                        price,
                         bio,
                         description,
                     };
