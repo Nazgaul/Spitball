e91eac9de97006f4274bc660b18364d8a6e102b7
idanKal <idankallmann_91@hotmail.com>
2020-06-08 11:13:53 +0300

    reroll pending payment

--- Cloudents.Web/ClientApp/src/plugins/componentPlugin.js
+++ Cloudents.Web/ClientApp/src/plugins/componentPlugin.js
@@ -3,11 +3,11 @@ import { auth_SETTER } from '../store/constants/authConstants';
 export default () => {
   return store => {
     store.subscribe((mutation) => {
-      // if(mutation.type === 'updateUser') {
-      //   if(store.getters.getPendingPayment > 0) {
-      //     store.commit('setComponent', 'linkToaster');
-      //   }
-      // }
+      if(mutation.type === 'updateUser') {
+        if(store.getters.getPendingPayment > 0) {
+          store.commit('setComponent', 'linkToaster');
+        }
+      }
       
       if(mutation.payload?.query?.authDialog === auth_SETTER.register) {
           let isLogged = store.getters.getUserLoggedInStatus     
