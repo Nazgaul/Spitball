a1e75c76ab76102ee87f349cf43e963a45e80954
Elad Levavi <elad@cloudents.com>
2020-06-11 13:34:30 +0300

    Added invisible css to revoke cache.

--- Cloudents.Web/ClientApp/src/components/studyroom/windows/class/classFullScreen.vue
+++ Cloudents.Web/ClientApp/src/components/studyroom/windows/class/classFullScreen.vue
@@ -142,6 +142,7 @@ export default {
 <style lang="less">
 .classFullScreen{
    background-color: #212123;
+   display: block; //Just to remove cache
    .v-footer{
       justify-content: center;
    }
