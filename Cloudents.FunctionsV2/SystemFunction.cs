9c5d4ee02aa88d74cc0325bbe7f49468a36aec7f
Ram <ram@cloudents.com>
2020-06-11 12:33:32 +0300

    Fixes of email

--- Cloudents.FunctionsV2/SystemFunction.cs
+++ Cloudents.FunctionsV2/SystemFunction.cs
@@ -11,22 +11,24 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Cloudents.Core.Entities;
+using Cloudents.Core.Interfaces;
 using Cloudents.FunctionsV2.Operations;
 using Cloudents.Infrastructure;
 using NHibernate;
 using NHibernate.Linq;
 using SendGrid.Helpers.Mail;
 using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Cloudents.FunctionsV2
 {
     public static class SystemFunction
     {
 
-       
+
 
         [FunctionName("SystemFunction")]
-        public static async Task Run([QueueTrigger(QueueName.BackgroundQueueName)]string queueMsg,
+        public static async Task Run([QueueTrigger(QueueName.BackgroundQueueName)] string queueMsg,
             [Inject] ILifetimeScope lifetimeScope,
             IBinder binder,
             ILogger log,
@@ -69,12 +71,14 @@ namespace Cloudents.FunctionsV2
         /// </summary>
         /// <param name="myTimer"></param>
         /// <param name="session"></param>
+        /// <param name="countryProvider"></param>
         /// <param name="messageCollector"></param>
         /// <param name="token"></param>
         /// <returns></returns>
         [FunctionName("Email-tutor-newCountries")]
         public static async Task RunEmailNewCountriesAsync([TimerTrigger("0 0 0 * * 0")] TimerInfo myTimer,
             [Inject] IStatelessSession session,
+            [Inject] ICountryProvider countryProvider,
             [SendGrid(ApiKey = "SendgridKey")] IAsyncCollector<SendGridMessage> messageCollector,
             CancellationToken token
         )
@@ -82,17 +86,78 @@ namespace Cloudents.FunctionsV2
             var listOfCountries = await session.Query<Tutor>().Fetch(f => f.User)
                 .Select(s => s.User.Country).Distinct().ToListAsync(token);
 
-            var notSupportedCountry = listOfCountries.Except(TwilioProvider.CountryToRegionMap.Select(s => s.Key)).ToList();
+            var notSupportedCountry = listOfCountries
+                .Except(TwilioProvider.CountryToRegionMap.Select(s => s.Key))
+                .Select(countryProvider.GetCountryParams)
+                .ToList();
             if (notSupportedCountry.Count > 0)
             {
+
                 var message = new SendGridMessage();
                 message.AddTo("ram@cloudents.com");
-                message.AddContent("text/html", string.Join("<br>", notSupportedCountry));
-                message.SetFrom(new EmailAddress("suuport@cloudents.com"));
+                message.AddContent("text/html", notSupportedCountry.ToHtmlTable());
+                message.SetFrom(new EmailAddress("support@cloudents.com"));
                 message.SetSubject("not supported countries in twilio");
 
                 await messageCollector.AddAsync(message, token);
             }
         }
+
+
+
     }
+
+
+    public static class HtmlTable
+    {
+        public static string ToHtmlTable<T>(this List<T> listOfClassObjects)
+        {
+            var ret = string.Empty;
+
+            return listOfClassObjects == null || !listOfClassObjects.Any()
+                ? ret
+                : "<table>" +
+                  listOfClassObjects.First().GetType().GetFields().Select(p => p.Name).ToList().ToColumnHeaders() +
+                  listOfClassObjects.Aggregate(ret, (current, t) => current + t.ToHtmlTableRow()) +
+                  "</table>";
+        }
+
+        private static string ToColumnHeaders<T>(this List<T> listOfProperties)
+        {
+            var ret = string.Empty;
+
+            return listOfProperties == null || !listOfProperties.Any()
+                ? ret
+                : "<tr>" +
+                  listOfProperties.Aggregate(ret,
+                      (current, propValue) =>
+                          current +
+                          ("<th style='font-size: 11pt; font-weight: bold; border: 1pt solid black'>" +
+                           (Convert.ToString(propValue).Length <= 100
+                               ? Convert.ToString(propValue)
+                               : Convert.ToString(propValue).Substring(0, 100)) + "..." + "</th>")) +
+                  "</tr>";
+        }
+
+        private static string ToHtmlTableRow<T>(this T classObject)
+        {
+            var ret = string.Empty;
+
+            return classObject == null
+                ? ret
+                : "<tr>" +
+                  classObject.GetType()
+                      .GetFields()
+                      .Aggregate(ret,
+                          (current, prop) =>
+                              current + ("<td style='font-size: 11pt; font-weight: normal; border: 1pt solid black'>" +
+                                         (Convert.ToString(prop.GetValue(classObject)).Length <= 100
+                                             ? Convert.ToString(prop.GetValue(classObject))
+                                             : Convert.ToString(prop.GetValue(classObject)).Substring(0, 100) +
+                                               "...") +
+                                         "</td>")) + "</tr>";
+        }
+    }
+
+
 }
