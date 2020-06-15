ba041d70a673b5aa62f6eb3038973d9b7c4e9f69
Ram <ram@cloudents.com>
2020-06-15 16:31:16 +0300

    Remove twilio provider and use gll

--- Cloudents.FunctionsV2/SystemFunction.cs
+++ Cloudents.FunctionsV2/SystemFunction.cs
@@ -27,81 +27,81 @@ namespace Cloudents.FunctionsV2
 
 
 
-        [FunctionName("SystemFunction")]
-        public static async Task Run([QueueTrigger(QueueName.BackgroundQueueName)] string queueMsg,
-            [Inject] ILifetimeScope lifetimeScope,
-            IBinder binder,
-            ILogger log,
-            CancellationToken token)
-        {
-            //log.LogInformation($"Got message {queueMsg}");
+        //[FunctionName("SystemFunction")]
+        //public static async Task Run([QueueTrigger(QueueName.BackgroundQueueName)] string queueMsg,
+        //    [Inject] ILifetimeScope lifetimeScope,
+        //    IBinder binder,
+        //    ILogger log,
+        //    CancellationToken token)
+        //{
+        //    //log.LogInformation($"Got message {queueMsg}");
 
-            var settings = new JsonSerializerSettings()
-            {
-                TypeNameHandling = TypeNameHandling.All,
-                //Converters.Add(new EnumerationConverter<Country>());
-            };
-            settings.Converters.Add(new EnumerationConverter<Country>());
+        //    var settings = new JsonSerializerSettings()
+        //    {
+        //        TypeNameHandling = TypeNameHandling.All,
+        //        //Converters.Add(new EnumerationConverter<Country>());
+        //    };
+        //    settings.Converters.Add(new EnumerationConverter<Country>());
 
-            var message = JsonConvert.DeserializeObject<ISystemQueueMessage>(queueMsg, settings);
+        //    var message = JsonConvert.DeserializeObject<ISystemQueueMessage>(queueMsg, settings);
 
-            var handlerType = typeof(ISystemOperation<>).MakeGenericType(message.GetType());
-            var handlerCollectionType = typeof(IEnumerable<>).MakeGenericType(handlerType);
+        //    var handlerType = typeof(ISystemOperation<>).MakeGenericType(message.GetType());
+        //    var handlerCollectionType = typeof(IEnumerable<>).MakeGenericType(handlerType);
 
-            using var child = lifetimeScope.BeginLifetimeScope();
-            if (child.Resolve(handlerCollectionType) is IEnumerable handlersCollection)
-            {
-                foreach (var handler in handlersCollection)
-                {
-                    try
-                    {
-                        dynamic operation = child.Resolve(handler.GetType());
-                        await operation.DoOperationAsync((dynamic)message, binder, token);
-                    }
-                    catch (Exception e)
-                    {
-                        log.LogInformation(e.Message);
-                    }
-                }
-            }
-        }
+        //    using var child = lifetimeScope.BeginLifetimeScope();
+        //    if (child.Resolve(handlerCollectionType) is IEnumerable handlersCollection)
+        //    {
+        //        foreach (var handler in handlersCollection)
+        //        {
+        //            try
+        //            {
+        //                dynamic operation = child.Resolve(handler.GetType());
+        //                await operation.DoOperationAsync((dynamic)message, binder, token);
+        //            }
+        //            catch (Exception e)
+        //            {
+        //                log.LogInformation(e.Message);
+        //            }
+        //        }
+        //    }
+        //}
 
-        /// <summary>
-        /// Send an email to update twilio dictionary if new country show up, run every sunday
-        /// </summary>
-        /// <param name="myTimer"></param>
-        /// <param name="session"></param>
-        /// <param name="countryProvider"></param>
-        /// <param name="messageCollector"></param>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        [FunctionName("Email-tutor-newCountries")]
-        public static async Task RunEmailNewCountriesAsync([TimerTrigger("0 0 0 * * 0")] TimerInfo myTimer,
-            [Inject] IStatelessSession session,
-            [Inject] ICountryProvider countryProvider,
-            [SendGrid(ApiKey = "SendgridKey")] IAsyncCollector<SendGridMessage> messageCollector,
-            CancellationToken token
-        )
-        {
-            var listOfCountries = await session.Query<Tutor>().Fetch(f => f.User)
-                .Select(s => s.User.Country).Distinct().ToListAsync(token);
+        ///// <summary>
+        ///// Send an email to update twilio dictionary if new country show up, run every sunday
+        ///// </summary>
+        ///// <param name="myTimer"></param>
+        ///// <param name="session"></param>
+        ///// <param name="countryProvider"></param>
+        ///// <param name="messageCollector"></param>
+        ///// <param name="token"></param>
+        ///// <returns></returns>
+        //[FunctionName("Email-tutor-newCountries")]
+        //public static async Task RunEmailNewCountriesAsync([TimerTrigger("0 0 0 * * 0")] TimerInfo myTimer,
+        //    [Inject] IStatelessSession session,
+        //    [Inject] ICountryProvider countryProvider,
+        //    [SendGrid(ApiKey = "SendgridKey")] IAsyncCollector<SendGridMessage> messageCollector,
+        //    CancellationToken token
+        //)
+        //{
+        //    var listOfCountries = await session.Query<Tutor>().Fetch(f => f.User)
+        //        .Select(s => s.User.Country).Distinct().ToListAsync(token);
 
-            var notSupportedCountry = listOfCountries
-                .Except(TwilioProvider.CountryToRegionMap.Select(s => s.Key))
-                .Select(countryProvider.GetCountryParams)
-                .ToList();
-            if (notSupportedCountry.Count > 0)
-            {
+        //    var notSupportedCountry = listOfCountries
+        //        .Except(TwilioProvider.CountryToRegionMap.Select(s => s.Key))
+        //        .Select(countryProvider.GetCountryParams)
+        //        .ToList();
+        //    if (notSupportedCountry.Count > 0)
+        //    {
 
-                var message = new SendGridMessage();
-                message.AddTo("ram@cloudents.com");
-                message.AddContent("text/html", notSupportedCountry.ToHtmlTable());
-                message.SetFrom(new EmailAddress("support@cloudents.com"));
-                message.SetSubject("not supported countries in twilio");
+        //        var message = new SendGridMessage();
+        //        message.AddTo("ram@cloudents.com");
+        //        message.AddContent("text/html", notSupportedCountry.ToHtmlTable());
+        //        message.SetFrom(new EmailAddress("support@cloudents.com"));
+        //        message.SetSubject("not supported countries in twilio");
 
-                await messageCollector.AddAsync(message, token);
-            }
-        }
+        //        await messageCollector.AddAsync(message, token);
+        //    }
+        //}
 
 
 
