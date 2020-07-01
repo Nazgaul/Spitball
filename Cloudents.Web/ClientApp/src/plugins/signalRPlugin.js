05045932e8ae9dc7406dfc68041fd49bf96f7bfa
idanKal <idankallmann_91@hotmail.com>
2020-07-01 11:30:37 +0300

    import signalR async and only for logged user

--- Cloudents.Web/ClientApp/src/plugins/signalRPlugin.js
+++ Cloudents.Web/ClientApp/src/plugins/signalRPlugin.js
@@ -1,44 +1,50 @@
-import signalRInit from '../services/signalR/init';
 import { SignalR } from '../services/Dto/signalR';
 import { signlaREvents } from '../services/signalR/signalREventHandler';
 
 export default ({ hubPath }) => {
   return store => {
-    const connection = signalRInit(hubPath);
-    connection.on('Message', (data) => {
-      let event = new SignalR.Notification(data);
-      if (event.type === 'chat') {
-        if (event.action === 'add') {
-          store.dispatch("signalRAddMessage", event.data[0]);
-        } else if (event.action === 'update') {
-          store.dispatch("checkUnreadMessageFromSignalR", event.data[0]);
-        }
-      } else {
-        signlaREvents[event.type][event.action](event.data);
-      }
-    });
-    connection.on("studyRoomToken", (jwtToken,studyRoomId) => {
-      if (store.getters.getRoomIdSession == studyRoomId) {
-        store.dispatch('updateJwtToken', jwtToken);
-      }
-    });
-
-    // signalR Reconnecting
-    connection.onreconnecting(() => {
-      store.dispatch('setIsSignalRConnected', false);
-    });
-    connection.onreconnected(() => {
-      store.dispatch('setIsSignalRConnected', true);
-    });
-
+    let connection = null
     store.subscribe((mutation) => {
       switch (mutation.type) {
         case 'updateUser':
-          if (connection.state === 'Disconnected') {
-            connection.start().then(() => {
+          import('@microsoft/signalr').then((signalR) => {
+            connection = new signalR.HubConnectionBuilder().withUrl(hubPath)
+            .configureLogging(signalR.LogLevel.Information)
+            .withAutomaticReconnect()
+            .build();
+
+            connection.on('Message', (data) => {
+              let event = new SignalR.Notification(data);
+              if (event.type === 'chat') {
+                if (event.action === 'add') {
+                  store.dispatch("signalRAddMessage", event.data[0]);
+                } else if (event.action === 'update') {
+                  store.dispatch("checkUnreadMessageFromSignalR", event.data[0]);
+                }
+              } else {
+                signlaREvents[event.type][event.action](event.data);
+              }
+            });
+            connection.on("studyRoomToken", (jwtToken,studyRoomId) => {
+              if (store.getters.getRoomIdSession == studyRoomId) {
+                store.dispatch('updateJwtToken', jwtToken);
+              }
+            });
+        
+            // signalR Reconnecting
+            connection.onreconnecting(() => {
+              store.dispatch('setIsSignalRConnected', false);
+            });
+            connection.onreconnected(() => {
               store.dispatch('setIsSignalRConnected', true);
             });
-          }
+
+            if (connection.state === 'Disconnected') {
+              connection.start().then(() => {
+                store.dispatch('setIsSignalRConnected', true);
+              });
+            }
+        })
           break;
         case 'signalR_emit':
           connection.invoke(mutation.payload.message, mutation.payload.data);
