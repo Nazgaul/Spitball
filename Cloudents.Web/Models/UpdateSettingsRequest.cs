dedd477a36e474d0e856298d505b6aa886a0bad7
Ram <ram@cloudents.com>
2020-07-02 12:46:22 +0300

    xxx

--- Cloudents.Web/Models/UpdateSettingsRequest.cs
+++ Cloudents.Web/Models/UpdateSettingsRequest.cs
@@ -10,10 +10,10 @@ namespace Cloudents.Web.Models
         [StringLength(255, MinimumLength = 2, ErrorMessage = "StringLength")]
         [Required]
         public string LastName { get; set; }
-        [StringLength(52, MinimumLength = 0, ErrorMessage = "StringLength")]
+        [StringLength(52, ErrorMessage = "StringLength")]
         public string? Title { get; set; }
 
-        [StringLength(100, MinimumLength = 0, ErrorMessage = "StringLength")]
+        [StringLength(100, ErrorMessage = "StringLength")]
         public string? ShortParagraph { get; set; }
 
         //[StringLength(1000, MinimumLength = 0, ErrorMessage = "StringLength")]
