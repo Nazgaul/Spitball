ef78b2bd88370e3ab88299deab8f9e67d9a176a5
idan <idan@cloudents.com>
2020-07-02 21:16:38 +0300

    fix buttons mobile wrap issue

--- Cloudents.Web/ClientApp/src/components/new_profile/components/profileCoverActions/profileCoverActions.vue
+++ Cloudents.Web/ClientApp/src/components/new_profile/components/profileCoverActions/profileCoverActions.vue
@@ -2,23 +2,29 @@
     <div class="profileCoverActions text-sm-center">
         <h1 dir="auto" class="mainTitle mb-sm-4 mb-2 white--text px-4">{{title}}</h1>
         <h2 dir="auto" class="subTitle white--text mb-sm-7 mb-5 px-4">{{paragraph}}</h2>
-        <div class="mb-sm-5 actionWrapper text-center d-flex d-sm-block flex-wrap ">
-            <!-- mb-4 on both buttons, no Exception-->
-            <!-- :class="{'mb-4': isMyProfile}" -->
+        <div class="mb-sm-5 actionWrapper text-center d-flex d-sm-block flex-wrap align-end">
             <v-btn
-                class="btn white--text me-sm-4 mb-sm-0"
-                :class="{'mb-4': isMyProfile}"
                 @click="sendMessage"
-                rounded
-                depressed
-                color="#ff6927"
+                class="btn white--text me-sm-4 mb-sm-0"
                 :width="isMobile ? '166' : '200'"
                 height="46"
+                color="#ff6927"
+                rounded
+                depressed
             >
                 <chatIcon class="me-2" width="23" />
                 <span class="flex-grow-1 flex-sm-grow-0 pe-sm-0" v-t="'message_me'"></span>
             </v-btn>
-            <v-btn class="btn white--text mt-sm-0 mb-sm-0" @click="openCalendar" v-if="!isCalendar" rounded depressed color="#4c59ff" :width="isMobile ? '166' : '200'" height="46">
+            <v-btn
+                v-if="isCalendar && !isMyProfile"
+                @click="openCalendar"
+                class="btn white--text mt-4 mt-sm-0 mb-sm-0"
+                :width="isMobile ? '166' : '200'"
+                height="46"
+                color="#4c59ff"
+                rounded
+                depressed
+            >
                 <calendarIcon class="me-2" width="23" />
                 <span class="flex-grow-1 flex-sm-grow-0 pe-sm-0" v-t="'book_lesson'"></span>
             </v-btn>
@@ -64,7 +70,7 @@ export default {
     },
     methods: {
         sendMessage() {
-            if(this.isMyProfile) {return}
+            if(this.isMyProfile) return
             if(!this.isLogged) {
                 let profile = this.user
                 this.$ga.event('Tutor_Engagement', 'contact_BTN_profile_page', `userId:GUEST`);
@@ -99,10 +105,9 @@ export default {
             }
         },
         openCalendar() {
-            if(this.isMyProfile) {return}
+            if(this.isMyProfile) return
             if(this.isLogged) {
                 this.$emit('setCalendarActive', true)
-                // this.activeTab = 5;
                 this.$nextTick(() => {
                     this.$vuetify.goTo(this.$parent.$refs.calendarTab)
                 })
