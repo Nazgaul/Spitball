8b4a32a18a4422d5ee8f4f792b538fd0752b3e56
Ram <ram@cloudents.com>
2020-07-08 14:36:26 +0300

    #4246 Task 4246: Admin - remove verify

--- Cloudents.Admin2/ClientApp/components/userMainView/userMainView.js
+++ Cloudents.Admin2/ClientApp/components/userMainView/userMainView.js
@@ -118,7 +118,7 @@ export default {
           //  "getUserTypes",
            // "updateUserType",
             "setUserCurrentStatus",
-            "verifyUserPhone",
+           // "verifyUserPhone",
             "getUserPurchasedDocuments",
             "clearUserState",
             "updateFilterValue",
@@ -141,17 +141,17 @@ export default {
         closeSuspendDialog() {
             this.setSuspendDialogState(false);
         },
-        userInfoAction(actionItem) {
-            if (actionItem === "phoneNumber") {
-                let userObj = {
-                    id: this.info.id.value
-                };
-                this.verifyUserPhone(userObj).then((resp) => {
-                    console.log(resp);
-                    this.openTokensDialog();
-                });
-            }
-        },
+        // userInfoAction(actionItem) {
+        //     if (actionItem === "phoneNumber") {
+        //         let userObj = {
+        //             id: this.info.id.value
+        //         };
+        //         this.verifyUserPhone(userObj).then((resp) => {
+        //             console.log(resp);
+        //             this.openTokensDialog();
+        //         });
+        //     }
+        // },
         openTokensDialog() {
             this.setTokensDialogState(true);
         },
