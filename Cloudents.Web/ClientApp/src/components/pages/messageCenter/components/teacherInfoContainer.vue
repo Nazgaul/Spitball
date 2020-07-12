b565a1d6806a46bb5dfb018d705869c07bac62ce
Maor Barel <maor@cloudents.com>
2020-07-12 14:32:28 +0300

    fix participant list in chat

--- Cloudents.Web/ClientApp/src/components/pages/messageCenter/components/teacherInfoContainer.vue
+++ Cloudents.Web/ClientApp/src/components/pages/messageCenter/components/teacherInfoContainer.vue
@@ -217,6 +217,7 @@ export default {
                }
             }
             .list{
+               background: none;
                padding: 0;
                overflow-y: auto;
                .dividerList{
