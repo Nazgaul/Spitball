52ca858f5b2febb2855bd0398d1da2a76894819d
Maor Barel <maor@cloudents.com>
2020-07-12 14:35:03 +0300

    chat msgs box-shadow

--- Cloudents.Web/ClientApp/src/components/chat/components/messageComponents/remoteMessage.vue
+++ Cloudents.Web/ClientApp/src/components/chat/components/messageComponents/remoteMessage.vue
@@ -62,7 +62,7 @@ export default {
    }
    max-width: 70%;
    width: fit-content;
-   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.25);
+   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.25) !important;
    margin-right: auto;
    margin-left: unset;
    .messageTitle{
