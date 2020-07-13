9bc3b250461bd11bc1a0efb542e9b037a6b5c9ef
idanKal <idankallmann_91@hotmail.com>
2020-07-13 15:02:29 +0300

    Bug #4317: No documents api call on profile page

--- Cloudents.Web/ClientApp/src/components/new_profile/components/profileItemsBox/profileItemsBox.vue
+++ Cloudents.Web/ClientApp/src/components/new_profile/components/profileItemsBox/profileItemsBox.vue
@@ -1,5 +1,5 @@
 <template>
-   <div id="profileItemsBox" v-intersect.quiet="{handler: onIntersect, options: { rootMargin: '300px' }}">
+   <div id="profileItemsBox" v-intersect.quiet="onIntersect">
       <div
          v-for="(item, index) in filterItems"
          :key="index"
