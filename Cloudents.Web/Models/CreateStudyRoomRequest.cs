e80ec1c4fb7499c6c96678b180d7ab835f1757e0
Ram <ram@cloudents.com>
2020-07-14 15:27:34 +0300

    Split api of create study room

--- Cloudents.Web/Models/CreateStudyRoomRequest.cs
+++ Cloudents.Web/Models/CreateStudyRoomRequest.cs
@@ -6,36 +6,48 @@ using Cloudents.Core.Enum;
 
 namespace Cloudents.Web.Models
 {
+    public class CreateLiveStudyRoomRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+
+        [Required]
+        [Range(0, 10000000)]
+        public decimal Price { get; set; }
+
+       
+        [Required]
+        public DateTime? Date { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            
+            if (Date.GetValueOrDefault(DateTime.MaxValue) < DateTime.UtcNow)
+            {
+                yield return new ValidationResult(
+                    "Date should be in the future",
+                    new[] { nameof(Name) });
+            }
+        }
+
+        public string? Description { get; set; }
+    }
     public class CreateStudyRoomRequest : IValidatableObject
     {
         [Required]
         public string Name { get; set; }
 
+        [Required]
         public IEnumerable<long> UserId { get; set; }
 
         [Required]
         [Range(0, 10000000)]
         public decimal Price { get; set; }
 
-        [Required]
-        public StudyRoomType Type { get; set; }
-
-        public DateTime? Date { get; set; }
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (Type == StudyRoomType.Broadcast && !Date.HasValue)
-            {
-                yield return new ValidationResult(
-                    "Need a date",
-                    new[] { nameof(Name) });
-            }
-            if (Type == StudyRoomType.Broadcast && Date.GetValueOrDefault(DateTime.MaxValue) < DateTime.UtcNow)
-            {
-                yield return new ValidationResult(
-                    "Date should be in the future",
-                    new[] { nameof(Name) });
-            }
-            if (Type == StudyRoomType.Private && UserId?.Any() == false)
+           
+            if ( UserId.Any() == false)
             {
                 yield return new ValidationResult(
                     "Need to enter or users",
@@ -43,7 +55,6 @@ namespace Cloudents.Web.Models
             }
         }
 
-        public string? Description { get; set; }
         
     }
 
