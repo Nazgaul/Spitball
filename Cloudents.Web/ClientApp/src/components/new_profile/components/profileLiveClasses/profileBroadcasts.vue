dcd06af06f8f678160ae07294e7606f01b68bc27
idanKal <idankallmann_91@hotmail.com>
2020-07-20 14:11:35 +0300

    Bug #4439: Alignment

--- Cloudents.Web/ClientApp/src/components/new_profile/components/profileLiveClasses/profileBroadcasts.vue
+++ Cloudents.Web/ClientApp/src/components/new_profile/components/profileLiveClasses/profileBroadcasts.vue
@@ -11,7 +11,7 @@
                     <div class="leftSide d-sm-flex me-sm-6">
                         <img class="cursor-pointer" @click="goStudyRoomLandingPage(session.id)"  :src="liveImage(session)" alt="" width="330" height="220">
                     </div>
-                    <div class="rightSide d-flex flex-column justify-space-between flex-grow-1 pa-3 pt-2 pt-sm-2 pe-0 ps-0 pe-sm-4">
+                    <div class="rightSide d-flex flex-column justify-space-between flex-grow-1 px-3 pa-3 pa-sm-0 pt-2 pt-sm-0 pe-0 ps-0 pe-sm-4">
                         <div class="occurrenceWrap mb-5 mb-sm-0">
                             <div class="sessionTitle mb-2" v-if="!isMobile">{{session.name}}</div>
                             <div class="d-flex align-center flex-wrap flex-sm-nowrap">
