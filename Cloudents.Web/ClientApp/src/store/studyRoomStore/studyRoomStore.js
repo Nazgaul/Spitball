c7f51a2eb7b0de1dd649264913ca4167fd686586
Maor Barel <maor@cloudents.com>
2020-07-20 15:10:53 +0300

    Bug #4443: Reoccurring session should show "Every" before days

--- Cloudents.Web/ClientApp/src/store/studyRoomStore/studyRoomStore.js
+++ Cloudents.Web/ClientApp/src/store/studyRoomStore/studyRoomStore.js
@@ -252,7 +252,7 @@ const getters = {
          }
       }).sort((a,b)=>a.digit - b.digit)
 
-      let days = Array.from(new Set(daysObj.map(d=>d.text))).join(' ');
+      let days = Array.from(new Set(daysObj.map(d=>d.text))).join(', ');
       let start = nextEvents[0];
       let startNext = nextEvents.filter(dateEvent=> Moment(dateEvent).isAfter())[0];
       return {
