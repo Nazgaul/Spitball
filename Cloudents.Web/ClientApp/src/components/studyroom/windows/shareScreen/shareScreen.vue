2a927ed06f77335f78997c20bb7c79669fdbdfed
Maor Barel <maor@cloudents.com>
2020-07-21 12:54:44 +0300

    Bug #4480: Study Room - Need to add border to share screen window

--- Cloudents.Web/ClientApp/src/components/studyroom/windows/shareScreen/shareScreen.vue
+++ Cloudents.Web/ClientApp/src/components/studyroom/windows/shareScreen/shareScreen.vue
@@ -153,6 +153,7 @@ export default {
             }
             #shareScreenPreviewVideo{
                video{
+                  border: 1px solid #ddd;
                   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.55);
                   width: 100%;
                }
