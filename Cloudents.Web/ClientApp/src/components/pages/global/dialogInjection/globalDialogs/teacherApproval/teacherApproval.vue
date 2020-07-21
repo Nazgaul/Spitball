d53d97d4425e2907b1f7ba0610bf08eec9bb755f
idanKal <idankallmann_91@hotmail.com>
2020-07-21 15:11:59 +0300

    Bug #4482: Payment in Hebrew

--- Cloudents.Web/ClientApp/src/components/pages/global/dialogInjection/globalDialogs/teacherApproval/teacherApproval.vue
+++ Cloudents.Web/ClientApp/src/components/pages/global/dialogInjection/globalDialogs/teacherApproval/teacherApproval.vue
@@ -11,7 +11,7 @@
                     <span class="white--text" v-t="'teacherApproval_error'"></span>
                 </div>
 
-                <table class="table text-left">
+                <table class="table text-start">
                     <tr>
                         <td>
                             <div class="pb-3" v-t="'teacherApproval_date'"></div>
