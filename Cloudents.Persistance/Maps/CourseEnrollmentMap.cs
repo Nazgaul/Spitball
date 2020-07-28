73c3e727a8cd10524844419939c419ec43879077
Ram <ram@cloudents.com>
2020-07-28 12:57:13 +0300

    remove mapping

--- Cloudents.Persistance/Maps/CourseEnrollmentMap.cs
+++ Cloudents.Persistance/Maps/CourseEnrollmentMap.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Cloudents.Core.Entities;
-using FluentNHibernate.Mapping;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Cloudents.Core.Entities;
+//using FluentNHibernate.Mapping;
 
-namespace Cloudents.Persistence.Maps
-{
-    public class CourseEnrollmentMap : ClassMap<CourseEnrollment>
-    {
-        public CourseEnrollmentMap()
-        {
-            Id(x => x.Id).GeneratedBy.Guid();
-            References(x => x.Course).Not.Nullable();
-            References(x => x.User).Not.Nullable();
+//namespace Cloudents.Persistence.Maps
+//{
+//    public class CourseEnrollmentMap : ClassMap<CourseEnrollment>
+//    {
+//        public CourseEnrollmentMap()
+//        {
+//            Id(x => x.Id).GeneratedBy.Guid();
+//            References(x => x.Course).Not.Nullable();
+//            References(x => x.User).Not.Nullable();
 
-        }
-    }
-}
+//        }
+//    }
+//}
