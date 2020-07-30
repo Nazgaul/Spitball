b72fca16c847de2e303d95f789fe4884387144bb
Ram <ram@cloudents.com>
2020-07-30 12:44:34 +0300

    Trim down llive study room request

---
+++ Cloudents.Web/Models/CreateCourseRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cloudents.Web.Models
+{
+    public class CreateCourseRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0,int.MaxValue)]
+        public int Price { get; set; }
+
+        public int? SubscriptionPrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string? Image { get; set; }
+
+        public IEnumerable<CreateLiveStudyRoomRequest> StudyRooms { get; set; }
+        public IEnumerable<CreateDocumentRequest> Documents { get; set; }
+        public bool IsPublish { get; set; }
+    }
+}
--- Cloudents.Web/Models/CreateDocumentRequest.cs
+++ Cloudents.Web/Models/CreateDocumentRequest.cs
@@ -1,6 +1,5 @@
 ﻿using Cloudents.Core.Entities;
 using Cloudents.Web.Extensions;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Cloudents.Core.Enum;
 
@@ -42,25 +41,5 @@ namespace Cloudents.Web.Models
         //            new[] { nameof(Price) });
         //    }
         //}
-    }
-
-
-    public class CreateCourseRequest
-    {
-        [Required]
-        public string Name { get; set; }
-
-        [Range(0,int.MaxValue)]
-        public int Price { get; set; }
-
-        public int? SubscriptionPrice { get; set; }
-
-        public string Description { get; set; }
-
-        public string? Image { get; set; }
-
-        public IEnumerable<CreateLiveStudyRoomRequest> StudyRooms { get; set; }
-        public IEnumerable<CreateDocumentRequest> Documents { get; set; }
-        public bool IsPublish { get; set; }
     }
 }
