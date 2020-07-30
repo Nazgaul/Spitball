4d41461591e6cf1ed7b5398118a290d52cdb32c0
Ram <ram@cloudents.com>
2020-07-30 12:45:35 +0300

    Trim down document

--- Cloudents.Web/Models/CreateDocumentRequest.cs
+++ Cloudents.Web/Models/CreateDocumentRequest.cs
@@ -1,7 +1,5 @@
 ﻿using Cloudents.Core.Entities;
-using Cloudents.Web.Extensions;
 using System.ComponentModel.DataAnnotations;
-using Cloudents.Core.Enum;
 
 namespace Cloudents.Web.Models
 {
@@ -13,33 +11,8 @@ namespace Cloudents.Web.Models
         [StringLength(Document.MaxLength, ErrorMessage = "StringLength", MinimumLength = 4)]
         public string Name { get; set; }
 
-        [Required]
-        [StringLength(Document.MaxLength, ErrorMessage = "StringLength", MinimumLength = Document.MinLength)]
-        public string Course { get; set; }
 
+        public bool Visible { get; set; }
 
-        //[Range(0, int.MaxValue)]
-        //public decimal? Price { get; set; }
-
-        public string? Description { get; set; }
-
-        //public PriceType PriceType { get; set; }
-
-        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        //{
-        //    if (string.IsNullOrEmpty(FriendlyUrlHelper.GetFriendlyTitle(Name)))
-        //    {
-        //        yield return new ValidationResult(
-        //            "File Name is invalid",
-        //            new[] { nameof(Name) });
-        //    }
-
-        //    if (PriceType == PriceType.HasPrice && Price == null)
-        //    {
-        //        yield return new ValidationResult(
-        //            "Need to have price",
-        //            new[] { nameof(Price) });
-        //    }
-        //}
     }
 }
