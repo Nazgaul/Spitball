212b3314bbb49456517cf0845a0fb7fe6a58a815
Maor Barel <maor@cloudents.com>
2020-08-06 15:57:40 +0300

    Task #4887: follow unfollow btn

--- Cloudents.Web/ClientApp/src/components/new_profile/components/profileCoverActions/profileCoverActions.vue
+++ Cloudents.Web/ClientApp/src/components/new_profile/components/profileCoverActions/profileCoverActions.vue
@@ -3,20 +3,10 @@
         <h1 dir="auto" class="mainTitle mb-sm-4 mb-2 white--text px-4">{{title}}</h1>
         <h2 dir="auto" class="subTitle white--text mb-sm-7 mb-5 px-4">{{paragraph}}</h2>
         <div class="mb-sm-5 actionWrapper text-center d-flex d-sm-block flex-wrap align-end">
-            <!-- <v-btn
-                class="btn white--text me-sm-4 mb-sm-0"
-                :width="isMobile ? '166' : '200'"
-                height="46"
-                color="#ff6927"
-                rounded
-                depressed
-            >
-                <span class="flex-grow-1 flex-sm-grow-0 pe-sm-0" v-t="'message_me'"></span>
-            </v-btn> -->
             <v-btn
+                class="btn white--text me-sm-4 mb-sm-0"
                 v-if="isCalendar && !isMyProfile"
                 @click="openCalendar"
-                class="btn white--text mt-4 mt-sm-0 mb-sm-0"
                 :width="isMobile ? '166' : '200'"
                 height="46"
                 color="#4c59ff"
@@ -25,6 +15,18 @@
             >
                 <calendarIcon class="me-2" width="23" />
                 <span class="flex-grow-1 flex-sm-grow-0 pe-sm-0" v-t="'book_lesson'"></span>
+            </v-btn>
+            <v-btn
+                :loading="isLoading"
+                @click="toggleFollowing"
+                class="btn white--text mt-4 mt-sm-0 mb-sm-0"
+                :width="isMobile ? '166' : '200'"
+                height="46"
+                color="#ff6927"
+                rounded
+                depressed>
+                <v-icon size="18" class="me-0 me-sm-2">sbf-person</v-icon>
+                <span class="flex-grow-1 flex-sm-grow-0 pe-sm-0" v-t="''">{{isFollowing ? $t('unfollow_me') : $t('follow_me')}}</span>
             </v-btn>
         </div>
     </div>
@@ -36,6 +38,11 @@ import calendarIcon from './calendar.svg'
 
 export default {
     name: 'profileCoverActions',
+    data() {
+        return {
+            isLoading:false,
+        }
+    },
     components: {
         calendarIcon
     },
@@ -57,6 +64,9 @@ export default {
         },
         isLogged() {
             return this.$store.getters.getUserLoggedInStatus
+        },
+        isFollowing(){
+            return this.$store.getters.getIsProfileFollowing
         }
     },
     methods: {
@@ -73,6 +83,19 @@ export default {
                 // setTimeout(()=>{
                 //     document.getElementById(`tab-${this.activeTab}`).lastChild.click();
                 // },200);
+            }
+        },
+        toggleFollowing(){
+            if(this.isMyProfile) return;
+            if(this.isLogged){
+                if(this.isLoading) return;
+                let self = this;
+                this.isLoading = true;
+                this.$store.dispatch('toggleProfileFollower',!this.isFollowing).then(()=>{
+                    self.isLoading = false;
+                })
+            }else{
+                this.$store.commit('setComponent', 'register')
             }
         }
     },
