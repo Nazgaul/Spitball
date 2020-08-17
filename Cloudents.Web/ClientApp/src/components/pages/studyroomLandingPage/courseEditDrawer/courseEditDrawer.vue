ab370a199647efa833a889d842b41c957d36dabf
Maor Barel <maor@cloudents.com>
2020-08-17 11:59:35 +0300

    add stepn number

--- Cloudents.Web/ClientApp/src/components/pages/studyroomLandingPage/courseEditDrawer/courseEditDrawer.vue
+++ Cloudents.Web/ClientApp/src/components/pages/studyroomLandingPage/courseEditDrawer/courseEditDrawer.vue
@@ -1,7 +1,10 @@
 <template>
   <v-navigation-drawer class="courseDrawer" :right="$vuetify.rtl" permanent width="338" app fixed touchless clipped>
 	<div class="drawerHeader">
-		<div class="ps-5 pb-4 pt-12" v-t="'title_edit'"/>
+		<div class="px-5 pb-4 pt-12 d-flex align-center justify-space-between">
+			<div v-t="'title_edit'"/>
+			<span class="white--text stepCircle">2</span>
+		</div>
 	</div>
 
 	<div class="courseEditSections ps-5 pe-5 pt-6">
@@ -80,6 +83,17 @@ export default {
 			font-weight: 600;
 			line-height: 1.15;
 			color: #43425d;
+			.stepCircle{
+				background: #4c59ff;
+				width: 24px;
+				height: 24px;
+				border-radius: 50%;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-size: 13px;
+				padding-bottom: 2px;
+			}
 		}
 		.courseEditSections{
 
