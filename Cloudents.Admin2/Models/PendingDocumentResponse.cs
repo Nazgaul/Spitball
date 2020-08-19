5ddc0db5c173e7068450d144101a46dc8b3bf7bf
Ram <ram@cloudents.com>
2020-08-19 14:16:42 +0300

    Some resharper fixes.

--- Cloudents.Admin2/Models/PendingDocumentResponse.cs
+++ Cloudents.Admin2/Models/PendingDocumentResponse.cs
@@ -5,6 +5,7 @@ namespace Cloudents.Admin2.Models
 {
     public class PendingDocumentResponse
     {
+
         public IEnumerable<PendingDocumentDto> Documents { get; set; }
 
         public string? NextLink { get; set; }
