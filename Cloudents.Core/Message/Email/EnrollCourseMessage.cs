e37311f8a5deabda69d4247f391a180d606fa90b
Ram <ram@cloudents.com>
2020-08-26 13:11:13 +0300

    Fix #5053

---
+++ Cloudents.Core/Message/Email/EnrollCourseMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Cloudents.Core.Message.Email
+{
+    public class EnrollCourseMessage : BaseEmail
+    {
+        public EnrollCourseMessage(string to, string tutorFirstName,string userFirstName, string courseName, string userEmail)
+        :base("ram@cloudents.com","Great news someone enrolled to you course Spitball",null)
+        {
+            Body = @$"Hey {tutorFirstName}
+You have a new student on your service on Spitball. {userFirstName} has enrolled for {courseName}.
+
+If you wish to send them a personal welcome email here is email : {userEmail}.
+
+The Spitball Team";
+        }
+
+        public override string? Campaign => null;
+        public override UnsubscribeGroup UnsubscribeGroup => UnsubscribeGroup.Update;
+        protected override IDictionary<CultureInfo, string>? Templates => null;
+        public string Body { get; private set; }
+
+        public override string ToString()
+        {
+            return Body;
+        }
+    }
+}
--- Cloudents.Core/Message/Email/RequestTutorMessage.cs
+++ Cloudents.Core/Message/Email/RequestTutorMessage.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using Cloudents.Core.Message.System;
 
 namespace Cloudents.Core.Message.Email
@@ -13,29 +11,5 @@ namespace Cloudents.Core.Message.Email
         }
 
         public Guid LeadId{ get; private set; }
-    }
-
-    public class EnrollCourseMessage : BaseEmail
-    {
-        public EnrollCourseMessage(string tutorFirstName,string userFirstName, string courseName, string userEmail)
-        {
-            Subject = "Great news some one enrolled to you course Spitball";
-            Body = @$"Hey {tutorFirstName}
-You have a new student on your service on Spitball. {userFirstName} has enrolled for {courseName}.
-
-If you wish to send them a personal welcome email here is email : {userEmail}.
-
-The Spitball Team";
-        }
-
-        public override string? Campaign => null;
-        public override UnsubscribeGroup UnsubscribeGroup => UnsubscribeGroup.Update;
-        protected override IDictionary<CultureInfo, string>? Templates => null;
-        public string Body { get; set; }
-
-        public override string ToString()
-        {
-            return Body;
-        }
     }
 }
