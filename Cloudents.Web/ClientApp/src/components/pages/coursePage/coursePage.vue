a5dc9a61afd1da226eb1acc1c754706f76488b06
idanKal <idankallmann_91@hotmail.com>
2020-09-01 14:55:49 +0300

    add box shadow to create course sticky box

--- Cloudents.Web/ClientApp/src/components/pages/coursePage/coursePage.vue
+++ Cloudents.Web/ClientApp/src/components/pages/coursePage/coursePage.vue
@@ -324,7 +324,7 @@ export default {
             position: sticky;
             z-index: 9;
             top: 70px;
-            box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.15);
+            box-shadow: 0 1px 12px 0 rgba(0, 0, 0, 0.15);
             .courseStepHeader {
                 background: #fff;
                 .stepStteper {
