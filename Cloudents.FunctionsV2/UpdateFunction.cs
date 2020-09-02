6d44097b4a477b463e625f54b69f418b9a59d34b
Ram <ram@cloudents.com>
2020-09-02 16:20:38 +0300

    Added another function

--- Cloudents.FunctionsV2/UpdateFunction.cs
+++ Cloudents.FunctionsV2/UpdateFunction.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using NHibernate;
 using NHibernate.Linq;
+using SendGrid.Helpers.Mail;
 using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
@@ -14,7 +15,34 @@ namespace Cloudents.FunctionsV2
 {
     public static class UpdateFunction
     {
-       
+
+        [FunctionName("DeleteTutorsForEidan")]
+        public static async Task DeleteTutorCheckForEidan(
+            [TimerTrigger("0 0 * * * *")] TimerInfo timer,
+            [Inject] IStatelessSession statelessSession,
+            [SendGrid(ApiKey = "SendgridKey", From = "Spitball <no-reply@spitball.co>")] IAsyncCollector<SendGridMessage> emailProvider,
+            CancellationToken token
+            )
+        {
+            const string sql = @"select ID, email, country, created from sb.[user] where id in
+(Select ID from sb.TutorHistory where created> getdate()-2
+except
+Select ID from sb.Tutor)
+and id not in (525015,525046,525055)";
+
+            var result = await statelessSession.CreateSQLQuery(sql).ListAsync();
+            if (result.Count > 0)
+            {
+                var message = new SendGridMessage()
+                {
+
+                    Subject = "A tutor/s was deleted",
+                    PlainTextContent = $"A tutor/s was deleted {result.Count}"
+                };
+                message.AddTo("eidan@cloudents.com");
+                await emailProvider.AddAsync(message, token);
+            }
+        }
 
         [FunctionName("DeleteOldUserLocation")]
         public static async Task DeleteOldUserLocationAsync([TimerTrigger("0 0 2 * * *")] TimerInfo myTimer,
