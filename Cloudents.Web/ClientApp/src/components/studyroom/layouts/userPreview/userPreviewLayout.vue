aaab426b5570c55fe5ff9a112c80d43234efe379
Maor Barel <maor@cloudents.com>
2020-09-09 13:36:11 +0300

    clean

--- Cloudents.Web/ClientApp/src/components/studyroom/layouts/userPreview/userPreviewLayout.vue
+++ Cloudents.Web/ClientApp/src/components/studyroom/layouts/userPreview/userPreviewLayout.vue
@@ -1,6 +1,6 @@
 <template>
    <v-card :id="participant.id" color="black" 
-   :class="['userPreviewZoom']" >
+   :class="['userPreviewLayout']" >
    <div :class="['previewControls']">
       <v-tooltip top>
          <template v-slot:activator="{ on }">
@@ -247,7 +247,7 @@ export default {
 </script>
 
 <style lang="less">
-.userPreviewZoom{
+.userPreviewLayout{
    border-radius: 0 !important;
    position: relative;
    .previewControls{
